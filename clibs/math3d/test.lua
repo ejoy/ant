54442d5209fa29b7f1f3100f5fadbe258a9f1396
Cloud Wu <cloudwu@gmail.com>
2018-01-22 15:12:30 +0800

    complete document of math3d

--- clibs/math3d/test.lua
+++ clibs/math3d/test.lua
@@ -1,31 +1,32 @@
 local math3d = require "math3d"
 --[[
-	P : pop and return id
-	v : pop and return vector4 pointer
-	m : pop and return matrix pointer
-	V : top to string for debug
-	D : dup stack top (1 -> 1,1)
-	S : swap stack top ( 1,2 -> 2,1 )
-	R : remove stack top ( 1 -> )
-	M : mark stack top and pop
+	P : pop and return id ( ... , 1 -> ... )
+	v : pop and return vector4 pointer ( ... , 1 -> ... )
+	m : pop and return matrix pointer ( ... , 1 -> ... )
+	f : pop and return the first float of a vector4 ( ... , 1 -> ... )
+	V : top to string for debug ( ... -> ... )
+	D : dup stack top (..., 1 -> ..., 1,1)
+	S : swap stack top (..., 1,2 -> ..., 2,1 )
+	R : remove stack top ( ..., 1 -> ... )
+	M : mark stack top and pop ( ..., 1 -> ...)
 
 	{ 1,2,3,4 }	  push vector4(1,2,3,4)
 	{ 1,2,3,4, .... 16 } push matrix4x4
-	{} push indenty
-	{ s = 2 } push scale 2,2,2
+	{} push identity matrix
+	{ s = 2 } push scaled matrix (2,2,2)
 	{ sx = 1, sy = 2, sz = 3 }
 	{ rx = 1, ry = 0, rz = 0 }
-	{ tx = 0, ty = 0 , tz = 1 },
+	{ tx = 0, ty = 0 , tz = 1 }
 
-	* matrix mul ( 1, 2 - > 1*2 )
+	* matrix mul ( ..., 1,2 - > ..., 1*2 )
 	* vector4 * matrix4x4
-	+ vector4 + vector4
-	- vec4 - vec4
-	. vec3 * vec3  ( 1,2 -> (result, 0,0,1) )
-	x cross (vec3 , vec3)
-	i
-	t
-	n
+	+ vector4 + vector4 ( ..., 1,2 - > ..., 1+2 )
+	- vec4 - vec4 ( ..., 1,2 - > ..., 1-2 )
+	. vec3 * vec3  ( ..., 1,2 -> ..., { dot(1,2) , 0 , 0 ,1 } )
+	x cross (vec3 , vec3) ( ..., 1, 2, -> ... , cross(1,2) )
+	i inverted matrix  ( ..., 1 -> ..., invert(1) )
+	t transposed matrix ( ..., 1 -> ..., transpose(1) )
+	n normalize vector3 ( ..., 1 -> ..., {normalize(1) , 1} )
 ]]
 
 local stack = math3d.new()
