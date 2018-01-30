186ddb7405afa63460e20d7e37059a92cefb1168
junjie020 <junjie020@163.com>
2018-01-30 12:42:22 +0800

    need . not : when define new function for component

--- test/cameratest/camera_component.lua
+++ test/cameratest/camera_component.lua
@@ -4,7 +4,7 @@ local ecs = ...
 local math3d = require "math3d"
 local math3d_comp = ecs.component "math3d"
 
-function math3d_comp:new()    
+function math3d_comp.new()    
 	return math3d.new()
 end
 --@}
