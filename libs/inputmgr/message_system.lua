426dd1fc0f6da3dd72d83af3c90c3d83a34e64be
junjie020 <junjie020@163.com>
2018-02-13 16:20:26 +0800

    extend message system to support message observer

--- libs/inputmgr/message_system.lua
+++ libs/inputmgr/message_system.lua
@@ -2,6 +2,29 @@ local ecs = ...
 
 local world = ecs.world
 local bgfx = require "bgfx"
+
+--[@ message_component
+local msg_comp = ecs.component "message_component"
+
+function msg_comp:init()
+	local message_observers = {}
+	
+	function message_observers:add(observer)
+		table.insert(self, observer)
+	end
+	function message_observers:remove(observer)
+		for i, v in ipairs(self) do
+			if v == observer then
+				table.remove(i)
+				return
+			end
+		end
+	end
+
+	self.msg_observers = setmetatable({}, message_observers)
+end
+--@]
+
 --[@
 local message = {}
 
@@ -21,13 +44,23 @@ end
 --[@
 local iup_message = ecs.system "iup_message"
 iup_message.singleton "viewport"
+iup_message.singleton "message_component"
+
+function iup_message:init()
+	self.message_component.msg_observers:add(message)		
+end
 
 function iup_message:update()
-	for idx, msg, v1,v2,v3 in pairs(world.args.mq) do
-		local f = message[msg]
-		if f then
-			f(self,v1,v2,v3)
+	for idx, msg, v1,v2,v3,v4 in pairs(world.args.mq) do
+		print("iup_message receive message : " .. msg)
+		local observers = self.message_component.msg_observers
+		for idx, observer in ipairs(observers) do
+			local action_cb = observer[msg]
+			if action_cb then
+				action_cb(self,v1,v2,v3,v4)
+			end
 		end
+
 	end
 end
 --@]
