dcce337a183af2a57fee7cd3b7066cbce1a452dd
junjie020 <junjie020@163.com>
2018-02-26 09:54:38 +0800

    fix scintilla project linking error

--- clibs/iup/luascintilla.c
+++ clibs/iup/luascintilla.c
@@ -9,3 +9,11 @@ luaopen_scintilla(lua_State *L) {
 	lua_getglobal(L, "iup");
 	return 1;
 }
+
+// just for simple workaround iup build in msvc cause the linking error in iupwindows_main.c file
+// the appropriate way the handle this should add a new msvc project the same as iupcore project except iupwindows_main.c/iupwindows_info.c files
+#ifdef _MSC_VER 
+int main(int argc, char **argv) {
+	return 0;
+}
+#endif //_MSC_VER 
