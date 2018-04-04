638ec6b501dbddf8829156aaf0a3c2d2cbfda57b
junjie020 <junjie020@163.com>
2018-04-04 17:21:13 +0800

    change assimp-lua folder name to assimplua, and the dll name

--- clibs/assimp-lua/assimp.lua
+++
@@ -1,4 +0,0 @@
-local assimp = require"assimplua"
-local inpath = "D:/Engine/ant/clibs/assimp-lua/assets/sword.fbx"
-local outpath = "D:/Engine/ant/clibs/assimp-lua/assets/sword.bin"
-assimp.assimp_import(inpath, outpath)
