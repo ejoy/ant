b3c9c095e081c38dedfbe668c068833c7de60a9f
junjie020 <junjie020@163.com>
2018-06-06 14:58:30 +0800

    move update_hierarchy_entity function to util file

--- libs/render/editor/build_hierarchy_system.lua
+++ libs/render/editor/build_hierarchy_system.lua
@@ -2,39 +2,10 @@ local ecs = ...
 local world = ecs.world
 
 local hierarchy_module = require "hierarchy"
+local hu = require "scene.hierarchy.util"
+
 local build_system = ecs.system "build_hierarchy_system"
 build_system.singleton "math_stack"
-local mu = require "math.util"
-
-local function update_child_srt(ms, e, srt, node)
-    local rot = ms({type="q", table.unpack(node.r)}, "eT")
-    rot[1], rot[2] = rot[2], rot[1]
-
-	local localsrt = mu.srt(ms, node.s, rot, node.t);
-	local s, r, t = ms(localsrt, srt, "*~PPP");
-	ms(e.scale.v, s, "=", e.rotation.v, r, "=", e.position.v, t, "=")
-end
-
-local function update_hierarchy_entiy(ms, h_entity)
-	
-	local hierarchy = assert(h_entity.hierarchy)
-	
-	local rootsrt = mu.srt_from_entity(ms, h_entity)
-	local builddata = hierarchy.builddata
-
-	local mapper = h_entity.hierarchy_name_mapper.v
-	for _, node in ipairs(builddata) do
-		local name = node.name
-		local c_eid = mapper[name]                
-		local c_entity = world[c_eid]
-
-		if c_entity then				
-			update_child_srt(ms, c_entity, rootsrt, node)
-		else
-			error(string.format("not found entity by hierarchy name mapper, name is : %s", name))
-		end
-	end
-end
 
 local function build(ms, eid)
 	local e = world[eid]
@@ -45,7 +16,7 @@ local function build(ms, eid)
 
 		local root = assert(e.editable_hierarchy.root)
 		e.hierarchy.builddata = assert(hierarchy_module.build(root))
-		update_hierarchy_entiy(ms, e)
+		hu.update_hierarchy_entiy(ms, world, e)
 
 		local hie_np = e.hierarchy_name_mapper
 		if hie_np then
