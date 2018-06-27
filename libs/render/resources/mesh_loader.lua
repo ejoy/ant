63725553c73ebc429aa2d050de96a7e83ab67f88
junjie020 <junjie020@163.com>
2018-06-27 16:14:55 +0800

    calc tangent when loading mesh

--- libs/render/resources/mesh_loader.lua
+++ libs/render/resources/mesh_loader.lua
@@ -29,24 +29,52 @@ do
 
 		return flag
 	end
+
+	local vb_decode = nil
 	
 	mesh_decode["VB \1"] = function(mesh, group, data, offset, param)
 		offset = read_mesh_header(mesh, data, offset)
 		local stride, numVertices		
 		mesh.vdecl, stride, offset = bgfx.vertex_decl(data, offset)
 		numVertices, offset = string.unpack("<I2", data, offset)
+
 		vb_data[2] = data
 		vb_data[3] = offset
 		offset = offset + stride * numVertices
 		vb_data[4] =  offset - 1
-		local flag = gen_vc_flag(param)
-		group.vb = bgfx.create_vertex_buffer(vb_data, mesh.vdecl, flag)
+
+		local function decode()
+			local flag = gen_vc_flag(param)
+			local decl = mesh.vdecl
+			if param and param.calctangent then
+				vb_data[1] = decl
+				local t_decl = bgfx.export_vertex_decl(decl)
+				table.insert(t_decl, {"TANGENT", 3, "FLOAT"})
+				decl = bgfx.vertex_decl(t_decl)
+			end			
+			group.vb = bgfx.create_vertex_buffer(vb_data, decl, flag, ib_data)
+		end
+
+		local function need_delay_decode()
+			if param and param.calctangent then
+				return group.ib == nil
+			end
+			return false
+		end
+		
+
+		if need_delay_decode() then
+			vb_data[1] = mesh.vdecl
+			vb_decode = decode			
+		else
+			decode()
+		end
 		return offset
 	end
 
 	mesh_decode["IB \0"] = function(mesh, group, data, offset)
 		local numIndices
-		numIndices, offset = string.unpack("<I4", data, offset)
+		numIndices, offset = string.unpack("<I4", data, offset)		
 		ib_data[1] = data
 		ib_data[2] = offset
 		offset = offset + numIndices * 2
@@ -100,6 +128,10 @@ do
 			end
 
 			offset = decoder(mesh, group, data, offset + 4, param)
+		end
+
+		if vb_decode then
+			vb_decode()
 		end
 
 		return mesh
