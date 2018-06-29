cf4dd1fe4b84ab0795caf339a94a05753d05c3b1
actboy168 <actboy168@gmail.com>
2018-06-29 11:19:44 +0800

    [lua patch] support for exception breakpoints

--- clibs/lua/ldblib.c
+++ clibs/lua/ldblib.c
@@ -326,6 +326,7 @@ static int makemask (const char *smask, int count) {
   if (strchr(smask, 'c')) mask |= LUA_MASKCALL;
   if (strchr(smask, 'r')) mask |= LUA_MASKRET;
   if (strchr(smask, 'l')) mask |= LUA_MASKLINE;
+  if (strchr(smask, 'e')) mask |= LUA_MASKEXCEPTION;
   if (count > 0) mask |= LUA_MASKCOUNT;
   return mask;
 }
@@ -339,6 +340,7 @@ static char *unmakemask (int mask, char *smask) {
   if (mask & LUA_MASKCALL) smask[i++] = 'c';
   if (mask & LUA_MASKRET) smask[i++] = 'r';
   if (mask & LUA_MASKLINE) smask[i++] = 'l';
+  if (mask & LUA_MASKEXCEPTION) smask[i++] = 'e';
   smask[i] = '\0';
   return smask;
 }
