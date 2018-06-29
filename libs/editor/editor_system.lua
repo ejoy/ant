72a501ac54f4035d6124ab8c62321f555ed48457
junjie020 <junjie020@163.com>
2018-06-29 15:50:35 +0800

    fix editor not show entity and remove duplicate entity

--- libs/editor/editor_system.lua
+++ libs/editor/editor_system.lua
@@ -23,48 +23,60 @@ local function build_hierarchy_tree()
 		local name = ename and ename.n or "main_camera"
 		ud_table[name] = eid
         table.insert(htree, name)
-    end
+	end
+	
+	local function is_obj_transform(e)
+		local obj_transform_names = {"pos_transform", "scale_transform", "rotation_transform"}
+		for _, n in ipairs(obj_transform_names) do
+			if e[n] then
+				return true
+			end
+		end
+
+		return false
+	end
 
     for _, eid in world:each("editable_hierarchy") do
-		eidin_hierarchy[eid] = true
+		eidin_hierarchy[eid] = true		
+		local e = world[eid]
 		
-        local e = world[eid]
-    
-        local hierarchy_tree = e.editable_hierarchy.root
-        local name_mapper = e.hierarchy_name_mapper.v
-        local function build_hierarchy_entity_tree(ehierarchy, name_mapper)
-            local t = {}
-            local num = #ehierarchy
-            for i=1, num do
-                local child = ehierarchy[i]
-                local childnum = #child
-                local ceid = name_mapper[child.name]
-                if ceid then
-                    eidin_hierarchy[ceid] = true
-					ud_table[child.name] = ceid
-                    if childnum ~= 0 then
-                        local ct = build_hierarchy_entity_tree(child, name_mapper)
-                        t[child.name] = ct
-                    else
-                        table.insert(t, child.name)
-                    end
-                end
+		if not is_obj_transform(e) then
+			local hierarchy_tree = e.editable_hierarchy.root
+			local name_mapper = e.hierarchy_name_mapper.v
+			local function build_hierarchy_entity_tree(ehierarchy, name_mapper)
+				local t = {}
+				local num = #ehierarchy
+				for i=1, num do
+					local child = ehierarchy[i]
+					local childnum = #child
+					local ceid = name_mapper[child.name]
+					if ceid and eidin_hierarchy[ceid] == nil then
+						eidin_hierarchy[ceid] = true
+						ud_table[child.name] = ceid
+						if childnum ~= 0 then
+							local ct = build_hierarchy_entity_tree(child, name_mapper)
+							t[child.name] = ct
+						else
+							table.insert(t, child.name)
+						end
+					end
 
-            end
-            return t
-        end
+				end
+				return t
+			end
 
-        local t = build_hierarchy_entity_tree(hierarchy_tree, name_mapper)        
-        local ename = e.name
-		local name = ename and ename.n or "hierarchy_entity"
-		htree[name] = t
-		ud_table[name] = eid
+			local t = build_hierarchy_entity_tree(hierarchy_tree, name_mapper)        
+			local ename = e.name
+			local name = ename and ename.n or "hierarchy_entity"
+			htree[name] = t
+			ud_table[name] = eid
+		end
     end
 
-    for _, eid in world:each("render") do
-        if not eidin_hierarchy[eid] then
+    for _, eid in world:each("can_render") do
+		if not eidin_hierarchy[eid] then			
             local e = world[eid]
-            if e.render.visible then
+            if not is_obj_transform(e) and e.can_render.visible then
 				local ename = e.name
 				local name = ename and ename.n or "entity"
 				table.insert(htree, name)
