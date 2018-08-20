dd1cade6f439d2e9cfced674c96194d2381a1faa
junjie020 <junjie020@163.com>
2018-08-20 10:02:29 +0800

    move enable_pack to config.lua file

--- libs/init.lua
+++ libs/init.lua
@@ -3,25 +3,6 @@
 local root = os.getenv "ANTGE" or "."
 local local_binpath = (os.getenv "BIN_PATH" or "clibs")
 
-local function check_enable_pack()
-	local env_ENABLE_PACK = os.getenv "ENABLE_PACK"
-	if env_ENABLE_PACK == nil then
-		return true
-	end
-
-	return env_ENABLE_PACK == "ON"
-end
-
-local enable_pack = check_enable_pack()
-
-function enable_pack_framework(state)
-	if state then
-		enable_pack = state
-	end
-
-	return enable_pack
-end
-
 package.cpath = root .. "/" .. local_binpath .. "/?.dll;" .. 
                 root .. "/bin/?.dll"
 
@@ -29,6 +10,7 @@ package.path = root .. "/libs/?.lua;" .. root .. "/libs/?/?.lua"
 
 require "common/import"
 require "common/log"
+require "common/config"
 
 print_r = require "common/print_r"
 require "filesystem"
