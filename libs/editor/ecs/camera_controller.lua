d35c8ad9864bf824d63f17c274cf5a2ea29b18d4
junjie020 <junjie020@163.com>
2018-08-23 15:38:54 +0800

    fix when main_camera is not added, camera_controller will failed

--- libs/editor/ecs/camera_controller.lua
+++ libs/editor/ecs/camera_controller.lua
@@ -34,8 +34,15 @@ camera_controller_system.depend "iup_message"
 
 function camera_controller_system:init()
 	local ms = self.math_stack
-	local camera = world:first_entity("main_camera")
+	local camera_eid = world:first_entity_id("main_camera")
+	if camera_eid then
+		return 
+	end
 
+	local camera = world[camera_eid]
+
+
+	local move_speed = 1
 	local message = {}
 
 	local button_status = {}
@@ -46,10 +53,9 @@ function camera_controller_system:init()
 	local last_xy
 	function message:motion(x, y, status)		
 		local xy = point2d(x, y)
-		if last_xy then			
-			--if (status.LEFT or status.RIGHT) and last_xy then
+		if last_xy then
 			if status.RIGHT then
-				local speed = message.move_speed * 0.1
+				local speed = move_speed * 0.1
 				local delta = (xy - last_xy) * speed	--we need to reverse the drag direction so that to rotate angle can reverse
 				camera_util.rotate(ms, camera, delta.x, delta.y)				
 			end
@@ -61,67 +67,45 @@ function camera_controller_system:init()
 	function message:keypress(c, p, status)
 		if c == nil then return end
 		
-		do
-			if p then
-				local ms = message.ms
+		if p then
+			local move_step = move_speed
+			local rot = camera.rotation.v
+			local eye = camera.position.v
 
-				local move_step = message.move_speed
-				local rot = camera.rotation.v
-				local eye = camera.position.v
+			local rightbtn_down = button_status.RIGHT
+			local leftbtn_down = button_status.LEFT
 
-				local rightbtn_down = button_status.RIGHT
-				local leftbtn_down = button_status.LEFT
-
-				local nomouse_down = not (rightbtn_down or leftbtn_down)
-				if nomouse_down and (c == "r" or c == "R") then
-					ms(	eye, {0, 0, -10}, "=")
-					ms( rot, {0, 0, 0}, "=")
-					return 
-				end
-
-				if rightbtn_down then					
-					local dx, dy, dz = 0, 0, 0			
-
-					if c == "a" or c == "A" then					
-						dx = -move_step
-					elseif c == "d" or c == "D" then					
-						dx = move_step
-					elseif c == "w" or c == "W" then					
-						dz = move_step
-					elseif c == "s" or c == "S" then					
-						dz = -move_step
-					elseif c == "q" or c == "Q" then
-						dy = -move_step
-					 elseif c == "e" or c == "E" then
-						dy = move_step
-					end
-
-					camera_util.move(ms, camera, dx, dy, dz)
-				end
+			local nomouse_down = not (rightbtn_down or leftbtn_down)
+			if nomouse_down and (c == "r" or c == "R") then
+				ms(	eye, {0, 0, -10}, "=")
+				ms( rot, {0, 0, 0}, "=")
+				return 
 			end
-		end
+
+			if rightbtn_down then					
+				local dx, dy, dz = 0, 0, 0			
+
+				if c == "a" or c == "A" then					
+					dx = -move_step
+				elseif c == "d" or c == "D" then					
+					dx = move_step
+				elseif c == "w" or c == "W" then					
+					dz = move_step
+				elseif c == "s" or c == "S" then					
+					dz = -move_step
+				elseif c == "q" or c == "Q" then
+					dy = -move_step
+					elseif c == "e" or c == "E" then
+					dy = move_step
+				end
+
+				camera_util.move(ms, camera, dx, dy, dz)
+			end
+		end	
 	end
 
 	self.message_component.msg_observers:add(message)
-	message.cb = {}
-	message.msg_comp = self.message_component
-	message.ms = self.math_stack
-	message.move_speed = 1
 end
-
--- function camera_controller_system:update()
--- 	local camera = world:first_entity("main_camera")
--- 	if camera then
--- 		local cs = self.control_state.state
--- 		if cs == "camera" or cs == "default" then
--- 			for _, cb in pairs(message.cb) do
--- 				cb(camera)
--- 			end
--- 		end
--- 	end
-	
--- 	message.cb = {}
--- end
 
 function camera_controller_system.notify:focus_selected_obj(objects)
 	--only using first obj
