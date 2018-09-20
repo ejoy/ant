4b5247649ef40080c7fdd4ee3de94ddac52ad977
junjie020 <junjie020@163.com>
2018-09-20 09:17:40 +0800

    ani shader

--- assets/shaders/src/common/transform.sh
+++ assets/shaders/src/common/transform.sh
@@ -1,6 +1,5 @@
 mat4 calc_bone_transform(ivec4 indices, vec4 weights)
 {
-	vec4 pos = vec4(0, 0, 0, 1);
 	mat4 wolrdMat;
 	for (int ii = 0; ii < 4; ++ii)
 	{
@@ -18,6 +17,20 @@ mat3 calc_tbn_with_nt(vec3 n, vec3 t, float bitangent_sign, mat4 worldMat)
 	vec3 normal = normalize(mul(worldMat, vec4(n, 1)).xyz);
 	vec3 tangent = normalize(mul(worldMat, vec4(t, 1)).xyz);
 	vec3 bitangent = normalize(cross(normal, tangent) * bitangent_sign);
+
+ 	return transpose(
+			mat3(
+			tangent,
+			bitangent,
+			normal)
+		);
+}
+
+mat3 calc_tbn_with_nt_ex(vec3 n, vec3 t, mat4 worldMat))
+{
+	vec3 normal = normalize(mul(worldMat, vec4(n, 1)).xyz);
+	vec3 tangent = normalize(mul(worldMat, vec4(t, 1)).xyz);
+	vec3 bitangent = cross(normal, tangent);
 
  	return transpose(
 			mat3(
--- assets/shaders/src/mesh/vs_meshani.sc
+++ assets/shaders/src/mesh/vs_meshani.sc
@@ -1,5 +1,5 @@
 $input a_position, a_texcoord0, a_normal, a_tangent, a_indices, a_weight
-$output v_texcoord0, v_lightdir, v_viewdir, v_normal, v_tangent, v_bitangent
+$output v_texcoord0, v_lightdir, v_viewdir, v_normal
 
 #include <bgfx_shader.sh>
 #include "common/uniforms.sh"
@@ -8,7 +8,11 @@ $output v_texcoord0, v_lightdir, v_viewdir, v_normal, v_tangent, v_bitangent
 void main()
 {
 	mat4 worldMat = calc_bone_transform(a_indices, a_weight);
-	gl_Position = u_viewProj * wolrdMat * (pos * 0.25);
+	vec4 pos = mul(worldMat, a_position);
+	gl_Position = mul(u_viewProj, pos);
 
+	v_texcoord0 = a_texcoord0;
+	v_normal = a_normal;
 
+	mat3 tbn = calc_tbn(a_normal, a_tangent, worldMat);
 }
