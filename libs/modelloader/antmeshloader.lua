dc74e701a460d734a4cf9e916fbfcf0155cbd7c0
junjie020 <junjie020@163.com>
2018-09-27 15:55:52 +0800

    support old version and new version antmesh

--- libs/modelloader/antmeshloader.lua
+++ libs/modelloader/antmeshloader.lua
@@ -71,7 +71,10 @@ return function (filename)
 			ff = nil
 		end
 
-		return function ()
+		return function (seek)
+			if seek then
+				return ff:seek(seek.where, seek.offset)
+			end
 			local prefix = read(4)	-- full content size and elem size
 			if prefix == nil or #prefix < 4 then
 				close()
@@ -141,90 +144,223 @@ return function (filename)
 		})
 	end
 
+	local function get_group_mapper()
+		return {
+			bounding	= bounding_unpack,
+			name 		= string_unpack,
+			primitives = function (v)
+				return array_unpack(v, {
+					bounding= bounding_unpack,
+					transform =matrix_unpack,
+					name = string_unpack,
+					material_idx = uint_unpack,
+
+					start_vertex = uint_unpack,
+					num_vertices = uint_unpack,
+
+					start_index = uint_unpack,
+					num_indices = uint_unpack,
+				}) 
+			end,
+		}
+	end
+
+	local function get_mapper(version)
+		local groupmapper = get_group_mapper()
+
+		local root = {
+			srcfile = string_unpack,
+			bounding = bounding_unpack,
+
+			materials = function (v)	
+				return array_unpack(v, {
+					name = string_unpack,
+					textures = function (v) 
+						return struct_unpack(v, {
+							diffuse=string_unpack,
+							ambient=string_unpack,
+							specular=string_unpack,
+							normals=string_unpack,
+							shininess=string_unpack,
+							lightmap=string_unpack,
+						})
+					end,
+					colors = function (v)
+						return struct_unpack(v, {
+							diffuse=v3_unpack,
+							specular=v3_unpack,
+							ambient=v3_unpack,
+						})
+					end
+				})
+			end,
+			groups = function(v)
+				return array_unpack(v, groupmapper)
+			end
+		}
+
+		if version >= (1 << 16) then
+			groupmapper.vb = function(v) 
+				return struct_unpack(v,{
+				layout		= string_unpack,
+				num_vertices= uint_unpack,
+				vbraws		= function (v)
+					local mapper = {}
+					mapper.__index = function (t, key)
+						return string_unpack
+					end
+					local t = array_unpack(v, mapper)
+					assert(#t == 0)
+					local sorted_t = {}
+					for k in pairs(t) do
+						table.insert(sorted_t, k)
+					end
+					local tt = {}
+					for _, k in ipairs(sorted_t) do
+						local v = t[k]
+						table.insert(tt, v)
+					end
+					return tt
+				end})
+			end
+			groupmapper.ib = function(v)
+				return struct_unpack(v, {
+					format		=uint8_unpack,
+					num_indices	= uint_unpack,
+					ibraw		= string_unpack,						
+				})
+			end
+		elseif version == 0 then
+			groupmapper.vb_layout	= string_unpack
+			groupmapper.num_vertices= uint_unpack
+			groupmapper.vbraw		= string_unpack
+
+			groupmapper.ib_format	= uint8_unpack			
+			groupmapper.num_indices = uint_unpack
+			groupmapper.ibraw 		= string_unpack
+
+			local groups = root.groups
+			local function new_groups(v)
+				local result = groups(v)
+				assert(result.vb == nil)
+				result.vb = {
+					layout=assert(result.layout),
+					num_vertices=assert(result.num_vertices),
+					vbraws={assert(result.vbraw)}
+				}
+
+				assert(result.ib == nil)
+				result.ib = {
+					format=assert(result.ib_format),
+					num_indices=assert(result.num_indices),
+					ibraw=assert(result.ibraw),
+				}
+				return result
+			end
+
+			root.groups = new_groups
+		end
+
+		return root
+	end
+
+	local function get_version()
+		local k, v = kvpairs()
+		if k == "version" then
+			return math.tointeger(v)
+		end
+
+		kvpairs({where="set"})	-- seek to begin
+		return v
+	end
+
+	local mapper = get_mapper(get_version())
+
 	-- actually, we can provide a common mapper for some common members, 
 	-- like : bounding, transform, num_vertiecs, num_indices etc.	
 	-- and when we need something special member mapper, we can provide from 
 	-- mapper function. when a member not found in current level, it should 
 	-- try to found in it's parent level, until it not found, and it will 
 	-- raise an lua error(access to nil member).
-	return struct_unpack(nil, 
-	{
-		version = uint_unpack,
-		srcfile = string_unpack,
-		bounding = bounding_unpack,
 
-		materials = function (v)	
-			return array_unpack(v, {
-				name = string_unpack,
-				textures = function (v) 
-					return struct_unpack(v, {
-						diffuse=string_unpack,
-						ambient=string_unpack,
-						specular=string_unpack,
-						normals=string_unpack,
-						shininess=string_unpack,
-						lightmap=string_unpack,
-					})
-				end,
-				colors = function (v)
-					return struct_unpack(v, {
-						diffuse=v3_unpack,
-						specular=v3_unpack,
-						ambient=v3_unpack,
-					})
-				end
-			})
-		end,
+	return struct_unpack(nil, mapper)
+	-- return struct_unpack(nil, 
+	-- {
+	-- 	srcfile = string_unpack,
+	-- 	bounding = bounding_unpack,
 
-		groups = function(v)
-			return array_unpack(v, {
-				bounding	= bounding_unpack,
-				name 		= string_unpack,
-				vb 			= function(v) 
-					return struct_unpack(v,{
-					layout		= string_unpack,
-					num_vertices= uint_unpack,
-					vbraws		= function (v)
-						local mapper = {}
-						mapper.__index = function (t, key)
-							return string_unpack
-						end
-						local t = array_unpack(v, mapper)
-						assert(#t == 0)
-						local sorted_t = {}
-						for k in pairs(t) do
-							table.insert(sorted_t, k)
-						end
-						local tt = {}
-						for _, k in ipairs(sorted_t) do
-							local v = t[k]
-							table.insert(tt, v)
-						end
-						return tt
-					end})
-				end,
-				ib			= function(v)
-					return struct_unpack(v, {
-						format		=uint8_unpack,
-						num_indices	= uint_unpack,
-						ibraw		= string_unpack,						
-					})
-				end,				
-				primitives = function (v)
-					return array_unpack(v, {
-						bounding= bounding_unpack,
-						transform =matrix_unpack,
-						name = string_unpack,
-						material_idx = uint_unpack,
+	-- 	materials = function (v)	
+	-- 		return array_unpack(v, {
+	-- 			name = string_unpack,
+	-- 			textures = function (v) 
+	-- 				return struct_unpack(v, {
+	-- 					diffuse=string_unpack,
+	-- 					ambient=string_unpack,
+	-- 					specular=string_unpack,
+	-- 					normals=string_unpack,
+	-- 					shininess=string_unpack,
+	-- 					lightmap=string_unpack,
+	-- 				})
+	-- 			end,
+	-- 			colors = function (v)
+	-- 				return struct_unpack(v, {
+	-- 					diffuse=v3_unpack,
+	-- 					specular=v3_unpack,
+	-- 					ambient=v3_unpack,
+	-- 				})
+	-- 			end
+	-- 		})
+	-- 	end,
 
-						start_vertex = uint_unpack,
-						num_vertices = uint_unpack,
+	-- 	groups = function(v)
+	-- 		return array_unpack(v, {
+	-- 			bounding	= bounding_unpack,
+	-- 			name 		= string_unpack,
+	-- 			vb 			= function(v) 
+	-- 				return struct_unpack(v,{
+	-- 				layout		= string_unpack,
+	-- 				num_vertices= uint_unpack,
+	-- 				vbraws		= function (v)
+	-- 					local mapper = {}
+	-- 					mapper.__index = function (t, key)
+	-- 						return string_unpack
+	-- 					end
+	-- 					local t = array_unpack(v, mapper)
+	-- 					assert(#t == 0)
+	-- 					local sorted_t = {}
+	-- 					for k in pairs(t) do
+	-- 						table.insert(sorted_t, k)
+	-- 					end
+	-- 					local tt = {}
+	-- 					for _, k in ipairs(sorted_t) do
+	-- 						local v = t[k]
+	-- 						table.insert(tt, v)
+	-- 					end
+	-- 					return tt
+	-- 				end})
+	-- 			end,
+	-- 			ib			= function(v)
+	-- 				return struct_unpack(v, {
+	-- 					format		=uint8_unpack,
+	-- 					num_indices	= uint_unpack,
+	-- 					ibraw		= string_unpack,						
+	-- 				})
+	-- 			end,				
+	-- 			primitives = function (v)
+	-- 				return array_unpack(v, {
+	-- 					bounding= bounding_unpack,
+	-- 					transform =matrix_unpack,
+	-- 					name = string_unpack,
+	-- 					material_idx = uint_unpack,
 
-						start_index = uint_unpack,
-						num_indices = uint_unpack,
-					}) 
-				end,
-			})
-		end,
-	})
+	-- 					start_vertex = uint_unpack,
+	-- 					num_vertices = uint_unpack,
+
+	-- 					start_index = uint_unpack,
+	-- 					num_indices = uint_unpack,
+	-- 				}) 
+	-- 			end,
+	-- 		})
+	-- 	end,
+	-- })
 end
