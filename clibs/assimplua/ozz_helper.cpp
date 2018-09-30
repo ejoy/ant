369d4df0450dcd2d4d650efce93dc3382d24cfa6
junjie020 <junjie020@163.com>
2018-09-30 10:56:46 +0800

    fix ozz mesh reading bug

--- clibs/assimplua/ozz_helper.cpp
+++ clibs/assimplua/ozz_helper.cpp
@@ -130,27 +130,34 @@ create_vb_layout(const ozz::sample::Mesh &mesh) {
 	std::string layout;
 	std::string def = GetDefaultVertexLayoutElem();
 
+	auto append_elem = [&layout](const std::string &e) {
+		if (layout.empty())
+			layout = e;
+		else
+			layout += "|" + e;
+	};
+
 	assert(!part.positions.empty());
 	std::string pos = def;
 	pos[0] = 'p';
-	pos[1] = ozz::sample::Mesh::Part::kPositionsCpnts + '0';
-	layout += pos;
+	pos[1] = ozz::sample::Mesh::Part::kPositionsCpnts + '0';	
+	append_elem(pos);
 
 	if (!part.normals.empty()) {
 		std::string normal = def;
 		normal[0] = 'n';
 		normal[1] = ozz::sample::Mesh::Part::kNormalsCpnts + '0';
-		normal[3] = 'n';	// have been normalize
-		layout += normal;
+		normal[3] = 'n';	// have been normalize		
+		append_elem(normal);
 	}
-
 
 	if (!part.tangents.empty()) {
 		std::string tangent = def;
 		tangent[0] = 'T';
 		tangent[1] = ozz::sample::Mesh::Part::kTangentsCpnts + '0';
 		tangent[3] = 'n';
-		layout += tangent;
+		
+		append_elem(tangent);
 	}
 
 	if (!part.colors.empty()) {
@@ -158,7 +165,7 @@ create_vb_layout(const ozz::sample::Mesh &mesh) {
 		color[0] = 'c';
 		color[1] = ozz::sample::Mesh::Part::kColorsCpnts + '0';
 		color[3] = 'n';
-		layout += color;
+		append_elem(color);
 	}
 
 	if (!part.uvs.empty()) {
@@ -166,7 +173,7 @@ create_vb_layout(const ozz::sample::Mesh &mesh) {
 		tex[0] = 't';
 		tex[1] = ozz::sample::Mesh::Part::kUVsCpnts + '0';
 		tex[3] = 'n';
-		layout += tex;
+		append_elem(tex);		
 	}
 
 	auto maxinf = mesh.max_influences_count();
@@ -175,15 +182,15 @@ create_vb_layout(const ozz::sample::Mesh &mesh) {
 		indices[0] = 'i';
 		indices[1] = maxinf + '0';
 		indices[4] = 'i';	// as int
-		indices[5] = 'u';	// as uint8
-		layout += indices;
+		indices[5] = 'u';	// as uint8		
+		append_elem(indices);
 	}
 
 	if (maxinf > 1) {
 		std::string weight = def;
 		weight[0] = 'w';
 		weight[1] = maxinf - 1 + '0';
-		layout += weight;
+		append_elem(weight);		
 	}
 
 	return layout;
@@ -191,55 +198,71 @@ create_vb_layout(const ozz::sample::Mesh &mesh) {
 
 
 static void
-init_vertex_buffer(const ozz::sample::Mesh &mesh, vb_info &vb) {
+init_vertex_buffer(const ozz::sample::Mesh &mesh, const load_config &config, vb_info &vb) {
 	assert(vb.soa);
 
 	auto maxinf = mesh.max_influences_count();	
 	auto calc_size = [](const ozz::sample::Mesh &mesh, const std::string &elem) {
 		size_t sizeInBytes = 0;
-		const size_t count = elem[1] - '0';
+		
 		switch (elem[0])
 		{
 		case 'p': {			
 			for (const auto &p : mesh.parts)
-				sizeInBytes += p.positions.size() * count * sizeof(float);
+				sizeInBytes += p.positions.size() * sizeof(float);				
 			break;
 		}			
 		case 'n': {
 			for (const auto &p : mesh.parts)
-				sizeInBytes += p.normals.size() * count * sizeof(float);
+				sizeInBytes += p.normals.size() * sizeof(float);
 
 			break;
 		}
 			
 		case 'T': {
 			for (const auto &p : mesh.parts)
-				sizeInBytes += p.tangents.size() * count * sizeof(float);
+				sizeInBytes += p.tangents.size() * sizeof(float);
 
 			break;
 		}
 		case 't': {
 			for (const auto &p : mesh.parts)
-				sizeInBytes += p.uvs.size() * count * sizeof(float);
+				sizeInBytes += p.uvs.size() * sizeof(float);
 
 			break;
 		}
 		case 'c': {
 			for (const auto &p : mesh.parts)
-				sizeInBytes += p.colors.size() * count * sizeof(float);
+				sizeInBytes += p.colors.size() * sizeof(float);
 
 			break;
 		}
 
-		case 'i': {
-			for (const auto &p : mesh.parts)
-				sizeInBytes += p.joint_indices.size() * count * sizeof(uint8_t);
+		case 'i': {			
+			size_t maxinf = mesh.max_influences_count();
 
+			for (const auto &p : mesh.parts) {
+				size_t inf = p.influences_count();
+
+				size_t size = (p.joint_indices.size() / inf) * maxinf;
+				sizeInBytes += size * sizeof(uint8_t);
+			}
+			
 			break;
 		}
 		case 'w': {
-			for (const auto &p : mesh.parts)
-				sizeInBytes += p.joint_weights.size() * count * sizeof(float);
+			size_t maxinf = mesh.max_influences_count() - 1;
+
+			for (const auto &p : mesh.parts) {
+				if (p.joint_weights.empty())
+					continue;
+
+				size_t inf = p.influences_count() - 1;
+				size_t size = (p.joint_weights.size() / inf) * maxinf;
+
+				sizeInBytes += size * sizeof(float);
+			}
+				
 
 			break;
 		}
@@ -260,28 +283,72 @@ init_vertex_buffer(const ozz::sample::Mesh &mesh, vb_info &vb) {
 	}
 	
 	size_t startVB = 0;
+	size_t weightOffset = 0;
 	for (auto &part : mesh.parts) {
 		assert(!part.positions.empty());
 
-		auto copy_data = [&elem_ptr_mapper, &vb](size_t startVB, char elemName, size_t elemSize, auto &srcArray) {
+		auto copy_data = [&elem_ptr_mapper, &vb](size_t offset, char elemName, size_t elemSize, auto &srcArray) {
 			const auto idx = elem_ptr_mapper[elemName];
 			auto &ptr = vb.vbraws[idx];			
-			memcpy(ptr.data, ozz::array_begin(srcArray), startVB * elemSize + srcArray.size() * elemSize);
+			memcpy(ptr.data + offset, ozz::array_begin(srcArray), srcArray.size() * elemSize);
 		};
 
-		copy_data(startVB, 'p', ozz::sample::Mesh::Part::kPositionsCpnts * sizeof(float), part.positions);		
-		copy_data(startVB, 'n', ozz::sample::Mesh::Part::kNormalsCpnts * sizeof(float), part.normals);
-		copy_data(startVB, 'T', ozz::sample::Mesh::Part::kTangentsCpnts * sizeof(float), part.tangents);
-		copy_data(startVB, 'c', ozz::sample::Mesh::Part::kColorsCpnts * sizeof(float), part.colors);
-		copy_data(startVB, 't', ozz::sample::Mesh::Part::kUVsCpnts * sizeof(float), part.uvs);
+		copy_data(startVB * ozz::sample::Mesh::Part::kPositionsCpnts * sizeof(float),	'p', sizeof(float), part.positions);		
+		copy_data(startVB * ozz::sample::Mesh::Part::kNormalsCpnts * sizeof(float),		'n', sizeof(float), part.normals);
+		copy_data(startVB * ozz::sample::Mesh::Part::kTangentsCpnts * sizeof(float),	'T', sizeof(float), part.tangents);
+		copy_data(startVB * ozz::sample::Mesh::Part::kColorsCpnts * sizeof(float),		'c', sizeof(float), part.colors);
+		copy_data(startVB * ozz::sample::Mesh::Part::kUVsCpnts * sizeof(float),			't', sizeof(float), part.uvs);
 
+		auto inf = part.influences_count();
 		
-		if (maxinf > 0)
-			copy_data(startVB, 'i', maxinf * sizeof(uint16_t), part.joint_indices);
-		if (maxinf > 1)
-			copy_data(startVB, 'w', (maxinf - 1) * sizeof(float), part.joint_weights);
+		if (inf > 0) {
+			const auto idx = elem_ptr_mapper['i'];
+			auto &ptr = vb.vbraws[idx];
+			uint8_t *data = ptr.data + startVB * maxinf;
+			for (size_t iV = 0; iV < part.vertex_count(); ++iV) {
+				for (size_t iInf = 0; iInf < inf; ++iInf) {
+					size_t idx = iV * inf + iInf;
+					uint16_t joint_idx = part.joint_indices[idx];
+					assert(joint_idx < 256);
+					size_t dataidx = iV * maxinf + iInf;
+					data[dataidx] = (uint8_t)joint_idx;
+				}
 
-		startVB += part.positions.size();
+				for (size_t iInfLeft = inf; iInfLeft < maxinf; ++iInfLeft) {
+					data[iV * maxinf + iInfLeft] = 0;
+				}
+			}
+		}
+			
+		if (inf > 1) {
+			const auto idx = elem_ptr_mapper['w'];
+			auto &ptr = vb.vbraws[idx];
+
+			const size_t weight_inf = inf - 1;
+			const size_t max_weight_inf = maxinf - 1;
+
+			float *data = (float*)ptr.data + weightOffset * max_weight_inf;
+
+			for (size_t iV = 0; iV < part.vertex_count(); ++iV) {
+				const size_t data_vertex_idx = iV * max_weight_inf;
+				const size_t joint_vertex_idx = iV * weight_inf;
+
+				for (size_t iInf = 0; iInf < weight_inf; ++iInf) {
+					const size_t idx = iV * weight_inf + iInf;
+					float weight = part.joint_weights[idx];					
+					const size_t dataidx = data_vertex_idx + iInf;
+					data[dataidx] = weight;
+				}
+
+				for (size_t iInfLeft = weight_inf; iInfLeft < max_weight_inf; ++iInfLeft) {
+					data[data_vertex_idx + iInfLeft] = 0;
+				}
+			}
+
+			weightOffset += part.vertex_count();
+		}
+		
+		startVB += part.vertex_count();
 	}
 }
 
@@ -304,7 +371,7 @@ convertOZZ(lua_State *L, const std::string &srcpath, const std::string &outputfi
 	vb.num_vertices = mesh.vertex_count();
 	vb.layout = create_vb_layout(mesh);
 	
-	init_vertex_buffer(mesh, vb);
+	init_vertex_buffer(mesh, config, vb);
 	
 
 	//auto vbPointer = group.vbraw;
