520ae7b19ac1808282d6ab612d18e3fd29faf680
actboy168 <actboy168@gmail.com>
2018-10-09 11:06:45 +0800

    Update vscode-ant

--- libs/debugger/frontend/main.lua
+++ libs/debugger/frontend/main.lua
@@ -1,9 +1,3 @@
-(function()
-    local exepath = package.cpath:sub(1, (package.cpath:find(';') or 0)-6)
-    package.path = arg[1]:gsub('!', exepath)
-    package.cpath = arg[2]:gsub('!', exepath)
-end)()
-
 --log = require 'debugger.log'
 --log.file = 'dbg.log'
 print = nil
