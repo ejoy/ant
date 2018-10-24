36edc18da814fb94c01a7cdbb863272188c3b785
lincolnwty <909581047@qq.com>
2018-10-24 17:42:19 +0800

    avoid crash due to system order

--- libs/scene/filter_system.lua
+++ libs/scene/filter_system.lua
@@ -2,6 +2,7 @@ local ecs = ...
 local world = ecs.world
 
 ecs.import "render.math3d.math_component"
+ecs.import "scene.shadow.generate_shadow_system"
 
 local cu = require "render.components.util"
 
@@ -174,6 +175,8 @@ shadow_primitive_filter_sys.singleton "math_stack"
 
 shadow_primitive_filter_sys.depend "primitive_filter_system"
 
+shadow_primitive_filter_sys.depend "generate_shadow_system"  -- remove debug for crash & flicker 
+
 -- get shadow uniforms 
 local function get_shadow_properties()
 	local properties = {} 
@@ -181,31 +184,35 @@ local function get_shadow_properties()
 	for _,l_eid in world:each("shadow_maker") do 
 		local  sm_ent   = world[l_eid]
 		local  uniforms = sm_ent.shadow_rt.uniforms 
+		-- print(" get shadow uniforms in filter",#uniforms )
 		-- uniforms.shadowMap
-		properties["u_params1"] = { name = "u_params1",type="v4",value = { uniforms.shadowMapBias,
-																			uniforms.shadowMapOffset,0.5,1} } 
+		properties["u_params1"] = { name = "u_params1",type="v4",value = {  uniforms.shadowMapBias,
+																			uniforms.shadowMapOffset, 0.5, 1 } } 
 		properties["u_params2"] = { name = "u_params2",type="v4",
-									value = { uniforms.depthValuePow,
+									value = {   uniforms.depthValuePow,
 												uniforms.showSmCoverage,
 												uniforms.shadowMapTexelSize, 0 } }
 		properties["u_smSamplingParams"] = { name = "u_smSamplingParams",
 									type  ="v4",
 									value = { 0, 0, uniforms.ss_offsetx, uniforms.ss_offsety } }
 
-		-- -- shadow matrices 
-		properties["u_shadowMapMtx0"] = { name  = "u_shadowMapMtx0", type  = "m4", value = uniforms.shadowMapMtx0 }
-		properties["u_shadowMapMtx1"] = { name  = "u_shadowMapMtx1", type  = "m4", value = uniforms.shadowMapMtx1 }
-		properties["u_shadowMapMtx2"] = { name  = "u_shadowMapMtx2", type  = "m4", value = uniforms.shadowMapMtx2 }
-		properties["u_shadowMapMtx3"] = { name  = "u_shadowMapMtx3", type  = "m4", value = uniforms.shadowMapMtx3 }
-		-- -- shadow textures  ?	
-		--if sm_ent.shadow_rt.ready == true then 
+		-- 
+		if sm_ent.shadow_rt.ready == true then 
+			-- shadow matrices 
+			properties["u_shadowMapMtx0"] = { name  = "u_shadowMapMtx0", type  = "m4", value = uniforms.shadowMapMtx0 }
+			properties["u_shadowMapMtx1"] = { name  = "u_shadowMapMtx1", type  = "m4", value = uniforms.shadowMapMtx1 }
+			properties["u_shadowMapMtx2"] = { name  = "u_shadowMapMtx2", type  = "m4", value = uniforms.shadowMapMtx2 }
+			properties["u_shadowMapMtx3"] = { name  = "u_shadowMapMtx3", type  = "m4", value = uniforms.shadowMapMtx3 }
+		 	-- shadow textures  ?	
+
 			-- set_texture 函数比较严格，当 texturehandle 非法时引发崩溃
 			-- properties["s_normal"] = {name="normal", type="texture", stage=1, value = 0}
 			properties["s_shadowMap0"] = {  name = "s_shadowMap0", type = "texture", stage = 4, value = uniforms.s_shadowMap0 }
 			properties["s_shadowMap1"] = {  name = "s_shadowMap1", type = "texture", stage = 5, value = uniforms.s_shadowMap1 }
 			properties["s_shadowMap2"] = {  name = "s_shadowMap2", type = "texture", stage = 6, value = uniforms.s_shadowMap2 }
 			properties["s_shadowMap3"] = {  name = "s_shadowMap3", type = "texture", stage = 7, value = uniforms.s_shadowMap3 }
-		--end 
+		end 
+		
 	end 
 
 	return properties 
