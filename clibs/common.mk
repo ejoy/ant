f6f256d2b18d91796a800ebfa6adabdb8aa1c414
junjie020 <junjie020@163.com>
2018-10-25 09:50:49 +0800

    when enable debug build, using -g command for gcc

--- clibs/common.mk
+++ clibs/common.mk
@@ -7,9 +7,15 @@ LUABIN = ../lua/lua.exe
 
 AR= ar rcu
 CC= gcc
-CXX = g++
-CFLAGS = -O2 -Wall
-CXXFLAGS = -lstdc++ -std=c++17
 
 BUILD_CONFIG = Release
 
+CXX = g++
+ifeq ("$(BUILD_CONFIG)","Release")
+DEBUG_INFO = -O2
+else
+DEBUG_INFO = -g
+endif
+
+CFLAGS = $(DEBUG_INFO) -Wall
+CXXFLAGS = -lstdc++ -std=c++17
