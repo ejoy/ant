6082a04a0adcbad9ebd8e23d1425bb2095141514
actboy168 <actboy168@gmail.com>
2018-11-01 12:47:22 +0800

    Use thread instead of debugger.backend

--- libs/debugger/backend/master.lua
+++ libs/debugger/backend/master.lua
@@ -1,19 +1,17 @@
 local mgr = require 'debugger.backend.master.mgr'
-local cdebug = require 'debugger.backend'
+local thread = require 'thread'
 
 local m = {}
 
 function m.init(io)
-    local master = cdebug.start 'master'
-    if not master then
-        return false
-    end
+    thread.newchannel 'DbgMaster'
+    local masterThread = thread.channel 'DbgMaster'
     if not io then
         local type = os.getenv('_DBG_IOTYPE') or 'tcp_server'
         local ioFactory = require('debugger.io.' .. type)
         io = ioFactory('127.0.0.1', os.getenv('_DBG_IOPORT') and tonumber(os.getenv('_DBG_IOPORT')) or 4278)
     end
-    mgr.init(io, master)
+    mgr.init(io, masterThread)
     return true
 end
 
