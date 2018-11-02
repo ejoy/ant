dcf218a8c8133cea14e4a31dd670c5ccea103641
junjie020 <junjie020@163.com>
2018-11-02 17:51:52 +0800

    build_mingw.sh using 3rd as ANT3RD path

--- build_mingw.sh
+++ build_mingw.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-ANT3RD="../ant3rd"
+ANT3RD="3rd"
 if [ ! -d "$ANT3RD" ]; then
 	echo $ANT3RD is not exist!
 	exist 1
@@ -7,16 +7,9 @@ fi
 
 echo Found $ANT3RD
 cd $ANT3RD
-if [ "$UPDATE_GIT" != "" ]; then
-	git pull && git submodule update --init
-fi
 make init 
 make all $MP
 
 cd -
-if [ "$UPDATE_GIT" != ""]; then
-	git pull && git submodule update --init
-fi
-
 cd clibs
 make $MP $BIN
