7343341f7959d5dcfabe737ecca436359c8a9bd5
junjie020 <junjie020@163.com>
2018-11-07 11:59:49 +0800

    fix modeleditor

--- tools/modeleditor/modeleditor.lua
+++ tools/modeleditor/modeleditor.lua
@@ -3,6 +3,16 @@
 
 dofile "libs/init.lua"
 
+local fs = require "filesystem"
+local vfs = require "vfs"
+local cwd = fs.currentdir()
+vfs.mount({	
+	['engine/assets']=cwd .. "/assets", 
+	['engine/libs'] = cwd .. "/libs"
+}, cwd)
+
+require "iuplua"
+
 local rhwi = require "render.hardware_interface"
 local su = require "scene.util"
 
