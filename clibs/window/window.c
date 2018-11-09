fc9acd243e6aa18dbcf743f0a3b5fcc1497e1da5
Cloud Wu <cloudwu@gmail.com>
2018-11-09 16:34:50 +0800

    add window module

---
+++ clibs/window/window.c
@@ -0,0 +1,212 @@
+#define LUA_LIB
+
+#include <stdio.h>
+#include <lua.h>
+#include <lauxlib.h>
+#include "window.h"
+
+#define CALLBACK_ERROR 1
+#define CALLBACK_UPDATE 2
+#define CALLBACK_EXIT 3
+#define CALLBACK_TOUCH 4
+#define CALLBACK_MOVE 5
+#define CALLBACK_COUNT 5
+
+struct callback_context {
+	lua_State *callback;
+	lua_State *functions;
+};
+
+static int
+push_callback_function(struct callback_context * context, int id) {
+	lua_pushvalue(context->functions, id);
+	lua_xmove(context->functions, context->callback, 1);
+	int ret = lua_type(context->callback, 2) == LUA_TFUNCTION;
+	if (!ret) {
+		lua_pop(context->callback, 1);
+	}
+	return ret;
+}
+
+static void
+push_update_args(lua_State *L, struct ant_window_update *update) {
+}
+
+static void
+push_exit_args(lua_State *L, struct ant_window_exit *exit) {
+}
+
+static void
+push_touch_args(lua_State *L, struct ant_window_touch *touch) {
+	lua_pushinteger(L, touch->what);
+	lua_pushinteger(L, touch->x);
+	lua_pushinteger(L, touch->y);
+}
+
+static void
+push_move_args(lua_State *L, struct ant_window_move *move) {
+	lua_pushinteger(L, move->x);
+	lua_pushinteger(L, move->y);
+}
+
+static int
+lraise_error_string(lua_State *L) {
+	const char * msg = (const char *)lua_touserdata(L, 2);
+	if (lua_type(L, 1) != LUA_TFUNCTION) {
+		// no error handle
+		printf("Error: %s\n", msg);
+	} else {
+		lua_pop(L, 1);
+		lua_pushstring(L, msg);
+		lua_call(L, 1, 0);
+	}
+	return 0;
+}
+
+static void
+raise_error_string(struct callback_context *context, const char *errmsg) {
+	lua_State *L = context->callback;
+	lua_pushcfunction(L, lraise_error_string);
+	lua_pushvalue(context->functions, CALLBACK_ERROR);
+	lua_xmove(context->functions, L, 1);
+	lua_pushlightuserdata(L, (void *)errmsg);
+	if (lua_pcall(L, 2, 0, 0) != LUA_OK) {
+		printf("Error in error handle : %s\n", lua_tostring(L, -1));
+		lua_pop(L, 1);
+	}
+}
+
+static void
+raise_error(struct callback_context *context) {
+	lua_State *L = context->callback;
+	lua_pushvalue(context->functions, CALLBACK_ERROR);
+	lua_xmove(context->functions, L, 1);
+	if (lua_type(L, -1) == LUA_TFUNCTION) {
+		lua_insert(L, -2);	// error_handler, error_string
+		if (lua_pcall(L, 1, 0, 0) != LUA_OK) {
+			printf("Error in error handle : %s\n", lua_tostring(L, -1));
+			lua_pop(L, 1);
+		}
+	} else {
+		printf("Error: %s\n", lua_tostring(L, -1));
+		lua_pop(L, 2);
+	}
+}
+
+static void
+callback(void *ud, struct ant_window_message *msg) {
+	struct callback_context * context = (struct callback_context *)ud;
+	lua_State *L = context->callback;
+	switch(msg->type) {
+	case ANT_WINDOW_UPDATE:
+		if (push_callback_function(context, CALLBACK_UPDATE)) {
+			push_update_args(L, &msg->u.update);
+			break;
+		} else {
+//			raise_error_string(context, "No update handler");
+			return;
+		}
+	case ANT_WINDOW_EXIT:
+		if (push_callback_function(context, CALLBACK_EXIT)) {
+			push_exit_args(L, &msg->u.exit);
+			break;
+		} else {
+//			raise_error_string(context, "No exit handler");
+			return;
+		}
+	case ANT_WINDOW_TOUCH:
+		if (push_callback_function(context, CALLBACK_TOUCH)) {
+			push_touch_args(L, &msg->u.touch);
+			break;
+		} else {
+//			raise_error_string(context, "No touch handler");
+			return;
+		}
+	case ANT_WINDOW_MOVE:
+		if (push_callback_function(context, CALLBACK_MOVE)) {
+			push_move_args(L, &msg->u.move);
+			break;
+		} else {
+//			raise_error_string(context, "No move handler");
+			return;
+		}
+	default:
+		raise_error_string(context, "Unknown callback");
+		return;
+	}
+	int nargs = lua_gettop(L) - 2;
+	if (lua_pcall(L, nargs, 0, 1) != LUA_OK) {
+		raise_error(context);
+	}
+}
+
+static void
+register_function(lua_State *L, int index, const char *name, lua_State *fL, int id) {
+	lua_getfield(L, index, name);
+	lua_xmove(L, fL, 1);
+	lua_replace(fL, id);
+}
+
+static void
+register_functions(lua_State *L, int index, lua_State *fL) {
+	int i;
+	luaL_checkstack(fL, CALLBACK_COUNT+2, NULL);	// 2 for temp
+	for (i=0;i<CALLBACK_COUNT;i++) {
+		lua_pushnil(fL);
+	}
+	register_function(L, index, "error", fL, CALLBACK_ERROR);
+	register_function(L, index, "update", fL, CALLBACK_UPDATE);
+	register_function(L, index, "exit", fL, CALLBACK_EXIT);
+	register_function(L, index, "touch", fL, CALLBACK_TOUCH);
+	register_function(L, index, "move", fL, CALLBACK_MOVE);
+}
+
+static int
+ltraceback(lua_State *L) {
+	const char *msg = lua_tostring(L, 1);
+	if (msg == NULL && !lua_isnoneornil(L, 1)) {
+		lua_pushvalue(L, 1);
+	} else {
+		luaL_traceback(L, L, msg, 2);
+	}
+	return 1;
+}
+
+static int
+lregistercallback(lua_State *L) {
+	luaL_checktype(L, 1, LUA_TTABLE);
+
+	if (lua_getfield(L, LUA_REGISTRYINDEX, ANT_WINDOW_CALLBACK) != LUA_TUSERDATA) {
+		return luaL_error(L, "Create native window first");
+	}
+	struct ant_window_callback *cb = (struct ant_window_callback *)lua_touserdata(L, -1);
+	lua_pop(L, 1);
+
+	struct callback_context * context = lua_newuserdata(L, sizeof(*context));
+	lua_createtable(L, 2, 0);	// for callback and functions thread
+	context->callback = lua_newthread(L);
+	lua_pushcfunction(context->callback, ltraceback);	// push traceback function
+	lua_rawseti(L, -2, 1);
+	context->functions = lua_newthread(L);
+	lua_rawseti(L, -2, 2);
+	lua_setuservalue(L, -2);	// ref 2 threads to context userdata
+	lua_setfield(L, LUA_REGISTRYINDEX, "ANT_CALLBACK_CONTEXT");
+
+	register_functions(L, 1, context->functions);
+
+	cb->message = callback;
+	cb->ud = context;
+
+	return 0;
+}
+
+LUAMOD_API int
+luaopen_window(lua_State *L) {
+	luaL_checkversion(L);
+	luaL_Reg l[] = {
+		{ "register", lregistercallback },
+		{ NULL, NULL },
+	};
+	luaL_newlib(L, l);
+	return 1;
+}
---
+++ clibs/window/window.h
@@ -0,0 +1,45 @@
+#ifndef ant_window_h
+#define ant_window_h
+
+#include <lua.h>
+
+#define ANT_WINDOW_UPDATE 0
+#define ANT_WINDOW_EXIT 1
+#define ANT_WINDOW_TOUCH 2
+#define ANT_WINDOW_MOVE 3
+
+#define ANT_WINDOW_CALLBACK "ANT_WINDOW_CALLBACK"
+
+struct ant_window_update {
+};
+
+struct ant_window_exit {
+};
+
+struct ant_window_touch {
+	int what;	// 0: lbutton up ; 1: lbutton down
+	int x;
+	int y;
+};
+
+struct ant_window_move {
+	int x;
+	int y;
+};
+
+struct ant_window_message {
+	int type;
+	union {
+		struct ant_window_update update;
+		struct ant_window_exit exit;
+		struct ant_window_touch touch;
+		struct ant_window_move move;
+	} u;
+};
+
+struct ant_window_callback {
+	void (*message)(void *ud, struct ant_window_message *);
+	void *ud;
+};
+
+#endif
