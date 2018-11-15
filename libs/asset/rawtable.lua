3e5e7132625152a079dd5ee385a728367da8d7c7
junjie020 <junjie020@163.com>
2018-11-15 14:49:31 +0800

    rawtable not depend any other code

--- libs/asset/rawtable.lua
+++ libs/asset/rawtable.lua
@@ -1,9 +1,5 @@
---luacheck: globals import
-local require = import and import(...) or require
-local vfs_fs = require "vfs.fs"
-
 return function (filename)
-	local f = vfs_fs.open(filename,"rb")
+	local f = io.open(filename,"rb")
 	local data = f:read "a"
 	f:close()
 	local env = {}
