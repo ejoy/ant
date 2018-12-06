edbb4a9b1bb4ae33cdec0af1694947551f07c62c
actboy168 <actboy168@gmail.com>
2018-12-06 17:38:56 +0800

    Fix ios link bug

--- clibs/bgfx/bgfx_define.mk
+++ clibs/bgfx/bgfx_define.mk
@@ -26,9 +26,11 @@ IB_COMPRESSLIB:= -L$(IB_COMPRESSROOT) -lib-compress
 ifeq "$(PLAT)" "mingw"
 BGFXLIB = -L$(BGFXSRC)/.build/win64_mingw-gcc/bin -lbgfx$(BUILD_CONFIG) $(BXLIB) -lstdc++ -lgdi32 -lpsapi -luuid
 else ifeq "$(PLAT)" "osx"
-BGFXLIB = -L$(BGFXSRC)/.build/osx64_clang/bin -lbgfx$(BUILD_CONFIG) $(BXLIB) -lstdc++ -framework Foundation -framework Metal -framework QuartzCore -framework Cocoa
+BGFXLIB = -L$(BGFXSRC)/.build/osx64_clang/bin -lbgfx$(BUILD_CONFIG) $(BXLIB) -lstdc++
+BGFXLIB += -framework Foundation -framework Metal -framework QuartzCore -framework Cocoa
 else ifeq "$(PLAT)" "ios"
 BGFXLIB = -L$(BGFXSRC)/.build/ios-arm64/bin -lbgfx$(BUILD_CONFIG) $(BXLIB) -lstdc++
+BGFXLIB += -framework CoreFoundation -framework Foundation -framework OpenGLES -framework UIKit -framework QuartzCore -weak_framework Metal
 endif
 
 BGFXUTILLIB = -lexample-common$(BUILD_CONFIG)
