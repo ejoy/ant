4aa4518c9ffb3aac7aec3b894181106f8043d043
actboy168 <actboy168@gmail.com>
2018-12-11 10:43:17 +0800

    Fix mingw build

--- clibs/bullet/bullet.mk
+++ clibs/bullet/bullet.mk
@@ -1,5 +1,3 @@
-include ../common.mk
-
 BULLETROOT = $(ANT3RD)
 BULLETINC = -I$(BULLETROOT)/bullet3/src 
 BULLETLIB = -L$(BULLETROOT)/build/bullet3/$(PLAT)/lib -lBulletDynamics -lBulletCollision -lLinearMath -lstdc++
