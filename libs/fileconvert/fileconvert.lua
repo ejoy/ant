e687d991c1145a52a00399e1489b70095600a48e
junjie020 <junjie020@163.com>
2018-12-12 09:54:30 +0800

    add log info

--- libs/fileconvert/fileconvert.lua
+++ libs/fileconvert/fileconvert.lua
@@ -11,7 +11,7 @@ local converter_names = {
 
 local logfile = nil
 
-local function log_err(src, lk, err)
+local function get_logfile()
 	if logfile == nil then
 		if not fu.exist("log") then
 			lfs.mkdir("log")
@@ -20,8 +20,20 @@ local function log_err(src, lk, err)
 		logfile = localfile.open("log/fileconvert.log", "wb")
 	end
 
-	logfile:write(string.format("[fileconvert]src:%s, lk:%s, error:%s\n", src, lk, err))
-	logfile:flush()
+	return assert(logfile)
+end
+
+local function log_err(src, lk, err)
+	local log = get_logfile()
+
+	log:write(string.format("[fileconvert]src:%s, lk:%s, error:%s\n", src, lk, err))
+	log:flush()
+end
+
+local function log_info(info)
+	local log = get_logfile()
+	log:write(string.format("[fileconvert-info:]", info))
+	log:flush()
 end
 
 return function (plat, sourcefile, lkfile, dstfile)
@@ -31,6 +43,7 @@ return function (plat, sourcefile, lkfile, dstfile)
 	local converter_name = assert(converter_names[ctype])
 
 	local c = require(converter_name)
+	log_info(string.format("plat:%s, src:%s, lk:%s, dst:%s, cvt type:%s", plat, sourcefile, lkfile, dstfile, ctype))
 	local success, err = c(plat, sourcefile, lkcontent, dstfile)
 	if err then
 		log_err(sourcefile, lkfile, err)
