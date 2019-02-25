96a9a553ede6760d00fe0dd78f03eaeeee50990e
actboy168 <actboy168@gmail.com>
2019-02-25 14:16:34 +0800

    Fix network bug

--- test/fileserver.lua
+++ test/fileserver.lua
@@ -179,7 +179,6 @@ local function dbgserver_update(obj)
 		response(dbg.server, "DBG", data)
 	end
 	if obj._status == "CONNECTING" then
-		obj._status = "CONNECTED"
 		LOG("New DBG", obj._peer, obj._ref)
 		if dbg.client then
 			network.close(obj)
