430dd4d6d099c7689f474a52ba9ba226f3451a10
actboy168 <actboy168@gmail.com>
2019-03-15 14:31:14 +0800

    Fix paired device

--- packages/mobiledevice/proxy.lua
+++ packages/mobiledevice/proxy.lua
@@ -3,6 +3,12 @@ package.path = table.concat({
     "packages/?/?.lua",
 }, ";")
 
+function log()
+    return function(fmt, ...)
+        print(fmt:format(...))
+    end
+end
+
 local network = require 'network'
 local usbmuxd = require 'mobiledevice.usbmuxd'
 
@@ -55,6 +61,8 @@ local function update_event()
         elseif event.MessageType == 'Detached' then
             LOG('device add', devices[event.DeviceID].sn)
             devices[event.DeviceID].status = 'closed'
+        elseif event.MessageType == 'Paired' then
+            LOG('device paired', devices[event.DeviceID].sn)
         else
             assert(false, 'Unknown message: ' .. event.MessageType)
         end
