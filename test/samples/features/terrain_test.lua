29820376cf113c22fecc961092e78923e31d47a4
junjie020 <junjie020@163.com>
2019-04-15 16:40:51 +0800

    add terrain texture

--- test/samples/features/terrain_test.lua
+++ test/samples/features/terrain_test.lua
@@ -11,36 +11,49 @@ terrain_test.depend 'init_loader'
 function terrain_test:init()
 	local terrainfolder = fs.path '//ant.resources' / 'terrain'
 	local function create_properties(basetexpath, masktexpath)
+		local texpath = terrainfolder / "textures"
 		return {
 			textures = {
-				s_baseTexture = { name = "base texture", type = "texture", stage = 0, 
-					ref_path = terrainfolder / basetexpath},
-				s_maskTexture = { name = "mask texture", type = "texture", stage = 1, 
-					ref_path = terrainfolder / masktexpath},
+				s_baseTexture = { 
+					name = "base texture", 
+					type = "texture", 
+					stage = 0, 
+					ref_path = texpath / basetexpath
+				},
+				s_maskTexture = { 
+					name = "mask texture", 
+					type = "texture",
+					stage = 1,
+					ref_path = texpath / masktexpath
+				},
 			}
 		}
 	end
 
+	local function create_material_content()
+	return {
+		{
+			ref_path = terrainfolder / 'terrain.material',
+			properties = create_properties("ground_099-512.texture", "pvp2_mask_a.texture"),
+		},
+		{
+			ref_path = terrainfolder / 'terrain_mask.material',
+			properties = create_properties("Scene_Texture_Terrain_BD_SOIL_0070_1.texture", "pvp2_mask_r.texture")
+		},
+		{
+			ref_path = terrainfolder / 'terrain_mask.material',
+			properties = create_properties("BH-Scene-JiangJunZong-Wall-02-D.texture", "pvp2_mask_g.texture")
+		},
+		{
+			ref_path = terrainfolder / 'terrain_mask.material',
+			properties = create_properties("Scene_Texture_Terrain_BD_ROCK_040_1.texture", "pvp2_mask_b.texture")
+		}
+	}
+	end
+
 	world:create_entity {
         material = {
-            content = {
-                {
-					ref_path = terrainfolder / 'terrain.material',
-					properties = create_properties("ground_099-512.dds", "pvp2_mask_a.dds"),
-                },
-                {
-					ref_path = terrainfolder / 'terrain_mask.material',
-					properties = create_properties("Scene_Texture_Terrain_BD_SOIL_0070_1.dds", "pvp2_mask_r.dds")
-                },
-                {
-					ref_path = terrainfolder / 'terrain_mask.material',
-					properties = create_properties("BH-Scene-JiangJunZong-Wall-02-D.dds", "pvp2_mask_g.dds")
-                },
-                {
-					ref_path = terrainfolder / 'terrain_mask.material',
-					properties = create_properties("Scene_Texture_Terrain_BD_ROCK_040_1.dds", "pvp2_mask_b.dds")
-                }
-            }
+            content = create_material_content()
         },
         transform = {
             s = {1, 1, 1, 0},
@@ -58,25 +71,8 @@ function terrain_test:init()
 		
 	world:create_entity {
         material = {
-            content = {
-                {
-					ref_path = terrainfolder / 'terrain.material',
-					properties = create_properties("ground_099-512.dds", "pvp2_mask_a.dds")
-                },
-                {
-					ref_path = terrainfolder / 'terrain_mask.material',
-					properties = create_properties("Scene_Texture_Terrain_BD_SOIL_0070_1.dds", "pvp2_mask_r.dds")
-                },
-                {
-					ref_path = terrainfolder / 'terrain_mask.material',
-					properties = create_properties("BH-Scene-JiangJunZong-Wall-02-D.dds", "pvp2_mask_g.dds")
-                },
-                {
-					ref_path = terrainfolder / 'terrain_mask.material',
-					properties = create_properties("Scene_Texture_Terrain_BD_ROCK_040_1.dds", "pvp2_mask_b.dds")
-                }
-            }
-        },
+			content = create_material_content(),
+		},
         transform = {
             s = {1, 1, 1, 0},
             r = {0, 0, 0, 0},
