1c92986e09091980abfa2fb5ed4573a3d9770ce7
actboy168 <actboy168@gmail.com>
2019-04-16 15:40:04 +0800

    cleanup

--- packages/debugger/backend/master.lua
+++ packages/debugger/backend/master.lua
@@ -3,11 +3,6 @@ local mgr = require 'backend.master.mgr'
 local m = {}
 
 function m.init(io)
-    if not io then
-        local type = os.getenv('_DBG_IOTYPE') or 'tcp_server'
-        local ioFactory = require('debugger.io.' .. type)
-        io = ioFactory('127.0.0.1', os.getenv('_DBG_IOPORT') and tonumber(os.getenv('_DBG_IOPORT')) or 4278)
-    end
     mgr.init(io)
     return true
 end
