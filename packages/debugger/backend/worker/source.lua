6569a094f006f5b6ef803362681ac93afe518005
actboy168 <actboy168@gmail.com>
2019-04-16 10:30:05 +0800

    Server path needs to be an absolute path

--- packages/debugger/backend/worker/source.lua
+++ packages/debugger/backend/worker/source.lua
@@ -21,14 +21,14 @@ ev.on('initializing', function(config)
     if config.sourceMaps then
         for _, pattern in ipairs(config.sourceMaps) do
             local sm = {}
-            sm[1] = ('^%s$'):format(fs.normalize_serverpath(pattern[1]):gsub('[%^%$%(%)%%%.%[%]%+%-%?]', '%%%0'))
+            sm[1] = ('^%s$'):format(fs.narive_normalize_serverpath(pattern[1]):gsub('[%^%$%(%)%%%.%[%]%+%-%?]', '%%%0'))
             if sm[1]:find '%*' then
                 sm[1] = sm[1]:gsub('%*', '(.*)')
                 local r = {}
-                fs.normalize(pattern[2]):gsub('[^%*]+', function (w) r[#r+1] = w end)
+                fs.normalize_clientpath(pattern[2]):gsub('[^%*]+', function (w) r[#r+1] = w end)
                 sm[2] = r
             else
-                sm[2] = fs.normalize(pattern[2])
+                sm[2] = fs.normalize_clientpath(pattern[2])
             end
             sourceMaps[#sourceMaps + 1] = sm
         end
@@ -66,7 +66,7 @@ end
 local function serverPathToClientPath(p)
     -- TODO: utf8 or ansi
     local skip = false
-    local nativePath = fs.normalize_serverpath(p)
+    local nativePath = fs.narive_normalize_serverpath(p)
     for _, pattern in ipairs(skipFiles) do
         if glob_match(pattern, nativePath) then
             skip = true
@@ -80,7 +80,7 @@ local function serverPathToClientPath(p)
         end
     end
     -- TODO: 忽略没有映射的source？
-    return skip, fs.normalize(p)
+    return skip, fs.normalize_serverpath(p)
 end
 
 local function codeReference(s)
@@ -149,9 +149,9 @@ function m.c2s(clientsrc)
             end
         end
     else
-        local nativepath = fs.normalize_clientpath(clientsrc.path)
+        local nativepath = fs.narive_normalize_clientpath(clientsrc.path)
         for _, source in pairs(sourcePool) do
-            if source.path and not source.sourceReference and fs.normalize_clientpath(source.path) == nativepath then
+            if source.path and not source.sourceReference and fs.narive_normalize_clientpath(source.path) == nativepath then
                 return source
             end
         end
@@ -171,7 +171,7 @@ function m.output(s)
     elseif s.path ~= nil then
         return {
             name = fs.filename(s.path),
-            path = fs.normalize(s.path),
+            path = fs.normalize_clientpath(s.path),
         }
     end
 end
