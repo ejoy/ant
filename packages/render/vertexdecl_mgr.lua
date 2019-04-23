85bd6e384dcc368cc493fbd79e119199969354a9
actboy168 <actboy168@gmail.com>
2019-04-23 10:03:14 +0800

    optimize using layout string to get vertex decl

--- packages/render/vertexdecl_mgr.lua
+++ packages/render/vertexdecl_mgr.lua
@@ -67,11 +67,13 @@ end
 
 function mgr.get(layout)	
 	local decl = declmapper[layout]	
-	if decl == nil then
-		layout = correct_layout(layout)
+	if decl then
+		return decl
 	end
 
-	decl = declmapper[layout]
+	local newlayout = correct_layout(layout)
+
+	decl = declmapper[newlayout]
 	if decl == nil then
 		decl = create_decl(layout)
 		declmapper[layout] = decl
