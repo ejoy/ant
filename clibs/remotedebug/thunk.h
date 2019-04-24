31aa12cab4c238164a87335adbcfb76f0c345dbb
actboy168 <actboy168@gmail.com>
2019-04-24 16:26:25 +0800

    Thunk support macos

--- clibs/remotedebug/thunk.h
+++ clibs/remotedebug/thunk.h
@@ -1,12 +1,13 @@
 #pragma once
 
 #include <stdint.h>
+#include <stddef.h>
 
 struct thunk;
 thunk* thunk_create_hook(intptr_t dbg, intptr_t hook);
 thunk* thunk_create_panic(intptr_t dbg, intptr_t panic, intptr_t old_panic);
 
-#if defined(_WIN32) || defined(__linux__)
+#if defined(_WIN32) || defined(__linux__) || defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__)
 #	define thunk_bind(...)
 struct thunk {
 	void*  data = 0;
@@ -18,6 +19,9 @@ struct thunk {
 #else
 struct thunk {
 	void*  data = 0;
+	intptr_t dbg = 0;
+	intptr_t func1 = 0;
+	intptr_t func2 = 0;
 };
 void thunk_bind(intptr_t L, intptr_t dbg);
 #endif
