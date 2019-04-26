5531fb2654c0a258692beddc0b8532c058ef2edb
actboy168 <actboy168@gmail.com>
2019-04-26 17:56:34 +0800

    glb encode

--- packages/glTF/glb.lua
+++ packages/glTF/glb.lua
@@ -1,5 +1,7 @@
+local jsonDecode = require "json".decode
+local jsonEncode = require "json".encode
 
-local function chunk(f, checktype)
+local function decode_chunk(f, checktype)
     local header = f:read(8)
     local length, type = ("<I4c4"):unpack(header)
     assert(checktype == type)
@@ -11,13 +13,36 @@ local function decode(filename)
     local header = f:read(12)
     local magic, version, _ = ("<c4I4I4"):unpack(header)
     assert(magic == "glTF")
-    local json = chunk(f, "JSON")
-	local bin = chunk(f, "BIN\0")
+    local json = decode_chunk(f, "JSON")
+	local bin = decode_chunk(f, "BIN\0")
 	assert(f:read(1) == nil)
     f:close()
-    return version, json, bin
+    return {
+        version = version,
+        info = jsonDecode(json),
+        bin = bin
+    }
+end
+
+local function encode_chunk(f, checktype, data)
+    f:write(("<I4"):pack(#data))
+    f:write(checktype)
+    f:write(data)
+end
+
+local function encode(filename, glb)
+    local json = jsonEncode(glb.info)
+    local f = assert(io.open(filename, "wb"))
+    f:write("glTF")
+    f:write(("<I4"):pack(glb.version))
+    local length = #json + #glb.bin + 28
+    f:write(("<I4"):pack(length))
+    encode_chunk(f, "JSON", json)
+    encode_chunk(f, "BIN\0", glb.bin)
+    f:close()
 end
 
 return {
     decode = decode,
+    encode = encode,
 }
--- packages/glTF/test.lua
+++ packages/glTF/test.lua
@@ -4,19 +4,19 @@ package.path = package.path .. ";" .. relate_path .. "/?.lua"
 local glb = require "glb"
 local json = require "json"
 local stringify = require "stringify"
-local _, jsonData, binData = glb.decode(relate_path .. "/test_resources/Bee.glb")
-local t = json.decode(jsonData)
+local glbData = glb.decode(relate_path .. "/test_resources/Bee.glb")
 
 local function savefile(filename, data)
     local f = assert(io.open(filename, "w"))
     f:write(data)
     f:close()
 end
-local luaData = stringify(t, true)
+local luaData = stringify(glbData.info, true)
 
 savefile(relate_path .. "/test_resources/test-bee.lua", luaData)
-savefile(relate_path .. "/test_resources/test-bee.json", jsonData)
-savefile(relate_path .. "/test_resources/test-bee.bin", binData)
+savefile(relate_path .. "/test_resources/test-bee.json", json.encode(glbData.info))
+savefile(relate_path .. "/test_resources/test-bee.bin", glbData.bin)
+glb.encode(relate_path .. "/test_resources/test-bee.glb", glbData)
 
 local function EQUAL(a, b)
 	for k, v in pairs(a) do
@@ -28,6 +28,6 @@ local function EQUAL(a, b)
 	end
 end
 
-EQUAL(assert(load(luaData))(), t)
+EQUAL(assert(load(luaData))(), glbData.info)
 
 print("ok")
