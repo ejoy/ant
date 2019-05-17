04a9dc72dd54df45dd901ebbc907974b25c5ba98
Cloud Wu <cloudwu@gmail.com>
2019-05-17 15:33:44 +0800

    add BGFX_STATIC_LINK macro

--- clibs/bgfx/bgfx_interface.h
+++ clibs/bgfx/bgfx_interface.h
@@ -1,38 +1,46 @@
-#pragma once
+#ifndef lua_bgfx_interface_h
+#define lua_bgfx_interface_h
 
 #include <lua.h>
 #include <lauxlib.h>
 #include <bgfx/c99/bgfx.h>
-#include "simplelock.h"
 
 static bgfx_interface_vtbl_t* bgfx_inf_ = 0;
-static spinlock_t             bgfx_inf_lock;
 
-static void init_interface(lua_State* L) {
-	atom_spinlock(&bgfx_inf_lock);
+static inline void
+init_interface(lua_State* L) {
 	if (bgfx_inf_) {
-		atom_spinunlock(&bgfx_inf_lock);
 		return;
 	}
+#ifdef BGFX_STATIC_LINK
+	bgfx_interface_vtbl_t* inf = bgfx_get_interface(BGFX_API_VERSION);
+	if (inf == NULL) {
+		luaL_error(L, "BGFX_API_VERSION mismatch.");
+		return;
+	}
+	bgfx_inf_ = inf;
+	lua_pushcfunction(L, (lua_CFunction)bgfx_get_interface);
+	lua_setfield(L, LUA_REGISTRYINDEX, "BGFX_GET_INTERFACE");
+#else
 	if (LUA_TFUNCTION != lua_getfield(L, LUA_REGISTRYINDEX, "BGFX_GET_INTERFACE")) {
-		atom_spinunlock(&bgfx_inf_lock);
 		luaL_error(L, "BGFX_GET_INTERFACE is missing.");
 		return;
 	}
 	lua_CFunction fn = lua_tocfunction(L, -1);
-	if (!fn) {
-		atom_spinunlock(&bgfx_inf_lock);
+	if (fn == NULL) {
 		luaL_error(L, "BGFX_GET_INTERFACE is not a C function.");
 		return;
 	}
-	bgfx_inf_ = ((PFN_BGFX_GET_INTERFACE)fn)(BGFX_API_VERSION);
-	if (!bgfx_inf_) {
-		atom_spinunlock(&bgfx_inf_lock);
+	bgfx_interface_vtbl_t* inf = ((PFN_BGFX_GET_INTERFACE)fn)(BGFX_API_VERSION);
+	if (inf == NULL) {
 		luaL_error(L, "BGFX_API_VERSION mismatch.");
 		return;
 	}
+	bgfx_inf_ = inf;
 	lua_pop(L, 1);
-	atom_spinunlock(&bgfx_inf_lock);
+#endif
 }
 
 #define BGFX(api) bgfx_inf_->api
+
+#endif
