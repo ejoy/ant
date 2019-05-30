f94ab4bfd5ee6bb550713a96d6f7a2f741074e49
junjie020 <junjie020@163.com>
2019-05-30 10:38:13 +0800

    fix msvc compile

--- 3rd/build_msvc_all.bat
+++ 3rd/build_msvc_all.bat
@@ -1,4 +1,3 @@
-devenv build\assimp\msvc\Assimp.sln /Build "Debug|x64"
 devenv build\freetype\msvc\freetype.sln /Build "Debug|x64"
 devenv build\ozz-animation\msvc\ozz.sln /Build "Debug|x64"
 devenv build\zlib\msvc\zlib.sln /Build "Debug|x64"
@@ -7,4 +6,3 @@ devenv build\bullet3\msvc\BULLET_PHYSICS.sln /Build "Debug|x64"
 devenv cd\mak.vc15\cd.sln /Build Debug /project cd\mak.vc15\cd.vcxproj /projectconfig "Debug|x64" /project cd\mak.vc15\cdlua.vcxproj /projectconfig "Debug|x64"
 
 devenv bgfx\.build\projects\vs2017\bgfx.sln /Build "Debug|x64"
-devenv ib-compress\ib-compress.sln /Build "Debug|x64"
