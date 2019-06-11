7d3871e2a1397412ababdcdd2f3ff79da86759ab
lincolnwty <909581047@qq.com>
2019-06-11 15:17:00 +0800

    trans mat_info into mat_info{} mode

--- packages/render/util.lua
+++ packages/render/util.lua
@@ -47,23 +47,25 @@ local function update_properties(shader, properties, render_properties)
 	end
 end
 
+
+
 function util.draw_primitive(vid, primgroup, mat, render_properties)
     bgfx.set_transform(mat)
 
-	-- local material = primgroup.material[3]  or primgroup.material
-	-- local properties = primgroup.properties[3] or primgroup.properties
-    -- bgfx.set_state(bgfx.make_state(material.state)) -- always convert to state str
-    -- update_properties(material.shader, properties, render_properties)
-
 	-- 保留兼容，最好按实验结果重构
-	local material = primgroup.material[i]  or primgroup.material
-	local prog = material.shader.prog
+	local material = primgroup.material[1]
+	local properties =  primgroup.properties[1]
 	
 	local mg = assert(primgroup.mgroup)
 	local ib, vb = mg.ib, mg.vb
 
 	local prims = mg.primitives
 	if prims == nil or next(prims) == nil then
+		-- 地形走的是这个分支? 
+		bgfx.set_state(bgfx.make_state(material.state)) -- always convert to state str
+		update_properties(material.shader, properties, render_properties)
+		local prog = material.shader.prog
+
 		if ib then
 			bgfx.set_index_buffer(ib.handle)
 		end
@@ -78,14 +80,15 @@ function util.draw_primitive(vid, primgroup, mat, render_properties)
 		for idx, v in ipairs(vb.handles) do
 			bgfx.set_vertex_buffer(idx - 1, v)
 		end
+
 		for i=1, numprim do
 			-- 这部分后续需要优化，提供机制判断一个mesh 是否具备多个材质，分开处理
-			local material = primgroup.material[i]  or primgroup.material
-		    local properties = primgroup.properties[i] or primgroup.properties
+			local material = primgroup.material[i]  or primgroup.material[1]
+			local properties = primgroup.properties[i] or primgroup.properties[1]
+			local prog = material.shader.prog
+
 			bgfx.set_state(bgfx.make_state(material.state)) -- always convert to state str
 			update_properties(material.shader, properties, render_properties)
-
-			local prog = material.shader.prog
 
 			local prim = prims[i]
 			if ib and prim.start_index and prim.num_indices then
@@ -139,34 +142,27 @@ function util.insert_primitive(eid, meshhandle, materials, worldmat, filter,grou
 		local mc = materials[ group_id ] or materials[1] 
 
 		-- this two variable trans to table parameters
-		local mat_info = mc.materialinfo  				-- maybe need materials[] for primitives[],futhur extend
-		local mat_properties = mc.properties 
+		local mat_info = {} --mc.materialinfo  				-- maybe need materials[] for primitives[],futhur extend
+		local mat_properties = {}  --mc.properties 
 
 		-- 一个primitive 带 n 个materials ，需要把这些materials 传到render 
 		-- 这部分需要重写，合并到加载时候，不需要每次动态装配
-		-- 存在这使用一个子材质，两个拷贝的现象，这个流程数据结构相关，也需要优化
-		-- tested 
+		-- 原mat_info,mat_properties 存在一个引用关系，需要从加载时，就为每个mesh 提供 materials，properties
+		-- 这里需要从整个fbx materials 去找出对应material 装备，推前到加载时完成，需要新需求修改统一
+		-- tested install mat&properties for submesh'submesh
 		local num_primitives = #g.primitives 
-		if num_primitives > 1 and mat_info.ajust ~= 1 then 
+		if num_primitives >= 1 and mat_info.ajust ~= 1 then 
 			local base = g.primitives[1].material_idx
-			-- for i=1,num_primitives do 
-			-- 	local idx = g.primitives[i].material_idx
-			-- 	if base > idx then 
-			-- 		base = idx 
-			-- 	end 
-			-- end  
-			--local base = g.primitives[1].material_idx
 			for i=1,num_primitives do 
 				local m_idx = g.primitives[i].material_idx
 				m_idx = m_idx - base + 1
 				local m_content = materials[m_idx] or materials[1]   -- some prim 
 				table.insert(mat_info,m_content.materialinfo)
-				--table.insert(mat_properties,m_content.properties)
 			end 
 			mat_info.ajust = 1
 		end 
 
-		if num_primitives > 1 and mat_properties.ajust ~= 1 then  
+		if num_primitives >= 1 and mat_properties.ajust ~= 1 then  
 			local base = g.primitives[1].material_idx
 			for i=1,num_primitives do 
 				local m_idx = g.primitives[i].material_idx
@@ -176,10 +172,9 @@ function util.insert_primitive(eid, meshhandle, materials, worldmat, filter,grou
 			end 
 			mat_properties.ajust = 1
 		end 
+        -- tested 
 
-		 
-
-		if mat_info.surface_type.transparency == "translucent" then
+		if mat_info[1].surface_type.transparency == "translucent" then
 			add_result(eid, g, mat_info, mat_properties, worldmat, results.translucent)
 		else
 			add_result(eid, g, mat_info, mat_properties, worldmat, results.opaque)
@@ -191,12 +186,13 @@ function util.insert_primitive(eid, meshhandle, materials, worldmat, filter,grou
 			local mc = materials[i] or materials[1]
 
 
-			local mi = mc.materialinfo
+			local mi = { mc.materialinfo }
+			local properties = { mc.properties }
 			
-			if mi.surface_type.transparency == "translucent" then
-				add_result(eid, g, mi, mc.properties, worldmat, results.translucent)
+			if mi[1].surface_type.transparency == "translucent" then
+				add_result(eid, g, mi, properties, worldmat, results.translucent)
 			else
-				add_result(eid, g, mi, mc.properties, worldmat, results.opaque)
+				add_result(eid, g, mi, properties, worldmat, results.opaque)
 			end
 		end
 	end 
