7fe4bd2634f0b390be36bf624d240ed02d4f6491
junjie020 <junjie020@163.com>
2019-06-24 11:08:28 +0800

    remove some test meshes

--- tools/modelviewer/renderworld.lua
+++ tools/modelviewer/renderworld.lua
@@ -76,41 +76,6 @@ function model_review_system:init()
 		return {material_refs=material_refs, visible=true}
 	end
 
-	local meshes = {
-		{
-			path = fs.path "/pkg/ant.resources/depiction/meshes/build_boat_01.mesh",
-			transform = mu.srt({1, 1, 1, 0}, nil, {15, 0, 0, 1}),
-			material = create_material_item(singlecolor_material, {1, 0, 0, 0}),
-		},
-		{
-			path = fs.path "/pkg/ant.resources/depiction/meshes/build_small_house_tall_roof_01.mesh",
-			transform = mu.srt({1, 1, 1, 0}, nil, {-15, 0, 0, 1}),
-			material = create_material_item(singlecolor_material, {0, 1, 0, 0}),
-		},
-		{
-			path = fs.path "/pkg/ant.resources/depiction/meshes/build_big_storage_01.mesh",
-			transform = mu.srt({1, 1, 1, 0}, mu.to_radian {0, 23.248, 0, 0}, {-35.40032, 2.527851, 35.10002, 1}),
-			material = create_material_item(singlecolor_material, {0, 0, 1, 0}),
-		},
-		
-	}
-
-	for _, m in ipairs(meshes)do
-		world:create_entity {
-			transform = m.transform,
-			can_render = true,
-			mesh = {
-				ref_path = m.path,
-			},
-			material = {
-				content = {
-					m.material,
-				}
-			},
-			main_view = true,
-		}
-	end
-
 	world:create_entity {
 		transform = mu.srt({0.1, 0.1, 0.1}, nil,  {0, 0, 10}),
 		can_render = true,
