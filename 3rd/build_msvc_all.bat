fbcdc1ae60e32d42794e6ead8b8642a33302b49b
unknown <cjj158579@hz.ali.com>
2019-06-25 17:00:52 +0800

    update to vs2019

--- 3rd/build_msvc_all.bat
+++ 3rd/build_msvc_all.bat
@@ -1,3 +1,3 @@
 devenv build\ozz-animation\msvc\ozz.sln /Build "Debug|x64"
 devenv build\bullet3\msvc\BULLET_PHYSICS.sln /Build "Debug|x64"
-devenv bgfx\.build\projects\vs2017\bgfx.sln /Build "Debug|x64"
+devenv bgfx\.build\projects\vs2019\bgfx.sln /Build "Debug|x64"
