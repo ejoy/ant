e09891dbd2499871724b80f77b91ba38fb8e2c54
junjie020 <junjie020@163.com>
2019-07-02 17:01:58 +0800

    enable cull_system in modelview and commit frusum interface's code

--- packages/scene/cull_system.lua
+++ packages/scene/cull_system.lua
@@ -2,7 +2,7 @@
 local ecs = ...
 local world = ecs.world
 
-local math3d_baselib = require "math3d.baselib"
+local mathbaselib = require "math3d.baselib"
 
 local math = import_package "ant.math"
 local mu = math.util
@@ -11,39 +11,27 @@ local ms = math.stack
 local cull_sys = ecs.system "cull_system"
 
 cull_sys.depend "primitive_filter_system"
-cull_sys.dependby "lighting_primitive_filter_system"
 
-function cull_sys:update()	
-	for _, eid in world:each("camera") do
-		local e = world[eid]		
-		local filter = e.primitive_filter
-		local view, proj = mu.view_proj_matrix(e)
-		-- plane is in world space
-		local planes = math3d_baselib.extract_planes(ms(proj, view, "*m"))
-		
-		local newfilter_result = {}
-		local results = filter.result
-		for _, prim in ipairs(results) do			
-			local psrt = prim.srt
-			local srt = ms({type="srt",s=psrt.s, r=psrt.r, t=psrt.t}, "m")
-			local function need_filter_out()
-				local group = prim.mgroup
-				local bounding = group.bounding
-				if bounding == nil then
-					return false
-				end
-
-				local aabb = math3d_baselib.transform_aabb(srt, bounding.aabb)
-				return "outside" == math3d_baselib.intersect(planes, aabb)				
-			end
-
-			if not need_filter_out() then
-				table.insert(newfilter_result, prim)			
-			end
+function cull_sys:update()
+	local mq = world:first_entity "main_queue"
+	local filter = mq.primitive_filter
+	local camera = mq.camera
+	local _, _, viewproj = ms:view_proj(camera, camera.frustum, true)
+	-- plane is in world space
+	local planes = mathbaselib.extract_planes()
+	local frustum = mathbaselib.new_frustum(ms(proj, view, "*m"))
+	
+	local results = filter.result
+	for _, resulttarget in pairs(results) do
+		local boundings = {}
+		for _, prim in ipairs(resulttarget) do
+			local tb = prim.transformed_bounding
+			boundings[#boundings+1] = tb or true
 		end
 
-		if #newfilter_result ~= #results then
-			filter.result = newfilter_result
-		end
+		local visible_set = frustum:interset_list(boundings)
+		assert(#visible_set == #boundings)
+
+		resulttarget.visible_set = visible_set
 	end
 end
