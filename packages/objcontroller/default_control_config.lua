b87135d997662e81c8c9b160621ffefb961707b8
junjie020 <junjie020@163.com>
2019-07-18 15:09:44 +0800

    fix message bug

--- packages/objcontroller/default_control_config.lua
+++ packages/objcontroller/default_control_config.lua
@@ -1,38 +1,40 @@
+local keyvalid_mousestate = {what="RIGHT", state = "DOWN"}
+
 return {
 	tigger = {
 		rotate = {
-			{name = 'mouse_move', state = {LEFT=true},},
-			{name = 'mouse_move', state = {RIGHT=true},}
+			{name = 'mouse', what="LEFT",	state="MOVE",},
+			{name = 'mouse', what="RIGHT", 	state="MOVE",},
 		},
 		hitstart = {
-			{name = 'mouse_click', what='LEFT', press=true},
-			{name = 'mouse_click', what='RIGHT', press=true}
+			{name = 'mouse', what='LEFT', 	state="DOWN"},
+			{name = 'mouse', what='RIGHT', 	state="DOWN"},
 		},
 		hitend = {
-			{name = 'mouse_click', what='LEFT', press=false},
-			{name = 'mouse_click', what='RIGHT', press=false}
-		}
+			{name = 'mouse', what='LEFT', 	state="UP"},
+			{name = 'mouse', what='RIGHT', 	state="UP"},
+		},
 	},
 	constant = {
 		move_forward = {
-			{name = 'keyboard', key = 'W', scale=1, state = {RIGHT=true}},
-			{name = 'keyboard', key = 'w', scale=1, state = {RIGHT=true}},
-			{name = 'keyboard', key = 'S', scale=-1, state = {RIGHT=true}},
-			{name = 'keyboard', key = 's', scale=-1, state = {RIGHT=true}},
+			{name = 'keyboard', key = 'W', scale=1,  mouse=keyvalid_mousestate},
+			{name = 'keyboard', key = 'w', scale=1,  mouse=keyvalid_mousestate},
+			{name = 'keyboard', key = 'S', scale=-1, mouse=keyvalid_mousestate},
+			{name = 'keyboard', key = 's', scale=-1, mouse=keyvalid_mousestate},
 		},
 
 		move_left = {			
-			{name = 'keyboard', key = 'A', scale=-1, state = {RIGHT=true}},
-			{name = 'keyboard', key = 'a', scale=-1, state = {RIGHT=true}},
-			{name = 'keyboard', key = 'D', scale=1, state = {RIGHT=true}},
-			{name = 'keyboard', key = 'd', scale=1, state = {RIGHT=true}}
+			{name = 'keyboard', key = 'A', scale=-1, mouse=keyvalid_mousestate},
+			{name = 'keyboard', key = 'a', scale=-1, mouse=keyvalid_mousestate},
+			{name = 'keyboard', key = 'D', scale=1, mouse=keyvalid_mousestate},
+			{name = 'keyboard', key = 'd', scale=1, mouse=keyvalid_mousestate},
 		},
 		
 		move_up = {			
-			{name = 'keyboard', key = 'Q', scale=1, state = {RIGHT=true}},
-			{name = 'keyboard', key = 'q', scale=1, state = {RIGHT=true}},
-			{name = 'keyboard', key = 'E', scale=-1, state = {RIGHT=true}},
-			{name = 'keyboard', key = 'e', scale=-1, state = {RIGHT=true}}
+			{name = 'keyboard', key = 'Q', scale=1, mouse=keyvalid_mousestate},
+			{name = 'keyboard', key = 'q', scale=1, mouse=keyvalid_mousestate},
+			{name = 'keyboard', key = 'E', scale=-1, mouse=keyvalid_mousestate},
+			{name = 'keyboard', key = 'e', scale=-1, mouse=keyvalid_mousestate},
 		},
 	}
 
