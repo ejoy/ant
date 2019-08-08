28586a84aa9bb61a2130257c687f24b68fbe465d
junjie020 <junjie020@163.com>
2019-08-08 18:23:01 +0800

    update shadow

--- packages/render/shadow/shadow_maker.lua
+++ packages/render/shadow/shadow_maker.lua
@@ -7,8 +7,6 @@ local viewidmgr = require "viewid_mgr"
 local renderutil= require "util"
 local computil 	= require "components.util"
 
-local geodrawer = import_package "ant.geometry".drawer
-
 local assetpkg 	= import_package "ant.asset"
 local assetmgr 	= assetpkg.mgr
 
@@ -16,11 +14,9 @@ local mathpkg 	= import_package "ant.math"
 local ms 		= mathpkg.stack
 
 local fs 		= require "filesystem"
-local bgfx 		= require "bgfx"
 local mathbaselib = require "math3d.baselib"
 
 ecs.component "shadow"
-	.material "material"
 	.shadowmap_with "int" (1024)
 	.shadowmap_height "int" (1024)
 
@@ -35,7 +31,8 @@ local function calc_scene_bounding()
 	local sb = mathbaselib.new_bounding(ms)
 	local transformed_boundings = {}
 	computil.calc_transform_boundings(world, transformed_boundings)
-	for _, tb in ipairs(transformed_boundings) do
+	for i=1, transformed_boundings do
+		local tb = transformed_boundings[i]
 		sb:merge(tb)
 	end
 	return sb
@@ -53,8 +50,11 @@ function maker_camera:update()
 	ms(camera.eyepos, sphere.center, {sphere.radius}, camera.viewdir, "i*+=")
 	
 	local viewmat = ms(camera.eyepos, camera.viewdir, camera.updir, ms.lookfrom3, "P")
-	local aabb_vs = mathbaselib.transform_aabb(ms, viewmat, scenebounding.aabb)
-	local lengthaxis = ms({0.5}, aabb_vs.max, aabb_vs.min, "-*T")
+	scenebounding:transform(viewmat)
+
+	local aabb = scenebounding:get "aabb"
+
+	local lengthaxis = ms({0.5}, aabb.max, aabb.min, "-*T")
 	local frustum = camera.frustum
 	--assert(frustum.ortho)
 
@@ -73,10 +73,10 @@ function sm:init()
 	local sm_width, sm_height = 1024, 1024
 	--local half_sm_width, half_sm_height = sm_width * 0.5, sm_height * 0.5
 	world:create_entity {
+		material = {
+			ref_path = fs.path "/pkg/ant.resources/depiction/shadow/mesh_cast_shadow.material"
+		},
 		shadow = {
-			material = {
-				ref_path = fs.path "/pkg/ant.resources/depiction/shadow/mesh_cast_shadow.material"
-			},
 			shaodwmap_width = sm_width,
 			shadowmap_height = sm_height,
 			shadow_distance = 10,
@@ -143,96 +143,29 @@ function sm:update()
 	local filter = sm.primitive_filter
 	local results = filter.result
 	local function replace_material(result, material)
+		local mi = assetmgr.get_material(material.ref_path)	-- must only one material content
 		for _, p in ipairs(result) do
-			p.material = material
+			p.material = mi
 		end
 	end
 
-	local shadowmat = sm.shadow.material.materialinfo
+	local shadowmat = sm.shadow.material
 	replace_material(results.opaque, 		shadowmat)
 	replace_material(results.translucent, 	shadowmat)
 
 	update_shadow_camera(sm.camera, world:first_entity "directional_light", sm.shadow.distance)
 end
 
-ecs.tag "mesh_bounding_debug"
-ecs.tag "frustum_debug"
 local debug_sm = ecs.system "debug_shadow_maker"
 debug_sm.depend "shadow_maker11"
 debug_sm.dependby "frustum_bounding_update"
 
 function debug_sm:init()
 	local qw, qh = 128,128
-	local eidshadow = computil.create_shadow_quad_entity(world, {x=0, y=0, w=qw, h=qh})
-	local shadow_entity = world[eidshadow]
-
+	computil.create_shadow_quad_entity(world, {x=0, y=0, w=qw, h=qh})
+	
+	local shadow_entity = world:first_entity "shadow"
 	local _, _, vp = ms:view_proj(shadow_entity.camera, shadow_entity.camera.frustum)
 	local frustum = mathbaselib.new_frustum(ms, vp)
 	computil.create_frustum_entity(world, frustum, "shadow frustum")
-end
-
-local function update_bounding_mesh()
-	local boundingdebug = world:first_entity "mesh_bounding_debug"
-	local desc = {
-		vb = {"fffd"}, ib = {},
-		primitives = {},
-	}
-	
-	for _, eid in world:each "can_render" do
-		local e = world[eid]
-		if e.can_render and e.can_cast then
-			local meshhandle = assetmgr.get_mesh(e.rendermesh.reskey).handle
-
-			local startvb = #desc.vb - 1
-			local startib = #desc.ib
-			geodrawer.draw_aabb_box(meshhandle.bounding.aabb, 0xff8f0000, ms:srtmat(e.transform), desc)
-
-			local endvb = #desc.vb - 1
-			local endib = #desc.ib
-			desc.primitives[#desc.primitives+1] = {
-				start_vertex=startvb, num_vertices=(endvb-startvb) / 4,
-				start_index=startib, num_indices=endib-startib,
-			}
-		end
-	end
-	
-	local meshhandle = assetmgr.get_mesh(boundingdebug.rendermesh.reskey).handle
-	local group = meshhandle.groups[1]
-	group.primitives = desc.primitives
-	bgfx.update(group.vb.handles[1], 0, desc.vb)
-	bgfx.update(group.ib.handle, 0, desc.ib)
-
-	boundingdebug.can_render = true
-end
-
-function debug_sm:post_init()
-	local function has_new_render_entity()
-		for _ in world:each_new "can_render" do
-			return true
-		end
-	end
-	if has_new_render_entity() then
-		update_bounding_mesh()
-	end
-end
-
-function debug_sm:delete()
-	local function check_has_entity_removed()
-		for _ in world:each_removed "can_render" do
-			return true
-		end
-	end
-
-	if check_has_entity_removed() then
-		update_bounding_mesh()
-	end
-end
-
-local frustum_bounding_update = ecs.system "frustum_bounding_update"
-function frustum_bounding_update:update()
-	local frsutum_bounding = world:first_entity "frustum_debug"
-	local shadow = world:first_entity "shadow"
-	local shadowcamera = shadow.camera
-	frsutum_bounding.transform.watcher.t = shadowcamera.eyepos
-	frsutum_bounding.transform.watcher.r = ms(shadowcamera.viewdir, "DT")
 end
