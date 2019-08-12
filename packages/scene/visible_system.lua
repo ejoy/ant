002533e1775307b3f223e3aa22d0a0eac61a10a0
zengruihong <ruihong.crh@alibaba-inc.com>
2019-08-12 15:19:59 +0800

    update:visible system will effect entity with "hierarchy_visible" only

--- packages/scene/visible_system.lua
+++ packages/scene/visible_system.lua
@@ -27,31 +27,42 @@ local visible_system = ecs.system "visible_system"
 --     end
 -- end
 
+local NotUpdated = nil
+local DontHave = 0
+local HaveTrue = true
+local HaveFalse = false
+--updated_visible:
+    -- nil -> not updated
+    -- 0 -> dont have hierarchy_visible
+    -- 1 -> have hierarchy_visible and AND(all) true
+    -- -1 -> have hierarchy_visible and AND(all) is false
 local function combine_and_return_visible(eid,updated_visible)
     local v = updated_visible[eid]
-    if v ~= nil then
+    if v ~= NotUpdated then
         return v
     end
     local entity = world[eid]
     if not entity then
-        updated_visible[eid] = true
-        return true
+        updated_visible[eid] = DontHave
+        return DontHave
     end
-    if entity.hierarchy_visible == false then
-        updated_visible[eid] = false
-        return false
-    else--visible is true or nil
-        local t = entity.transform or entity.hierarchy_transform
-        local pid = t and t.parent
-        if pid then
-            local parent_visible = combine_and_return_visible(pid,updated_visible)
-            updated_visible[eid] = parent_visible
-            return parent_visible
-        else
-            updated_visible[eid] = true
-            return true
+    if entity.hierarchy_visible ~= nil then
+        if entity.hierarchy_visible == false then
+            updated_visible[eid] = HaveFalse
+        else -- entity.hierarchy_visible == true
+            local t = entity.transform or entity.hierarchy_transform
+            local pid = t and t.parent
+            if pid then
+                local parent_visible = combine_and_return_visible(pid,updated_visible)
+                updated_visible[eid] = parent_visible and true
+            else
+                updated_visible[eid] = true
+            end
         end
+    else
+        updated_visible[eid] = DontHave
     end
+    return updated_visible[eid]
 end
 
 function visible_system:after_update()
@@ -60,7 +71,10 @@ function visible_system:after_update()
     for _,eid in world:each("can_render") do
         local entity = world[eid]
         local v = combine_and_return_visible(eid,updated_visible)
-        entity.can_render = v
+        assert( v ~= nil )
+        if v ~= DontHave then
+            entity.can_render = v
+        end
     end
     -- self.visible_change_cache.clear()
 
