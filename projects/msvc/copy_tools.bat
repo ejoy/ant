6c7932874514d999747ff0e469e5a01ad5ea8949
junjie020 <junjie020@163.com>
2019-08-19 11:03:53 +0800

    update copy_tool.bat

--- projects/msvc/copy_tools.bat
+++ projects/msvc/copy_tools.bat
@@ -1,10 +1,10 @@
-copy /B /Y ..\..\3rd\bgfx\.build\win64_vs2019\bin\texturecDebug.exe     .\vs_bin\x64\Debug\texturecDebug.exe
-copy /B /Y ..\..\3rd\bgfx\.build\win64_vs2019\bin\texturecRelease.exe   .\vs_bin\x64\Release\texturecRelease.exe
-copy /B /Y ..\..\3rd\bgfx\.build\win64_vs2019\bin\texturecRelease.exe   ..\..\bin\texturec.exe
+copy /B /Y ..\..\3rd\bgfx\.build\win64_vs2019\bin\texturecDebug.exe     .\vs_bin\Debug\texturecDebug.exe
+copy /B /Y ..\..\3rd\bgfx\.build\win64_vs2019\bin\texturecRelease.exe   .\vs_bin\Release\texturecRelease.exe
+copy /B /Y ..\..\3rd\bgfx\.build\win64_vs2019\bin\texturecRelease.exe   ..\..\bin\msvc\texturec.exe
 
-copy /B /Y ..\..\3rd\bgfx\.build\win64_vs2019\bin\shadercDebug.exe      .\vs_bin\x64\Debug\shadercDebug.exe
-copy /B /Y ..\..\3rd\bgfx\.build\win64_vs2019\bin\shadercRelease.exe    .\vs_bin\x64\Release\shadercRelease.exe
-copy /B /Y ..\..\3rd\bgfx\.build\win64_vs2019\bin\shadercRelease.exe    ..\..\bin\shaderc.exe
+copy /B /Y ..\..\3rd\bgfx\.build\win64_vs2019\bin\shadercDebug.exe      .\vs_bin\Debug\shadercDebug.exe
+copy /B /Y ..\..\3rd\bgfx\.build\win64_vs2019\bin\shadercRelease.exe    .\vs_bin\Release\shadercRelease.exe
+copy /B /Y ..\..\3rd\bgfx\.build\win64_vs2019\bin\shadercRelease.exe    ..\..\bin\msvc\shaderc.exe
 
-copy /B /Y ..\..\3rd\bgfx\.build\win64_vs2019\bin\bgfx-shared-libDebug.dll      .\vs_bin\x64\Debug\bgfx-core.dll
-copy /B /Y ..\..\3rd\bgfx\.build\win64_vs2019\bin\bgfx-shared-libRelease.dll    .\vs_bin\x64\Release\bgfx-core.dll
+copy /B /Y ..\..\3rd\bgfx\.build\win64_vs2019\bin\bgfx-shared-libDebug.dll      .\vs_bin\Debug\bgfx-core.dll
+copy /B /Y ..\..\3rd\bgfx\.build\win64_vs2019\bin\bgfx-shared-libRelease.dll    .\vs_bin\Release\bgfx-core.dll
