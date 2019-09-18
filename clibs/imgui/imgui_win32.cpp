ca0da3b536940bfe54d17b04b8ed64a9aa30424a
actboy168 <actboy168@gmail.com>
2019-09-18 10:43:05 +0800

    Clean up the same code

--- clibs/imgui/imgui_win32.cpp
+++ clibs/imgui/imgui_win32.cpp
@@ -1,5 +1,6 @@
 #include <imgui.h>
 #include <Windows.h>
+#include <imm.h>
 #include <mingw/mingw_window.h>
 
 void init_cursor() {
@@ -19,7 +20,7 @@ void set_cursor(ImGuiMouseCursor im_cursor) {
 	case ImGuiMouseCursor_ResizeNESW: cursor = IDC_SIZENESW; break;
 	case ImGuiMouseCursor_ResizeNWSE: cursor = IDC_SIZENWSE; break;
 	case ImGuiMouseCursor_Hand:       cursor = IDC_HAND;     break;
-	case ImGuiMouseCursor_None:      cursor = NULL; break;
+	case ImGuiMouseCursor_None:       cursor = NULL;         break;
 	}
 	ImGuiViewport* main_viewport = ImGui::GetMainViewport();
 	HWND  window = (HWND)(main_viewport->PlatformHandle);
@@ -30,30 +31,7 @@ void set_cursor(ImGuiMouseCursor im_cursor) {
 	else
 		::SetCursor(NULL);
 }
-#if defined(__MINGW32__)
 
-#include <Windows.h>
-#include <imm.h>
-
-static void 
-ImGui_ImplWin32_SetImeInputPos(ImGuiViewport* viewport, ImVec2 pos)
-{
-	COMPOSITIONFORM cf = { CFS_FORCE_POSITION,{ (LONG)(pos.x - viewport->Pos.x), (LONG)(pos.y - viewport->Pos.y) },{ 0, 0, 0, 0 } };
-	if (HWND hwnd = (HWND)viewport->PlatformHandle)
-		if (HIMC himc = ::ImmGetContext(hwnd))
-		{
-			::ImmSetCompositionWindow(himc, &cf);
-			::ImmReleaseContext(hwnd, himc);
-		}
-}
-
-#endif
-#if defined(_WIN32) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS) && !defined(IMGUI_DISABLE_WIN32_DEFAULT_IME_FUNCTIONS) && !defined(__GNUC__)
-#define HAS_WIN32_IME   1
-#include <imm.h>
-#ifdef _MSC_VER
-#pragma comment(lib, "imm32")
-#endif
 static void ImGui_ImplWin32_SetImeInputPos(ImGuiViewport * viewport, ImVec2 pos)
 {
 	COMPOSITIONFORM cf = { CFS_FORCE_POSITION,{ (LONG)(pos.x - viewport->Pos.x), (LONG)(pos.y - viewport->Pos.y) },{ 0, 0, 0, 0 } };
@@ -64,22 +42,10 @@ static void ImGui_ImplWin32_SetImeInputPos(ImGuiViewport * viewport, ImVec2 pos)
 			::ImmReleaseContext(hwnd, himc);
 		}
 }
-#else
-#define HAS_WIN32_IME   0
-#endif
 
 void init_ime(void* window) {
 	ImGuiViewport* main_viewport = ImGui::GetMainViewport();
 	main_viewport->PlatformHandle = window;
-	//io.ImeWindowHandle = window;
 	ImGuiPlatformIO& platform_io = ImGui::GetPlatformIO();
-#if HAS_WIN32_IME
 	platform_io.Platform_SetImeInputPos = ImGui_ImplWin32_SetImeInputPos;
-#endif
-
-#if defined(__MINGW32__)
-	/*ImGuiIO& io = ImGui::GetIO();
-	io.ImeSetInputScreenPosFn = ImeSetInputScreenPosFn_DefaultImpl;*/
-	platform_io.Platform_SetImeInputPos = ImGui_ImplWin32_SetImeInputPos;
-#endif
 }
