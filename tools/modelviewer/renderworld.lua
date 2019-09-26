0db8445f7ecd38802330671a6f7139105f83ab58
actboy168 <actboy168@gmail.com>
2019-09-26 14:45:03 +0800

    Add camera controller package

--- tools/modelviewer/renderworld.lua
+++ tools/modelviewer/renderworld.lua
@@ -10,6 +10,7 @@ ecs.import "ant.math.adapter"
 ecs.import "ant.sky"
 ecs.import "ant.asset"
 ecs.import "ant.imguibase"
+ecs.import "ant.camera_controller"
 
 local mathpkg = import_package "ant.math"
 local ms = mathpkg.stack
