5916ab1f251cc5ccd7867ef50987982891fc8827
actboy168 <actboy168@gmail.com>
2019-09-29 14:49:18 +0800

    Fixes xcode11 build error

--- clibs/filesystem/filesystem.h
+++ clibs/filesystem/filesystem.h
@@ -1,6 +1,22 @@
 #pragma once
 
-#if __has_include(<filesystem>) && !defined(__MINGW32__)
+#if __has_include(<filesystem>)
+#   if defined(__MINGW32__)
+#   elif defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__)
+#       if __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ >= 101500
+#           define ANT_ENABLE_FILESYSTEM 1
+#       endif
+#   elif defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__)
+#       if __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ >= 130000
+#           define ANT_ENABLE_FILESYSTEM 1
+#       endif
+#   else
+#       define ANT_ENABLE_FILESYSTEM 1
+#   endif
+#endif
+
+#if defined(ANT_ENABLE_FILESYSTEM)
+#undef ANT_ENABLE_FILESYSTEM
 #include <filesystem>
 namespace fs = std::filesystem;
 #else
