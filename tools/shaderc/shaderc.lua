fdb3856ac1563a8bad8b819b88b8944c98c17c2e
junjie020 <kitchen.gz.020@gmail.com>
2019-10-11 15:49:35 +0800

    add howto

--- tools/shaderc/shaderc.lua
+++ tools/shaderc/shaderc.lua
@@ -1,3 +1,10 @@
+--[[
+    lua.exe .\tools\shaderc\main.lua 
+        .Windows_DIRECT3D11
+        .\packages\resources\shaders\mesh\fs_colormesh.sc 
+        .\abc.bin 
+]]
+
 local fs = require "filesystem.local"
 local shaderc = import_package "ant.fileconvert".converter.shader
 local identity, input, output = table.unpack(arg)
