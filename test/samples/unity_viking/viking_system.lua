ef163cd4e5fbe6a669e0c7cf5fa0abbb993523ef
junjie020 <kitchen.gz.020@gmail.com>
2019-10-14 17:02:00 +0800

    clean code

--- test/samples/unity_viking/viking_system.lua
+++ test/samples/unity_viking/viking_system.lua
@@ -55,38 +55,10 @@ end
 
 function scene_walker:init()
     do
-        -- 255,209,172
         lu.create_directional_light_entity(world, 'directional_light', 
             {1,0.81,0.67,0}, 1.8, mu.to_radian {-220,-235,0,0})
         lu.create_ambient_light_entity(world, 'ambient_light', 'gradient', {1, 1, 1, 1})
     end
-   
-    -- allscene.lua 13500000
-    -- spaceship_a_crashed.lua    90000
-    -- spaceship_background_a.lua 10000
-    -- fpsscene.lua 280000
-    -- buildingc_scene.lua
-    -- sample.lua
 
     load_world(vikingmap)
-    --computil.create_grid_entity(world, 'grid', 64, 64, 1)
-
 end
-
-function scene_walker:update()
-
-    -- local deltaTime =  timer.deltatime
-    -- print("deltaTime",deltaTime)
-
-	-- local mq = world:first_entity "main_queue"
-	-- local camera = camerautil.get_camera(world, mq.camera_tag)
-
-    -- local pos = ms(camera.eyepos,"T")
-    -- print("camera :",string.format("%08.4f",pos[1]), string.format("%08.4f",pos[2]),string.format("%08.4f",pos[3]) )
-
-    -- 注意屏蔽，简单统计测试函数，影响效率
-    -- Statics.reset()
-    -- Statics.collect(world)
-    -- Statics.print()
-
-end 
