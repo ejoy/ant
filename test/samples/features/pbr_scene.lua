d61ca6186de2db7394c35f2ea1de19ebdf02b095
junjie020 <kitchen.gz.020@gmail.com>
2019-11-20 11:08:49 +0800

    add pbr metallic roughness examples

--- test/samples/features/pbr_scene.lua
+++ test/samples/features/pbr_scene.lua
@@ -8,7 +8,7 @@ local fs = require "filesystem"
 
 local pbr_scene = {}
 
-local feature_path = fs.path "/pkg/ant.test.features/assets/pbr_test.pbrm"
+local feature_path = fs.path "/pkg/ant.test.features"
 local pbr_materialpath = feature_path / "assets/pbr_test.pbrm"
 local sphere_meshpath = feature_path / "assets/sphere.mesh"
 
@@ -29,7 +29,7 @@ local function create_pbr_entity(world,
                     },
                     u_metallic_roughness_factor = {
                         type="v4", name = "metallic roughness",
-                        value = {0.0, metallic, roughness, 0.0},
+                        value = {0.0, roughness, metallic, 0.0},
                     },
                     u_emissive_factor = {
                         type="color", name = "emissive",
@@ -47,27 +47,29 @@ end
 
 function pbr_scene.create_scene(world)
 
-    local num_samples = 10
+    local num_samples = 4
     local metallic_step = 1.0 / num_samples
     local roughness_step = 1.0 / num_samples
 
     local basecolor = {0.8, 0.2, 0.2, 1.0}
 
-    local x, y = 0.0, 0.0
+    local movestep = 2
+    local x = 0.0
     for row=1, num_samples do
         local metallic = row * metallic_step
+        local z = 0.0
         for col=1, num_samples do
             local roughness = col * roughness_step
             create_pbr_entity( world, "sphere",
-            mu.translate_mat {},
+            mu.translate_mat {x, 0.0, z, 1.0},
             sphere_meshpath,
             basecolor,
             metallic, roughness)
 
-            y = y + 1.0
+            z = z + movestep
         end
 
-        x = x + 1.0
+        x = x + movestep
     end
 end 
 
