c0bc2868e2b14f7e699dac959e8f52ff5425cc88
junjie020 <kitchen.gz.020@gmail.com>
2019-12-17 11:02:45 +0800

    using policy to rewrite skinning system

--- packages/animation/skinning/skinning_system.lua
+++ packages/animation/skinning/skinning_system.lua
@@ -17,139 +17,197 @@ local fs 		= require "filesystem"
 local animodule = require "hierarchy.animation"
 local bgfx 		= require "bgfx"
 
--- local ozzmesh = ecs.component_alias("ozz_mesh", "resource") {depend = {"rendermesh", "animation"}}
+ecs.component "skinning" {}
 
--- local function gen_mesh_assetinfo(ozzmesh)
--- 	local meshhandle = assetmgr.get_resource(ozzmesh.ref_path).handle
+local ozz_skinning_policy = ecs.policy "ozz_skinning"
+ozz_skinning_policy.require_component "animation"
+ozz_skinning_policy.require_component "skeleton"
+ozz_skinning_policy.require_component "rendermesh"
+ozz_skinning_policy.require_component "skinning"
+ozz_skinning_policy.require_component "mesh"
+ozz_skinning_policy.require_transform "ozzmesh_skinning"
 
--- 	local layouts = meshhandle:layout()
--- 	local num_vertices = meshhandle:num_vertices()
+local ozzmesh_skinning_transform = ecs.transform "ozzmesh_skinning"
+ozzmesh_skinning_transform.input 	"mesh"
+ozzmesh_skinning_transform.input 	"rendermesh"
+ozzmesh_skinning_transform.output 	"skinning"
 
--- 	local function find_layout(shortname, layouts)
--- 		for _, l in ipairs(layouts) do
--- 			if shortname == l:sub(1, 1) then
--- 				return l
--- 			end
--- 		end
--- 	end
+local function gen_mesh_assetinfo(ozzmesh)
+	local meshhandle = assetmgr.get_resource(ozzmesh.ref_path).handle
 
--- 	local function generate_layout(shortnames, layouts)
--- 		local layout = {}
--- 		for _, sn in ipairs(shortnames) do
--- 			local l = find_layout(sn, layouts)
--- 			if l then
--- 				layout[#layout+1] = l
--- 			end
--- 		end
+	local layouts = meshhandle:layout()
+	local num_vertices = meshhandle:num_vertices()
 
--- 		if next(layout) then
--- 			return table.concat(layout, '|')
--- 		end
--- 	end
+	local function find_layout(shortname, layouts)
+		for _, l in ipairs(layouts) do
+			if shortname == l:sub(1, 1) then
+				return l
+			end
+		end
+	end
 
--- 	local static_layout = generate_layout({'c', 't'}, layouts)
--- 	local static_stride = declmgr.layout_stride(static_layout)
--- 	local static_buffer = bgfx.memory_texture(num_vertices * static_stride)
--- 	meshhandle:combine_buffer(static_layout, static_buffer)
--- 	local static_vbhandle = {
--- 		handle = bgfx.create_vertex_buffer(static_buffer, declmgr.get(static_layout).handle)
--- 	}
+	local function generate_layout(shortnames, layouts)
+		local layout = {}
+		for _, sn in ipairs(shortnames) do
+			local l = find_layout(sn, layouts)
+			if l then
+				layout[#layout+1] = l
+			end
+		end
 
--- 	local dynamic_layout = generate_layout({'p', 'n', 'T'}, layouts)
--- 	local dynamic_stride = declmgr.layout_stride(dynamic_layout)
--- 	local dynamic_vbhandle = {
--- 		handle = bgfx.create_dynamic_vertex_buffer(num_vertices * dynamic_stride, declmgr.get(dynamic_layout).handle),
--- 		updatedata = animodule.new_aligned_memory(num_vertices * dynamic_stride, 4)
--- 	}
+		if next(layout) then
+			return table.concat(layout, '|')
+		end
+	end
 
--- 	local primitive = {
--- 		vb = {
--- 			start = 0,
--- 			num = num_vertices,
--- 			handles = {
--- 				static_vbhandle,
--- 				dynamic_vbhandle,
--- 			}
--- 		}
--- 	}
+	local static_layout = generate_layout({'c', 't'}, layouts)
+	local static_stride = declmgr.layout_stride(static_layout)
+	local static_buffer = bgfx.memory_texture(num_vertices * static_stride)
+	meshhandle:combine_buffer(static_layout, static_buffer)
+	local static_vbhandle = {
+		handle = bgfx.create_vertex_buffer(static_buffer, declmgr.get(static_layout).handle)
+	}
 
--- 	local num_indices = meshhandle:num_indices()
+	local dynamic_layout = generate_layout({'p', 'n', 'T'}, layouts)
+	local dynamic_stride = declmgr.layout_stride(dynamic_layout)
+	local dynamic_vbhandle = {
+		handle = bgfx.create_dynamic_vertex_buffer(num_vertices * dynamic_stride, declmgr.get(dynamic_layout).handle),
+		updatedata = animodule.new_aligned_memory(num_vertices * dynamic_stride, 4)
+	}
+
+	local primitive = {
+		vb = {
+			start = 0,
+			num = num_vertices,
+			handles = {
+				static_vbhandle,
+				dynamic_vbhandle,
+			}
+		}
+	}
+
+	local num_indices = meshhandle:num_indices()
 	
--- 	if num_indices ~= 0 then
--- 		local indices_buffer, stride = meshhandle:index_buffer()
--- 		primitive.ib = {
--- 			start = 0,
--- 			num = num_indices,
--- 			handle = bgfx.create_index_buffer({indices_buffer, num_indices * stride})
--- 		}
--- 	end
+	if num_indices ~= 0 then
+		local indices_buffer, stride = meshhandle:index_buffer()
+		primitive.ib = {
+			start = 0,
+			num = num_indices,
+			handle = bgfx.create_index_buffer({indices_buffer, num_indices * stride})
+		}
+	end
 
--- 	local ibm_pointer, ibm_count = meshhandle:inverse_bind_matrices()
--- 	local joint_remapp_pointer, count = meshhandle:joint_remap()
--- 	return {
--- 		sceneidx = 1,
--- 		scenescale = 1.0,
--- 		scenes = {
--- 			--scene
--- 			{
--- 				--meshnode
--- 				{
--- 					inverse_bind_pose 	= animodule.new_bind_pose(ibm_count, ibm_pointer),
--- 					joint_remap 		= animodule.new_joint_remap(joint_remapp_pointer, count),
--- 					primitive
--- 				}
--- 			}
--- 		}
--- 	}
--- end
+	local ibm_pointer, ibm_count = meshhandle:inverse_bind_matrices()
+	local joint_remapp_pointer, count = meshhandle:joint_remap()
+	return {
+		sceneidx = 1,
+		scenescale = 1.0,
+		scenes = {
+			--scene
+			{
+				--meshnode
+				{
+					inverse_bind_pose 	= animodule.new_bind_pose(ibm_count, ibm_pointer),
+					joint_remap 		= animodule.new_joint_remap(joint_remapp_pointer, count),
+					primitive
+				}
+			}
+		}
+	}
+end
 
--- function ozzmesh:postinit(e)
--- 	local rm = e.rendermesh
+function ozzmesh_skinning_transform:process(e)
+	local rm 		= e.rendermesh
+	local reskey 	= fs.path("//meshres/" .. self.ref_path:stem():string() .. ".mesh")
+	local meshscene = gen_mesh_assetinfo(self)
+	rm.reskey 		= assetmgr.register_resource(reskey, meshscene)
 
--- 	local reskey = fs.path("//meshres/" .. self.ref_path:stem():string() .. ".mesh")
--- 	local skehandle = assetmgr.get_resource(e.skeleton.ref_path).handle
--- 	rm.reskey = assetmgr.register_resource(reskey, gen_mesh_assetinfo(self, skehandle))
--- end
+	local meshres 	= assetmgr.get_resource(e.mesh.ref_path).handle
 
+	assert(#meshscene == 1 and #meshscene[1] == 1)
+	local meshnode = meshscene[1]
 
--- local skinningmesh = ecs.component_alias("skinning_mesh", "resource") {depend = {"rendermesh", "animation", "skeleton"}}
--- .ref_path "respath"
+	local primitive = meshnode[1]
 
--- function skinningmesh:postinit(e)
--- 	local rm = e.rendermesh
--- 	local res = assetmgr.get_resource(self.ref_path)
--- 	local meshscene = computil.create_mesh_buffers(res)
+	local skincomp = e.skinning
+	skincomp.jobs = {
+		{
+			inverse_bind_matrices = meshnode.inverse_bind_matrices,
+			joint_remap = meshnode.joint_remap,
+			parts = {},
+		}
+	}
 
--- 	rm.reskey = assetmgr.register_resource(fs.path("//meshres/" .. self.ref_path:stem():string() .. ".mesh"), meshscene)
+	local job = skincomp.jobs[1]
+	local parts = job.parts
+	
+	local vb = primitive.vb
 
--- 	return self
--- end
+	for _, handle in ipairs(vb.handles) do
+		local updatedata = handle.updatedata
+		if updatedata then
+			job.hwbuffer_handle = handle.handle
+			job.updatedata 		= updatedata
 
-ecs.component_alias("skinning_mesh", "resource") {depend = {"rendermesh", "animation"}}
+			local num_part = meshres:num_part()
+			local outptr = updatedata:pointer()
+
+			local output_buffer_offset = 0
+			for ipart=1, num_part do
+				local part_num_vertices = meshres:num_vertices(ipart)
+				local input_desc, output_desc = {}, {}
+
+				for _, n in ipairs {"p", "n", "T", "w", "i"} do
+					local name = declmgr.name_mapper[n]
+					input_desc[name] = meshres:vertex_buffer(ipart, name)
+				end
+
+				local offset = 0
+				for _, n in ipairs{"p", "n", "T"} do
+					local name = declmgr.name_mapper[n]
+					local input = input_desc[name]
+					output_desc[name] = {
+						outptr,
+						output_buffer_offset + offset + 1,
+						nil,
+					}
+					offset = offset + input[3]
+				end
+
+				local layout_stride = offset
+				for _, output in pairs(output_desc) do
+					output[3] = layout_stride
+				end
+
+				output_buffer_offset = output_buffer_offset + layout_stride * part_num_vertices
+
+				parts[#parts+1] = {
+					layout_stride 	= layout_stride,
+					num 			= part_num_vertices,
+					inputdesc 		= input_desc,
+					outputdesc 		= output_desc,
+					influences_count= meshres:influences_count(ipart),
+				}
+			end
+
+			job.buffersize = output_buffer_offset
+		end
+	end
+
+end
 
 local s = ecs.policy "skinning"
 s.require_component "animation"
 s.require_component "skeleton"
 s.require_component "rendermesh"
-s.require_component "skinning_mesh"
-s.require_transform "skinning"
+s.require_component "skinning"
+s.require_component "mesh"
+s.require_transform "mesh_skinning"
 
-local m = ecs.transform "skinning"
-m.input "rendermesh"
-m.input "animation"
-m.output "skinning_mesh"
-
-function m.process(e)
-	local rm = e.rendermesh
-	local reskey = fs.path("//meshres/" .. e.skinning_mesh.ref_path:stem():string() .. ".mesh")
-	rm.reskey = assetmgr.register_resource(reskey, gen_mesh_assetinfo(e.skinning_mesh))
-end
-
-
--- skinning system
-local skinning_sys = ecs.system "skinning_system"
-
-skinning_sys.depend "animation_system"
+local mesh_skinning_transform = ecs.transform "mesh_skinning"
+mesh_skinning_transform.input "mesh"
+mesh_skinning_transform.input "rendermesh"
+mesh_skinning_transform.output "skinning"
 
 local function find_elem(name, layout_elems)
 	assert(#name == 1)
@@ -181,118 +239,89 @@ local function layout_desc(elem_prefixs, layout_elems, layout_stride, pointer, o
 	return desc
 end
 
-local function build_skinning_matrices(meshnode, aniresult)
-	local skinning_matrices = meshnode.skinning_matrices
-	if skinning_matrices == nil then
-		skinning_matrices = animodule.new_bind_pose(aniresult:count())
-		meshnode.skinning_matrices = skinning_matrices
-	end
 
-	animodule.build_skinning_matrices(skinning_matrices, aniresult, meshnode.inverse_bind_pose, meshnode.joint_remap)
-	return skinning_matrices
-end
+function mesh_skinning_transform.process(e)
+	local meshres = assetmgr.get_resource(e.mesh.ref_path)
+	local meshscene = assetmgr.get_resource(e.rendermesh).handle
 
-function skinning_sys:update()
-	for _, eid in world:each "skinning_mesh" do
-		local e = world[eid]
+	local skinning = e.skinning
 
-		local meshscene = assetmgr.get_resource(assert(e.rendermesh.reskey))
-		local aniresult = e.animation.aniresult
-		local meshres = assetmgr.get_resource(e.skinning_mesh.ref_path)
+	local jobs = {}
+	skinning.jobs = jobs
 
-		for meshidx, meshnode in ipairs(meshscene.scenes[meshscene.sceneidx]) do
-			local res_meshnode = meshres.scenes[meshscene.sceneidx][meshidx]
-			for groupidx, group in ipairs(meshnode) do
-
-				local skinning_matrices = build_skinning_matrices(meshnode, aniresult)
-				local res_group = res_meshnode[groupidx]
-
-				local vb = group.vb
-				local res_vb = res_group.vb
-				for idx, handle in ipairs(vb.handles) do
-					local updatedata = handle.updatedata
-					if updatedata then
-						local outptr = updatedata:pointer()
-						local res_value = res_vb.values[idx]
-						local vbvalue = res_value.value
-						local offset = res_value.start
-						local declname = res_value.declname
-						local layout_stride = declmgr.get(declname).stride
-						local layout_elems = {}
-						for elem in declname:gmatch "%w+" do
-							layout_elems[#layout_elems+1] = elem
-						end
-
-						animodule.mesh_skinning(aniresult, skinning_matrices,
-							layout_desc({'p', 'n', 'T', 'w', 'i'}, layout_elems, layout_stride, vbvalue, offset),
-							layout_desc({'p', 'n', 'T'}, layout_elems, layout_stride, outptr), vb.num)
-
-						bgfx.update(handle.handle, 0, {"!", outptr, layout_stride * vb.num})
+	for meshidx, meshnode in ipairs(meshscene[meshscene.sceneidx]) do
+		local res_meshnode = meshres.scenes[meshres.sceneidx][meshidx]
+		for groupidx, group in ipairs(meshnode) do
+			local res_group = res_meshnode[groupidx]
+			local vb = group.vb
+			local res_vb = res_group.vb
+			for idx, handle in ipairs(vb.handles) do
+				local updatedata = handle.updatedata
+				if updatedata then
+					local outptr = updatedata:pointer()
+					local res_value = res_vb.values[idx]
+					local vbvalue = res_value.value
+					local offset = res_value.start
+					local declname = res_value.declname
+					local layout_stride = declmgr.get(declname).stride
+					local layout_elems = {}
+					for elem in declname:gmatch "%w+" do
+						layout_elems[#layout_elems+1] = elem
 					end
+
+					jobs[#jobs+1] = {
+						inverse_bind_matrices = meshnode.inverse_bind_matrices,
+						joint_remap = meshnode.joint_remap,
+						hwbuffer_handle = handle.handle,
+						updatedata = updatedata,
+						buffersize = layout_stride * vb.num,
+						parts = {
+							{
+								inputdesc = layout_desc({'p', 'n', 'T', 'w', 'i'}, layout_elems, layout_stride, vbvalue, offset),
+								outputdesc = layout_desc({'p', 'n', 'T'}, layout_elems, layout_stride, outptr),
+								num = vb.num,
+								layout_stride = layout_stride,
+								influences_count = 4,
+							}
+						}
+					}
 				end
 			end
 		end
 	end
+end
 
-	for _, eid in world:each "ozz_mesh" do
+-- skinning system
+local skinning_sys = ecs.system "skinning_system"
+
+skinning_sys.depend "animation_system"
+
+local function build_skinning_matrices(skinningjob, aniresult)
+	local skinning_matrices = skinningjob.skinning_matrices
+	if skinning_matrices == nil then
+		skinning_matrices = animodule.new_bind_pose(aniresult:count())
+		skinningjob.skinning_matrices = skinning_matrices
+	end
+
+	animodule.build_skinning_matrices(skinning_matrices, aniresult, skinningjob.inverse_bind_pose, skinningjob.joint_remap)
+	return skinning_matrices
+end
+
+function skinning_sys:update()
+	for _, eid in world:each "skinning" do
 		local e = world[eid]
-		local meshres = assetmgr.get_resource(e.ozz_mesh.ref_path).handle
-		local meshscene = assetmgr.get_resource(e.rendermesh.reskey)
-
+		local skincomp = e.skinning
 		local aniresult = e.animation.aniresult
 
-		local meshscene = meshscene.scenes[meshscene.sceneidx]
-		assert(#meshscene == 1 and #meshscene[1] == 1)
-		local meshnode = meshscene[1]
-
-		local primitive = meshscene[1][1]
-		local skinning_matrices = build_skinning_matrices(meshnode, aniresult)
-
-		local vb = primitive.vb
-
-		for _, handle in ipairs(vb.handles) do
-			local updatedata = handle.updatedata
-			if updatedata then
-				local num_part = meshres:num_part()
-				local outptr = updatedata:pointer()
-
-				local output_buffer_offset = 0
-				for ipart=1, num_part do
-					local part_num_vertices = meshres:num_vertices(ipart)
-					local input_desc, output_desc = {}, {}
-
-					for _, n in ipairs {"p", "n", "T", "w", "i"} do
-						local name = declmgr.name_mapper[n]
-						input_desc[name] = meshres:vertex_buffer(ipart, name)
-					end
-
-					local offset = 0
-					for _, n in ipairs{"p", "n", "T"} do
-						local name = declmgr.name_mapper[n]
-						local input = input_desc[name]
-						output_desc[name] = {
-							outptr,
-							output_buffer_offset + offset + 1,
-							nil,
-						}
-						offset = offset + input[3]
-					end
-
-					local layout_stride = offset
-					for _, output in pairs(output_desc) do
-						output[3] = layout_stride
-					end
-
-					output_buffer_offset = output_buffer_offset + layout_stride * part_num_vertices
-
-					animodule.mesh_skinning(aniresult, skinning_matrices,
-						input_desc, output_desc,
-						part_num_vertices, meshres:influences_count(ipart)
-					)
-				end
-
-				bgfx.update(handle.handle, 0, {'!', outptr, output_buffer_offset})
+		for _, job in ipairs(skincomp.jobs) do
+			local skinning_matrices = build_skinning_matrices(job, aniresult)
+			local handle = job.hwbuffer_handle
+			local updatedata = job.updatedata
+			for _, part in ipairs(job.parts) do
+				animodule.mesh_skinning(aniresult, skinning_matrices, part.inputdesc, part.outputdesc, part.num)
 			end
+
+			bgfx.update(handle, 0, {"!", updatedata:pointer(), job.buffersize})
 		end
 	end
 end
