d73ceaa070fe5bc6aed03bc1b1df0995782a4fd8
actboy168 <actboy168@gmail.com>
2019-12-20 16:40:31 +0800

    Add event

--- packages/camera_controller/camera_controller_2.lua
+++ packages/camera_controller/camera_controller_2.lua
@@ -35,35 +35,57 @@ local function camera_move(forward_axis, position, dx, dy, dz)
 			forward_axis, {dz}, "*+=")
 end
 
-function camera_controller_system:init()	
+function camera_controller_system:init()
 	local mq = world:first_entity "main_queue"
 
 	local camera = camerautil.get_camera(world, mq.camera_tag)
 	camera.frustum.f = 100	--set far distance to 100
 	camera_reset(camera)
 
-	local data = self.camera_temp_data
+	--local message = {}
+	--local w,s,a,d=0,0,0,0
+	--function message:steering(code, press)
+	--	if code == "W" then
+	--		w = press
+	--	elseif code == "S" then
+	--		s = press
+	--	elseif code == "A" then
+	--		a = press
+	--	elseif code == "D" then
+	--		d = press
+	--	end
+	--	data.dz = w - s
+	--	data.dx = d - a
+	--end
+	--self.message.observers:add(message)
+	self.eventMouseLeft = world:sub {"mouse", "LEFT"}
+	self.eventKeyboard = world:sub {"keyboard"}
+end
 
-	local move_speed = 0.5
-	local lastx, lasty
+local function get_camera()
+	local mq = world:first_entity "main_queue"
+	return camerautil.get_camera(world, mq.camera_tag)
+end
+
+function camera_controller_system:update()
+	local data = self.camera_temp_data
+	local move_speed <const> = 0.5
 	local xdpi, ydpi = rhwi.dpi()
 
-	local message = {}
-
-	function message:mouse(x, y, what, state)
-		if what ~= "RIGHT" then
-			return
-		end
+	for msg in self.eventMouseLeft:each() do
+		local _,_,state,x,y = table.unpack(msg)
 		if state == "MOVE" then
-			local dx = (x - lastx) / xdpi * move_speed
-			local dy = (y - lasty) / ydpi * move_speed
+			local camera = get_camera()
+			local dx = (x - data.lastx) / xdpi * move_speed
+			local dy = (y - data.lasty) / ydpi * move_speed
 			local right, up = ms:base_axes(camera.viewdir)
 			ms(camera.viewdir, {type="q", axis=up, radian={dx}}, {type="q", axis=right, radian={dy}}, "3**n=")
 		end
-		lastx, lasty = x, y
+		data.lastx, data.lasty = x, y
 	end
 
-	function message:keyboard(code, press)
+	for msg in self.eventKeyboard:each() do
+		local _,code,press = table.unpack(msg)
 		local delta = press == 1 and 1 or (press == 0 and -1 or 0)
 		if code == "W" then
 			data.dz = data.dz + delta
@@ -76,29 +98,8 @@ function camera_controller_system:init()
 		end
 	end
 
-	local w,s,a,d=0,0,0,0
-	function message:steering(code, press)
-		if code == "W" then
-			w = press
-		elseif code == "S" then
-			s = press
-		elseif code == "A" then
-			a = press
-		elseif code == "D" then
-			d = press
-		end
-		data.dz = w - s
-		data.dx = d - a
-	end
-
-	self.message.observers:add(message)
-end
-
-function camera_controller_system:update()
-	local data = self.camera_temp_data
 	if data.dx ~= 0 or data.dy ~= 0 or data.dz ~= 0 then
-		local mq = world:first_entity "main_queue"
-		local camera = camerautil.get_camera(world, mq.camera_tag)
+		local camera = get_camera()
 		local speed = 0.5
 		camera_move(camera.viewdir, camera.eyepos, speed * data.dx, speed * data.dy, speed * data.dz)
 	end
