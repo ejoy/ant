adaa351d462190ec3c0e96bcc5c7474f2965fc93
actboy168 <actboy168@gmail.com>
2019-12-26 10:25:24 +0800

    cleanup

--- packages/imgui/system/world_profile_system.lua
+++ packages/imgui/system/world_profile_system.lua
@@ -2,8 +2,6 @@ local ecs = ...
 local world = ecs.world
 local WatcherEvent = require "hub_event"
 
-local timer = import_package "ant.timer"
-
 local profile_cache = ecs.singleton "profile_cache"
 function profile_cache.init()
     return {
@@ -14,19 +12,20 @@ end
 local world_profile_system =  ecs.system "world_profile_system"
 world_profile_system.singleton "profile_cache"
 
-function world_profile_system:system_begin()
-    local sys,what = world:get_cur_system()
-    local time_ms = timer.cur_time()
-    if sys ~= "world_profile_system" then
-        table.insert(self.profile_cache.list,{sys,what,"begin",time_ms})
-    end
-end
+local eventSystemBegin = world:sub {"system_begin"}
+local eventSystemEnd = world:sub {"system_end"}
 
-function world_profile_system:system_end()
-    local sys,what = world:get_cur_system()
-    local time_ms = timer.cur_time()
-    if sys ~= "world_profile_system" then
-        table.insert(self.profile_cache.list,{sys,what,"end",time_ms})
+function world_profile_system:update()
+    for _,sys,what,time_ms in eventSystemBegin:unpack() do
+        if sys ~= "world_profile_system" then
+            table.insert(self.profile_cache.list,{sys,what,"begin",time_ms})
+        end
+    end
+    
+    for _,sys,what,time_ms in eventSystemEnd:unpack() do
+        if sys ~= "world_profile_system" then
+            table.insert(self.profile_cache.list,{sys,what,"end",time_ms})
+        end
     end
 end
 
