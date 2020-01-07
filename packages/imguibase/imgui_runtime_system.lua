deca183fc516cc97b370324ae4762c5062ac5d97
junjie020 <kitchen.gz.020@gmail.com>
2020-01-07 18:31:41 +0800

    bind ui viewid to frame buffer in post_init function

--- packages/imguibase/imgui_runtime_system.lua
+++ packages/imguibase/imgui_runtime_system.lua
@@ -8,16 +8,16 @@ local viewidmgr = renderpkg.viewidmgr
 
 local m = ecs.system "imgui_system"
 
+function m:post_init()
+    local main_viewid = assert(viewidmgr.get "main_view")
+    local vid = imgui.ant.viewid()
+    fbmgr.bind(vid, assert(fbmgr.get_fb_idx(main_viewid)))
+end
+
 function m:ui_start()
     local time = timer.deltatime/1000
     if time <= 0.0 then
         time = 1.0/60
-    end
-
-    local vid = imgui.ant.viewid()
-    if nil == fbmgr.get_fb_idx(vid) then
-        local main_viewid = assert(viewidmgr.get "main_view")
-        fbmgr.bind(vid, assert(fbmgr.get_fb_idx(main_viewid)))
     end
 
     imgui.begin_frame(time)
