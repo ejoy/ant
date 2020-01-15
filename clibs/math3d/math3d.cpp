a8ec751075e703842ed9fed880479b6ecca98c11
junjie020 <kitchen.gz.020@gmail.com>
2020-01-15 15:11:30 +0800

    fix error

--- clibs/math3d/math3d.cpp
+++ clibs/math3d/math3d.cpp
@@ -904,10 +904,10 @@ static void mulH_2values(lua_State *L, struct lastack *LS){
 	int64_t v1 = pop(L, LS);
 	int64_t v0 = pop(L, LS);
 	int t0, t1;
-	const glm::mat4x4 * mat = (const glm::mat4x4 *)lastack_value(LS, v1, &t1);
-	const glm::vec4 * v = (const glm::vec4 *)lastack_value(LS, v0, &t0);
-	if (t0 != LINEAR_TYPE_VEC4 && t1 != LINEAR_TYPE_MAT)
-		luaL_error(L, "'%%' operator only support vec4 * mat, type0 is : %d, type1 is : %d", t0, t1);
+	const glm::vec4 * v = (const glm::vec4 *)lastack_value(LS, v1, &t1);
+	const glm::mat4x4 * mat = (const glm::mat4x4 *)lastack_value(LS, v0, &t0);
+	if (t0 != LINEAR_TYPE_MAT && t1 != LINEAR_TYPE_VEC4)
+		luaL_error(L, "'%%' operator only support mat * vec4, type0 is : %d, type1 is : %d", t0, t1);
 
 	glm::vec4 r = *mat * *v;	
 	if (!is_zero(r)){
