4846c4fde29ffd57631a6a4440e26597d3998d5c
actboy168 <actboy168@gmail.com>
2020-02-03 21:32:13 +0800

    Support build msvc release

--- 3rd/build_msvc_all.bat
+++ 3rd/build_msvc_all.bat
@@ -8,6 +8,6 @@ call "%InstallDir%\Common7\Tools\vsdevcmd.bat" -arch=x64 -host_arch=x64
 
 @echo on
 
-msbuild build\ozz-animation\msvc\debug\ozz.sln      /m /v:m /t:build /p:Configuration="Debug",Platform="x64"
-msbuild build\bullet3\msvc\debug\BULLET_PHYSICS.sln /m /v:m /t:build /p:Configuration="Debug",Platform="x64"
-msbuild bgfx\.build\projects\vs2019\bgfx.sln        /m /v:m /t:build /p:Configuration="Debug",Platform="x64"
+msbuild build\ozz-animation\msvc\%1\ozz.sln      /m /v:m /t:build /p:Configuration="%1",Platform="x64"
+msbuild build\bullet3\msvc\%1\BULLET_PHYSICS.sln /m /v:m /t:build /p:Configuration="%1",Platform="x64"
+msbuild bgfx\.build\projects\vs2019\bgfx.sln        /m /v:m /t:build /p:Configuration="%1",Platform="x64"
