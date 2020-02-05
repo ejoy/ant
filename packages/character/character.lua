ac77e86528fc15509846ba34f39db53285cdb009
kitchen020 <junjie020@163.com>
2020-02-05 16:42:47 +0800

    add character_height and foot_ik raycast

--- packages/character/character.lua
+++ packages/character/character.lua
@@ -18,46 +18,85 @@ character_policy.require_component "skeleton"
 character_policy.require_policy "ant.aniamtion|animaiton"
 character_policy.require_policy "ant.bullet|raycast"
 
-local char_sys = ecs.system "character_system"
-char_sys.require_system     "ant.bullet|collider_system"
-char_sys.require_interface "ant.bullet|collider"
+ecs.component "character_height_raycast"
+    .dir "vector4" (0, -2, 0, 0)
+local char_height_policy = ecs.policy "character_height_raycast"
+char_height_policy.require_component "character_height_raycast"
+char_height_policy.require_component "character"
+char_height_policy.require_component "raycast"
+char_height_policy.require_system "character_height_system"
+
+local char_height_sys = ecs.system "character_height_system"
+char_height_sys.require_system     "ant.bullet|collider_system"
+char_height_sys.require_interface  "ant.bullet|collider"
 
 local icollider = world:interface "ant.bullet|collider"
 
 local character_motion = world:sub {"character_motion"}
 local character_spawn = world:sub {"component_register", "character"}
 
-local char_height_dir = ms:vector(0, -1, 0, 0)
+local function generate_height_test_ray(e)
+    local height_raycast = e.character_height_raycast
+    local c_aabb_min, c_aabb_max = icollider(e)
 
-function char_sys:data_changed()
-    for _, eid in world:each "character" do
+    if c_aabb_min and c_aabb_max then
+        local center = ms({0.5}, c_aabb_min, c_aabb_max, "+*T")
+        local startpt = ms({center[1], c_aabb_min[2], center[2]}, "P")
+        local endpt = ms(startpt, height_raycast.dir, "+P")
+        return {
+            startpt, endpt,
+        }
+    end
+end
+function char_height_sys:data_changed()
+    for _, eid in world:each "character_height_raycast" do
         local e = world[eid]
-
-        local char = e.character
         local rc = e.raycast
 
-        local collider = e.collider.handle
-        local c_aabb_min, c_aabb_max = icollider(e)
-
-        if c_aabb_min and c_aabb_max then
-            
-            rc.rays["height"] = {
-                
-            }
+        local ray = generate_height_test_ray(e)
+        if ray then
+            rc.rays["height"] = ray
         end
+    end
+end
 
-        
-
+function char_height_sys:character_height()
+    for _, eid in world:each "character" do
+        local e = world[eid]
+        local char = e.character
+        local rc = e.raycast
 
     end
 end
 
-function char_sys:character_height()
-    for _, eid in world:each "character" do
+ecs.component "foot_ik_ray"
+    .target_names "string[]"
+    .dir "vector4" (0, -2, 0, 0)
+
+local foot_ik_policy = ecs.policy "foot_ik_raycast"
+foot_ik_policy.require_component "character"
+foot_ik_policy.require_component "foot_ik_ray"
+foot_ik_policy.require_component "skeleton"
+foot_ik_policy.require_component "animation"
+foot_ik_policy.require_component "raycast"
+
+foot_ik_policy.require_system "character_foot_ik_system"
+
+local char_foot_ik_sys = ecs.system "character_foot_ik_system"
+
+function char_foot_ik_sys:data_changed()
+    for _, eid in world:each "foot_ik_raycast" do
         local e = world[eid]
-        local char = e.character
         local rc = e.raycast
 
+        local ske = e.skeleton
+        local foot_rc = e.foot_ik_raycast
+
+        for _, tn in ipairs(foot_rc.target_names) do
+            assert(ske:joint_idx(tn), "not exist target name in skeleton:" .. tn)
+            
+            
+        end
 
     end
 end
