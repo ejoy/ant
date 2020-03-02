f22c9c2ea45b0f0474cbc02b2236bc84a772bdf8
Cloud Wu <cloudwu@gmail.com>
2020-03-02 13:46:17 +0800

    update README

--- clibs/bullet/bullet.mk
+++
@@ -1,9 +0,0 @@
-BULLETROOT = $(ANT3RD)
-BULLETINC = -I$(BULLETROOT)/bullet3/src
-BULLETLIB = -L$(BULLETROOT)/build/bullet3/$(PLAT)/$(MODE)/lib
-ifeq "$(MODE)" "debug"
-BULLETLIB += -lBulletDynamics_Debug -lBulletCollision_Debug -lLinearMath_Debug
-else
-BULLETLIB += -lBulletDynamics -lBulletCollision -lLinearMath
-endif
-BULLETLIB += -lstdc++
