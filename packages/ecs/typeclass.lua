7d5f58626d96299e19b37d19f8458b9abf1863a3
zengruihong <ruihong.crh@alibaba-inc.com>
2020-03-13 12:46:56 +0800

    fix tag type bug

--- packages/ecs/typeclass.lua
+++ packages/ecs/typeclass.lua
@@ -51,7 +51,7 @@ local function gen_method(c, callback)
 end
 
 local function decl_basetype(schema)
-	schema:primtype("ant.ecs", "tag", "boolean", true)
+	schema:primtype("ant.ecs", "tag", true)
 	schema:primtype("ant.ecs", "entityid", -1)
 	schema:primtype("ant.ecs", "int", 0)
 	schema:primtype("ant.ecs", "real", 0.0)
