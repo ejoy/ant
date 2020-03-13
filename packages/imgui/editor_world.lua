018ece47ddd6e1f43c14d8034c2e2ddb7ba86dbb
zengruihong <ruihong.crh@alibaba-inc.com>
2020-03-13 12:54:30 +0800

    modify editor_world,add instantiate_entity func

--- packages/imgui/editor_world.lua
+++ packages/imgui/editor_world.lua
@@ -1,5 +1,8 @@
-
+    
 local function gen_editor_world(base_world)
+    local datalist = require "datalist"
+    local serialize = import_package 'ant.serialize'
+
     local editor_world = {}
     function editor_world:set_entity(eid, policies, dataset)
         local h = false
@@ -19,6 +22,22 @@ local function gen_editor_world(base_world)
         end
     end
 
+    -- create new serialize_id,otherwise,serialize_id may conflict with origin entity
+    function editor_world:instantiate_entity(t)
+        if type(t) == 'string' then
+            local d = datalist.parse(t)
+            return self:instantiate_entity({policy=d[1],data=d[2]})
+        elseif type(t) == 'table' then -- {policy={},data={}}
+            local component = t.data
+            if component and component.serialize then
+                component.serialize = serialize.create()
+            end
+            return self:create_entity(t)
+        else
+            log.error("Instantiate_entity support str/tbl only")
+        end
+    end
+
     function editor_world:add_policy(eid,t)
         base_world.add_policy(self,eid,t)
         for _,p in ipairs(t.policy) do
@@ -34,7 +53,6 @@ local function gen_editor_world(base_world)
     editor_world.__index = editor_world
     return editor_world
 end
-
 
 return function()
     local ecs = import_package "ant.ecs"
