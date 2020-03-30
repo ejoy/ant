4fa2788f126bc216fa9ffd45b5bc34f6bd5c21dd
junjie020 <kitchen.gz.020@gmail.com>
2020-03-30 18:02:18 +0800

    add lock target type 'ignore_scale'

--- packages/objcontroller/obj_lock_system.lua
+++ packages/objcontroller/obj_lock_system.lua
@@ -6,7 +6,7 @@ local math3d = require "math3d"
 ecs.component "lock_target"
 	.type	"string"
 	.target	"entityid"
-	.offset	"vector"
+	["opt"].offset	"vector"
 
 local obj_lock_p = ecs.policy "obj_lock_policy"
 obj_lock_p.require_component "lock_target"
@@ -35,16 +35,38 @@ function lock_target_sys:lock_target()
 
         local locktype = lt.type
         if locktype == "move" then
-            local targetentity = world[lt.target]
-            local transform = targetentity.transform
-            im.set_position(math3d.add(transform.srt.t, lt.offset))
+            if lt.offset then
+                local te = world[lt.target]
+                local transform = te.transform
+                local pos = math3d.index(transform.srt, 3)
+                im.set_position(math3d.add(pos, lt.offset))
+            end
         elseif locktype == "rotate" then
-            local targetentity = world[lt.target]
-            local transform = targetentity.transform
+            local te = world[lt.target]
+            local transform = te.transform
 
             local eyepos = im.get_position(e)
-            local targetpos = transform.srt.t
+            local targetpos = math3d.index(transform.srt, 3)
             im.set_direction(e, math3d.normalize(math3d.sub(targetpos, eyepos)))
+            if lt.offset then
+                im.set_position(math3d.add(targetpos, lt.offset))
+            end
+        elseif locktype == "ignore_scale" then
+            local trans = e.transform
+            if trans == nil then
+                error(string.format("'ignore_scale' could not bind to entity without 'transform' component"))
+            end
+
+            local te = world[lt.target]
+            local target_trans = te.transform
+
+            local _, r, t = math3d.srt(target_trans)
+            local m = math3d.matrix{s=1, r=r, t=t}
+            trans.srt.m = math3d.mul(m, trans.srt)
+
+            if e.hierarchy then
+                world:pub {"component_changed", "transform", "srt"}
+            end
         else
             error(string.format("not support locktype:%s", locktype))
         end
