ced18dc61598b304888517b1df8e3d4ecd35c888
zengruihong <ruihong.crh@alibaba-inc.com>
2020-04-01 11:05:18 +0800

    Use FastTree in GuiSceneHierarchyView;

--- packages/imgui/editor/gui_scene_hierarchy_view.lua
+++ packages/imgui/editor/gui_scene_hierarchy_view.lua
@@ -8,6 +8,7 @@ local enum = imgui.enum
 local IO = imgui.IO
 local gui_input = require "gui_input"
 local gui_mgr = require "gui_mgr"
+local FastTree = require "controls.fast_tree"
 
 local GuiBase = require "gui_base"
 local GuiHierarchyView = GuiBase.derive("GuiHierarchyView")
@@ -32,6 +33,9 @@ function GuiHierarchyView:_init()
     self.title_id = string.format("SceneHierarchy###%s",self.GuiName)
     self.selected_map = OrderedMap:new()
     self._scroll_flag = false
+    self.fast_tree = FastTree.new()
+    self.fast_tree:set_update_item_func(self._render_tree_node,self)
+    self.tree_open_state = {}
     ---
     self:_init_subcribe()
 end
@@ -69,6 +73,53 @@ function GuiHierarchyView:_on_refresh_hierarchy(tbl)
     log.trace_a("_on_refresh_hierarchy",tbl)
     self.hierarchy_data = tbl
     self.sorted_map = {}
+    self:transform_hierarchy_data(tbl)
+    assert(self.root_node)
+    self.fast_tree:set_tree_data(self.root_node,self.tree_open_state)
+end
+
+function GuiHierarchyView:transform_hierarchy_data(tbl)
+    self.root_node = {
+        key = -1,
+        data = {
+            eid = -1,
+            name = "Scene"
+        }
+    }
+    local tree_open_state = self.tree_open_state
+    local function transform_r( node,tbl )
+        local sorted_ids = {}
+        for eid,_ in pairs(tbl) do
+            table.insert(sorted_ids,eid)
+        end
+        if #sorted_ids > 0 then
+            node.children = node.children or {}
+            table.sort(sorted_ids)
+            for i,eid in ipairs(sorted_ids) do
+                local data = tbl[eid]
+                local child_node = {
+                    key = eid,
+                    data = {
+                        eid = eid,
+                        name = data.name,
+                        childnum = data.childnum
+                    }
+                }
+                if tree_open_state[child_node.key] == nil then
+                    if data.childnum > 10 then
+                        tree_open_state[child_node.key] = false
+                    end 
+                end
+                table.insert(node.children,child_node)
+                if data.childnum > 0 then
+                    transform_r(child_node,data.children)
+                end
+            end
+
+        end
+    end
+    transform_r(self.root_node,tbl)
+    self.root_node.data.childnum = #(self.root_node.children)
 end
 
 function GuiHierarchyView:on_update()
@@ -76,10 +127,12 @@ function GuiHierarchyView:on_update()
     if not self.hierarchy_data then
         return 
     end
-    if widget.TreeNode("Scene",flags.TreeNode.DefaultOpen) then
-        self:_render_children(self.hierarchy_data)
-        widget.TreePop()
-    end
+    self.fast_tree:update()
+
+    -- if widget.TreeNode("Scene",flags.TreeNode.DefaultOpen) then
+    --     self:_render_children(self.hierarchy_data)
+    --     widget.TreePop()
+    -- end
     self.context_menu_opened = false
     if not self.context_menu_opened and windows.BeginPopupContextWindow() then
         if widget.MenuItem("New Entity") then
@@ -89,7 +142,7 @@ function GuiHierarchyView:on_update()
             local eids = self.selected_map:get_list()
             gui_mgr.getMgr("EntityMgr"):request_duplicate_entity(eids)
         end
-        windows.EndPopup()
+        windows.EndPopup() 
     end
 end
 
@@ -102,70 +155,61 @@ function GuiHierarchyView:_update_menu_bar()
     end
 end
 
-function GuiHierarchyView:_render_children(children)
-    local sorted = self.sorted_map[children]
-    if not sorted then
-        local cache = {}
-        for id,_ in pairs(children) do
-            table.insert(cache,id)
-        end
-        table.sort(cache)
-        self.sorted_map[children] = cache
-        sorted = cache
-    end
+function GuiHierarchyView:_render_tree_node(node,opened)
 
-    for _,id in ipairs(sorted) do
-        self:_render_entity(id,children[id])
-    end
-end
-
-
-function GuiHierarchyView:_render_entity(id,entity)
-    local children = entity.children
+    local entity = node.data
+    local id = entity.eid
     local flag = ParentFlag
-    if not children then
-        flag = LeafFlag
-    end
-    local name = string.format("[%d]%s",id,entity.name)
-    if self.selected_map:has(id) then
-        flag = flag | flags.TreeNode.Selected
-    end
-    if entity.childnum >= 1 and entity.childnum <= 9 then
-        widget.SetNextItemOpen(true,"f")
-    end
-    local cur_open = widget.TreeNode(name,flag)
-    if self._scroll_flag and self.selected_map:has(id) then
-        windows.SetScrollHereY()
-        self._scroll_flag = false
-    end
-
-    if util.IsItemClicked(0) and gui_input.key_state.CTRL then
-        self.selected_map:insert(id)
-        self:publish_selected_entity(self.selected_map:get_list(),util.IsMouseDoubleClicked(0))
-    elseif util.IsItemClicked(0) or util.IsItemClicked(1) then
-        self.selected_map:removeAll()
-        self.selected_map:insert(id)
-        self:publish_selected_entity(self.selected_map:get_list(),util.IsMouseDoubleClicked(0))
-    end
-
-    if not self.context_menu_opened then
-        self.context_menu_opene = self:_show_selected_entity_menu(id,entity)
-    end
-
-    if util.IsItemHovered() then
-        widget.BeginTooltip()
-        widget.Text("Detail")
-        cursor.Separator()
-        widget.BulletText(string.format("EntityID:%d",id));
-        widget.BulletText("...")
-        widget.EndTooltip()
-    end
-
-    if cur_open then
-        if children then
-            self:_render_children(children)
+    if id <= 0 then
+        local new_opened =  widget.TreeNode("Scene",flag)
+        if new_opened then
+            widget.TreePop()
         end
-        widget.TreePop()
+        return new_opened
+    else
+        if entity.childnum <= 0  then
+            flag = LeafFlag
+        end
+        local name = string.format("[%d]%s",id,entity.name)
+        if self.selected_map:has(id) then
+            flag = flag | flags.TreeNode.Selected
+        end
+        widget.SetNextItemOpen(opened)
+        -- if entity.childnum >= 1 and entity.childnum <= 9 then
+        --     widget.SetNextItemOpen(true,"f")
+        -- end
+        local cur_open = widget.TreeNode(name,flag)
+        if self._scroll_flag and self.selected_map:has(id) then
+            windows.SetScrollHereY()
+            self._scroll_flag = false
+        end
+
+        if util.IsItemClicked(0) and gui_input.key_state.CTRL then
+            self.selected_map:insert(id)
+            self:publish_selected_entity(self.selected_map:get_list(),util.IsMouseDoubleClicked(0))
+        elseif util.IsItemClicked(0) or util.IsItemClicked(1) then
+            self.selected_map:removeAll()
+            self.selected_map:insert(id)
+            self:publish_selected_entity(self.selected_map:get_list(),util.IsMouseDoubleClicked(0))
+        end
+
+        if not self.context_menu_opened then
+            self.context_menu_opene = self:_show_selected_entity_menu(id,entity)
+        end
+
+        if util.IsItemHovered() then
+            widget.BeginTooltip()
+            widget.Text("Detail")
+            cursor.Separator()
+            widget.BulletText(string.format("EntityID:%d",id));
+            widget.BulletText("...")
+            widget.EndTooltip()
+        end
+
+        if cur_open then
+            widget.TreePop()
+        end
+        return cur_open
     end
 end
 
