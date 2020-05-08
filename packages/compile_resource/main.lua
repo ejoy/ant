4c0e7ff28966a0500af91f8f6b5e4320e554cda2
junjie020 <kitchen.gz.020@gmail.com>
2020-05-08 18:09:24 +0800

    refactor get setting method

--- packages/compile_resource/main.lua
+++ packages/compile_resource/main.lua
@@ -4,23 +4,34 @@ local function load_fx_setting()
     local renderpkg = import_package "ant.render"
     local setting = renderpkg.setting.get()
 
-    return {
-        graphic = {
-            shadow = {
-                type = setting.graphic.shadow.type,
-            },
-            postprocess = {
-                bloom = {
-                    enable = setting.graphic.postprocess.bloom.enable,
-                }
-            }
-        },
-        animation = {
-            skinning = {
-                type = setting.animation.skinning.type
-            },
-        },
-    }
+    local function get_setting(path, r)
+        local s = setting
+        local paths = {}
+        for m in path:gmatch "[^/]+" do
+            paths[#paths+1] = m
+        end
+        local last = r
+        for i=1, #paths-1 do
+            local p = paths[i]
+            local c = last[p]
+            if c == nil then
+                c = {}
+                last[p] = c
+            end
+
+            last = c
+            s = s[p]
+        end
+        local p = paths[#paths]
+        last[p] = s[p]
+        return r
+    end
+
+    local r = {}
+    get_setting("graphic/shadow/type", r)
+    get_setting("graphic/postprocess/bloom/enable", r)
+    get_setting("animation/skinning/type", r)
+    return r
 end
 
 local compiler = {
