19f43ad64abab14a6200d3ed9739c502af1837c3
junjie020 <kitchen.gz.020@gmail.com>
2020-05-13 17:47:18 +0800

    use cpu as default

--- packages/compile_resource/fx/compile.lua
+++ packages/compile_resource/fx/compile.lua
@@ -97,7 +97,7 @@ local function load_surface_type(fxcontent)
 			receive = "on",			-- "on"/"off"
 		},
 		skinning = {
-			type = "GPU",
+			type = "CPU",
 		},
 		subsurface = "off",			-- "on"/"off"? maybe has other setting
 	}
