fda169b03d5eabeb1fceff23d7f1a1ad8cd80c9b
actboy168 <actboy168@gmail.com>
2020-05-20 10:44:41 +0800

    process's output is writable

--- packages/ecs/policy.lua
+++ packages/ecs/policy.lua
@@ -93,6 +93,7 @@ local function create(w, policies)
             if class.process then
                 res.process_entity[#res.process_entity+1] = class.process
                 for _, v in ipairs(class.output) do
+                    res.writable[v] = true
                     res.register_component[v] = true
                 end
             end
