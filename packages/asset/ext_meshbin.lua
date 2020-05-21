5dffc1a0aeab65970b0b93153cf8299d1538a48c
actboy168 <actboy168@gmail.com>
2020-05-21 10:53:06 +0800

    compile glb

--- packages/asset/ext_meshbin.lua
+++ packages/asset/ext_meshbin.lua
@@ -1,8 +1,9 @@
-local fs_rt = import_package "ant.utility".fs_rt
+local utility = require "utility"
 local thread = require "thread"
+
 return {
     loader = function (filename)
-        local c = fs_rt.read_file(filename)
+        local c = utility.read_file(filename)
         return thread.unpack(c)
     end,
     unloader = function ()
--- tools/viewer/editor/import_prefab.lua
+++ tools/viewer/editor/import_prefab.lua
@@ -1,37 +1,10 @@
 local lfs  = require "filesystem.local"
 local fs   = require "filesystem"
-local sp   = require "subprocess"
-local task = require "task"
-
-local function import(input, voutput)
-    local function luaexe()
-        local i = -1
-        while arg[i] ~= nil do
-            i= i - 1
-        end
-        return arg[i + 1]
-    end
-    local loutput = voutput:localpath()
-    lfs.remove_all(loutput)
-    lfs.create_directories(loutput)
-    local p = sp.spawn {
-        luaexe(),
-        "./tools/import_model/import.lua",
-        "-i", input,
-        "-o", loutput,
-        "-v", voutput:string(),
-        "--config", "tools/import_model/cfg.txt",
-        stderr = true,
-        hideWindow = true,
-    }
-    while p:is_running() do
-        task.wait(100)
-    end
-    assert(p:wait() == 0, p.stderr:read "a")
-end
 
 return function (filename)
-    local output = fs.path "/pkg/tools.viewer.prefab_viewer/res/"
-    import(lfs.path(filename), output)
-    return (output / "mesh.prefab"):string()
+    local output = "/pkg/tools.viewer.prefab_viewer/res/root.glb"
+    local loutput = fs.path(output):localpath()
+    lfs.create_directories(loutput:parent_path())
+    lfs.copy_file(lfs.path(filename), loutput, true)
+    return output .. "|mesh.prefab"
 end
