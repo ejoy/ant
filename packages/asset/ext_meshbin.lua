e719a04aff809f2868e9a4fac60850a32d6fac2c
junjie020 <kitchen.gz.020@gmail.com>
2020-05-21 10:54:56 +0800

    fix meshbin loader not create bounding as math3d object

--- packages/asset/ext_meshbin.lua
+++ packages/asset/ext_meshbin.lua
@@ -1,9 +1,25 @@
 local fs_rt = import_package "ant.utility".fs_rt
 local thread = require "thread"
+local math3d = require "math3d"
+local function create_bounding(bounding)
+	if bounding then
+		bounding.aabb = math3d.ref(math3d.aabb(bounding.aabb[1], bounding.aabb[2]))
+	end
+end
 return {
     loader = function (filename)
         local c = fs_rt.read_file(filename)
-        return thread.unpack(c)
+        local meshscene = thread.unpack(c)
+        for _, scene in pairs(meshscene.scenes) do
+            for _, meshnode in pairs(scene) do
+                local skin = meshnode.skin
+                create_bounding(meshnode.bounding)
+                for _, prim in ipairs(meshnode) do
+                    create_bounding(prim.bounding)
+                    prim.skin = skin
+                end
+            end
+        end
     end,
     unloader = function ()
     end,
