ace100bf288cef1ce3a4b081bd3c2107343f6348
Cloud Wu <cloudwu@gmail.com>
2020-06-01 18:15:34 +0800

    update rp3d 0.8.0

--- clibs/rp3d/rp3d.mk
+++ clibs/rp3d/rp3d.mk
@@ -1,4 +1,4 @@
 RP3DROOT = $(ANT3RD)
-RP3DINC = -I$(RP3DROOT)/reactphysics3d/src
-RP3DLIB = -L$(RP3DROOT)/build/reactphysics3d/$(PLAT)/$(MODE)/lib
+RP3DINC = -I$(RP3DROOT)/reactphysics3d/include
+RP3DLIB = -L$(RP3DROOT)/build/reactphysics3d/$(PLAT)/$(MODE)
 RP3DLIB += -lreactphysics3d
--- packages/collision/rp3d.lua
+++ packages/collision/rp3d.lua
@@ -7,7 +7,8 @@ local function shape_interning()
 	local function all_shapes_gc(self)
 		for key , shape in pairs(all_shapes) do
 			all_shapes[key] = nil
-			lib.delete_shape(shape)
+			local what = (key:match "[^:]*") or key
+			lib.destroy_shape[what](shape)
 		end
 	end
 	setmetatable(all_shapes, { __gc = all_shapes_gc })
@@ -47,16 +48,19 @@ local function shape_interning()
 	end
 end
 
-function lib.init()
+function lib.init(logger)
+	if logger then
+		lib.logger(logger)
+	end
 	shape_interning()
 	local world_mt = lib.collision_world_mt
 
 	world_mt.__index = world_mt
 
-	world_mt.body_create 	= math3d_adapter.vector(world_mt.body_create, 2)
+	world_mt.body_create 	= math3d_adapter.format(world_mt.body_create, "vq", 2)
 	world_mt.set_transform 	= math3d_adapter.format(world_mt.set_transform, "vq", 3)
 	world_mt.get_aabb 		= math3d_adapter.getter(world_mt.get_aabb, "vv")
-	world_mt.add_shape 		= math3d_adapter.vector(world_mt.add_shape, 5)
+	world_mt.add_shape 		= math3d_adapter.vector(world_mt.add_shape, 4)
 
 	lib.shape.heightfield	= math3d_adapter.vector(lib.shape.heightfield, 7)
 
@@ -65,14 +69,10 @@ function lib.init()
 
 	function world_mt.raycast(world, p0, p1, maskbits)
 		p0,p1 = rayfilter(p0,p1)
-		local hit, id, pos, norm = raycast(world, p0, p1, maskbits or 0)
+		local hit, pos, norm = raycast(world, p0, p1, maskbits or 0)
 		if hit then
-			return pos, norm, id
+			return pos, norm
 		end
-	end
-
-	function lib.collision_world(settings)
-		return lib.new_collision_world(settings, world_mt)
 	end
 end
 
