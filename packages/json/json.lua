d77f18a452e8a363ff80d1e5420b14cf41162434
actboy168 <actboy168@gmail.com>
2020-06-15 16:26:04 +0800

    Improve the speed of json decode

--- packages/json/json.lua
+++ packages/json/json.lua
@@ -3,15 +3,19 @@ local type = type
 local next = next
 local error = error
 local tonumber = tonumber
+local tostring = tostring
 local utf8_char = utf8.char
 local table_concat = table.concat
 local string_char = string.char
+local string_byte = string.byte
 local math_type = math.type
+local setmetatable = setmetatable
 local Inf = math.huge
 
 local json = {}
 
 json.null = function() end
+json.object_mt = {}
 
 -------------------------------------------------------------------------------
 -- Encode
@@ -19,10 +23,10 @@ json.null = function() end
 
 local encode
 
-local escape_char_map = {
+local encode_escape_map = {
+    [ "\"" ] = "\\\"",
     [ "\\" ] = "\\\\",
     [ "/" ]  = "\\/",
-    [ "\"" ] = "\\\"",
     [ "\b" ] = "\\b",
     [ "\f" ] = "\\f",
     [ "\n" ] = "\\n",
@@ -30,13 +34,15 @@ local escape_char_map = {
     [ "\t" ] = "\\t",
 }
 
-local escape_char_map_inv = { [ "\\/" ] = "/" }
-for k, v in pairs(escape_char_map) do
-    escape_char_map_inv[v] = k
+local decode_escape_set = {}
+local decode_escape_map = {}
+for k, v in pairs(encode_escape_map) do
+    decode_escape_map[v] = k
+    decode_escape_set[string_byte(v, 2)] = true
 end
 
-local function escape_char(c)
-    return escape_char_map[c] or ("\\u%04x"):format(c:byte())
+local function encode_escape(c)
+    return encode_escape_map[c] or ("\\u%04x"):format(c:byte())
 end
 
 local function encode_nil()
@@ -50,19 +56,34 @@ local function encode_null(val)
     error "cannot serialise function: type not supported"
 end
 
+local function encode_string(val)
+    return '"' .. val:gsub('[%z\1-\31\127\\"/]', encode_escape) .. '"'
+end
+
+local function convertreal(v)
+    local g = ('%.16g'):format(v)
+    if tonumber(g) == v then
+        return g
+    end
+    return ('%.17g'):format(v)
+end
+
+local function encode_number(val)
+    if val ~= val or val <= -Inf or val >= Inf then
+        error("unexpected number value '" .. tostring(val) .. "'")
+    end
+    return convertreal(val):gsub(',', '.')
+end
+
 local function encode_table(val, stack)
-    local res = {}
-    stack = stack or {}
-    if stack[val] then error("circular reference") end
-    stack[val] = true
-    if next(val) == nil then
-        local meta = getmetatable(val)
-        if meta and meta.__name == 'json.object' then
+    local first_val = next(val)
+    if first_val == nil then
+        if getmetatable(val) == json.object_mt then
             return "{}"
         else
             return "[]"
         end
-    elseif type(next(val)) == 'number' then
+    elseif type(first_val) == 'number' then
         local max = 0
         for k in pairs(val) do
             if math_type(k) ~= "integer" then
@@ -70,32 +91,29 @@ local function encode_table(val, stack)
             end
             max = max > k and max or k
         end
+        local res = {}
+        stack = stack or {}
+        if stack[val] then error("circular reference") end
+        stack[val] = true
         for i = 1, max do
             res[#res+1] = encode(val[i], stack)
         end
         stack[val] = nil
         return "[" .. table_concat(res, ",") .. "]"
     else
+        local res = {}
+        stack = stack or {}
+        if stack[val] then error("circular reference") end
+        stack[val] = true
         for k, v in pairs(val) do
             if type(k) ~= "string" then
                 error("invalid table: mixed or invalid key types")
             end
-            res[#res+1] = encode(k, stack) .. ":" .. encode(v, stack)
+            res[#res+1] = encode_string(k) .. ":" .. encode(v, stack)
         end
         stack[val] = nil
         return "{" .. table_concat(res, ",") .. "}"
     end
-end
-
-local function encode_string(val)
-    return '"' .. val:gsub('[%z\1-\31\127\\"/]', escape_char) .. '"'
-end
-
-local function encode_number(val)
-    if val ~= val or val <= -Inf or val >= Inf then
-        error("unexpected number value '" .. tostring(val) .. "'")
-    end
-    return ("%.14g"):format(val):gsub(',', '.')
 end
 
 local type_func_map = {
@@ -128,18 +146,6 @@ local decode
 local _buf
 local _pos
 
-local escape_chars = {
-    [ "\\" ] = true,
-    [ "/" ] = true,
-    [ '"' ] = true,
-    [ "b" ] = true,
-    [ "f" ] = true,
-    [ "n" ] = true,
-    [ "r" ] = true,
-    [ "t" ] = true,
-    [ "u" ] = true,
-}
-
 local literal_map = {
     [ "true"  ] = true,
     [ "false" ] = false,
@@ -147,29 +153,40 @@ local literal_map = {
 }
 
 local function next_word()
-    local f = _pos
-    local idx = _buf:find("[ \t\r\n%]},]", _pos)
-    _pos = idx and idx or (#_buf + 1)
-    return _buf:sub(f, _pos - 1), f
-end
-
-local function next_nonspace()
-    local idx = _buf:find("[^ \t\r\n]", _pos)
-    _pos = idx and idx or (#_buf + 1)
-end
-
-local function decode_error(msg, idx)
-    idx = idx or _pos
-    local line_count = 1
-    local col_count = 1
-    for i = 1, idx - 1 do
-        col_count = col_count + 1
-        if _buf:sub(i, i) == "\n" then
-            line_count = line_count + 1
-            col_count = 1
-        end
+    local _, newpos, word, eol = _buf:find("([^ \t\r\n%]},]*)([ \t\r\n%]},]?)", _pos)
+    if eol ~= "" then
+        return word, newpos
     end
-    error(("%s at line %d col %d"):format(msg, line_count, col_count))
+    return word, newpos + 1
+end
+
+local function next_byte()
+    _pos = _buf:find("[^ \t\r\n]", _pos)
+    if _pos then
+        return string_byte(_buf, _pos)
+    end
+    _pos = #_buf + 1
+end
+
+local function getline(str, n)
+    local line = 1
+    local pos = 1
+    while true do
+        local f, _, nl1, nl2 = str:find('([\n\r])([\n\r]?)', pos)
+        if not f then
+            return line, n - pos + 1
+        end
+        local newpos = f + ((nl1 == nl2 or nl2 == '') and 1 or 2)
+        if newpos > n then
+            return line, n - pos + 1
+        end
+        pos = newpos
+        line = line + 1
+    end
+end
+
+local function decode_error(msg)
+    error(("%s at line %d col %d"):format(msg, getline(msg, _pos)))
 end
 
 local function parse_unicode_escape(s)
@@ -186,89 +203,96 @@ local function parse_string()
     local has_unicode_escape = false
     local has_surrogate_escape = false
     local has_escape = false
-    local last
-    for j = _pos + 1, #_buf do
-        local x = _buf:byte(j)
-        if x < 32 then
-            decode_error("control character in string", j)
+    local i = _pos
+    while true do
+        i = _buf:find('[\0-\31\\"]', i + 1)
+        if not i then
+            decode_error "expected closing quote for string"
         end
-        if last == 92 then -- "\\" (escape char)
-            if x == 117 then -- "u" (unicode escape sequence)
-                local hex = _buf:sub(j + 1, j + 5)
-                if not hex:find("%x%x%x%x") then
-                    decode_error("invalid unicode escape in string", j)
+        local x = string_byte(_buf, i)
+        if x < 32 then
+            _pos = i
+            decode_error "control character in string"
+        end
+        if x == 92 --[[ "\\" ]] then
+            local nx = string_byte(_buf, i+1)
+            if nx == 117 --[[ "u" ]] then
+                local hex = _buf:sub(i+2, i+5)
+                if not hex:match "%x%x%x%x" then
+                    _pos = i
+                    decode_error "invalid unicode escape in string"
                 end
-                if hex:find("^[dD][89aAbB]") then
-                    if not _buf:sub(j + 5, j + 10):match('\\u%x%x%x%x') then
-                        decode_error("missing low surrogate", j)
+                if hex:match "^[dD][89aAbB]" then
+                    if not _buf:sub(i+6, i+11):match '\\u%x%x%x%x' then
+                        _pos = i
+                        decode_error "missing low surrogate"
                     end
                     has_surrogate_escape = true
+                    i = i + 11
                 else
                     has_unicode_escape = true
+                    i = i + 5
                 end
             else
-                local c = string_char(x)
-                if not escape_chars[c] then
-                    decode_error("invalid escape char '" .. c .. "' in string", j)
+                if not decode_escape_set[nx] then
+                    _pos = i
+                    decode_error("invalid escape char '" .. string_char(nx) .. "' in string")
                 end
                 has_escape = true
+                i = i + 1
             end
-            last = nil
-        elseif x == 34 then -- '"' (end of string)
-            local s = _buf:sub(_pos + 1, j - 1)
+        elseif x == 34 --[[ '"' ]] then
+            local s = _buf:sub(_pos + 1, i - 1)
             if has_surrogate_escape then
-                s = s:gsub("\\u[dD][89aAbB]..\\u....", parse_unicode_escape)
+                s = s:gsub("\\u[dD][89aAbB]%x%x\\u%x%x%x%x", parse_unicode_escape)
             end
             if has_unicode_escape then
-                s = s:gsub("\\u....", parse_unicode_escape)
+                s = s:gsub("\\u%x%x%x%x", parse_unicode_escape)
             end
             if has_escape then
-                s = s:gsub("\\.", escape_char_map_inv)
+                s = s:gsub("\\.", decode_escape_map)
             end
-            _pos = j + 1
+            _pos = i + 1
             return s
-        else
-            last = x
         end
     end
-    decode_error("expected closing quote for string")
 end
 
 local function parse_number()
-    local word, f = next_word()
+    local word, newpos = next_word()
     local n = tonumber(word)
     if not n or word:find '[^-+.%deE]' or word:match '^0[1-9]' then
-        decode_error("invalid number '" .. word .. "'", f)
+        decode_error("invalid number '" .. word .. "'")
     end
+    _pos = newpos
     return n
 end
 
 local function parse_literal()
-    local word, f = next_word()
-    if literal_map[word] == nil then
-        decode_error("invalid literal '" .. word .. "'", f)
+    local word, newpos = next_word()
+    local res = literal_map[word]
+    if res == nil then
+        decode_error("invalid literal '" .. word .. "'")
     end
-    return literal_map[word]
+    _pos = newpos
+    return res
 end
 
 local function parse_array()
     _pos = _pos + 1
-    next_nonspace()
-    if _buf:sub(_pos, _pos) == "]" then
+    if next_byte() == 93 --[[ "]" ]] then
         _pos = _pos + 1
         return {}
     end
     local res = {}
-    local n = 1
+    local n = 0
     while true do
-        next_nonspace()
-        res[n] = decode()
         n = n + 1
-        next_nonspace()
-        local chr = _buf:sub(_pos, _pos)
+        res[n] = decode()
+        local chr = next_byte()
         _pos = _pos + 1
-        if chr == "]" then return res end
-        if chr ~= "," then decode_error("expected ']' or ','") end
+        if chr == 93 --[[ "]" ]] then return res end
+        if chr ~= 44 --[[ "," ]] then decode_error "expected ']' or ','" end
     end
 end
 
@@ -276,62 +300,58 @@ local function parse_object()
     local res = {}
     _pos = _pos + 1
     while true do
-        next_nonspace()
-        if _buf:sub(_pos, _pos) == "}" then
+        local chr = next_byte()
+        if chr == 125 --[[ "}" ]] then
             _pos = _pos + 1
             break
         end
-        if _buf:sub(_pos, _pos) ~= '"' then
-            decode_error("expected string for key")
+        if chr ~= 34 --[[ '"' ]] then
+            decode_error "expected string for key"
         end
-        local key = decode()
-        next_nonspace()
-        if _buf:sub(_pos, _pos) ~= ":" then
-            decode_error("expected ':' after key")
+        local key = parse_string()
+        if next_byte() ~= 58 --[[ ":" ]] then
+            decode_error "expected ':' after key"
         end
         _pos = _pos + 1
-        next_nonspace()
-        local val = decode()
-        res[key] = val
-        next_nonspace()
-        local chr = _buf:sub(_pos, _pos)
+        res[key] = decode()
+        local chr = next_byte()
         _pos = _pos + 1
-        if chr == "}" then break end
-        if chr ~= "," then decode_error("expected '}' or ','") end
+        if chr == 125 --[[ "}" ]] then break end
+        if chr ~= 44 --[[ "," ]] then decode_error "expected '}' or ','" end
     end
     if next(res) == nil then
-        setmetatable(res, {__name = 'json.object'})
+        setmetatable(res, json.object_mt)
     end
     return res
 end
 
 local char_func_map = {
-    [ '"' ] = parse_string,
-    [ "0" ] = parse_number,
-    [ "1" ] = parse_number,
-    [ "2" ] = parse_number,
-    [ "3" ] = parse_number,
-    [ "4" ] = parse_number,
-    [ "5" ] = parse_number,
-    [ "6" ] = parse_number,
-    [ "7" ] = parse_number,
-    [ "8" ] = parse_number,
-    [ "9" ] = parse_number,
-    [ "-" ] = parse_number,
-    [ "t" ] = parse_literal,
-    [ "f" ] = parse_literal,
-    [ "n" ] = parse_literal,
-    [ "[" ] = parse_array,
-    [ "{" ] = parse_object,
+    [ string_byte '"' ] = parse_string,
+    [ string_byte "0" ] = parse_number,
+    [ string_byte "1" ] = parse_number,
+    [ string_byte "2" ] = parse_number,
+    [ string_byte "3" ] = parse_number,
+    [ string_byte "4" ] = parse_number,
+    [ string_byte "5" ] = parse_number,
+    [ string_byte "6" ] = parse_number,
+    [ string_byte "7" ] = parse_number,
+    [ string_byte "8" ] = parse_number,
+    [ string_byte "9" ] = parse_number,
+    [ string_byte "-" ] = parse_number,
+    [ string_byte "t" ] = parse_literal,
+    [ string_byte "f" ] = parse_literal,
+    [ string_byte "n" ] = parse_literal,
+    [ string_byte "[" ] = parse_array,
+    [ string_byte "{" ] = parse_object,
 }
 
 decode = function()
-    local chr = _buf:sub(_pos, _pos)
+    local chr = next_byte()
     local f = char_func_map[chr]
     if f then
         return f()
     end
-    decode_error("unexpected character '" .. chr .. "'")
+    decode_error("unexpected character '" .. string_char(chr) .. "'")
 end
 
 function json.decode(str)
@@ -340,11 +360,9 @@ function json.decode(str)
     end
     _buf = str
     _pos = 1
-    next_nonspace()
     local res = decode()
-    next_nonspace()
-    if _pos <= #str then
-        decode_error("trailing garbage")
+    if next_byte() ~= nil then
+        decode_error "trailing garbage"
     end
     return res
 end
