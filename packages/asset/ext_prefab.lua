b261058c9120af3b4f7e603bc1267c78ba1542c0
actboy168 <actboy168@gmail.com>
2020-06-19 16:01:02 +0800

    Fixes bug

--- packages/asset/ext_prefab.lua
+++ packages/asset/ext_prefab.lua
@@ -4,18 +4,25 @@ local ecs_policy = import_package "ant.ecs".policy
 
 local m = ecs.component "prefab"
 
+local function load_prefab(v)
+	return {
+		prefab = world.component "resource"(v.prefab),
+		args = v.args or {},
+	}
+end
+
 local function load_entity(v)
 	local policy, dataset, action = v.policy, v.data, v.action
-	local info = ecs_policy.create(world, policy)
+	local template = ecs_policy.create(world, policy)
 	local e = {}
-	for _, c in ipairs(info.component) do
+	for _, c in ipairs(template.component) do
 		e[c] = dataset[c]
 	end
-	for _, f in ipairs(info.process_prefab) do
+	for _, f in ipairs(template.process_prefab) do
 		f(e)
 	end
 	return {
-		policy = info,
+		template = template,
 		dataset = e,
 		action = action or {},
 	}
@@ -24,9 +31,7 @@ end
 function m:init()
 	local prefab = {}
 	for _, v in ipairs(self) do
-		prefab[#prefab+1] = v.prefab
-			and world.component "resource"(v.prefab)
-			or load_entity(v)
+		prefab[#prefab+1] = v.prefab and load_prefab(v) or load_entity(v)
 	end
 	return prefab
 end
