e7afa61e014dcde34f3ae88df1c3d2e4793ce21b
junjie020 <kitchen.gz.020@gmail.com>
2020-06-19 14:43:53 +0800

    print when not visible_set

--- packages/objcontroller/pickup_system.lua
+++ packages/objcontroller/pickup_system.lua
@@ -366,18 +366,22 @@ end
 function pickup_sys:pickup()
 	local pickupentity = world:singleton_entity "pickup"
 
-	if pickupentity.visible and has_any_visible_set(pickupentity.primitive_filter.result) then
-		local pickupcomp = pickupentity.pickup
-		local nextstep = pickupcomp.nextstep
-		if nextstep == "blit" then
-			local fb = fbmgr.get(pickupentity.render_target.fb_idx)
-			local rb = fbmgr.get_rb(fb[1])
-			blit(pickupcomp.blit_buffer, rb)
-		elseif nextstep	== "select_obj" then
-			select_obj(pickupcomp,pickupcomp.blit_buffer, pickupentity.render_target.viewport.rect)
-			enable_pickup(false)
-		end
+	if pickupentity.visible then 
+		if has_any_visible_set(pickupentity.primitive_filter.result) then
+			local pickupcomp = pickupentity.pickup
+			local nextstep = pickupcomp.nextstep
+			if nextstep == "blit" then
+				local fb = fbmgr.get(pickupentity.render_target.fb_idx)
+				local rb = fbmgr.get_rb(fb[1])
+				blit(pickupcomp.blit_buffer, rb)
+			elseif nextstep	== "select_obj" then
+				select_obj(pickupcomp,pickupcomp.blit_buffer, pickupentity.render_target.viewport.rect)
+				enable_pickup(false)
+			end
 
-		check_next_step(pickupcomp)
+			check_next_step(pickupcomp)
+		else
+			print("not found any eid")
+		end
 	end
 end
