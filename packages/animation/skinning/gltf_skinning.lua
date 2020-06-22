f8266e46fe3153047aee07038a6dc3e5dde29ca9
junjie020 <kitchen.gz.020@gmail.com>
2020-06-22 18:01:57 +0800

    rewrite cpu skinning for gltf mesh

--- packages/animation/skinning/gltf_skinning.lua
+++ packages/animation/skinning/gltf_skinning.lua
@@ -16,14 +16,6 @@ end
 
 local mesh_skinning_transform = ecs.transform "mesh_skinning"
 
-local function find_elem(namecode, layout_elems)
-	for _, elem in ipairs(layout_elems) do
-		if utf8.codepoint(elem:sub(1, 1)) == namecode then
-			return elem
-		end
-	end
-end
-
 local function create_node(elem, offset, layout_stride, pointer)
 	local elemsize = declmgr.elem_size(elem)
 	return {
@@ -33,83 +25,81 @@ local function create_node(elem, offset, layout_stride, pointer)
 	}, offset + elemsize
 end
 
-local name_mapper = {
-	[utf8.codepoint 'p'] = declmgr.name_mapper['p'],
-	[utf8.codepoint 'n'] = declmgr.name_mapper['n'],
-	[utf8.codepoint 'T'] = declmgr.name_mapper['T'],
-	[utf8.codepoint 'i'] = declmgr.name_mapper['i'],
-	[utf8.codepoint 'w'] = declmgr.name_mapper['w'],
-	[utf8.codepoint 'c'] = declmgr.name_mapper['c'],
-	[utf8.codepoint 't'] = declmgr.name_mapper['t'],
-}
-
-local function layout_desc(elem_prefixs, layout_elems, layout_stride, pointer, offset)
-	local desc = {}
+local function layout_desc(desc, declname, layout_stride, pointer, offset)
 	offset = offset or 1
-
-	for _, p in utf8.codes(elem_prefixs) do
-		local elem = find_elem(p, layout_elems)
-		if elem then
-			local name = name_mapper[p]
-			desc[name], offset = create_node(elem, offset, layout_stride, pointer)
-		end
+	for elem in declname:gmatch "%w+" do
+		local name = declmgr.name_mapper[elem:sub(1, 1)]
+		desc[name], offset = create_node(elem, offset, layout_stride, pointer)
 	end
-	return desc
 end
 
-local function need_dynamic_buffer(declname)
-	return declname:match "p....." ~= nil
+local function create_dynamic_buffer(buffer)
+	local num_bytes = buffer.memory[3]
+	return {
+		memory = buffer.memory,
+		handle = bgfx.create_dynamic_vertex_buffer(num_bytes, declmgr.get(buffer.declname).handle),
+		declname = buffer.declname,
+		updatedata = animodule.new_aligned_memory(num_bytes, 4),
+	}
+end
+
+local function create_job(pnT_buffer, wi_buffer)
+	local inputdesc = {}
+	local stride = declmgr.layout_stride(pnT_buffer.declname)
+	layout_desc(inputdesc, pnT_buffer.declname, stride, pnT_buffer.memory[1], pnT_buffer.memory[2])
+	layout_desc(inputdesc, wi_buffer.declname, declmgr.layout_stride(wi_buffer.declname), wi_buffer.memory[1], wi_buffer.memory[2])
+
+	local updatedata = pnT_buffer.updatedata
+	local outputdesc = {}
+	layout_desc(outputdesc, pnT_buffer.declname, stride, updatedata:pointer())
+	local buffersize = pnT_buffer.memory[3]
+	return {
+		hwbuffer_handle = pnT_buffer.handle,
+		updatedata = updatedata,
+		buffersize = buffersize,
+		parts = {
+			{
+				inputdesc = inputdesc,
+				outputdesc = outputdesc,
+				num = buffersize / stride,
+				layout_stride = stride,
+				influences_count = 4,
+			}
+		}
+	}
 end
 
 local function build_cpu_skinning_jobs(e, skinning)
+	local m = e.mesh
+	if #m.vb < 2 then
+		error(("invalid mesh for cpu skinning, vb should include at least 2 vertex buffer, one for position/normal/tangent, another for weight/indices: %d"):format(#m.vb))
+	end
+
+	for l in m.vb[1].declname:gmatch "%w+" do
+		local t = l:sub(1, 1)
+		assert(t == 'p' or t == 'n' or t == 'T')
+	end
+
+	for l in m.vb[2].declname:gmatch "%w+" do
+		local t = l:sub(1, 1)
+		assert(t == 'w' or t == 'i')
+	end
+
+	local pnT_buffer = create_dynamic_buffer(m.vb[1])
 	local jobs = {}
 	skinning.jobs = jobs
-	local m = e.mesh
-	local newmesh = {
+	jobs[#jobs+1] = create_job(pnT_buffer, m.vb[2])
+	e.mesh = {
 		ib = m.ib,
-		vb = {}
+		vb = {
+			start=m.vb.start,
+			num=m.vb.num,
+			handles={
+				pnT_buffer.handle,
+				m.vb[3] and m.vb[3].handle or nil,
+			},
+		}
 	}
-
-	local vb = newmesh.vb
-	for idx, v in ipairs(m.vb) do
-		local declname = v.declname
-		if need_dynamic_buffer(declname) then
-			local start_bytes = v.memory[2]
-			local num_bytes   = v.memory[3]
-
-			local updatedata = animodule.new_aligned_memory(num_bytes, 4)
-			local vbhandle = bgfx.create_dynamic_vertex_buffer(num_bytes, declmgr.get(declname).handle)
-
-			vb[idx] = {
-				memory = v.memory,
-				handle=vbhandle,
-			}
-
-			local outptr = updatedata:pointer()
-			local layout_stride = declmgr.get(declname).stride
-			local layout_elems = {}
-			for elem in declname:gmatch "%w+" do
-				layout_elems[#layout_elems+1] = elem
-			end
-
-			jobs[#jobs+1] = {
-				hwbuffer_handle = vbhandle,
-				updatedata = updatedata,
-				buffersize = num_bytes,
-				parts = {
-					{
-						inputdesc = layout_desc("pnTwi", layout_elems, layout_stride, v.memory[1], start_bytes),
-						outputdesc = layout_desc("pnT", layout_elems, layout_stride, outptr),
-						num = num_bytes / layout_stride,
-						layout_stride = layout_stride,
-						influences_count = 4,
-					}
-				}
-			}
-		else
-			vb[idx] = v
-		end
-	end
 end
 
 function mesh_skinning_transform.process_entity(e)
@@ -124,4 +114,13 @@ function mesh_skinning_transform.process_entity(e)
 	if e.skinning_type == "CPU" then
 		build_cpu_skinning_jobs(e, skinning)
 	end
+end
+
+
+local sknning_render = ecs.transform "skinning_rendermesh"
+function sknning_render.process_entity(e)
+	local m = e.mesh
+	local rc = e._rendercache
+	rc.vb = m.vb
+	rc.ib = m.ib
 end
