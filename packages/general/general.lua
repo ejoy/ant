cbdcca7fe9c9b29494e861498062155065321229
actboy168 <actboy168@gmail.com>
2020-06-24 15:10:16 +0800

    add action `import`

--- packages/general/general.lua
+++ packages/general/general.lua
@@ -26,3 +26,10 @@ local m = ecs.action "name"
 function m.init(prefab, i, value)
     prefab[value] = prefab[i]
 end
+
+local m = ecs.action "import"
+function m.init(prefab, i, value)
+    local name = value[1]
+    local key = value[2]
+    prefab[name] = assert(prefab[i])[key]
+end
