0c1af6c6f7e72506ee860866eaa6f14e1fdc0b33
junjie020 <kitchen.gz.020@gmail.com>
2020-07-02 10:52:40 +0800

    fix light error

--- packages/render/light/light.lua
+++ packages/render/light/light.lua
@@ -2,6 +2,12 @@ local ecs = ...
 local world = ecs.world
 
 local math3d = require "math3d"
+local mathpkg = import_package "ant.math"
+local mc = mathpkg.constant
+
+local ies = world:interface "ant.scene|ientity_state"
+local imaterial = world:interface "ant.asset|imaterial"
+local iom = world:interface "ant.objcontroller|obj_motion"
 
 local lt = ecs.transform "light_transform"
 function lt.process_entity(e)
@@ -98,6 +104,102 @@ function ilight.active_directional_light(eid)
 	local e = world[eid]
 	assert(e.light_type == "directional")
 	active_dl = eid
+end
+
+function ilight.create_light_direction_arrow(eid, data)
+	--[[
+		cylinde & cone
+		1. center in (0, 0, 0, 1)
+		2. size is 2
+		3. pointer to (0, 1, 0)
+
+		we need to:
+		1. rotate arrow, make it rotate to (0, 0, 1)
+		2. scale cylinder as it match cylinder_cone_ratio
+		3. scale cylinder radius
+	]]
+
+	local light_rot = iom.get_rotation(eid)
+	local local_rotator = math3d.quaternion{math.rad(90), 0, 0}
+	local srt = {s=data.scale, r=math3d.mul(light_rot, local_rotator), t=iom.get_position(eid)}
+	local arroweid = world:create_entity{
+		policy = {
+			"ant.general|name",
+			"ant.scene|transform_policy",
+		},
+		data = {
+			transform = srt,
+			scene_entity = true,
+			name = "directional light arrow",
+		},
+	}
+
+	local cone_rawlen<const> = 2
+	local cone_raw_halflen = cone_rawlen * 0.5
+	local cylinder_rawlen = cone_rawlen
+	local cylinder_len = cone_rawlen * data.cylinder_cone_ratio
+	local cylinder_halflen = cylinder_len * 0.5
+	local cylinder_scaleY = cylinder_len / cylinder_rawlen
+
+	local cylinder_radius = data.cylinder_rawradius or 0.65
+
+	local cone_raw_centerpos = mc.ZERO_PT
+	local cone_centerpos = math3d.add(math3d.add({0, cylinder_halflen, 0, 1}, cone_raw_centerpos), {0, cone_raw_halflen, 0, 1})
+
+	local cylinder_bottom_pos = math3d.vector(0, -cylinder_halflen, 0, 1)
+	local cone_top_pos = math3d.add(cone_centerpos, {0, cone_raw_halflen, 0, 1})
+
+	local arrow_center = math3d.mul(0.5, math3d.add(cylinder_bottom_pos, cone_top_pos))
+
+	local cylinder_raw_centerpos = mc.ZERO_PT
+	local cylinder_offset = math3d.sub(cylinder_raw_centerpos, arrow_center)
+
+	local cone_offset = math3d.sub(cone_centerpos, arrow_center)
+
+	local cylindereid = world:create_entity{
+		policy = {
+			"ant.render|render",
+			"ant.general|name",
+			"ant.scene|hierarchy_policy",
+		},
+		data = {
+			scene_entity = true,
+			state = ies.create_state "visible",
+			transform =  {
+				s = math3d.ref(math3d.mul(100, math3d.vector(cylinder_radius, cylinder_scaleY, cylinder_radius))),
+				t = math3d.ref(cylinder_offset),
+			},
+			material = world.component "resource" "/pkg/ant.resources/materials/singlecolor.material",
+			mesh = world.component "resource" '/pkg/ant.resources.binary/meshes/base/cylinder.glb|meshes/pCylinder1_P1.meshbin',
+			name = "arrow.cylinder",
+		},
+		action = {
+            mount = arroweid,
+        }
+	}
+
+	imaterial.set_property(cylindereid, "u_color", {1, 0, 0, 1})
+
+	local coneeid = world:create_entity{
+		policy = {
+			"ant.render|render",
+			"ant.general|name",
+			"ant.scene|hierarchy_policy",
+		},
+		data = {
+			scene_entity = true,
+			state = ies.create_state "visible",
+			transform =  {s=100, t=cone_offset},
+			material = world.component "resource" "/pkg/ant.resources/materials/singlecolor.material",
+			mesh = world.component "resource" '/pkg/ant.resources.binary/meshes/base/cone.glb|meshes/pCone1_P1.meshbin',
+			name = "arrow.cone"
+		},
+		action = {
+            mount = arroweid,
+		}
+	}
+
+	imaterial.set_property(coneeid, "u_color", {1, 0, 0, 1})
 end
 
 local mdl = ecs.action "main_directional_light"
