da5bfdad699e755667a293196cc5db2b1e3e9271
actboy168 <actboy168@gmail.com>
2020-07-09 11:23:42 +0800

    Fixes bug

--- engine/editor/init_cpath.lua
+++ engine/editor/init_cpath.lua
@@ -1,9 +1,11 @@
-local function getcpath()
-	local i = 0
-	while arg[i] ~= nil do
-		i = i - 1
-	end
-	local dir = arg[i + 1]:match("(.+)[/\\][%w_.-]+$")
-	return ("%s/?.dll"):format(dir)
+local path_sep = package.config:sub(1,1) == "\\" and ";" or ":"
+if package.cpath:match(path_sep) then
+	package.cpath = (function ()
+		local i = 0
+		while arg[i] ~= nil do
+			i = i - 1
+		end
+		local dir = arg[i + 1]:match("(.+)[/\\][%w_.-]+$")
+		return ("%s/?.dll"):format(dir)
+	end)()
 end
-package.cpath = getcpath()
