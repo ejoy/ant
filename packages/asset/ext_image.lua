604fb2babfe0d1ae43fbb50cc817cd54d4b2c2d5
junjie020 <kitchen.gz.020@gmail.com>
2020-07-23 15:28:10 +0800

    make ext_image support compile png

--- packages/asset/ext_image.lua
+++ packages/asset/ext_image.lua
@@ -1,7 +1,11 @@
 local img = require "image"
 local fs = require "filesystem"
 return {
-    loader = function (filename)
+    loader = function (filename, compile)
+        if compile then
+            local texloader = require "ext_texture"
+            return texloader.loader(filename)
+        end
         local f = fs.open(fs.path(filename), "rb")
         local c = f:read "a"
         f:close()
