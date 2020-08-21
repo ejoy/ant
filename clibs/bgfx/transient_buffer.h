c2adeeadd35fdfb7f5dfb1f977ba6096dca308e4
Cloud Wu <cloudwu@gmail.com>
2020-08-21 10:04:57 +0800

    transient_buffer.h

---
+++ clibs/bgfx/transient_buffer.h
@@ -0,0 +1,16 @@
+#ifndef transient_buffer_h
+#define transient_buffer_h
+
+#include <bgfx/c99/bgfx.h>
+
+struct transient_buffer {
+	bgfx_transient_vertex_buffer_t tvb;
+	bgfx_transient_index_buffer_t tib;
+	int cap_v;
+	int cap_i;
+	char format[1];
+};
+
+typedef int (*lua_TBFunction)(lua_State *L, struct transient_buffer *tb);
+
+#endif
