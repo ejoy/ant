1c0e4714b09148d65a290619a35eac978c6580f7
actboy168 <actboy168@gmail.com>
2020-09-03 16:13:39 +0800

    editor thread support debugger

--- engine/bootstrap.lua
+++ engine/bootstrap.lua
@@ -4,13 +4,7 @@ if __ANT_RUNTIME__ then
     require "runtime.vfs"
     require "runtime.errlog"
 else
-    local thread = require "thread"
-    if thread.id == 0 then
-        thread.newchannel "INITTHREAD"
-        require "editor.init_cpath"
-    else
-        arg = thread.channel_consume "INITTHREAD"()
-    end
+    require "editor.init_cpath"
     require "common.thread"
     require "editor.vfs"
     require "editor.log"
--- engine/editor/thread.lua
+++ engine/editor/thread.lua
@@ -1,14 +1,30 @@
 local thread = require "thread"
 
+if thread.id == 0 then
+	thread.newchannel "INITTHREAD"
+	local registry = debug.getregistry()
+	local debugger = registry["lua-debug"]
+	if debugger then
+		registry.DEBUG_ATTACH = debugger.attach_thread
+	end
+end
+
 local function createThread(name, code)
 	if code == nil then
 		code = name
 		name = "<thread>"
 	end
-	thread.channel_produce "INITTHREAD"(arg)
+	thread.channel_produce "INITTHREAD"(arg, debug.getregistry().DEBUG_ATTACH)
 	return thread.thread(([=[
 --%s
 package.cpath = %q
+local thread = require "thread"
+local attach
+arg, attach = thread.channel_consume "INITTHREAD"()
+if attach then
+	debug.getregistry().DEBUG_ATTACH = attach
+	assert(load(attach, "=(BOOTSTRAP)"))()
+end
 %s]=]):format(name, package.cpath, code))
 end
 
