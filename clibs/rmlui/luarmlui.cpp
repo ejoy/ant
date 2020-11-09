877600adbb5e2d73409c190863bc71a262717e46
actboy168 <actboy168@gmail.com>
2020-11-09 17:43:57 +0800

    Remove unused code

--- clibs/rmlui/luarmlui.cpp
+++ clibs/rmlui/luarmlui.cpp
@@ -105,87 +105,6 @@ lrmlui_frame(lua_State *L){
     return 0;
 }
 
-// RML Context
-
-struct rcontext {
-	Rml::Context* ctx;
-};
-
-static int
-lrelease_context(lua_State *L) {
-	struct rcontext *R = (struct rcontext *)lua_touserdata(L, 1);
-	if (R->ctx == NULL)
-		return 0;
-	Rml::RemoveContext(R->ctx->GetName());
-	R->ctx = NULL;
-	return 0;
-}
-
-static Rml::Context *
-get_context(lua_State *L) {
-	struct rcontext *R = (struct rcontext *)lua_touserdata(L, 1);
-	if (R == NULL || R->ctx == NULL) {
-		luaL_error(L, "Invalid Rml Context");
-	}
-	return R->ctx;
-}
-
-static int
-lctx_load_document(lua_State *L) {
-	const char * path = luaL_checkstring(L, 2);
-	Rml::ElementDocument * doc = get_context(L)->LoadDocument(path);
-	if (doc == NULL) {
-		return 0;
-	}
-	// todo : gen document ud
-	doc->Show();
-	lua_pushboolean(L, 1);
-	return 1;
-}
-
-static int
-lctx_update(lua_State *L) {
-	get_context(L)->Update();
-
-	return 0;
-}
-
-static int
-lctx_render(lua_State *L) {
-	get_context(L)->Render();
-	return 0;
-}
-
-static int
-lcreate_context(lua_State *L) {
-	const char * name = luaL_checkstring(L, 1);
-	int w = luaL_checkinteger(L, 2);
-	int h = luaL_checkinteger(L, 3);
-	struct rcontext * R = (struct rcontext *)lua_newuserdatauv(L, sizeof(*R), 0);
-	R->ctx = NULL;
-	if (luaL_newmetatable(L, RMLCONTEXT)) {
-		luaL_Reg lib[] = {
-			{ "__gc", lrelease_context },
-			{ "__index", NULL },
-			{ "LoadDocument", lctx_load_document },
-			{ "Update", lctx_update },
-			{ "Render", lctx_render },
-			{ NULL, NULL },
-		};
-		luaL_setfuncs(L, lib, 0);
-		lua_pushvalue(L, -1);
-		lua_setfield(L, -2, "__index");
-	}
-	lua_setmetatable(L, -2);
-	Rml::Context * ctx = Rml::CreateContext(name, Rml::Vector2i(w,h));
-	if (ctx == NULL) {
-		return luaL_error(L, "Init Rml context failed");
-	}
-	R->ctx = ctx;
-
-	return 1;
-}
-
 extern "C" {
 LUAMOD_API int
 luaopen_rmlui(lua_State* L) {
@@ -196,8 +115,6 @@ luaopen_rmlui(lua_State* L) {
         { "preload_file", lrmlui_preload_file },
         { "update",     lrmlui_update},
         { "frame",      lrmlui_frame},
-
-		{ "CreateContext", lcreate_context },
         { nullptr, nullptr },
     };
     luaL_newlib(L, l);
