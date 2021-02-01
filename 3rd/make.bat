745e1c3ba4042fee20bb270281c3b56cda3875a1
junjie020 <kitchen.gz.020@gmail.com>
2021-02-01 10:30:04 +0800

    make defalut mode as Debug when call make.bat without argument and call make init

--- 3rd/make.bat
+++ 3rd/make.bat
@@ -1,14 +1,19 @@
+if "%1" == "" (
+    set BUILD_MODE=Debug
+    call make init PLAT=msvc MODE=%BUILD_MODE%
+)
+
 call scripts\msvc.bat
 
-msbuild build\ozz-animation\msvc\%1\ozz.sln             /m /v:m /t:build /p:Configuration="%1",Platform="x64"
-msbuild build\reactphysics3d\msvc\%1\REACTPHYSICS3D.sln /m /v:m /t:build /p:Configuration="%1",Platform="x64"
-msbuild bgfx\.build\projects\vs2019\bgfx.sln            /m /v:m /t:build /p:Configuration="%1",Platform="x64"
+msbuild build\ozz-animation\msvc\%BUILD_MODE%\ozz.sln             /m /v:m /t:build /p:Configuration="%BUILD_MODE%",Platform="x64"
+msbuild build\reactphysics3d\msvc\%BUILD_MODE%\REACTPHYSICS3D.sln /m /v:m /t:build /p:Configuration="%BUILD_MODE%",Platform="x64"
+msbuild bgfx\.build\projects\vs2019\bgfx.sln            /m /v:m /t:build /p:Configuration="%BUILD_MODE%",Platform="x64"
 
-copy /B /Y bgfx\.build\win64_vs2019\bin\texturec%1.exe ..\bin\msvc\%1\texturec.exe
-copy /B /Y bgfx\.build\win64_vs2019\bin\shaderc%1.exe ..\bin\msvc\%1\shaderc.exe
-copy /B /Y bgfx\.build\win64_vs2019\bin\bgfx-shared-lib%1.dll ..\bin\msvc\%1\bgfx-core.dll
+copy /B /Y bgfx\.build\win64_vs2019\bin\texturec%BUILD_MODE%.exe ..\bin\msvc\%BUILD_MODE%\texturec.exe
+copy /B /Y bgfx\.build\win64_vs2019\bin\shaderc%BUILD_MODE%.exe ..\bin\msvc\%BUILD_MODE%\shaderc.exe
+copy /B /Y bgfx\.build\win64_vs2019\bin\bgfx-shared-lib%BUILD_MODE%.dll ..\bin\msvc\%BUILD_MODE%\bgfx-core.dll
 copy /B /Y bgfx\src\bgfx_shader.sh ..\packages\resources\shaders\bgfx_shader.sh
 copy /B /Y bgfx\src\bgfx_compute.sh ..\packages\resources\shaders\bgfx_compute.sh
 copy /B /Y bgfx\examples\common\common.sh ..\packages\resources\shaders\common.sh
 copy /B /Y bgfx\examples\common\shaderlib.sh ..\packages\resources\shaders\shaderlib.sh
-copy /B /Y build\ozz-animation\msvc\%1\src\animation\offline\gltf\gltf2ozz.exe ..\bin\msvc\%1\gltf2ozz.exe
+copy /B /Y build\ozz-animation\msvc\%BUILD_MODE%\src\animation\offline\gltf\gltf2ozz.exe ..\bin\msvc\%BUILD_MODE%\gltf2ozz.exe
