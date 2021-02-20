77ca055e47f8276911d35bfc63e92dfb5f846350
junjie020 <kitchen.gz.020@gmail.com>
2021-02-20 17:58:41 +0800

    fix light buffer not bind to bgfx

--- packages/render/light/light.lua
+++ packages/render/light/light.lua
@@ -278,7 +278,7 @@ function ilight.update_properties(system_properties)
 		local n = #lights * 4
 		
 		if light_buffer == nil then
-			light_buffer = bgfx.create_dynamic_vertex_buffer(n, declmgr.get "t40".handle, "a")
+			light_buffer = bgfx.create_dynamic_vertex_buffer(n, declmgr.get "t40".handle, "ra")
 		end
 		bgfx.update(light_buffer, 0, bgfx.memory_buffer(table.concat(lights, "")))
 		system_properties["u_light_count"].v = {#lights, 0, 0, 0}
