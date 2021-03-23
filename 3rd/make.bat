2b74f786d2cdd2825b0c821f44e28b2a7edb3ff1
junjie020 <kitchen.gz.020@gmail.com>
2021-03-23 10:26:27 +0800

    add gltf2ozz tool

--- 3rd/make.bat
+++ 3rd/make.bat
@@ -6,6 +6,12 @@ if "%1" == "" (
     set BUILD_MODE=%1
 )
 
+if "%2" == "MAKE_OZZ_GLTF_TOOL" (
+    set OZZ_GLTF_TOOL=build\ozz-animation\msvc\%BUILD_MODE%\src\animation\offline\gltf\gltf2ozz.exe
+) else (
+    set OZZ_GLTF_TOOL=tools\msvc\gltf2ozz.exe
+)
+
 @echo on
 
 call scripts\msvc.bat
@@ -21,4 +27,4 @@ copy /B /Y bgfx\src\bgfx_shader.sh ..\packages\resources\shaders\bgfx_shader.sh
 copy /B /Y bgfx\src\bgfx_compute.sh ..\packages\resources\shaders\bgfx_compute.sh
 copy /B /Y bgfx\examples\common\common.sh ..\packages\resources\shaders\common.sh
 copy /B /Y bgfx\examples\common\shaderlib.sh ..\packages\resources\shaders\shaderlib.sh
-copy /B /Y build\ozz-animation\msvc\%BUILD_MODE%\src\animation\offline\gltf\gltf2ozz.exe ..\bin\msvc\%BUILD_MODE%\gltf2ozz.exe
+copy /B /Y %OZZ_GLTF_TOOL% ..\bin\msvc\%BUILD_MODE%\gltf2ozz.exe
