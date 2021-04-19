5c757d5dbc22c487c3be720c8e76ac1ce45b3504
chenmou.cm <chenmou.cm@alibaba-inc.com>
2021-04-19 18:00:48 +0800

    fixed camea bind bug

--- test/simple/init_system.lua
+++ test/simple/init_system.lua
@@ -2,6 +2,7 @@ local ecs = ...
 local world = ecs.world
 local m = ecs.system 'init_system'
 local irq = world:interface "ant.render|irenderqueue"
+local math3d = require "math3d"
 function m:init()
     irq.set_view_clear_color(world:singleton_entity_id "main_queue", 0)
 
@@ -9,7 +10,8 @@ function m:init()
     -- local prefab = world:prefab_instance "res/Fox.glb|mesh.prefab"
     -- -- prefab_event(prefab, event, tag, param)
     -- world:prefab_event(prefab, "autoplay", "fox", "Survey")
-
+    -- local camera_prefab = world:instance "res/camera.prefab"
+    local camera_prefab = world:prefab_instance "res/camera.prefab"
     local prefab = world:prefab_instance "res/female.prefab"
     --[[
         autoplay : auto play animation
@@ -24,6 +26,9 @@ function m:init()
         get_rotation
         get_scale
     --]]
+    local cam_pos = world:prefab_event(camera_prefab, "get_position", "camera")
+    local tpos = math3d.totable(cam_pos)
+    --world:prefab_event(camera_prefab, "set_position", "camera", {1, 1, 1})
     local tag = "body"
     world:prefab_event(prefab, "autoplay", tag, "walking")
     -- world:prefab_event(prefab, "set_events", tag, "walking", "res/walking.event")
@@ -32,7 +37,7 @@ function m:init()
     -- world:prefab_event(prefab, "time", tag, 0.08)
     -- local duration = world:prefab_event(prefab, "duration", tag)
     -- world:prefab_event(prefab, "set_position", tag, {1, 0, 0})
-    -- world:prefab_event(prefab, "get_position", tag)
+    local female_pos = world:prefab_event(prefab, "get_position", tag)
     -- world:prefab_event(prefab, "set_rotation", tag, {45, 0, 0})
     world:prefab_event(prefab, "set_scale", tag, {0.5, 0.5, 0.5})
     -- test clip and group
