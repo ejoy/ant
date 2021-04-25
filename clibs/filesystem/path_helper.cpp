93bb2be2f9c4bafdddb555a8e8edcc8ce0b11f25
actboy168 <actboy168@gmail.com>
2021-04-25 17:48:59 +0800

    Updated filesystem

--- clibs/filesystem/path_helper.cpp
+++ clibs/filesystem/path_helper.cpp
@@ -10,61 +10,121 @@
 extern "C" IMAGE_DOS_HEADER __ImageBase;
 
 namespace ant::path_helper {
-    auto dll_path(void* module_handle)->nonstd::expected<fs::path, std::exception> {
+    fs::path dll_path(void* module_handle) {
         wchar_t buffer[MAX_PATH];
         DWORD path_len = ::GetModuleFileNameW((HMODULE)module_handle, buffer, _countof(buffer));
         if (path_len == 0) {
-            return nonstd::make_unexpected(make_syserror("GetModuleFileNameW"));
+            throw make_syserror("GetModuleFileNameW");
         }
         if (path_len < _countof(buffer)) {
-            return std::move(fs::path(buffer, buffer + path_len));
+            return fs::path(buffer, buffer + path_len);
         }
         for (DWORD buf_len = 0x200; buf_len <= 0x10000; buf_len <<= 1) {
             std::dynarray<wchar_t> buf(buf_len);
-            DWORD path_len = ::GetModuleFileNameW((HMODULE)module_handle, buf.data(), buf_len);
+            path_len = ::GetModuleFileNameW((HMODULE)module_handle, buf.data(), buf_len);
             if (path_len == 0) {
-                return nonstd::make_unexpected(make_syserror("GetModuleFileNameW"));
+                throw make_syserror("GetModuleFileNameW");
             }
             if (path_len < _countof(buffer)) {
-                return std::move(fs::path(buf.data(), buf.data() + path_len));
+                return fs::path(buf.data(), buf.data() + path_len);
             }
         }
-        return nonstd::make_unexpected(std::runtime_error("::GetModuleFileNameW return too long."));
+        throw std::runtime_error("::GetModuleFileNameW return too long.");
     }
 
-    auto exe_path()->nonstd::expected<fs::path, std::exception> {
+    fs::path exe_path() {
         return dll_path(NULL);
     }
 
-    auto dll_path()->nonstd::expected<fs::path, std::exception> {
+    fs::path dll_path() {
         return dll_path(reinterpret_cast<void*>(&__ImageBase));
     }
 }
 
 #else
 
-#include <dlfcn.h>
-#include <lua.hpp>
+#if defined(__APPLE__)
+
+#include <mach-o/dyld.h>
 
 namespace ant::path_helper {
-    auto dll_path(void* module_handle)->nonstd::expected<fs::path, std::exception> {
+    fs::path exe_path() {
+        uint32_t path_len = 0;
+        _NSGetExecutablePath(0, &path_len);
+        if (path_len <= 1) {
+            throw std::runtime_error("_NSGetExecutablePath failed.");
+        }
+        dynarray<char> buf(path_len);
+        int rv = _NSGetExecutablePath(buf.data(), &path_len);
+        if (rv != 0) {
+            throw std::runtime_error("_NSGetExecutablePath failed.");
+        }
+        return fs::path(buf.data(), buf.data() + path_len - 1);
+    }
+}
+
+#else
+
+#include <unistd.h>
+
+namespace ant::path_helper {
+    fs::path exe_path() {
+        char buffer[0x100];
+        ssize_t path_len = ::readlink("/proc/self/exe", buffer, sizeof(buffer) - 1);
+        if (path_len < 0) {
+            throw make_syserror("readlink");
+        }
+        if (path_len < (ssize_t)sizeof(buffer) - 1) {
+            return fs::path(buffer, buffer + path_len);
+        }
+        for (size_t buf_len = 0x200; buf_len <= 0x10000; buf_len <<= 1) {
+            dynarray<char> buf(buf_len);
+            ssize_t path_len = ::readlink("/proc/self/exe", buf.data(), buf_len - 1);
+            if (path_len == 0) {
+                throw make_syserror("readlink");
+            }
+            if (path_len < (ssize_t)sizeof(buffer) - 1) {
+                return fs::path(buf.data(), buf.data() + path_len);
+            }
+        }
+        throw std::runtime_error("readlink return too long.");
+    }
+}
+
+#endif
+
+#if defined(BEE_DISABLE_DLOPEN)
+
+namespace ant::path_helper {
+    fs::path dll_path(void* module_handle) {
+        throw std::runtime_error("disable dl.");
+    }
+    fs::path dll_path() {
+        return dll_path(nullptr);
+    }
+}
+
+#else
+
+#include <dlfcn.h>
+
+namespace ant::path_helper {
+    fs::path dll_path(void* module_handle) {
         ::Dl_info dl_info;
         dl_info.dli_fname = 0;
         int const ret = ::dladdr(module_handle, &dl_info);
         if (0 != ret && dl_info.dli_fname != NULL) {
             return fs::absolute(dl_info.dli_fname).lexically_normal();
         }
-        return nonstd::make_unexpected(std::runtime_error("::dladdr failed."));
+        throw std::runtime_error("::dladdr failed.");
     }
 
-    auto exe_path()->nonstd::expected<fs::path, std::exception> {
-        return dll_path((void*)&lua_newstate);
-    }
-
-    auto dll_path()->nonstd::expected<fs::path, std::exception> {
+    fs::path dll_path() {
         return dll_path((void*)&exe_path);
     }
 }
+
+#endif
 
 #endif
 
@@ -90,7 +150,7 @@ namespace ant::path_helper {
             ++l; ++r;
         }
         return *l == *r;
-}
+    }
 #else
     bool equal(fs::path const& lhs, fs::path const& rhs) {
         return lhs.lexically_normal() == rhs.lexically_normal();
