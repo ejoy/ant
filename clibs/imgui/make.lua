1c6bb6c53ec8d4c2717d6957394afac8fc6b1782
actboy168 <actboy168@gmail.com>
2021-05-11 16:01:47 +0800

    fixes bug

--- clibs/imgui/make.lua
+++ clibs/imgui/make.lua
@@ -2,30 +2,7 @@ local lm = require "luamake"
 
 dofile "../common.lua"
 
-lm:source_set "imgui_core" {
-    rootdir = Ant3rd .. "imgui",
-    includes = {
-        ".",
-    },
-    sources = {
-        "imgui_draw.cpp",
-        "imgui_tables.cpp",
-        "imgui_widgets.cpp",
-        "imgui.cpp",
-    },
-    defines = {
-        "IMGUI_DISABLE_OBSOLETE_FUNCTIONS",
-        "IMGUI_DISABLE_METRICS_WINDOW",
-        "IMGUI_DISABLE_DEMO_WINDOWS",
-        "IMGUI_DISABLE_DEFAULT_ALLOCATORS",
-    },
-    windows = {
-        sources = "backends/imgui_impl_win32.cpp"
-    }
-}
-
 lm:lua_dll "imgui" {
-    deps = "imgui_core",
     includes = {
         ".",
         Ant3rd .. "imgui",
@@ -39,16 +16,25 @@ lm:lua_dll "imgui" {
         "imgui_window.cpp",
         "luaimgui_tables.cpp",
         "luaimgui.cpp",
-        "widgets/*.cpp"
+        "widgets/*.cpp",
+        Ant3rd .. "imgui/imgui_draw.cpp",
+        Ant3rd .. "imgui/imgui_tables.cpp",
+        Ant3rd .. "imgui/imgui_widgets.cpp",
+        Ant3rd .. "imgui/imgui.cpp",
     },
     defines = {
         "IMGUI_DISABLE_OBSOLETE_FUNCTIONS",
         "IMGUI_DISABLE_METRICS_WINDOW",
         "IMGUI_DISABLE_DEMO_WINDOWS",
         "IMGUI_DISABLE_DEFAULT_ALLOCATORS",
+        "IMGUI_USER_CONFIG=<imgui_config.h>",
+        "_UNICODE"
     },
     windows = {
-        sources = "win32/*.cpp",
+        sources = {
+            "win32/*.cpp",
+            Ant3rd .. "imgui/backends/imgui_impl_win32.cpp",
+        },
         defines = "UNICODE",
         links = {
             "user32",
