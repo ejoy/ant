ed26377640355130c35a349b84a642b5c1bd026e
junjie020 <kitchen.gz.020@gmail.com>
2021-05-14 17:01:52 +0800

    fix tetrahedron render

--- test/features/shadow_test.lua
+++ test/features/shadow_test.lua
@@ -32,14 +32,40 @@ local iom = world:interface "ant.objcontroller|obj_motion"
 local function create_tetrahedron_entity(r)
 	local c = (2*math.sqrt(2.0)/3.0)*r
 	local d = (1.0/3.0)*r
+	local d2= r
 	local b = c * 0.5
 	local a = (math.sqrt(6.0)/3.0)*r
+	-- local vertices = {
+	-- 	{ 0, r, 0},
+	-- 	{ 0,-d, c},
+	-- 	{-a,-d,-b},
+	-- 	{ a,-d,-b},
+	-- }
+
 	local vertices = {
-		{ 0, r, 0},
-		{ 0,-d, c},
-		{-a,-d,-b},
-		{ a,-d,-b},
+		{ 0, d, a},
+		{ 0, d,-a},
+		{ a, r, 0},
+		{-a, r, 0},
 	}
+
+	-- do
+	-- 	local e1 = math3d.mul(0.5, math3d.add(vertices[2], vertices[4]))
+	-- 	local he2 = math3d.mul(0.5, math3d.add(vertices[1], vertices[3]))
+	-- 	local ipl = world:interface "ant.render|ipolyline"
+	-- 	ipl.add_linelist({math3d.tovalue(e1), math3d.tovalue(he2)}, 1.0, {1, 1, 1, 1.0})
+	-- end
+
+	local line_indices = {
+		{0, 1}, {0, 2}, {0, 3}, 
+		{1, 2}, {2, 3}, {3, 1}
+	}
+
+	local lengths = {}
+	for _, ii in ipairs(line_indices) do
+		local v1, v2 = vertices[ii[1]+1], vertices[ii[2]+1]
+		lengths[#lengths+1] = math3d.length(v1, v2)
+	end
 
 	local colors = {
 		0xff0000ff, --red
@@ -49,16 +75,16 @@ local function create_tetrahedron_entity(r)
 	}
 
 	local indices = {
-		{0, 1, 4}, --red
-		{0, 3, 1}, --green
-		{0, 4, 3}, --blue
-		{1, 3, 4}, --yellow
+		{0, 2, 1}, --red
+		{0, 1, 3}, --green
+		{0, 3, 2}, --blue
+		{1, 2, 3}, --yellow
 	}
 	local vb = {}
 	for i=1, 4 do
 		local cc= colors[i]
 		for _, ii in ipairs(indices[i]) do
-			local v = vertices[ii]
+			local v = vertices[ii+1]
 			for j=1, 3 do
 				vb[#vb+1] = v[j]
 			end
@@ -66,11 +92,13 @@ local function create_tetrahedron_entity(r)
 		end
 	end
 
-	local m = ientity.create_mesh("p3|c40niu", vb)
-	ientity.create_simple_render_entity(nil, "/pkg/ant.resources/materials/")
+	local m = ientity.create_mesh({"p3|c40niu", vb})
+	return ientity.create_simple_render_entity("tetrahedron", "/pkg/ant.resources/materials/meshcolor.material", m)
 end
 
 function st_sys:init()
+	create_tetrahedron_entity(10)
+
 	world:create_entity {
 		policy = {
 			"ant.render|render",
