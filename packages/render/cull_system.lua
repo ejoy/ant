a0b7d7097effb90f48bc08e5346c432f39f6eb73
junjie020 <kitchen.gz.020@gmail.com>
2021-05-26 11:08:25 +0800

    add icull_primitives interface

--- packages/render/cull_system.lua
+++ packages/render/cull_system.lua
@@ -4,29 +4,33 @@ local world = ecs.world
 
 local math3d = require "math3d"
 
+local icp = ecs.interface "icull_primitive"
+function icp.cull(e)
+	local filter = e.primitive_filter
+	local vp = world[e.camera_eid]._rendercache.viewprojmat
+	local frustum_planes = math3d.frustum_planes(vp)
+
+	local results = filter.result
+	for _, resulttarget in pairs(results) do
+		local vs = resulttarget.visible_set
+		if vs then
+			local items = resulttarget.items
+			if #items > 0 then
+				vs.n = math3d.frustum_intersect_aabb_list(frustum_planes, items, vs)
+			else
+				vs.n = 0
+			end
+		end
+	end
+end
+
 local cull_sys = ecs.system "cull_system"
 
 function cull_sys:cull()
 	for _, eid in world:each "primitive_filter" do
 		local e = world[eid]
-		local filter = e.primitive_filter
-
 		if e.visible then
-			local vp = world[e.camera_eid]._rendercache.viewprojmat
-			local frustum_planes = math3d.frustum_planes(vp)
-
-			local results = filter.result
-			for _, resulttarget in pairs(results) do
-				local vs = resulttarget.visible_set
-				if vs then
-					local items = resulttarget.items
-					if #items > 0 then
-						vs.n = math3d.frustum_intersect_aabb_list(frustum_planes, items, vs)
-					else
-						vs.n = 0
-					end
-				end
-			end
+			icp.cull(e)
 		end
 	end
 end
