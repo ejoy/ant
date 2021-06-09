0598b3748d3e6d016d0e4bddc646ac183fce8e9a
actboy168 <actboy168@gmail.com>
2021-06-09 16:39:26 +0800

    fixes bug

--- engine/common/init_bgfx.lua
+++ engine/common/init_bgfx.lua
@@ -2,4 +2,9 @@ if debug.getregistry().BGFX_GET_INTERFACE then
     return
 end
 local interface = package.preload["bgfx_get_interface"]
-debug.getregistry().BGFX_GET_INTERFACE = interface
+if interface then
+    debug.getregistry().BGFX_GET_INTERFACE = interface
+    return
+end
+local path = assert(package.searchpath("bgfx-core", package.cpath))
+debug.getregistry().BGFX_GET_INTERFACE = assert(package.loadlib(path, "bgfx_get_interface"))
