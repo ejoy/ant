06e6d612149afd0c63bc10d3a2d5adb7e3b9ff12
actboy168 <actboy168@gmail.com>
2021-07-27 10:58:00 +0800

    remove makefile

--- 3rd/scripts/ozz-animation.bat
+++
@@ -1,5 +0,0 @@
-call scripts\msvc.bat
-
-msbuild build\ozz-animation\msvc\%1\ozz.sln      /m /v:m /t:build /p:Configuration="%1",Platform="x64"
-
-copy /B /Y build\ozz-animation\msvc\%1\src\animation\offline\gltf\gltf2ozz.exe ..\bin\msvc\%1\gltf2ozz.exe
--- clibs/rp3d/rp3d.mk
+++
@@ -1,4 +0,0 @@
-RP3DROOT = $(ANT3RD)
-RP3DINC = -I$(RP3DROOT)/reactphysics3d/include
-RP3DLIB = -L$(RP3DROOT)/build/reactphysics3d/$(PLAT)/$(MODE)
-RP3DLIB += -lreactphysics3d
