c04962fed30baabc611fccff05a63f063824750d
actboy168 <actboy168@gmail.com>
2021-08-05 11:12:20 +0800

    fixes bug

--- test/rmlui/package.lua
+++ test/rmlui/package.lua
@@ -11,6 +11,14 @@ return {
         },
         system = {
             "ant.test.rmlui|init_system",
+        },
+        policy_v2 = {
+            "ant.general|name",
+            "ant.scene|scene_object",
+            "ant.scene|scene_node",
+            "ant.render|render",
+            "ant.render|render_queue",
+            "ant.objcontroller|pickup",
         }
     }
 }
