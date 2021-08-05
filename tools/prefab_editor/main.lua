b6dcc7b36bb2ae413178c8a19046137c335eb404
chenmou.cm <chenmou.cm@alibaba-inc.com>
2021-08-05 17:05:56 +0800

    change editor init size

--- tools/prefab_editor/main.lua
+++ tools/prefab_editor/main.lua
@@ -1,3 +1,3 @@
 package.path = "engine/?.lua"
 require "bootstrap"
-import_package "ant.imgui".start("tools.prefab_editor.editor", 1920, 1080)
+import_package "ant.imgui".start("tools.prefab_editor.editor", 1400, 900)
