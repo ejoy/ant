97c069f0e5374c7cd42307b609753e20b4827703
junjie020 <kitchen.gz.020@gmail.com>
2021-08-06 10:01:24 +0800

    remove code

--- test/bake_example/shaders/build.bat
+++
@@ -1,3 +0,0 @@
-set wdir=..\..\..
-%wdir%\bin\msvc\Debug\shaderc.exe --platform windows --type fragment -p ps_5_0 -f fs_scene.sc -o mesh\fs_scene.bin --depends -i %wdir%\packages\resources\shaders
-%wdir%\bin\msvc\Debug\shaderc.exe --platform windows --type vertex -p vs_5_0 -f vs_scene.sc -o mesh\vs_scene.bin --depends -i %wdir%\packages\resources\shaders
--- test/bake_example/shaders/vs_scene.sc
+++
@@ -1,9 +0,0 @@
-$input a_position a_texcoord0;
-$output v_texcoord0;
-#include <bgfx_shader.sh>
-
-void main()
-{
-    gl_Position = mul(u_proj, mul(u_view * vec4(a_position, 1.0)));
-    v_texcoord0 = a_texcoord0;
-}
