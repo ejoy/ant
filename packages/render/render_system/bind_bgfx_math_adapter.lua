74ba87640e255a9339341b508dbc4d8c417c30ee
Cloud Wu <cloudwu@gmail.com>
2021-08-13 17:34:10 +0800

    Add setter api (bgfx binding)

--- packages/render/render_system/bind_bgfx_math_adapter.lua
+++ packages/render/render_system/bind_bgfx_math_adapter.lua
@@ -7,6 +7,7 @@ mathadapter_util.bind("bgfx", function ()
 	bgfx.set_transform = math3d_adapter.matrix(bgfx.set_transform, 1, 1)
 	bgfx.set_view_transform = math3d_adapter.matrix(bgfx.set_view_transform, 2, 2)
 	bgfx.set_uniform = math3d_adapter.variant(bgfx.set_uniform_matrix, bgfx.set_uniform_vector, 2)
+	bgfx.set_uniform_command = math3d_adapter.variant(bgfx.set_uniform_matrix_command, bgfx.set_uniform_vector_command, 2)
 	local idb = bgfx.instance_buffer_metatable()
 	idb.pack = math3d_adapter.format(idb.pack, idb.format, 3)
 	idb.__call = idb.pack
