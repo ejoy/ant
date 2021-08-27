3e0c0731eba632c70a168b16ed71dc80dd52bc11
junjie020 <kitchen.gz.020@gmail.com>
2021-08-27 22:47:52 +0800

    finish lua scene to Model

--- clibs/bake2/BakerInterface.cpp
+++ clibs/bake2/BakerInterface.cpp
@@ -3,6 +3,8 @@
 #include "Meshbaker/BakingLab/BakingLab.h"
 #include "Meshbaker/BakingLab/AppSettings.h"
 
+#include <functional>
+
 static inline uint32_t _FindDirectionalLight(const Scene *scene){
     for (uint32_t idx=0; idx<scene->lights.size(); ++idx){
         auto l = scene->lights[idx];
@@ -94,6 +96,11 @@ void Model::CreateFromScene(ID3D11Device *device, const Scene *scene, bool force
     }
 }
 
+static inline const char*
+src_ptr(const BufferData &b, size_t idx){
+    return b.data + b.offset + idx * b.stride;
+}
+
 void Mesh::InitFromSceneMesh(ID3D11Device *device, const MeshData& meshdata)
 {
     numVertices = meshdata.vertexCount;
@@ -105,120 +112,127 @@ void Mesh::InitFromSceneMesh(ID3D11Device *device, const MeshData& meshdata)
     uint32 indexSize = meshdata.indices.type == BT_Uint16 ? 2 : 4;
 
     // Figure out the vertex layout
-    uint32 currOffset = 0;
     D3D11_INPUT_ELEMENT_DESC elemDesc;
     elemDesc.InputSlot = 0;
     elemDesc.InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
     elemDesc.InstanceDataStepRate = 0;
 
-    std::vector<const BufferData*> buffers;
+    struct buffer {
+        const BufferData *b;
+        std::function<void (uint8* dst, const BufferData &b, size_t vidx)> f;
+    };
+    std::vector<buffer> buffers;
+
+    auto default_copy = [=](auto dst, auto b, size_t vidx, uint32 n){
+        assert(b.type == BT_Float);
+        auto src = src_ptr(b, vidx);
+        memcpy(dst, src, n * sizeof(float));
+    };
+
+    auto transform_pt = [](const auto &m, auto dst, auto b, size_t vidx, uint32 n){
+        assert(b.type == BT_Float);
+        assert(n == 3);
+        glm::vec3* src = (glm::vec3*)src_ptr(b, vidx);
+        glm::vec4 v = m * glm::vec4(*src, 1.0f);
+        memcpy(dst, &v.x, sizeof(float)*3);
+    };
+
+    const glm::mat3 nm = meshdata.normalmat;
+    auto transform_vec = [](const auto &m, auto dst, auto b, size_t vidx, uint32 n){
+        assert(b.type == BT_Float);
+        assert(n == 3);
+        glm::vec3 *src = (glm::vec3*)src_ptr(b, vidx);
+        auto v = m * *src;
+        memcpy(dst, &v.x, sizeof(float)*3);
+    };
 
     assert(meshdata.positions.type == BT_Float);
 
+    vertexStride = 0;
     elemDesc.Format = DXGI_FORMAT_R32G32B32_FLOAT;
-    elemDesc.AlignedByteOffset = currOffset;
+    elemDesc.AlignedByteOffset = vertexStride;
     elemDesc.SemanticName = "POSITION";
     elemDesc.SemanticIndex = 0;
     inputElements.push_back(elemDesc);
-    currOffset += 12;
-    buffers.push_back(&meshdata.positions);
+    vertexStride += 12;
+    buffers.push_back(buffer{&meshdata.positions, std::bind(transform_pt, meshdata.worldmat, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, 3)});
 
     assert(meshdata.normals.type == BT_Float);
     elemDesc.Format = DXGI_FORMAT_R32G32B32_FLOAT;
-    elemDesc.AlignedByteOffset = currOffset;
+    elemDesc.AlignedByteOffset = vertexStride;
     elemDesc.SemanticName = "NORMAL";
     elemDesc.SemanticIndex = 0;
     inputElements.push_back(elemDesc);
-    currOffset += 12;
+    vertexStride += 12;
 
-    buffers.push_back(&meshdata.normals);
-
+    buffers.push_back(buffer{&meshdata.normals, std::bind(transform_vec, nm, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, 3)});
     assert(meshdata.texcoords0.type == BT_Float);
 
     elemDesc.Format = DXGI_FORMAT_R32G32_FLOAT;
-    elemDesc.AlignedByteOffset = currOffset;
+    elemDesc.AlignedByteOffset = vertexStride;
     elemDesc.SemanticName = "TEXCOORD";
     elemDesc.SemanticIndex = 0;
     inputElements.push_back(elemDesc);
-    currOffset += 8;
-    buffers.push_back(&meshdata.texcoords0);
+    vertexStride += 8;
+    buffers.push_back(buffer{&meshdata.texcoords0, std::bind(default_copy, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, 2)});
 
     assert(meshdata.texcoords1.type == BT_Float);
     elemDesc.Format = DXGI_FORMAT_R32G32_FLOAT;
-    elemDesc.AlignedByteOffset = currOffset;
+    elemDesc.AlignedByteOffset = vertexStride;
     elemDesc.SemanticName = "TEXCOORD";
     elemDesc.SemanticIndex = 1;
     inputElements.push_back(elemDesc);
-    currOffset += 8;
-    buffers.push_back(&meshdata.texcoords1);
+    vertexStride += 8;
+    buffers.push_back(buffer{&meshdata.texcoords1, std::bind(default_copy, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, 2)});
 
-    // if(assimpMesh.HasTangentsAndBitangents())
-    // {
-    //     elemDesc.Format = DXGI_FORMAT_R32G32B32_FLOAT;
-    //     elemDesc.AlignedByteOffset = currOffset;
-    //     elemDesc.SemanticName = "TANGENT";
-    //     elemDesc.SemanticIndex = 0;
-    //     inputElements.push_back(elemDesc);
-    //     currOffset += 12;
-    //     vertexData.push_back(assimpMesh.mTangents);
+    if (meshdata.tangents.type != BT_None && meshdata.bitangents.type != BT_None){
+        elemDesc.Format = DXGI_FORMAT_R32G32B32_FLOAT;
+        elemDesc.AlignedByteOffset = vertexStride;
+        elemDesc.SemanticName = "TANGENT";
+        elemDesc.SemanticIndex = 0;
+        inputElements.push_back(elemDesc);
+        vertexStride += 12;
+        buffers.push_back(buffer{&meshdata.tangents, std::bind(transform_vec, nm, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, 3)});
 
-    //     elemDesc.Format = DXGI_FORMAT_R32G32B32_FLOAT;
-    //     elemDesc.AlignedByteOffset = currOffset;
-    //     elemDesc.SemanticName = "BITANGENT";
-    //     elemDesc.SemanticIndex = 0;
-    //     inputElements.push_back(elemDesc);
-    //     currOffset += 12;
-    //     vertexData.push_back(assimpMesh.mBitangents);
-    // }
+        elemDesc.Format = DXGI_FORMAT_R32G32B32_FLOAT;
+        elemDesc.AlignedByteOffset = vertexStride;
+        elemDesc.SemanticName = "BITANGENT";
+        elemDesc.SemanticIndex = 0;
+        inputElements.push_back(elemDesc);
+        vertexStride += 12;
+        buffers.push_back(buffer{&meshdata.bitangents, std::bind(transform_vec, nm, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, 3)});
+    }
 
-    // vertexStride = currOffset;
+    // Copy and interleave the vertex data
+    vertices.resize(vertexStride * numVertices);
+    for(uint64 vtxIdx = 0; vtxIdx < numVertices; ++vtxIdx)
+    {
+        uint8* vtxStart = &vertices[vtxIdx * vertexStride];
+        for(uint64 elemIdx = 0; elemIdx < inputElements.size(); ++elemIdx)
+        {
+            const auto &ie = inputElements[elemIdx];
 
-    // // Copy and interleave the vertex data
-    // vertices.resize(vertexStride * numVertices, 0);
-    // for(uint64 vtxIdx = 0; vtxIdx < numVertices; ++vtxIdx)
-    // {
-    //     uint8* vtxStart = &vertices[vtxIdx * vertexStride];
-    //     for(uint64 elemIdx = 0; elemIdx < inputElements.size(); ++elemIdx)
-    //     {
-    //         uint64 offset = inputElements[elemIdx].AlignedByteOffset;
-    //         uint64 elemSize = elemIdx == inputElements.size() - 1 ? vertexStride - offset :
-    //                                                                 inputElements[elemIdx + 1].AlignedByteOffset - offset;
-    //         uint8* elemStart = vtxStart + inputElements[elemIdx].AlignedByteOffset;
-    //         memcpy(elemStart, vertexData[elemIdx] + vtxIdx, elemSize);
+            auto buffer = buffers[elemIdx];
+            buffer.f(vtxStart + ie.AlignedByteOffset, *(buffer.b), vtxIdx);
+        }
+    }
 
-    //         if(vertexData[elemIdx] == assimpMesh.mBitangents)
-    //             *reinterpret_cast<Float3*>(elemStart) *= -1.0f;
-    //     }
-    // }
+    // Copy the index data
+    size_t indexSizeBytes = indexSize * numIndices;
+    indices.resize(indexSizeBytes);
+    memcpy(indices.data(), meshdata.indices.data, indexSizeBytes);
 
-    // // Copy the index data
-    // indices.resize(indexSize * numIndices, 0);
-    // const uint64 numTriangles = assimpMesh.mNumFaces;
-    // for(uint64 triIdx = 0; triIdx < numTriangles; ++triIdx)
-    // {
-    //     void* triStart = &indices[triIdx * 3 * indexSize];
-    //     const aiFace& tri = assimpMesh.mFaces[triIdx];
-    //     if(indexType == IndexType::Index32Bit)
-    //         memcpy(triStart, tri.mIndices, sizeof(uint32) * 3);
-    //     else
-    //     {
-    //         uint16* triIndices = reinterpret_cast<uint16*>(triStart);
-    //         for(uint64 i = 0; i < 3; ++i)
-    //             triIndices[i] = uint16(tri.mIndices[i]);
-    //     }
-    // }
+    if (meshdata.tangents.type == BT_None || meshdata.bitangents.type == BT_None)
+        GenerateTangentFrame();
 
-    // CreateVertexAndIndexBuffers(device);
+    CreateVertexAndIndexBuffers(device);
 
-    // const uint32 numSubsets = 1;
-    // meshParts.resize(numSubsets);
-    // for(uint32 i = 0; i < numSubsets; ++i)
-    // {
-    //     MeshPart& part = meshParts[i];
-    //     part.IndexStart = 0;
-    //     part.IndexCount = numIndices;
-    //     part.VertexStart = 0;
-    //     part.VertexCount = numVertices;
-    //     part.MaterialIdx = assimpMesh.mMaterialIndex;
-    // }
+    const uint32 numSubsets = 1;
+    meshParts.resize(numSubsets);
+    auto &mp = meshParts.back();
+    mp.IndexStart  = 0;
+    mp.IndexCount  = numIndices;
+    mp.VertexStart = 0;
+    mp.VertexCount = numVertices;
+    mp.MaterialIdx = meshdata.materialidx;
 }
