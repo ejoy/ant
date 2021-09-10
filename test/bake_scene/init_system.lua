37995b3fd7ab8ccd6dc877db57b81ff2bc439f54
junjie020 <kitchen.gz.020@gmail.com>
2021-09-10 10:03:10 +0800

    change bake scene camera postion

--- test/bake_scene/init_system.lua
+++ test/bake_scene/init_system.lua
@@ -10,14 +10,16 @@ local is = ecs.system "init_system"
 
 function is:init()
     ientity.create_grid_entity("polyline_grid", 64, 64, 1, 5)
+    --world:instance "/pkg/ant.test.bake_scene/assets/scene/1.glb|mesh.prefab"
     -- world:instance "/pkg/ant.test.bake_scene/assets/scene/box.prefab"
-    -- world:instance "/pkg/ant.test.bake_scene/assets/scene/light.prefab"
-    world:instance "/pkg/ant.test.bake_scene/assets/scene/scene.prefab"
+    world:instance "/pkg/ant.test.bake_scene/assets/scene/light.prefab"
+    world:instance "/pkg/ant.test.bake_scene/assets/scene/Fox.glb|mesh.prefab"
+    --world:instance "/pkg/ant.test.bake_scene/assets/scene/scene.prefab"
 end
 
 function is:init_world()
     local mq = w:singleton("main_queue", "camera_ref:in")
-    local eyepos<const> = math3d.vector(0.0, 5.0, -5.0)
+    local eyepos<const> = math3d.vector(0.0, 2.5, 15.0)
     local dir<const> = math3d.sub(math3d.vector(0.0, 0.0, 0.0), eyepos)
     iom.set_position(mq.camera_ref, eyepos)
     iom.set_direction(mq.camera_ref, dir)
