ecab7c53f5ac7ad1df0f8e69975c2fc5ad9df46a
actboy168 <actboy168@gmail.com>
2021-10-09 16:24:03 +0800

    update test

--- test/simple/init_system.lua
+++ test/simple/init_system.lua
@@ -6,7 +6,7 @@ local irq = ecs.import.interface "ant.render|irenderqueue"
 function m:init_world()
     irq.set_view_clear_color("main_queue", 0xff0000ff)
     world:instance "/res/scenes.prefab"
-    ecs.create_instance "/res/light_directional.prefab"
+    ecs.create_instance "/res/scenes_v2.prefab"
     local camera = ecs.require "camera"
     world:call(camera.root, "set_position", {1, 1, 1})
     camera:send "hello"
