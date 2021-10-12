57d3f310091f06608b08e6aa207a49790d29c1c8
actboy168 <actboy168@gmail.com>
2021-10-12 14:12:21 +0800

    cleanup

--- packages/render/render_system/render.lua
+++ packages/render/render_system/render.lua
@@ -2,19 +2,14 @@ local ecs = ...
 local world = ecs.world
 local w = world.w
 
-local mathpkg 		=	import_package "ant.math"
-local mc, mu 		= mathpkg.constant, mathpkg.util
 local default_comp 	= import_package "ant.general".default
 local setting		= import_package "ant.settings".setting
 
-local math3d 		= require "math3d"
 local bgfx 			= require "bgfx"
 local viewidmgr 	= require "viewid_mgr"
 local fbmgr			= require "framebuffer_mgr"
 local samplerutil	= require "sampler"
-local icamera		= ecs.import.interface "ant.camera|camera"
 
-local wmt = ecs.transform "world_matrix_transform"
 local function set_world_matrix(rc)
 	bgfx.set_transform(rc.worldmat)
 end
@@ -22,11 +17,6 @@ end
 local function set_skinning_transform(rc)
 	local sm = rc.skinning_matrices
 	bgfx.set_multi_transforms(sm:pointer(), sm:count())
-end
-
-function wmt.process_entity(e)
-	local rc = e._rendercache
-	rc.set_transform = set_world_matrix
 end
 
 local world_trans_sys = ecs.system "world_transform_system"
