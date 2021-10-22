bff2aaf2c30c2c51816aabed094b17ae53bd2603
junjie020 <kitchen.gz.020@gmail.com>
2021-10-22 15:03:00 +0800

    remove _v2 file

--- packages/scene/entity_state.lua
+++ packages/scene/entity_state.lua
@@ -73,3 +73,14 @@ function ies.create_state(namelist)
 end
 
 ies.filter_mask = filter_mask
+
+local m = ecs.system "entity_state_system"
+function m:entity_init()
+    for e in w:select "INIT state:in render_object:in" do
+        local s = e.state
+        if type(s) == "string" then
+            s = ies.filter_mask(s)
+        end
+        e.render_object.entity_state = s or 0
+    end
+end
