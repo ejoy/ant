71a42fcdf170a4cc953f855b6175c036efa9c48f
junjie020 <kitchen.gz.020@gmail.com>
2021-11-30 17:52:38 +0800

    make lsp plugin support ecs.import.interface

--- misc/lsp-plugin.lua
+++ misc/lsp-plugin.lua
@@ -9,6 +9,8 @@
 local fs = require "bee.filesystem"
 local furi = require "file-uri"
 local packages = {}
+local package_names = {}
+local interfaces = {}
 
 do
     local ws = require "workspace"
@@ -26,20 +28,93 @@ do
         if not info then
             return
         end
-        if not info.name or not info.entry then
-            return
-        end
         return info.name, info.entry
     end
-    for _, folder in ipairs(searchFolder) do
-        for path in fs.pairs(workspaceFolder / folder) do
-            if fs.is_directory(path) then
-                local ok, name, entry = pcall(searchPackage, path)
-                if ok and name and entry then
-                    packages[name] = ("%s.%s.%s"):format(folder, path:filename():string(), entry)
+
+    local function findAllEcsFiles(folder, ecsFiles)
+        for p in fs.pairs(folder) do
+            if fs.is_directory(p) then
+                findAllEcsFiles(p, ecsFiles)
+            else
+                if p:equal_extension ".ecs" then
+                    ecsFiles[#ecsFiles+1] = p
                 end
             end
         end
+    end
+
+    local ecsFiles = {}
+
+    for _, folder in ipairs(searchFolder) do
+        local f = workspaceFolder / folder
+        findAllEcsFiles(f, ecsFiles)
+        for path in fs.pairs(f) do
+            if fs.is_directory(path) then
+                local ok, name, entry = pcall(searchPackage, path)
+                if ok and name then
+                    package_names[name] = ("%s.%s"):format(folder, path:filename():string())
+                    if entry then
+                        packages[name] = ("%s.%s.%s"):format(folder, path:filename():string(), entry)
+                    end
+                end
+            end
+        end
+    end
+
+    local function build_ecs_env()
+        local c
+        local function def() return c end
+        c = setmetatable({}, {
+            __call = def,
+            __index = def,
+        })
+    
+        local i
+        local subname
+        local interfacename
+        i = setmetatable({}, {
+            __call = function (t, n)
+                if subname == nil then
+                    interfaces[n] = {}
+                    interfacename = n
+                    return i
+                end
+    
+                if subname == "implement" then
+                    local ii = assert(interfaces[interfacename])
+                    local f = n:gsub("/", ".")
+                    ii.implement_file = f:gsub(".lua$", "")
+                end
+                subname = nil
+                return i
+            end,
+            __index = function (t, n)
+                subname = n
+                return i
+            end
+        })
+        return {
+            interface   = i,
+            system      = c,
+            policy      = c,
+            component   = c,
+            import      = c,
+        }
+    end
+    
+
+    local ecs_env = build_ecs_env()
+
+    local function load_ecs_file(f)
+        print("ecs file:", f)
+        local content, err = loadfile(f:string(), "t", ecs_env)
+        if content == nil then
+            error(("read file faild:%s, error:%s"):format(f:string(), err))
+        end
+        return content()
+    end
+    for _, f in ipairs(ecsFiles) do
+        pcall(load_ecs_file, f)
     end
 end
 
@@ -59,6 +134,35 @@ function OnSetText(uri, text)
                 text  = packages[name],
             }
         end
+    end
+
+    for rstart, rfinish, nstart, name, nfinish in text:gmatch [=[()ecs%.import%.interface()%s*["']()([^"']+)()["']]=] do
+        local pkgname, interfacename = name:match "([^|]+)|(.+)"
+        if package_names[pkgname] == nil then
+            print("package not found:", pkgname, package_names[pkgname])
+            goto continue
+        end
+
+        local ii = interfaces[interfacename]
+        if ii == nil then
+            print("not found interface name:", interfacename)
+            goto continue
+        end
+        
+        diffs[#diffs+1] = {
+            start = rstart,
+            finish = rfinish-1,
+            text  = 'require',
+        }
+        local rtext = ("%s.%s"):format(package_names[pkgname], ii.implement_file)
+        diffs[#diffs+1] = {
+            start = nstart,
+            finish = nfinish-1,
+            text  = rtext,
+        }
+
+        ::continue::
+
     end
     return diffs
 end
