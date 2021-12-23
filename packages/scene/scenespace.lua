12be0ac6242641617560702853f311cd3feabb8d
aimoonchen <kungfuchen@gmail.com>
2021-12-23 11:46:49 +0800

    slot system

--- packages/scene/scenespace.lua
+++ packages/scene/scenespace.lua
@@ -154,38 +154,7 @@ function s:update_hierarchy()
 end
 
 local evSceneChanged = world:sub {"scene_changed"}
-local r2l_mat<const> = math3d.ref(math3d.matrix{s={1.0, 1.0, -1.0}})
 function s:update_transform()
-	for v in w:select "scene:in follow_joint:in follow_flag:in follow_offset:in" do
-        if v.follow_joint ~= "None" then
-            for e in w:select "scene:in skeleton:in pose_result:in" do
-                if e.scene.id == v.scene.parent then
-                    local ske = e.skeleton._handle
-                    local joint_idx = ske:joint_index(v.follow_joint)
-                    local adjust_mat = math3d.mul(r2l_mat, e.pose_result:joint(joint_idx))
-					local offset_mat = math3d.matrix {
-						s = math3d.vector(v.follow_offset.s or {1,1,1}),
-						r = math3d.quaternion(v.follow_offset.r or {0,0,0,1}),
-						t = math3d.vector(v.follow_offset.t or {0,0,0})
-					}
-					adjust_mat = math3d.mul(adjust_mat, offset_mat)
-                    local scale, rotate, pos = math3d.srt(adjust_mat)
-                    if v.follow_flag == 1 then
-                        scale, rotate, pos = math3d.ref(math3d.vector({1, 1, 1})), math3d.ref(math3d.quaternion({0,0,0,1})), math3d.ref(pos)
-                    elseif v.follow_flag == 2 then
-						scale, rotate, pos = math3d.ref(math3d.vector({1, 1, 1})), math3d.ref(rotate), math3d.ref(pos)
-                    end
-					v.scene.srt = {
-						s = scale,
-						r = rotate,
-						t = pos
-					}
-                    --v.scene.srt = math3d.ref(math3d.mul(adjust_mat, v.scene.srt))
-                end
-            end
-        end
-    end
-
 	for _, e in evSceneChanged:unpack() do
 		if e then
 			w:sync("scene:in", e)
