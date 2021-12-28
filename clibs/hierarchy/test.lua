d419678b2e1817e917c83087a02d72c722cd3aa7
junjie020 <kitchen.gz.020@gmail.com>
2021-12-28 15:59:33 +0800

    fix hierarchy test

--- clibs/hierarchy/test.lua
+++ clibs/hierarchy/test.lua
@@ -1,12 +1,24 @@
-local hierarchy = require "hierarchy"
-local math3d = require "math3d"
+local skeleton     = require "hierarchy".skeleton
+local math3d        = require "math3d"
+local math3d_adapter= require "math3d.adapter"
 
-local root = hierarchy.new()
+local root          = skeleton.new()
+local nmt           = skeleton.node_metatable()
 
-local s, r = math3d.ref(math3d.vector(2, 1, 0.5)), math3d.ref(math3d.quaternion(math.cos(45), 0, 0, math.sin(45)))
-local c = root:add_child("child", s.p, r.p)
+nmt.add_child       = math3d_adapter.format(nmt.add_child, "vqv", 3)
+nmt.transform       = math3d_adapter.getter(nmt.transform, "vqv")
+nmt.set_transform   = math3d_adapter.format(nmt.set_transform, "vqv", 1)
+
+local s, r, t = 
+    math3d.vector(2, 1, 0.5, 0.0), 
+    math3d.quaternion(math.cos(math.rad(45)), 0, 0, math.sin(math.rad(45))),
+    math3d.vector(2, 2, 2, 1)
+local c = root:add_child("child", s, r, t)
 print(c:name())
 print(root:get_child(1):name())
+
+local ss, rr, tt = c:transform()
+print("root, s, r, t:", math3d.tostring(ss), math3d.tostring(rr), math3d.tostring(tt))
 
 local child = root:get_child(1)
 child:set_name "foobar"
@@ -17,7 +29,7 @@ local old = root:get_child(1)
 
 local s1, r1, t1 = 
     math3d.ref(math3d.vector(1, 3, 0.1)), 
-    math3d.ref(math3d.quaternion(math.cos(45), 0, 0, math.sin(45))), 
+    math3d.ref(math3d.quaternion(math.cos(math.rad(45)), 0, 0, math.sin(math.rad(45)))), 
     math3d.ref(math3d.vector(2, 2, 1))
 
 root:add_child("child2", s1.p, r1.p, t1.p)
@@ -32,8 +44,8 @@ for i=1, #root do
 	print("===>", i, root:get_child(i):name())
 end
 
-print(hierarchy.invalid(new))	-- Invalid node
-print(hierarchy.invalid(old))
+print(skeleton.invalid(new))	-- Invalid node
+print(skeleton.invalid(old))
 
 -- will rasie an error
 --print(old.name)	-- Invalid node
