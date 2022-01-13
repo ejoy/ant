ee18d5f0e4137652517a469a147b5b8ad5e28644
aimoonchen <kungfuchen@gmail.com>
2022-01-13 11:21:59 +0800

    rmlui page layout

--- test/rmlui/ui/page.lua
+++ test/rmlui/ui/page.lua
@@ -4,42 +4,53 @@ page_meta.__index = page_meta
 function page_meta:update_view_pages()
     local vitems = {}
     local view_page_idx = self.current_page - 1
-    local page_offset = -self.contain_size[1]
     local pages = self.virtual_pages
     for i = 1, 3 do
-        local start_index
-        local end_index
         if view_page_idx >= 1 and view_page_idx <= #pages then
-            start_index = pages[view_page_idx].start_index
-            end_index = pages[view_page_idx].end_index
-        end
-        if start_index and end_index then
-            for idx = 0, end_index - start_index do
-                local row = math.floor(idx/self.col)
-                local col = math.fmod(idx, self.col)
-                vitems[#vitems + 1] = {left = tostring(page_offset + col * self.item_size) .. 'px', top = tostring(row * self.item_size) .. 'px', data = self.data_source.items[start_index + idx]}
+            for _, item in  ipairs(pages[view_page_idx].vitems) do
+                vitems[#vitems + 1] = item
             end
         end
-        page_offset = page_offset + self.contain_size[1]
         view_page_idx = view_page_idx + 1
     end
     self.data_source.view_items = vitems
 end
 
 function page_meta:update_virtual_pages(items)
-    local pages = {}
-    local count = #items
+    local vpages = {}
     local count_per_page = self.row * self.col
+    local gapx = math.floor(math.fmod(self.contain_size[1], self.item_size) / (self.col + 1))
+    local gapy = math.floor(math.fmod(self.contain_size[2], self.item_size) / (self.row + 1))
+    local offset = 0
+    local index = 1
+    local total_item_count = #items
     local page_index = 0
-    
-    while count > 0 do
-        local page_offset = page_index * count_per_page
-        pages[#pages + 1] = {start_index = 1 + page_offset, end_index = (count > count_per_page) and (page_offset + count_per_page) or (page_offset + count)}
+    while index <= total_item_count do
+        local index_offset = page_index * count_per_page
+        local remain = total_item_count - index + 1
+        local new_page = {
+            start_index = 1 + index_offset,
+            end_index = (remain > count_per_page) and (index_offset + count_per_page) or (index_offset + remain),
+            vitems = {}
+        }
+        local current_count = new_page.end_index - new_page.start_index
+        local vitems = new_page.vitems
+        for idx = 0, current_count do
+            local row = math.floor(idx/self.col)
+            local col = math.fmod(idx, self.col)
+            vitems[#vitems + 1] = {
+                left = tostring(offset + col * (self.item_size + gapx) + gapx) .. 'px',
+                top = tostring(row * (self.item_size + gapy) + gapy) .. 'px',
+                data = items[index + idx]
+            }
+        end
+        vpages[#vpages + 1] = new_page
+        offset = offset + self.contain_size[1]
         page_index = page_index + 1
-        count = count - count_per_page
+        index = index + count_per_page
     end
-    self.virtual_pages = pages
-    self.data_source.virtual_pages = pages
+    self.virtual_pages = vpages
+    self.data_source.virtual_pages = vpages
 end
 
 function page_meta.create(size, source)
@@ -72,8 +83,6 @@ end
 
 function page_meta:on_mousedown(event)
     self.drag.mouse_pos = event.x
-    -- self.pos = -self.contain_size[1]
-    -- event.current.style.left = tostring(self.pos) .. 'px'
     self.drag.anchor = self.pos
 end
 
@@ -93,7 +102,7 @@ function page_meta:on_mouseup(event)
         page_change = true
     end
     self:update_view_pages()
-    self.pos = 0--self.contain_size[1]
+    self.pos = (1 - self.current_page) * self.contain_size[1]
     event.current.style.left = tostring(self.pos) .. 'px'
     return page_change
 end
