0037b90e64b25095c5fe3a5e77c0a74a0dc2fa42
junjie020 <kitchen.gz.020@gmail.com>
2022-02-17 17:20:03 +0800

    add code for create cubemap by render

--- packages/sky/skybox.lua
+++ packages/sky/skybox.lua
@@ -5,8 +5,14 @@ local w 	= world.w
 local iibl 		= ecs.import.interface "ant.render|iibl"
 local imesh		= ecs.import.interface "ant.asset|imesh"
 
+local bgfx		= require "bgfx"
+
 local geopkg 	= import_package "ant.geometry"
 local geo 		= geopkg.geometry
+
+local renderpkg	= import_package "ant.render"
+local sampler	= renderpkg.sampler
+local viewidmgr	= renderpkg.viewidmgr
 
 local math3d	= require "math3d"
 
@@ -34,15 +40,52 @@ function skybox_sys:component_init()
 	end
 end
 
+local cubemap_flags<const> = sampler.sampler_flag {
+    MIN="LINEAR",
+    MAG="LINEAR",
+    MIP="LINEAR",
+    U="CLAMP",
+    V="CLAMP",
+    W="CLAMP",
+    RT="RT_ON",
+}
+
 function skybox_sys:entity_ready()
 	for e in w:select "skybox_changed ibl:in render_object:in" do
 		local se_ibl = e.ibl
 		local ro = e.render_object
 		local t = ro.properties.s_skybox
 		local s = ro.fx.setting
-		local h = t.value.texture.handle
+		local tex = t.value.texture
+		local texhandle = tex.handle
+
+		if s.CUBEMAP_SKY == nil then
+			local ti = tex.texinfo
+			local size = math.min(ti.width, ti.height)
+			texhandle = bgfx.create_texturecube(size, true, 1, "RGBA32F", cubemap_flags)
+
+			local viewid = viewidmgr.generate "cvt_panorama"
+			for i=1, 6 do
+				local attachments = {
+					{
+						handle = texhandle,
+						resolve = true,
+						layer = i-1,
+						mip = 0,
+						numlayer = 6,
+					}
+				}
+				
+				local fbhandle = bgfx.create_frame_buffer(attachments)
+				bgfx.set_view_frame_buffer(viewid, fbhandle)
+				bgfx.submit(viewid)
+				bgfx.destroy(fbhandle)
+			end
+			
+		end
+
 		iibl.filter_all{
-			source 		= {handle = h, cubemap=s.CUBEMAP_SKY~=nil},
+			source 		= {handle = texhandle, cubemap=true},
 			irradiance 	= se_ibl.irradiance,
 			prefilter 	= se_ibl.prefilter,
 			LUT			= se_ibl.LUT,
