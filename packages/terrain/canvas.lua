d96d36792482184a906885900611f4dfa05a2572
kinbei <kinbei@qq.com>
2022-02-17 16:08:29 +0800

    public `new_entity` message

--- packages/terrain/canvas.lua
+++ packages/terrain/canvas.lua
@@ -153,7 +153,7 @@ end
 
 local gen_texture_id = id_generator()
 
-local function create_texture_item_entity(texpath, canvasentity, param)
+local function create_texture_item_entity(texpath, canvasentity)
     w:sync("reference:in canvas:in", canvasentity)
     local canvas_ref = canvasentity.reference
     local canvas = canvasentity.canvas
@@ -198,7 +198,7 @@ local function create_texture_item_entity(texpath, canvasentity, param)
                 local t = textures[texpath]
                 t.renderer = e.reference
                 world:pub{"canvas_update", "texture"}
-                world:pub {"canvas_update", "new_item", t.renderer, table.unpack(param)}
+                world:pub{"canvas_update", "new_entity", t.renderer}
             end
         }
     }
@@ -220,7 +220,7 @@ function icanvas.add_items(e, ...)
         local texpath = texture.path
         local t = textures[texpath]
         if t == nil then
-            create_texture_item_entity(texpath, e, item.param)
+            create_texture_item_entity(texpath, e)
             t = {
                 items = {},
             }
