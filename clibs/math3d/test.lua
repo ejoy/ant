89618d03b713dd96b4c0ce5b4ce00f48a0e5a25a
junjie020 <kitchen.gz.020@gmail.com>
2022-03-25 16:09:10 +0800

    fix plane test code error

--- clibs/math3d/test.lua
+++ clibs/math3d/test.lua
@@ -279,6 +279,16 @@ print_mat(r2l_mat2)
 
 print "-----plane test-----"
 do
+	--[[
+		    local d1dotn = math3d.dot(plane, ray.d)
+			local t = 0.0
+			if math.abs(d1dotn) > 1e-7 then
+				local dis = math3d.index(plane, 4)
+				local odotn = math3d.dot(ray.o, plane)
+				t = (dis - odotn) / d1dotn
+			end
+			local intersetion_pt = math3d.muladd(ray.d, tt, ray.o)
+	]]
 	local plane_pos = math3d.vector(0, 3, 0)
 	local plane_dir = math3d.vector(0, 10, 0)
 	local plane = math3d.plane(plane_pos, plane_dir)
@@ -293,6 +303,6 @@ do
 		print "plane intersetion with plane front face"
 	end
 
-	local intersetion_pt = math3d.muladd(ray.o, tt, ray.dir)
+	local intersetion_pt = math3d.muladd(ray.d, tt, ray.o)
 	print(math3d.tostring(intersetion_pt))
 end
