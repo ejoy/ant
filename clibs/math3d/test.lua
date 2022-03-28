6712d569af604ac46c8e9af6a5d55dca87c1822c
junjie020 <kitchen.gz.020@gmail.com>
2022-03-28 09:40:00 +0800

    fix some math3d test

--- clibs/math3d/test.lua
+++ clibs/math3d/test.lua
@@ -22,6 +22,36 @@ for i = 1,4 do
 	print("ref2 index", i, math3d.index(ref2,i))
 end
 
+print "-----plane test-----"
+do
+	--[[
+		    local d1dotn = math3d.dot(plane, ray.d)
+			local t = 0.0
+			if math.abs(d1dotn) > 1e-7 then
+				local dis = math3d.index(plane, 4)
+				local odotn = math3d.dot(ray.o, plane)
+				t = (dis - odotn) / d1dotn
+			end
+			local intersetion_pt = math3d.muladd(ray.d, tt, ray.o)
+	]]
+	local plane_pos = math3d.vector(0, 3, 0)
+	local plane_dir = math3d.vector(0, 10, 0)
+	local plane = math3d.plane(plane_pos, plane_dir)
+
+	local ray = {o = math3d.vector(0, 10, 0), d = math3d.vector(0.0, -3, 0.0)}
+	local tt = math3d.plane_ray(ray.o, ray.d, plane)
+	if tt == 0 then
+		print "plane parallel with ray"
+	elseif tt < 0 then
+		print "plane intersetion in ray backward"
+	else
+		print "plane intersetion with plane front face"
+	end
+
+	local intersetion_pt = math3d.muladd(ray.d, tt, ray.o)
+	print(math3d.tostring(intersetion_pt))
+end
+
 print "===SRT==="
 ref1.m = { s = 1, r = { 0, math.rad(60), 0 }, t = { 1,2,3} }
 print(ref1)
@@ -275,34 +305,3 @@ print "test matrix decompose to s, r, t"
 print_mat(r2l_mat)
 print_mat(r2l_mat1)
 print_mat(r2l_mat2)
-
-
-print "-----plane test-----"
-do
-	--[[
-		    local d1dotn = math3d.dot(plane, ray.d)
-			local t = 0.0
-			if math.abs(d1dotn) > 1e-7 then
-				local dis = math3d.index(plane, 4)
-				local odotn = math3d.dot(ray.o, plane)
-				t = (dis - odotn) / d1dotn
-			end
-			local intersetion_pt = math3d.muladd(ray.d, tt, ray.o)
-	]]
-	local plane_pos = math3d.vector(0, 3, 0)
-	local plane_dir = math3d.vector(0, 10, 0)
-	local plane = math3d.plane(plane_pos, plane_dir)
-
-	local ray = {o = math3d.vector(0, 10, 0), d = math3d.vector(2, 3, 4)}
-	local tt = math3d.plane_ray(ray.o, ray.d, plane)
-	if tt == 0 then
-		print "plane parallel with ray"
-	elseif tt < 0 then
-		print "plane intersetion in ray backward"
-	else
-		print "plane intersetion with plane front face"
-	end
-
-	local intersetion_pt = math3d.muladd(ray.d, tt, ray.o)
-	print(math3d.tostring(intersetion_pt))
-end
