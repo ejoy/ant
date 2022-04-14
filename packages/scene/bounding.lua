c2ca6c92f5ce4a2d70974b7c72b6988494ee9b61
junjie020 <kitchen.gz.020@gmail.com>
2022-04-14 16:31:34 +0800

    add simple mesh aabb to scene.aabb

--- packages/scene/bounding.lua
+++ packages/scene/bounding.lua
@@ -2,44 +2,20 @@ local ecs   = ...
 local world = ecs.world
 local w     = world.w
 
-local mathpkg   = import_package "ant.math"
-local mc        = mathpkg.constant
-
-local math3d = require "math3d"
-
-local ib = ecs.interface "ibounding"
-local function set_aabb_transform(e, transform)
-    local s = e.scene
-    if s.aabb_transform then
-        s.aabb_transform = math3d.mul(math3d.matrix(transform), s.aabb_transform)
-    else
-        s.aabb_transform = math3d.ref(math3d.matrix(transform))
-    end
-
-    s.aabb = math3d.aabb_transform(s.aabb_transform, s.aabb)
-end
-function ib.set_aabb_transform(e, transform)
-    w:sync("scene:in", e)
-    set_aabb_transform(e, transform)
-end
-
 local bounding_sys = ecs.system "bounding_system"
-
-local function transform_aabb(scene)
-    local t = scene.aabb_transform
-    if t then
-        local m = math3d.ref(math3d.matrix(t))
-        scene.aabb_transform = t
-        scene.aabb = math3d.aabb_transform(m, scene.aabb)
-    end
-end
 
 function bounding_sys:entity_init()
     for v in w:select "INIT mesh:in scene:in" do
-		local mesh = v.mesh
-		if mesh.bounding then
-			v.scene.aabb = mesh.bounding.aabb
-            transform_aabb(v.scene)
-		end
-	end
+		local m = v.mesh
+		if m.bounding then
+            v.scene.aabb = m.bounding.aabb
+        end
+    end
+
+    for v in w:select "INIT simplemesh:in scene:in" do
+        local sm = v.simplemesh
+		if sm.bounding then
+            v.scene.aabb = sm.bounding.aabb
+        end
+    end
 end
