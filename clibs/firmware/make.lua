7bc474cd9d6564fddfc10e3cc4ce90eb7d0f609d
actboy168 <actboy168@gmail.com>
2022-04-20 16:42:51 +0800

    luamake use runlua

--- clibs/firmware/make.lua
+++ clibs/firmware/make.lua
@@ -1,24 +1,22 @@
 local lm = require "luamake"
 
-lm:rule "firmware_embed" {
-    "$luamake", "lua", "@embed.lua", "$in", "$out",
-    description = "Firmware Embed $in",
-}
-
-lm:build {
-    rule = "firmware_embed",
+lm:runlua {
+    script = "embed.lua",
+    args = { "$in", "$out" },
     input = "../../engine/firmware/bootstrap.lua",
     output = "FirmwareBootstrap.h",
 }
 
-lm:build {
-    rule = "firmware_embed",
+lm:runlua {
+    script = "embed.lua",
+    args = { "$in", "$out" },
     input = "../../engine/firmware/io.lua",
     output = "FirmwareIo.h",
 }
 
-lm:build {
-    rule = "firmware_embed",
+lm:runlua {
+    script = "embed.lua",
+    args = { "$in", "$out" },
     input = "../../engine/firmware/vfs.lua",
     output = "FirmwareVfs.h",
 }
