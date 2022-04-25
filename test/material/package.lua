8156016d6036364c6859987b43f068603ef161bf
junjie020 <kitchen.gz.020@gmail.com>
2022-04-25 16:32:41 +0800

    add material test

---
+++ test/material/package.lua
@@ -0,0 +1,11 @@
+return {
+    name = "ant.test.efk",
+    dependencies = {
+        "ant.general",
+        "ant.compile_resource",
+        "ant.asset",
+        "ant.render",
+        "ant.sky",
+        "ant.efk",
+    }
+}
