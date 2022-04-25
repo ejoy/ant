389ca86ddbb6b49449b67f83f727cdb03c85e4aa
actboy168 <actboy168@gmail.com>
2022-04-25 11:29:46 +0800

    fixes bug

--- packages/rmlui/core/windowManager.lua
+++ packages/rmlui/core/windowManager.lua
@@ -1,30 +1,41 @@
-local constructor = require "core.DOM.constructor"
+local environment = require "core.environment"
 local contextManager = require "core.contextManager"
 local event = require "core.event"
 
 local m = {}
 
-local windows = {}
+local documents = {}
+
+local function find_window(name)
+    local document = documents[name]
+    if document then
+        local globals = environment[document]
+        if globals then
+            return globals.window
+        end
+    end
+end
+
 
 function m.open(name, url)
     local doc = contextManager.open(url)
     if doc then
-        windows[name] = constructor.Window(doc, "extern")
+        documents[name] = doc
         event("OnDocumentExternName", doc, name)
         contextManager.onload(doc)
     end
 end
 
 function m.close(name)
-    local window = windows[name]
+    local window = find_window(name)
     if window then
         window.close()
-        windows[name] = nil
+        documents[name] = nil
     end
 end
 
 function m.postMessage(name, data)
-    local window = windows[name]
+    local window = find_window(name)
     if window then
         window.postMessage(data)
     end
