2bb4a6d0cf7101811dbea5cd97eb7dce8c0122c9
junjie020 <kitchen.gz.020@gmail.com>
2022-04-25 16:32:41 +0800

    add material test

---
+++ test/material/init_system.lua
@@ -0,0 +1,52 @@
+local ecs   = ...
+local world = ecs.world
+local w     = world.w
+
+local imesh     = ecs.import.interface "ant.asset|imesh"
+local irender   = ecs.import.interface "ant.render|irender"
+
+local renderpkg = import_package "ant.render"
+local declmgr = renderpkg.declmgr
+local is = ecs.system "init_system"
+
+function is:init()
+    ecs.create_entity{
+        policy = {
+            "ant.render|simplerender",
+            "ant.general|name",
+        },
+        data = {
+            material_with_c = true,
+            c_material = "/pkg/ant.test.material/assets/test.material",
+            simplemesh = imesh.init_mesh({
+                ib = {
+                    start = 0,
+                    num = 6,
+                    handle = irender.quad_ib(),
+                },
+                vb = {
+                    start = 0,
+                    num = 4,
+                    handles = {
+                        bgfx.create_vertex_buffer(bgfx.memory_buffer("ffff", {
+                            100, 200, 0.0, 0.0,
+                            100, 132, 0.0, 1.0,
+                            420, 200, 1.0, 0.0,
+                            420, 132, 1.0, 1.0,
+                        }), declmgr.get "p2|t2".handle)
+                    }
+                }
+            }, true),
+            filter_state = "main_view",
+            name = "test_material",
+        }
+    }
+end
+
+function is:init_world()
+
+end
+
+function is:data_changed()
+
+end
