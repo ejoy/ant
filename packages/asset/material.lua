fc57d276ab21c63a0f5acbd2c658fe654dda7fd4
junjie020 <kitchen.gz.020@gmail.com>
2022-05-09 17:52:02 +0800

    read uniform handle from lua data

--- packages/asset/material.lua
+++ packages/asset/material.lua
@@ -22,22 +22,48 @@ local function buffer_value(stage, access)
 	return {stage=stage, access=access, value=nil, type='b'}
 end
 
-local SYS_ATTRIBS = rmat.system_attribs(CMATOBJ, {
+local function check(properties)
+	for k, v in pairs(properties) do
+		if v.type == "u" or v.type == "s" then
+			local n = 1
+			local ut
+			if v.stage == nil then
+				local function which_uniform_type(mv)
+					local vv = math3d.tovalue(mv)
+					return #vv == 16 and "m4" or "v"
+				end
+				if type(v.value) == "table" then
+					n = #v.value
+					ut = which_uniform_type(v.value[1])
+				else
+					ut = which_uniform_type(v.value)
+				end
+			else
+				ut = "s"
+			end
+
+			v.handle = bgfx.create_uniform(k, ut, n)
+		end
+	end
+	return properties
+end
+
+local SYS_ATTRIBS = rmat.system_attribs(CMATOBJ, check{
 	--camera
-	u_eyepos				= mc.ZERO_PT,
-	u_exposure_param		= math3d.vector(16.0, 0.008, 100.0, 0.0),
-	u_camera_param			= mc.ZERO,
+	u_eyepos				= {type="u", value=mc.ZERO_PT},
+	u_exposure_param		= {type="u", value=math3d.vector(16.0, 0.008, 100.0, 0.0)},
+	u_camera_param			= {type="u", value=mc.ZERO},
 	--lighting
-	u_cluster_size			= mc.ZERO,
-	u_cluster_shading_param	= mc.ZERO,
-	u_light_count			= mc.ZERO,
+	u_cluster_size			= {type="u", value=mc.ZERO},
+	u_cluster_shading_param	= {type="u", value=mc.ZERO},
+	u_light_count			= {type="u", value=mc.ZERO},
 	b_light_grids			= buffer_value(-1, "r"),
 	b_light_index_lists		= buffer_value(-1, "r"),
 	b_light_info			= buffer_value(-1, "r"),
-	u_time					= mc.ZERO,
+	u_time					= {type="u", value=mc.ZERO},
 
 	--IBL
-	u_ibl_param				= mc.ZERO,
+	u_ibl_param				= {type="u", value=mc.ZERO},
 	s_irradiance			= texture_value(5),
 	s_prefilter				= texture_value(6),
 	s_LUT					= texture_value(7),
@@ -50,35 +76,39 @@ local SYS_ATTRIBS = rmat.system_attribs(CMATOBJ, {
 		offsetWS = (amp*((dis-flat)/base)^exp) * dirWS
 		posWS = posWS + offsetWS
 	]]
-	u_curveworld_param		= mc.ZERO,	-- flat distance, base distance, exp, amplification
-	u_curveworld_dir		= mc.ZAXIS,	-- dir in view space
+	u_curveworld_param		= {type="u", value=mc.ZERO},	-- flat distance, base distance, exp, amplification
+	u_curveworld_dir		= {type="u", value=mc.ZAXIS},	-- dir in view space
 
 	-- shadow
 	--   csm
-	u_csm_matrix 		= {
-		mc.IDENTITY_MAT,
-		mc.IDENTITY_MAT,
-		mc.IDENTITY_MAT,
-		mc.IDENTITY_MAT,
+	u_csm_matrix 		= { type="u",
+		value = {
+			mc.IDENTITY_MAT,
+			mc.IDENTITY_MAT,
+			mc.IDENTITY_MAT,
+			mc.IDENTITY_MAT,
+		}
 	},
-	u_csm_split_distances= mc.ZERO,
-	u_depth_scale_offset = mc.ZERO,
-	u_shadow_param1		 = mc.ZERO,
-	u_shadow_param2		 = mc.ZERO,
+	u_csm_split_distances= {type="u", value=mc.ZERO},
+	u_depth_scale_offset = {type="u", value=mc.ZERO},
+	u_shadow_param1		 = {type="u", value=mc.ZERO},
+	u_shadow_param2		 = {type="u", value=mc.ZERO},
 	s_shadowmap			 = texture_value(8),
 
 	--   omni
-	u_omni_matrix = {
-		mc.IDENTITY_MAT,
-		mc.IDENTITY_MAT,
-		mc.IDENTITY_MAT,
-		mc.IDENTITY_MAT,
+	u_omni_matrix = { type = "u",
+		value = {
+			mc.IDENTITY_MAT,
+			mc.IDENTITY_MAT,
+			mc.IDENTITY_MAT,
+			mc.IDENTITY_MAT,
+		}
 	},
 
-	u_tetra_normal_Green	= mc.ZERO,
-	u_tetra_normal_Yellow	= mc.ZERO,
-	u_tetra_normal_Blue		= mc.ZERO,
-	u_tetra_normal_Red		= mc.ZERO,
+	u_tetra_normal_Green	= {type="u", value=mc.ZERO},
+	u_tetra_normal_Yellow	= {type="u", value=mc.ZERO},
+	u_tetra_normal_Blue		= {type="u", value=mc.ZERO},
+	u_tetra_normal_Red		= {type="u", value=mc.ZERO},
 
 	s_omni_shadowmap	= texture_value(9),
 })
@@ -104,7 +134,7 @@ end
 local imaterial = ecs.interface "imaterial"
 
 function imaterial.set_property(e, who, what)
-	e.render_object.material[who] = what
+	--e.render_object.material[who] = what
 end
 
 function imaterial.get_property(e, who)
