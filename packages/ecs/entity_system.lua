fb46e188657eb7a1014fcf5084a618024da47127
actboy168 <actboy168@gmail.com>
2022-06-23 17:30:37 +0800

    fixes bug

--- packages/ecs/entity_system.lua
+++ packages/ecs/entity_system.lua
@@ -5,15 +5,27 @@ local serialize = import_package "ant.serialize"
 
 local m = ecs.system "entity_system"
 
+local function update_group_tag(data)
+    local groupid = data.group
+    for tag, t in pairs(world._group.tags) do
+        if t[groupid] then
+            data[tag] = true
+        end
+    end
+end
+
 function m:entity_create()
+    local _ = world
     for v in w:select "create_entity:in" do
         local initargs = v.create_entity
         initargs.INIT = true
+        update_group_tag(initargs)
         w:new(initargs)
     end
     for v in w:select "create_entity_template:in" do
         local initargs = v.create_entity_template
         initargs.data.INIT = true
+        update_group_tag(initargs.data)
         w:template_instance(initargs.template, serialize.unpack, initargs.data)
     end
     w:group_update()
