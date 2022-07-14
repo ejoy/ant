a976cbcbf356719a719900c5baae99cc7c7750dc
actboy168 <actboy168@gmail.com>
2022-07-14 11:21:15 +0800

    add texture mgr

--- packages/asset/ext_texture.lua
+++ packages/asset/ext_texture.lua
@@ -1,24 +1,6 @@
-local ltask = require "ltask"
-local ServiceResource = ltask.uniqueservice "ant.compile_resource|resource"
-local cr        = import_package "ant.compile_resource"
-
-local function loader(filename)
-	local result = ltask.call(ServiceResource, "texture_create", filename, cr.compile_path(filename))
-	if result.uncomplete then
-		ltask.fork(function()
-			result.handle = ltask.call(ServiceResource, "texture_complete", filename)
-			result.uncomplete = nil
-		end)
-	end
-	return result
-end
-
-local function unloader(res)
-	--TODO
-	ltask.call(ServiceResource, "texture_destroy", res)
-end
+local mgr = require "texture_mgr"
 
 return {
-    loader = loader,
-    unloader = unloader,
+    loader = mgr.create,
+    unloader = mgr.destroy,
 }
