bb03e8f2111d691dfa18a2f21df4646103db87c3
junjie020 <kitchen.gz.020@gmail.com>
2022-07-19 16:28:33 +0800

    refactor mesh create

--- packages/asset/mesh.lua
+++ packages/asset/mesh.lua
@@ -7,28 +7,6 @@ local ext_meshbin 	= require "ext_meshbin"
 
 local meshcore		= require "render.mesh"
 
-local function create_rendermesh(mesh)
-	if mesh then
-		assert(0 == #mesh.vb)
-		local m = {
-			vb = {
-				start   = mesh.vb.start,
-				num     = mesh.vb.num,
-				handle 	= mesh.vb.handle
-			}
-		}
-
-		if mesh.ib then
-			m.ib = {
-				start	= mesh.ib.start,
-				num 	= mesh.ib.num,
-				handle	= mesh.ib.handle,
-			}
-		end
-		return meshcore.mesh(m)
-	end
-end
-
 local imesh = ecs.interface "imesh"
 function imesh.create_vb(vb)
 	return ext_meshbin.proxy_vb(vb)
@@ -50,14 +28,12 @@ end
 
 function ms:entity_init()
 	for e in w:select "INIT mesh:in render_object:in" do
-		local ro = e.render_object
-		ro.mesh = create_rendermesh(e.mesh)
+		e.render_object.mesh = meshcore.mesh(e.mesh)
 	end
 
 	for e in w:select "INIT simplemesh:in render_object:in owned_mesh_buffer?out" do
 		local sm = e.simplemesh
-		local ro = e.render_object
-		ro.mesh = create_rendermesh(sm)
+		e.render_object.mesh = meshcore.mesh(sm)
 		e.owned_mesh_buffer = sm.owned_mesh_buffer
 	end
 end
