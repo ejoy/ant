99f3072f4cdd49900711843ba1f2d8184b7b719f
actboy168 <actboy168@gmail.com>
2022-08-23 14:40:59 +0800

    fixes bug

--- packages/serialize/parse.lua
+++ packages/serialize/parse.lua
@@ -1,11 +1,33 @@
 local datalist = require "datalist"
 
+local function normalize_path(fullname)
+    local first = (fullname:sub(1, 1) == "/") and "/" or ""
+    local last = (fullname:sub(-1, -1) == "/") and "/" or ""
+	local t = {}
+	for m in fullname:gmatch("([^/]+)[/]?") do
+		if m == ".." and next(t) then
+			table.remove(t, #t)
+		elseif m ~= "." then
+			table.insert(t, m)
+		end
+	end
+	return first .. table.concat(t, "/") .. last
+end
+
+local function normalize(fullname)
+    local t = {}
+	for m in fullname:gmatch "([^|]+)" do
+        t[#t+1] = normalize_path(m)
+	end
+    return table.concat(t, "|")
+end
+
 local function absolute_path(base, path)
 	if path:sub(1,1) == "/" or not base then
 		return path
 	end
     base = base:match "^(.-)[^/|]*$"
-	return base .. (path:match "^%./(.+)$" or path)
+	return normalize(base .. (path:match "^%./(.+)$" or path))
 end
 
 return function (basepath, data)
