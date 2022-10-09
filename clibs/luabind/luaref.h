30c8a65869c561c8ee7a7e5acedbd563db740289
actboy168 <actboy168@gmail.com>
2022-10-09 10:01:48 +0800

    fixes bug

--- clibs/luabind/luaref.h
+++ clibs/luabind/luaref.h
@@ -9,3 +9,26 @@ bool   luaref_isvalid(luaref refL, int ref);
 int    luaref_ref    (luaref refL, lua_State* L);
 void   luaref_unref  (luaref refL, int ref);
 void   luaref_get    (luaref refL, lua_State* L, int ref);
+
+class luaref_box {
+public:
+    luaref_box(luaref refL, lua_State* L)
+        : refL(refL)
+        , ref(luaref_ref(refL, L))
+    {}
+    ~luaref_box() {
+        luaref_unref(refL, ref);
+    }
+    bool isvalid() const {
+        return luaref_isvalid(refL, ref);
+    }
+    void get(lua_State* L) const {
+        luaref_get(refL, L, ref);
+    }
+    int handle() const {
+        return ref;
+    }
+private:
+    luaref refL;
+    int ref;
+};
