384761f62a61153ac4e2d1652c8fc5bfec875709
actboy168 <actboy168@gmail.com>
2022-10-21 15:45:35 +0800

    rewrite tween

--- clibs/rmlui/core/Tween.h
+++ clibs/rmlui/core/Tween.h
@@ -1,27 +1,23 @@
 #pragma once
 
 #include <string>
+#include <stdint.h>
 
 namespace Rml {
 
 class Tween {
 public:
-	enum class Type : uint8_t { None, Back, Bounce, Circular, Cubic, Elastic, Exponential, Linear, Quadratic, Quartic, Quintic, Sine, Count };
-	enum class Direction : uint8_t { In = 1, Out = 2, InOut = 3 };
+	enum class Type : uint8_t { Linear = 0, Back, Bounce, Circular, Cubic, Elastic, Exponential, Quadratic, Quartic, Quintic, Sine };
+	enum class Direction : uint8_t { In = 0, Out, InOut };
 
 	Tween();
 	Tween(Type type, Direction direction);
-	float operator()(float t) const;
-	bool operator==(const Tween& other) const;
+	float get(float t) const;
+	bool operator==(const Tween& rhs) const;
 	std::string ToString() const;
 
 private:
-	float in(float t) const;
-	float out(float t) const;
-	float in_out(float t) const;
-
-	Type type = Type::Linear;
-	Direction direction = Direction::Out;
+	uint8_t v;
 };
 
 }
