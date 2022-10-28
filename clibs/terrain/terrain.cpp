7c429d9e10c52d42a56937e2512725c9363dc084
von <1052019421@qq.com>
2022-10-28 10:07:57 +0800

    terrain noise

--- clibs/terrain/terrain.cpp
+++ clibs/terrain/terrain.cpp
@@ -403,11 +403,107 @@ lterrain_create_renderdara(lua_State *L){
 	return 1;
 }
 
+static const unsigned char hash[] = {
+	208,34,231,213,32,248,233,56,161,78,24,140,71,48,140,254,245,255,
+	247,247,40, 185,248,251,245,28,124,204,204,76,36,1,107,28,234,163,
+	202,224,245,128,167,204,9,92,217,54,239,174,173,102,193,189,190,121,
+	100,108,167,44,43,77,180,204,8,81,70,223,11,38,24,254,210,210,177,
+	32,81,195,243,125,8,169,112,32,97,53,195,13,203,9,47,104,125,117,
+	114,124,165,203,181,235,193,206,70,180,174,0,167,181,41,164,30,116,
+	127,198,245,146,87,224,149,206,57,4,192,210,65,210,129,240,178,105,
+	228,108,245,148,140,40,35,195,38,58,65,207,215,253,65,85,208,76,62,
+	3,237,55,89,232,50,217,64,244,157,199,121,252,90,17,212,203,149,152,
+	140,187,234,177,73,174,193,100,192,143,97,53,145,135,19,103,13,90,
+	135,151,199,91,239,247,33,39,145,101,120,99,3,186,86,99,41,237,203,
+	111,79,220,135,158,42,30,154,120,67,87,167,135,176,183,191,253,115,
+	184,21,233,58,129,233,142,39,128,211,118,137,139,255,114,20,218,113,
+	154,27,127,246,250,1,8,198,250,209,92,222,173,21,88,102,219
+};
+
+
+
+static int noise2(int x, int y, int seed) {
+	int yindex = (y + seed) % 256;
+	if (yindex < 0) {
+		yindex += 256;
+	}
+	int  xindex = (hash[yindex] + x) % 256;
+	if (xindex < 0) {
+		xindex += 256;
+	}
+	return (int) hash[xindex];
+}
+
+
+
+static double lin_inter(double x, double y, double s) {
+	return x + s * (y-x);
+}
+
+
+
+static double smooth_inter(double x, double y, double s) {
+	return lin_inter(x, y, s * s * (3-2*s));
+}
+
+
+
+static double noise2d(double x, double y, double seed) {
+	int x_int = floor(x);
+	int y_int = floor(y);
+	double x_frac = x - x_int;
+	double y_frac = y - y_int;
+	int s = noise2(x_int, y_int, seed);
+	int t = noise2(x_int+1, y_int, seed);
+	int u = noise2(x_int, y_int+1, seed);
+	int v = noise2(x_int+1, y_int+1, seed);
+	double low = smooth_inter(s, t, x_frac);
+	double high = smooth_inter(u, v, x_frac);
+	return smooth_inter(low, high, y_frac);
+}
+
+
+
+static double perlin2d(double x, double y, double freq, int depth, int seed, float ox, float oy) {
+	double xa = x*freq + ox;
+	double ya = y*freq + oy;
+	double amp = 1.0;
+	double fin = 0;
+	double div = 0.0;
+
+	int i;
+	for(i=0; i<depth; i++) {
+		div += 256 * amp;
+		fin += noise2d(xa, ya, seed) * amp;
+		amp /= 2;
+		xa *= 2;
+		ya *= 2;
+	}
+
+	return fin/div;
+}
+
+
+
+static int lnoise(lua_State *L) {
+	float x = luaL_checknumber(L, 1);
+	float y = luaL_checknumber(L, 2);
+	float freq = luaL_checknumber(L, 3);
+	int depth = luaL_checkinteger(L, 4);
+	int seed = luaL_checkinteger(L, 5);
+	float offsetx = luaL_checknumber(L, 6);
+	float offsety = luaL_checknumber(L, 7);
+	lua_pushnumber(L, perlin2d(x, y, freq, depth, seed, offsetx, offsety));
+
+	return 1;
+}
+
 extern "C" {
 LUAMOD_API int
 	luaopen_terrain(lua_State *L) {
 	luaL_checkversion(L);
 	luaL_Reg l[] = {
+		{ "noise", lnoise },
 		{ "create_render_data",		lterrain_create_renderdara},
 		{ "calc_min_max_height",	lterrain_min_max_height},
 		{ "create_section_aabb",	lterrain_create_section_aabb},
