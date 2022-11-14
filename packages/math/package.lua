f4bcc73a7c173c799d4098969ed8dc9ccc5eefc4
Kitchen <kitchen.gz.020@gmail.com>
2022-11-14 11:58:29 +0800

    move math3d init to hardware_interface.lua

--- packages/math/package.lua
+++ packages/math/package.lua
@@ -1,11 +1,3 @@
-if not __ANT_RUNTIME__ then
-    --large 
-    if package.loaded.math3d then
-        error "need init math3d MAXPAGE"
-    end
-    debug.getregistry().MATH3D_MAXPAGE = 10480
-end
-
 return {
     name = "ant.math",
     entry = "main",
