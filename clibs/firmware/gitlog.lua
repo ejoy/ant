a4039170e82f15bc96cb1fbb751d256382a02fdf
actboy168 <actboy168@gmail.com>
2023-01-05 16:32:14 +0800

    remove ios_version.lua

--- clibs/firmware/gitlog.lua
+++
@@ -1,4 +0,0 @@
-local path = ...
-local p <close> = assert(io.popen("git log -n 1 --pretty=format:\"return {%n    commit = '%H',%n    date = '%ad',%n}\" --date=iso"))
-local f <close> = assert(io.open(path, 'w'))
-f:write(p:read "a")
