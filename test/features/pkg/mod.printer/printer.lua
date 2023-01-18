7cf59dc2955caf69663c3b5bcc02c8d161ccf557
junjie020 <kitchen.gz.020@gmail.com>
2023-01-18 15:15:35 +0800

    update printer test

--- test/features/pkg/mod.printer/printer.lua
+++ test/features/pkg/mod.printer/printer.lua
@@ -10,30 +10,6 @@ local iterrain  = ecs.import.interface "ant.terrain|iterrain"
 
 local printer_sys = ecs.system 'printer_system'
 function printer_sys:init()
-     ecs.create_entity {
-        policy = {
-            "ant.render|render",
-            "ant.general|name",
-        },
-        data = {
-            primitive_filter = {
-                filter_type = "main_view",
-            },
-            name        = "printer_test",
-            scene  = {s = 0.2, t = {5, 0, 5}},
-            material    = "/pkg/mod.printer/assets/printer.material",
-            visible_state = "main_view",
-            mesh        = "/pkg/mod.printer/assets/electric-pole-1.glb|meshes/Cylinder.006_P1.meshbin",
-            render_layer= "postprocess_obj",
-            -- add printer tag
-            -- previous still be zero
-            -- duration means generation duration time
-            printer = {
-                previous = 0,
-                duration = 5
-            }
-        },
-    }
 
 end
 
