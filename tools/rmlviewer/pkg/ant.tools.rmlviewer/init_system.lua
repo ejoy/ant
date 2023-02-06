38d6e6bdae7eddd47544716866479c2aac900f70
von <1052019421@qq.com>
2023-02-06 09:48:53 +0800

    update ui_rt test

--- tools/rmlviewer/pkg/ant.tools.rmlviewer/init_system.lua
+++ tools/rmlviewer/pkg/ant.tools.rmlviewer/init_system.lua
@@ -6,8 +6,24 @@ local iUiRt     = ecs.import.interface "ant.rmlui|iuirt"
 local init_sys   = ecs.system "init_system"
 local iRmlUi     = ecs.import.interface "ant.rmlui|irmlui"
 local ivs		= ecs.import.interface "ant.scene|ivisible_state"
+local kb_mb = world:sub{"keyboard"}
 local function getArguments()
     return ecs.world.args.ecs.args
+end
+
+function init_sys:init()
+        local gid = iUiRt.get_group_id("rt1")
+        local g = ecs.group(gid)
+            local p1 = g:create_instance "pkg/ant.resources/glb/light.prefab"
+            p1.on_ready = function (e)
+                for _, eid in ipairs(e.tag['*']) do
+                    local ee = w:entity(eid, "visible_state?in")
+                    if ee.visible_state then
+                        ivs.set_state(ee, "rt1_queue", true)
+                    end     
+                end        
+            end
+            world:create_object(p1)
 end
 
 function init_sys:post_init()
@@ -20,7 +36,6 @@ function init_sys:post_init()
     end)
 end
 
-local kb_mb = world:sub{"keyboard"}
 local math3d        = require "math3d"
 local ientity       = ecs.import.interface "ant.render|ientity"
 local imaterial     = ecs.import.interface "ant.asset|imaterial"
@@ -35,34 +50,16 @@ function init_sys:data_changed()
         if key == "T" and press == 0 then
             g:enable "view_visible"
             g:enable "scene_update"
---[[             local p = g:create_instance("/pkg/ant.resources/glb/Duck.glb|mesh.prefab")
-
-            p.on_ready = function (e)
+            local p2 = g:create_instance("/pkg/ant.resources/glb/Duck.glb|mesh.prefab")
+            p2.on_ready = function (e)
                 for _, eid in ipairs(e.tag['*']) do
                     local ee = w:entity(eid, "visible_state?in")
                     if ee.visible_state then
                         ivs.set_state(ee, "rt1_queue", true)
                     end     
                 end        
-            end
-            world:create_object(p)  ]]
-            ecs.create_entity{
-                policy = {
-                    "ant.render|simplerender",
-                    "ant.general|name",
-                },
-                data = {
-                    simplemesh = ientity.arrow_mesh(10),
-                    material = "/pkg/ant.resources/materials/meshcolor.material",
-                    visible_state = queuename,
-                    scene = {
-                    },
-                    name = "arrow",
-                    on_ready = function (ee)
-                        imaterial.set_property(ee, "u_color", math3d.vector(1.0, 0.0, 0.0, 1.0))
-                    end
-                }
-            }
+            end            
+            world:create_object(p2)  
 
         end
     end
