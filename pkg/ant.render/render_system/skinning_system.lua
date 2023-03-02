9537c3b6ad54e6f378d5087118e4078e107e4e37
von <1052019421@qq.com>
2023-03-02 15:54:11 +0800

    fix skinning compute shader bug

--- pkg/ant.render/render_system/skinning_system.lua
+++ pkg/ant.render/render_system/skinning_system.lua
@@ -21,10 +21,8 @@ local assetmgr  = import_package "ant.asset"
 
 
 local skinning_material
-local skinning_matrices_vb
-local skinning_in_dynamic_vb
-local skinning_out_dynamic_vb
-local function create_skinning_compute(vb_num)
+
+local function do_skinning_compute(vb_num, skinning_matrices_vb, skinning_in_dynamic_vb, skinning_out_dynamic_vb)
 	local dispatchsize = {
 		math.floor((vb_num + 63) / 64), 1 , 1
 	}
@@ -69,6 +67,27 @@ function skinning_sys:init()
 	skinning_material = assetmgr.resource("/pkg/ant.resources/materials/skinning/skinning.material")
 end
 
+function skinning_sys:entity_init()
+	local meshskin
+	for e in w:select "skinning scene?in meshskin?update render_object?update" do
+		if e.meshskin then
+			meshskin = e.meshskin
+		else
+			assert(meshskin, "Invalid skinning render object, meshskin should create before this object")
+			if meshskin.skinning_matrices_vb == nil or meshskin.skinning_in_dynamic_vb == nil and meshskin.skinning_out_dynamic_vb == nil then
+				local sm = meshskin.skinning_matrices
+				local memory_buffer = bgfx.memory_buffer(sm:pointer(), 64 * sm:count())
+				meshskin.skinning_matrices_vb = bgfx.create_dynamic_vertex_buffer(memory_buffer, declmgr.get("p4").handle, "r")
+				meshskin.skinning_in_dynamic_vb = e.render_object.vb_handle
+				local skinning_out_dynamic_vb = bgfx.create_dynamic_vertex_buffer(e.render_object.vb_num, declmgr.get "p40NIf|t40NIf|T40NIf".handle, "w")
+				meshskin.skinning_out_dynamic_vb = skinning_out_dynamic_vb
+	
+				e.render_object.vb_handle = skinning_out_dynamic_vb 
+			end
+		end
+	end
+end
+
 function skinning_sys:skin_mesh()
 	for e in w:select "meshskin:in scene:update" do
 		local skin = e.meshskin.skin
@@ -82,24 +101,14 @@ function skinning_sys:skin_mesh()
 
 	local meshskin
 	local worldmat
-	for e in w:select "skinning scene?in meshskin?in render_object?update bounding?update scene?in" do
+	for e in w:select "skinning scene?in meshskin?in render_object?update bounding?update" do
 		if e.meshskin then
 			meshskin = e.meshskin
 			worldmat = e.scene.worldmat
 		else
 			assert(meshskin, "Invalid skinning render object, meshskin should create before this object")
-			local sm = meshskin.skinning_matrices
 			e.render_object.worldmat = worldmat
- 			if skinning_matrices_vb == nil then
-				local memory_buffer = bgfx.memory_buffer(sm:pointer(), 64 * sm:count())
-				skinning_matrices_vb = bgfx.create_dynamic_vertex_buffer(memory_buffer, declmgr.get("p4").handle, "r")
-				skinning_in_dynamic_vb = e.render_object.vb_handle
-				skinning_out_dynamic_vb = bgfx.create_dynamic_vertex_buffer(e.render_object.vb_num, declmgr.get "p40NIf|t40NIf|T40NIf".handle, "w")
-				create_skinning_compute(e.render_object.vb_num)
-				e.render_object.vb_handle = skinning_out_dynamic_vb 
-			else
-				create_skinning_compute(e.render_object.vb_num)
-			end
+			do_skinning_compute(e.render_object.vb_num, meshskin.skinning_matrices_vb, meshskin.skinning_in_dynamic_vb, meshskin.skinning_out_dynamic_vb)
 			if mc.NULL ~= e.bounding.aabb then
 				math3d.unmark(e.bounding.scene_aabb)
 				e.bounding.scene_aabb = math3d.mark(math3d.aabb_transform(worldmat, e.bounding.aabb))
@@ -107,7 +116,3 @@ function skinning_sys:skin_mesh()
 		end
 	end
 end  
-
-function skinning_sys:render_preprocess()
-
-end 
