f5145690ea183ac5c277e076f7b06c8e4d51c84d
junjie020 <kitchen.gz.020@gmail.com>
2023-03-17 10:04:33 +0800

    fix texture_set error

--- pkg/ant.compile_resource/service/resource.lua
+++ pkg/ant.compile_resource/service/resource.lua
@@ -224,7 +224,8 @@ ltask.fork(function ()
                 local c = textureByName[name]
                 bgfx.destroy(c.handle)
                 c.handle = nil
-                textureman.texture_set(c.output.id, DefaultTexture)
+                local textype = which_texture_type(c.input.info)
+                textureman.texture_set(c.output.id, DefaultTexture[textype])
             end
         end
         while true do
