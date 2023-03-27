6fe8aafa93a93af9c7285db0289ee78c53fee271
junjie020 <kitchen.gz.020@gmail.com>
2023-03-27 10:50:46 +0800

    add comment for SH

--- pkg/ant.render/ibl/sh.lua
+++ pkg/ant.render/ibl/sh.lua
@@ -277,10 +277,12 @@ local compute_cos_SH; do
 end
 
 --[[
-    using SH to compress irradiance, we need to do two step:
+    using SH for irradiance, we need to do two step:
         1. compress, projection the irradiance map to Eml with SH basics function
-        2. render, use the compress Eml value with sample direction(the normal value of this function), and reprojection the Eml with SH basics function the get the irradiance value
-    see the reference: 'An efficient Representation for Irradiance Environment Maps'
+        2. render, use the compress Eml value with sample direction(the normal value), and reprojection the Eml with SH basics function to get the irradiance value
+    see the reference: 'An efficient Representation for Irradiance Environment Maps'[1]
+
+    the 'sh.lua' file, only for compress irradiance map, the render part is in ibl.sh file, defined by 'IRRADIANCE_SH_BAND_NUM'
 
     Kml = K(m, l) = sqrt(A/B), where:
             A = (2l+1)*(l-|m|)! = (2l+1) * factorial(l - abs(m))
@@ -291,22 +293,52 @@ end
         m < 0, sqrt(2) * Kml * sin(|m|*phi) * P-ml(cos(theta))
         m = 0, K0l * P0l(cos(theta))
 
+            Yml(theta, phi) has another version defined in Cartesian coordinates space:
+                (x, y, z) = (sin(theta)*cos(phi), sin(theta)*sin(phi), cos(theta))
+                Y00(theta, phi)                 = 0.282095
+                (Y11; Y10; Y1-1)(theta, phi)    = 0.488603(x; z; y)
+                (Y21; Y2-1;Y2-2)(theta, phi)    = 1.092548(xz;yz;xy)
+                Y20(theta, phi)                 = 0.315392(3*z*z-1)
+                Y22(theta, phi)                 = 0.546274(x*x-y*y)
+
     Kml = K(m, l) ==> K0l = K(0, l)
     Pml = P(|m|, l) ==> P0l = P(0, l)
     here, Pml is Associated Legendre polynomials
 
-    the enviment map SH formula is:
+    the compress part of SH formula:
         E = integral[omega](Li * cos(theta) * domega)
-        Lml = integral[omega](Li(theta, phi) * Yml(theta, phi) * domega) ==> Sum(Li(theta, phi) * Yml(theta, phi) * SolidAngle(theta, phi))
-        EE = Sum(Lml * Al)
-        BB = albedo * EE, where albedo is the material color, i.e. from base color texture
+            the E is the irradiance at some point, cos(theta) is the normal dot soild angle direction
+            and BRDF is miss here, bacause the reflection and visibility are ignore here, so only Li and cos(theta) will affect the result
+        
+        so, we need to use SH basics function Yml to weight Li and cos(theta), which is Lml and Al:
 
-        the BB is what we need
+            Lml = integral[omega](Li(theta, phi) * Yml(theta, phi) * domega) ==> Sum(Li(theta, phi) * Yml(theta, phi) * SolidAngle(theta, phi))
 
-        here, if bandnum = 2/3, Lml is 4/9 coefficients with RGB values
-        and, the Al is n dot w, which is the normal vector with sample direction's dot value, which projection to SH basics function
-            Al = Sum((n dot w) * Yml(theta, phi)) = Sum(max(cos theta, 0) * Yml(theta, phi))
+            Al = integral[omega](cos(theta) * Yml(theta, phi) * domega) ==> Sum(cos(theta) * Yml(theta, phi))
+                Al has some special features we can use. Because A = (normal dot solid angle direction), so A has no azimuthal dependence, then
+                    only m = 0 is valid for any l index.
 
+                we make:
+                    Al* = sqrt(4*math.pi/(2l+1)) * Al
+                Beside this, Al has an analytic formula(see the reference[2] for how to derived), we can derived, when:
+                    l = 1: Al* = 2*math.pi/3
+                    l > 1, odd: Al* = 0
+                          even: Al* = (2*math.pi)*(-1)^(l/2-1)/(l+2)(l+1)*((l!/(2^l*((l/2)!)^2)))
+            
+        then, we get what we need, so:
+            Eml = Sum(Lml * Al*)
+                Bml = albedo * Eml, where albedo is the material color, i.e. from base color texture
+                    the Bml is the output color(irradiance value) of this point
+
+                if bandnum = 2/3, Eml is 4/9 coefficients with RGB values
+
+    the render part of SH:
+        we need to use Eml to recover E, so:
+            E = Sum(Eml * Yml(theta, phi))  -- all the (theta, phi) paris mean it's a direction that is normal value of the sample point
+
+    References:
+    [1] An efficient Representation for Irradiance Environment Maps
+    [2] R. Ramamoorthi and P. Hanrahan. On the relationship between radiance and irradiance: Determining the illumination from images of a convex lambertian object. To appear, Journal of the Optical Society of America A, 2001
 ]]
 local function computeLml(Kml, bandnum, N)
     local Yml
