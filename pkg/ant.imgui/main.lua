c9e05b0aee63545fcd7cc020b308739114cccc8c
actboy168 <actboy168@gmail.com>
2023-04-21 15:21:10 +0800

    fixes bug

--- pkg/ant.imgui/main.lua
+++ pkg/ant.imgui/main.lua
@@ -1,11 +1,15 @@
 local function start(packagename, w, h)
     local task = dofile "/engine/task/bootstrap.lua"
+    local exclusive = { {"ant.imgui|imgui", packagename, w, h}, "timer", "ant.render|bgfx_main" }
+    if not __ANT_RUNTIME__ then
+        exclusive[#exclusive+1] = "subprocess"
+    end
     task {
         support_package = true,
         service_path = "${package}/service/?.lua",
         bootstrap = { "ant.imgui|boot" },
         logger = { "logger" },
-        exclusive = { {"ant.imgui|imgui", packagename, w, h}, "timer", "ant.render|bgfx_main", "subprocess" },
+        exclusive = exclusive,
         --debuglog = "log.txt",
     }
 end
--- pkg/ant.window/main.lua
+++ pkg/ant.window/main.lua
@@ -1,11 +1,15 @@
 local function start(initargs)
     local task = dofile "/engine/task/bootstrap.lua"
+    local exclusive = { "ant.window|window", "timer", "ant.render|bgfx_main" }
+    if not __ANT_RUNTIME__ then
+        exclusive[#exclusive+1] = "subprocess"
+    end
     task {
         support_package = true,
         service_path = "${package}/service/?.lua",
         bootstrap = { "ant.window|boot", initargs },
         logger = { "logger" },
-        exclusive = { "ant.window|window", "timer", "ant.render|bgfx_main", "subprocess" },
+        exclusive = exclusive,
         debuglog = "log.txt",
     }
 end
