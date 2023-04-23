b972ded1fbd93259fa859b5900acacb4d3658258
actboy168 <actboy168@gmail.com>
2023-04-23 15:30:48 +0800

    add MaxSubprocess

--- engine/task/service/subprocess.lua
+++ engine/task/service/subprocess.lua
@@ -6,12 +6,18 @@ local S = {}
 
 local progs = {}
 
+local MaxSubprocess <const> = 8
+local WaitQueue = {}
+
 function S.run(command)
+    while #progs > MaxSubprocess do
+        WaitQueue[#WaitQueue+1] = command
+        ltask.wait(command)
+    end
     local prog, err = subprocess.spawn(command)
     if not prog then
         return nil, err
     end
-    progs[#progs + 1] = prog
     return ltask.wait(prog)
 end
 
@@ -30,6 +36,12 @@ local function update()
             local errmsg = prog.stdout:read "a"
             local errcode = prog:wait()
             ltask.wakeup(prog, errcode, errmsg)
+        end
+    end
+    if #WaitQueue > 0 and #progs < MaxSubprocess then
+        local n = math.min(MaxSubprocess-#progs, #WaitQueue)
+        for _ = 1, n do
+            ltask.wakeup(table.remove(WaitQueue, 1))
         end
     end
 end
