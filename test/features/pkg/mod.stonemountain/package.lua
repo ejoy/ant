cfce3fb18380b9a729fd1e44b20b8552c28b6282
junjie020 <kitchen.gz.020@gmail.com>
2023-04-23 14:32:29 +0800

    remove code

--- test/features/pkg/mod.stonemountain/package.lua
+++
@@ -1,9 +0,0 @@
-return {
-    name = "mod.stonemountain",
-    dependencies = {
-        "ant.terrain",
-        "ant.render",
-        "ant.math",
-        "ant.camera"
-    }
-}
--- test/features/pkg/mod.terrain/package.lua
+++
@@ -1,9 +0,0 @@
-return {
-    name = "mod.terrain",
-    dependencies = {
-        "ant.render",
-        "ant.render",
-        "ant.math",
-        "ant.camera"
-    }
-}
