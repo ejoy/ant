86d4e0fc7734e2ec141f5e7c1e5b094b311a472c
junjie020 <kitchen.gz.020@gmail.com>
2023-05-10 10:08:49 +0800

    fix camera update bug

--- pkg/ant.camera/camera.lua
+++ pkg/ant.camera/camera.lua
@@ -186,15 +186,12 @@ local function update_camera_info(e)
 	sa:update("u_camera_param", math3d.vector(nn, ff, inv_nn, inv_ff))
 end
 
-local has_updated = {}
 function cameraview_sys:update_camera()
-    has_updated = {}
     for v in w:select "visible queue_name camera_depend:absent camera_ref:in" do
-        local e <close> = w:entity(v.camera_ref, "scene_changed?in camera_changed?in camera:in scene:in eid:in")
-        if has_updated[e.eid] == nil and e.scene_changed or e.camera_changed then
+        local e <close> = w:entity(v.camera_ref, "scene_changed?in camera_changed?in camera:in scene:in")
+        if e.scene_changed or e.camera_changed then
             update_camera(e)
             update_camera_info(e)
-            has_updated[e.eid] = true
         end
     end
 end
