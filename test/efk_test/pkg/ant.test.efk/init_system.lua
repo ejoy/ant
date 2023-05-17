3032d91af42929ce9f67d938d01f970e4ab7bb05
junjie020 <kitchen.gz.020@gmail.com>
2023-05-17 11:01:02 +0800

    fix efk_test

--- test/efk_test/pkg/ant.test.efk/init_system.lua
+++ test/efk_test/pkg/ant.test.efk/init_system.lua
@@ -10,12 +10,21 @@ local iom       = ecs.import.interface "ant.objcontroller|iobj_motion"
 local is = ecs.system "init_system"
 
 function is:init()
-    
+    iefk.preload{
+        "/pkg/ant.test.efk/assets/miner_efk/a1.texture",
+        "/pkg/ant.test.efk/assets/miner_efk/a2.texture",
+        "/pkg/ant.test.efk/assets/miner_efk/a3.texture",
+    }
+
+    iefk.create("/pkg/ant.test.efk/assets/miner_efk/miner_dust.efk", {
+        visible = true,
+        loop = true,
+        auto_play = true
+    })
 end
 
 function is:init_world()
-    iefk.create "/pkg/ant.test.efk/assets/miner_efk/miner_dust.efk"
-    
+
     local mq = w:first "main_queue camera_ref:in"
     local ce <close> = w:entity(mq.camera_ref)
     iom.set_position(ce, math3d.vector(0.0, 0.0, 10.0))
