03d6552cc21cf4a0075c65b9de5958e3236e7749
Cloud Wu <cloudwu@gmail.com>
2023-05-22 19:34:39 +0800

    Optimize cull

--- pkg/ant.render/cull.cpp
+++ pkg/ant.render/cull.cpp
@@ -7,6 +7,8 @@ extern "C"{
 	#include "math3dfunc.h"
 }
 
+#include <cassert>
+#include <cstring>
 #include <memory>
 #include <unordered_map>
 #include <vector>
@@ -15,6 +17,75 @@ extern "C"{
 
 using tags = std::vector<cid_t>;
 using cull_infos = std::unordered_map<uint64_t, tags>;
+
+struct cull_array {
+	uint64_t mid;
+	int n;
+};
+
+#define MAX_CULL_ARRAY 64
+#define MAX_CULL_CID 1024
+
+static inline int
+insert_cull_array(struct cull_array cull_a[], int n_cull, cid_t cid_a[], int n_cid, uint64_t mid, cid_t cid) {
+	int i;
+	int offset = 0;
+	for (i=0;i<n_cull;i++) {
+		offset += cull_a[i].n;
+		if (cull_a[i].mid == mid) {
+			++cull_a[i].n;
+			memmove(cid_a + offset + 1, cid_a + offset, sizeof(cid_t) * (n_cid - offset));
+			cid_a[offset] = cid;
+			return n_cull;
+		}
+	}
+	assert(n_cull < MAX_CULL_ARRAY);
+	assert(offset == n_cid);
+	cull_a[n_cull].mid = mid;
+	cull_a[n_cull].n = 1;
+	cid_a[n_cid] = cid;
+	return n_cull + 1;
+}
+
+/*
+static int
+lcull(lua_State *L) {
+	struct cull_array a[MAX_CULL_ARRAY];
+	int a_n = 0;
+	cid_t cid[MAX_CULL_CID];
+	int c_n = 0;
+
+	auto w = getworld(L);
+
+	for (auto e : ecs_api::select<ecs::cull_args>(w->ecs)){
+		const auto& i = e.get<ecs::cull_args>();
+		const auto id = (cid_t)i.renderable_id;
+		assert(c_n < MAX_CULL_CID);
+		a_n = insert_cull_array(a, a_n, cid, c_n++, i.frustum_planes.idx, id);
+		entity_clear_type(w->ecs, id);
+	}
+
+	if (a_n == 0)
+		return 0;
+
+	for (auto e : ecs_api::select<ecs::view_visible, ecs::render_object, ecs::bounding>(w->ecs)){
+		const auto &b = e.get<ecs::bounding>();
+		int i,j,offset = 0;
+		for (i = 0; i < a_n; i++) {
+			if (math_isnull(b.scene_aabb) || 
+				(math3d_frustum_intersect_aabb(w->math3d->M, math_t{a[i].mid}, b.scene_aabb) >= 0)) {
+				for (j = 0; j < a[i].n; j++) {
+					e.enable_tag(cid[offset+j]);
+				}
+			}
+			offset += a[i].n;
+		}
+
+	}
+
+	return 0;
+}
+*/
 
 template<typename Entity>
 static inline void
@@ -52,7 +123,6 @@ lcull(lua_State *L){
 
 	return 0;
 }
-
 
 extern "C" int
 luaopen_system_cull(lua_State *L) {
