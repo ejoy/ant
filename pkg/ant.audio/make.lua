61e4245b381fac1e78a4848071b008716aeef22d
actboy168 <actboy168@gmail.com>
2023-05-24 13:34:40 +0800

    cleanup

--- pkg/ant.audio/make.lua
+++ pkg/ant.audio/make.lua
@@ -7,24 +7,24 @@ local Ant3rd <const> = ROOT.."3rd/"
 local fmodDir = Ant3rd .. "fmod"
 local EnableLog = false
 
-local inputpaths =  {
-    fmodDir .. "/windows/core/lib/x64/" .. (EnableLog and "fmodL.dll" or "fmod.dll"),
-    fmodDir .. "/windows/studio/lib/x64/" .. (EnableLog and "fmodstudioL.dll" or "fmodstudio.dll"),
-}
-
-local outputpaths = {}
-
-for idx, d in ipairs(inputpaths) do
-    outputpaths[idx] = "../../" .. lm.bindir .. "/" .. fs.path(d):filename():string()
+if lm.os == "windows" then
+    local inputpaths =  {
+        fmodDir .. "/windows/core/lib/x64/" .. (EnableLog and "fmodL.dll" or "fmod.dll"),
+        fmodDir .. "/windows/studio/lib/x64/" .. (EnableLog and "fmodstudioL.dll" or "fmodstudio.dll"),
+    }
+    local outputpaths = {}
+    for idx, d in ipairs(inputpaths) do
+        outputpaths[idx] = "../../" .. lm.bindir .. "/" .. fs.path(d):filename():string()
+    end
+    lm:copy "copy_fmod" {
+        input = inputpaths,
+        output = outputpaths,
+    }
 end
 
-lm:copy "copy_fmod" {
-    input = inputpaths,
-    output = outputpaths,
-}
-
 lm:lua_source "audio" {
-    msvc = {
+    windows = {
+        deps = "copy_fmod",
         sources = "src/luafmod.c",
         includes = {
             fmodDir.."/windows/core/inc",
@@ -34,21 +34,14 @@ lm:lua_source "audio" {
             fmodDir.."/windows/core/lib/x64",
             fmodDir.."/windows/studio/lib/x64",
         },
+    },
+    msvc = {
         links = {
             EnableLog and "fmodL_vc" or "fmod_vc",
             EnableLog and "fmodstudioL_vc" or "fmodstudio_vc"
         },
     },
     mingw = {
-        sources = "src/luafmod.c",
-        includes = {
-            fmodDir.."/windows/core/inc",
-            fmodDir.."/windows/studio/inc",
-        },
-        linkdirs ={
-            fmodDir.."/windows/core/lib/x64",
-            fmodDir.."/windows/studio/lib/x64",
-        },
         links = {
             EnableLog and "fmodL" or "fmod",
             EnableLog and "fmodstudioL" or "fmodstudio"
@@ -86,8 +79,5 @@ lm:lua_source "audio" {
     },
     android = {
         sources = "src/empty_luafmod.c",
-    },
-    deps = {
-        "copy_fmod",
     }
 }
