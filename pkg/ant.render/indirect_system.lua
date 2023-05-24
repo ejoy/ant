7cf7ca3c213fb488815e8d444ea0acb45c5cc804
von <1052019421@qq.com>
2023-05-24 19:20:43 +0800

    fix indirect

--- pkg/ant.render/indirect_system.lua
+++ pkg/ant.render/indirect_system.lua
@@ -11,10 +11,10 @@ local renderpkg = import_package "ant.render"
 local viewidmgr = renderpkg.viewidmgr
 local main_viewid = viewidmgr.get "csm_fb"
 local indirect_material
-local function get_instance_memory_buffer(indirect_info)
+local function get_instance_memory_buffer(indirect_info, instance_data_count)
     local indirect_num = #indirect_info
     local fmt<const> = "ffff"
-    local memory_buffer = bgfx.memory_buffer(3 * 16 * indirect_num)
+    local memory_buffer = bgfx.memory_buffer(instance_data_count * 16 * indirect_num)
     local memory_buffer_offset = 1
     for indirect_idx = 1, indirect_num do
         local instance_data = indirect_info[indirect_idx]
@@ -57,14 +57,24 @@ local function check_destroy(ro)
     end
 end
 
+local function get_instance_data_count(instance_layout)
+    local cnt = 0
+    for v in string.gmatch(instance_layout, "|") do
+        cnt = cnt + 1
+    end
+    return cnt + 1
+end
+
 local function update_indirect_buffer(e)
     check_destroy(e.render_object)
     local indirect_info = e.indirect.indirect_info
     local indirect_num = #indirect_info
+    local instance_layout = e.indirect.instance_layout
     if indirect_num > 0 then
         local indirect_buffer = bgfx.create_indirect_buffer(indirect_num)
-        local instance_memory_buffer = get_instance_memory_buffer(indirect_info)
-        local instance_buffer = bgfx.create_dynamic_vertex_buffer(instance_memory_buffer, declmgr.get "t45NIf|t46NIf|t47NIf".handle, "r")
+        local instance_data_count = get_instance_data_count(instance_layout)
+        local instance_memory_buffer = get_instance_memory_buffer(indirect_info, instance_data_count)
+        local instance_buffer = bgfx.create_dynamic_vertex_buffer(instance_memory_buffer, declmgr.get(instance_layout).handle, "r")
         local instance_params = math3d.vector(0, e.render_object.vb_num, 0, e.render_object.ib_num)
         local indirect_params = math3d.vector(indirect_num, 0, 0, 0)
         create_indirect_compute(indirect_num, indirect_buffer, instance_buffer, instance_params, indirect_params)
