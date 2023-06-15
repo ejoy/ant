61061c7f44f6ead7063c905470185d598a29bbcb
aimoonchen <kungfuchen@gmail.com>
2023-06-15 17:00:24 +0800

    select file by keyboard

--- tools/prefab_editor/pkg/tools.prefab_editor/widget/resource_browser.lua
+++ tools/prefab_editor/pkg/tools.prefab_editor/widget/resource_browser.lua
@@ -12,7 +12,7 @@ local gd        = require "common.global_data"
 local icons     = require "common.icons"(assetmgr)
 local faicons   = require "common.fa_icons"
 local editor_setting=require "editor_setting"
-
+local global_data = require "common.global_data"
 local m = {
     dirty = true
 }
@@ -177,22 +177,36 @@ local function create_filter_cache(parent, dirs, files)
        return
     end
     local cache = {}
-    local function cache_path(path, at)
+    local count = 0
+    local offset = 0
+    local offset_map = {}
+    local function cache_path(path, at, pos)
         local str = tostring(path:filename())
         local prefix = string.upper(string.sub(str, 1, 1))
         if not at[prefix] then
             at[prefix] = {}
         end
         local t = at[prefix]
-        t[#t + 1] = path
+        t[#t + 1] = {path = path, pos = pos}
+        return prefix, t
     end
+    local pos = 1
     for _, v in pairs(dirs) do
-        cache_path(v[1], cache)
+        local k, t = cache_path(v[1], cache, pos)
+        offset_map[k] = offset
+        offset = offset + #t
+        pos = pos + 1
     end
     for _, v in pairs(files) do
-        cache_path(v, cache)
+        local k, t = cache_path(v, cache, pos)
+        offset_map[k] = offset
+        offset = offset + #t
+        pos = pos + 1
     end
-    filter_cache[key] = cache
+    for _, value in pairs(cache) do
+        count = count + #value
+    end
+    filter_cache[key] = {files_map = cache, count = count, offset_map = offset_map}
 end
 local current_filter_idx
 local current_filter_key
@@ -200,19 +214,24 @@ local function get_filter_path(parent, key)
     if #key > 1 then
         return
     end
+    local dirty = false
     if key ~= current_filter_key then
         current_filter_key = key
         current_filter_idx = 1
+        dirty = true
     end
-    local filter = filter_cache[tostring(parent)][current_filter_key]
+    local files_map = filter_cache[tostring(parent)].files_map
+    local filter = files_map[current_filter_key]
     if not filter or #filter < 1 then
         return
     end
-    current_filter_idx = current_filter_idx + 1
-    if current_filter_idx > #filter then
-        current_filter_idx = 1
+    if not dirty then
+        current_filter_idx = current_filter_idx + 1
+        if current_filter_idx > #filter then
+            current_filter_idx = 1
+        end
     end
-    return filter[current_filter_idx]
+    return filter[current_filter_idx].path, filter[current_filter_idx].pos
 end
 function m.show()
     if not gd.project_root then
@@ -289,12 +308,13 @@ function m.show()
         end
         imgui.windows.PopStyleVar(1)
         imgui.cursor.Separator()
-
-        --imgui.deprecated.Columns(3)
+        local filter_focus1 = false
+        local filter_focuse2 = false
         if imgui.table.Begin("InspectorTable", 3, imgui.flags.Table {'Resizable', 'ScrollY'}) then
             imgui.table.NextColumn()
             local child_width, child_height = imgui.windows.GetContentRegionAvail()
             imgui.windows.BeginChild("##ResourceBrowserDir", child_width, child_height, false)
+            filter_focus1 = imgui.windows.IsWindowFocused()
             do_show_browser(resource_tree)
             imgui.windows.EndChild()
 
@@ -305,19 +325,36 @@ function m.show()
             
             local folder = selected_folder[2]
             if folder then
-                -- if imgui.windows.IsWindowHovered() then
-                --     for _, key, press, status in key_mb:unpack() do
-                --         print(key, press)
-                --     end
-                -- end
                 if imgui.windows.IsWindowFocused() then
+                    filter_focuse2 = true
                     create_filter_cache(selected_folder[1], folder.dirs, folder.files)
                     for _, key, press, status in key_mb:unpack() do
-                        if press == 1 then
-                            selected_file = get_filter_path(selected_folder[1], key) or selected_file
+                        if #key == 1 and press == 1 then
+                            local cache = filter_cache[tostring(selected_folder[1])]
+                            local file_count = cache and cache.count or 0
+                            local file_path, file_pos = get_filter_path(selected_folder[1], key)
+                            if file_path then
+                                local _, sy = imgui.windows.GetWindowSize()
+                                -- local max_scrolly = imgui.windows.GetScrollMaxY()
+                                selected_file = file_path or selected_file
+                                -- local scroll_pos = ((file_pos - 1) / file_count) * max_scrolly
+                                -- if file_pos > 1 then
+                                --     scroll_pos = scroll_pos + 22
+                                -- end
+                                local current_pos = imgui.windows.GetScrollY()
+                                local target_pos = file_pos * 22
+                                if target_pos > current_pos + sy or target_pos < sy or target_pos < (current_pos - sy) then
+                                    local newpos = target_pos - sy
+                                    if newpos < 0 then
+                                        newpos = 0
+                                    end
+                                    imgui.windows.SetScrollY(newpos)
+                                end
+                            end
                         end
                     end
                 else
+                    filter_focuse2 = false
                     if current_filter_key then
                         current_filter_key = nil
                         selected_file = nil
@@ -437,6 +474,7 @@ function m.show()
             imgui.windows.EndChild()
         imgui.table.End()
         end
+        global_data.camera_lock = filter_focus1 or filter_focuse2
     end
     imgui.windows.End()
 end
