bb9cb6fc03e486893984051aa9c37c0b87edf803
actboy168 <actboy168@gmail.com>
2023-06-29 10:39:33 +0800

    rework select

--- clibs/ecs/ecs/select.h
+++ clibs/ecs/ecs/select.h
@@ -37,105 +37,100 @@ namespace ecs_api {
         constexpr bool has_element_v = has_element<Component, Components...>();
     }
 
-    template <typename MainKey, typename...Components>
-    struct cached {
-        using mainkey = MainKey;
-        struct ecs_context* ctx;
-        struct ecs_cache* c;
-        int n = 0;
-        cached(struct ecs_context* ctx) noexcept {
-            std::array<int, 1+sizeof...(Components)> keys {component<MainKey>::id, component<Components>::id...};
-            struct ecs_cache* c = entity_cache_create(ctx, keys.data(), static_cast<int>(keys.size()));
-            this->ctx = ctx;
-            this->c = c;
+    struct context: public ecs_context {
+    private:
+        context() noexcept {}
+    public:
+        static inline context& create(ecs_context* ctx) noexcept {
+            return *(context*)ctx;
         }
-        ~cached() noexcept {
-            entity_cache_release(ctx, c);
+        ecs_context* ctx() const noexcept {
+            return const_cast<ecs_context*>((const ecs_context*)this);
         }
-    };
-
-    template <typename Component, typename MainKey, typename...Components>
-    Component* cache_fetch(cached<MainKey, Components...>& cache, int index) noexcept {
-        static_assert(impl::has_element_v<Component, Components...>);
-        return (Component*)entity_cache_fetch(cache.ctx, cache.c, index, component<Component>::id);
-    }
-
-    template <typename MainKey, typename...Components>
-    void cache_sync(cached<MainKey, Components...>& cache) noexcept {
-        cache.n = entity_cache_sync(cache.ctx, cache.c);
-    }
-
-    namespace impl {
-        inline ecs_context* context(ecs_context& ctx) {
-            return &ctx;
+        void sync() noexcept {
         }
-        template <typename Cached>
-        ecs_context* context(Cached& cache) {
-            return cache.ctx;
-        }
-
         template <typename Component>
-        Component* iter(ecs_context& ctx, int i) noexcept {
+        Component* iter(int i) noexcept {
             static_assert(!std::is_function<Component>::value);
-            return (Component*)entity_iter(&ctx, component<Component>::id, i);
+            return (Component*)entity_iter(ctx(), component<Component>::id, i);
         }
-
-        template <typename Component, typename Cached>
-            requires (!std::is_function_v<Component> && component<Component>::tag)
-        Component* iter(Cached& cache, int i) noexcept {
-            return i < cache.n
-                ? (Component*)(~(uintptr_t)0)
-                : nullptr;
-        }
-
-        template <typename Component, typename Cached>
-            requires (!std::is_function_v<Component> && !component<Component>::tag && component<Component>::id != EID)
-        Component* iter(Cached& cache, int i) noexcept {
-            return cache_fetch<Component>(cache, i);
-        }
-
-        template <typename Component, typename Cached>
-            requires (!std::is_function_v<Component> && component<Component>::id == EID)
-        Component* iter(Cached& cache, int i) noexcept {
-            return (Component*)entity_iter(cache.ctx, EID, i);
-        }
-
         template <typename Component>
-        Component* sibling(ecs_context& ctx, int mainkey, int i) noexcept {
+        Component* sibling(int mainkey, int i) noexcept {
             static_assert(!std::is_function<Component>::value);
-            return (Component*)entity_sibling(&ctx, mainkey, i, component<Component>::id);
+            return (Component*)entity_sibling(ctx(), mainkey, i, component<Component>::id);
         }
-
-        template <typename Component, typename Cached>
-            requires (!std::is_function_v<Component> && component<Component>::id != EID)
-        Component* sibling(Cached& cache, [[maybe_unused]] int mainkey, int i) noexcept {
-            return cache_fetch<Component>(cache, i);
-        }
-
-        template <typename Component, typename Cached>
-            requires (!std::is_function_v<Component> && component<Component>::id == EID)
-        Component* sibling(Cached& cache, [[maybe_unused]] int mainkey, int i) noexcept {
-            return (Component*)entity_sibling(cache.ctx, component<typename Cached::mainkey>::id, i, EID);
-        }
-
         template <typename Component>
-        Component* sibling(ecs_context* ctx, int mainkey, int i, lua_State* L) {
+        Component* sibling(int mainkey, int i, lua_State* L) {
             static_assert(!std::is_function<Component>::value);
-            auto c = sibling<Component>(ctx, mainkey, i);
+            auto c = sibling<Component>(ctx(), mainkey, i);
             if (c == NULL) {
                 luaL_error(L, "component `%s` not found.", component<Component>::name);
             }
             return c;
         }
+    };
 
-        inline void sync(ecs_context& ctx) noexcept {
+    template <typename MainKey, typename...Components>
+    struct cached_context {
+    private:
+        struct ecs_context* context;
+        struct ecs_cache* c;
+        int n = 0;
+    public:
+        cached_context(struct ecs_context* ctx) noexcept {
+            std::array<int, 1+sizeof...(Components)> keys {component<MainKey>::id, component<Components>::id...};
+            struct ecs_cache* c = entity_cache_create(ctx, keys.data(), static_cast<int>(keys.size()));
+            this->context = ctx;
+            this->c = c;
+        }
+        ~cached_context() noexcept {
+            entity_cache_release(ctx(), c);
+        }
+        ecs_context* ctx() const noexcept {
+            return context;
+        }
+        template <typename Component>
+        Component* _fetch(int index) noexcept {
+            static_assert(impl::has_element_v<Component, Components...>);
+            return (Component*)entity_cache_fetch(ctx(), c, index, component<Component>::id);
+        }
+        void sync() noexcept {
+            n = entity_cache_sync(ctx(), c);
         }
 
-        template <typename Cached>
-        void sync(Cached& cache) noexcept {
-            cache_sync(cache);
+        template <typename Component>
+            requires (!std::is_function_v<Component> && component<Component>::tag)
+        Component* iter(int i) noexcept {
+            static_assert(std::is_same_v<Component, MainKey>);
+            return i < n
+                ? (Component*)(~(uintptr_t)0)
+                : nullptr;
         }
+        template <typename Component>
+            requires (!std::is_function_v<Component> && !component<Component>::tag && component<Component>::id != EID)
+        Component* iter(int i) noexcept {
+            static_assert(std::is_same_v<Component, MainKey>);
+            return _fetch<Component>(i);
+        }
+        template <typename Component>
+            requires (!std::is_function_v<Component> && component<Component>::id == EID)
+        Component* iter(int i) noexcept {
+            static_assert(std::is_same_v<Component, MainKey>);
+            return (Component*)entity_iter(ctx(), EID, i);
+        }
+        template <typename Component>
+            requires (!std::is_function_v<Component> && component<Component>::id != EID)
+        Component* sibling([[maybe_unused]] int mainkey, int i) noexcept {
+            return _fetch<Component>(i);
+        }
+        template <typename Component>
+            requires (!std::is_function_v<Component> && component<Component>::id == EID)
+        Component* sibling([[maybe_unused]] int mainkey, int i) noexcept {
+            return (Component*)entity_sibling(ctx(), component<MainKey>::id, i, EID);
+        }
+    };
 
+    namespace impl {
         template <typename...Ts>
         using components = decltype(std::tuple_cat(
             std::declval<std::conditional_t<std::is_empty<Ts>::value || std::is_function<Ts>::value,
@@ -158,6 +153,9 @@ namespace ecs_api {
     template <typename Context, typename MainKey, typename ...SubKey>
     struct basic_entity {
     public:
+        basic_entity(ecs_context* ctx) noexcept
+            : ctx(context::create(ctx))
+        { }
         basic_entity(Context& ctx) noexcept
             : ctx(ctx)
         { }
@@ -165,7 +163,7 @@ namespace ecs_api {
             requires (component<Component>::id == EID && component<MainKey>::id == EID)
         basic_entity(Context& ctx, Component eid) noexcept
             : ctx(ctx) {
-            index = entity_index(impl::context(ctx), (void*)eid);
+            index = entity_index(ctx.ctx(), (void*)eid);
             std::get<Component*>(c) = (Component*)eid;
         }
         static constexpr int kInvalidIndex = -1;
@@ -175,7 +173,7 @@ namespace ecs_api {
             eof,
         };
         fetch_status fetch(int id) noexcept {
-            auto v = impl::iter<MainKey>(ctx, id);
+            auto v = ctx.iter<MainKey>(id);
             if (!v) {
                 return fetch_status::eof;
             }
@@ -212,7 +210,7 @@ namespace ecs_api {
             }
         }
         void remove() const noexcept {
-            entity_remove(impl::context(ctx), component<MainKey>::id, index);
+            entity_remove(ctx.ctx(), component<MainKey>::id, index);
         }
         int getid() const noexcept {
             return index;
@@ -238,48 +236,48 @@ namespace ecs_api {
         template <typename T>
             requires (component<T>::id != EID)
         bool has() const noexcept {
-            return !!impl::sibling<T>(ctx, component<MainKey>::id, index);
+            return !!ctx.sibling<T>(component<MainKey>::id, index);
         }
         template <typename T>
             requires (component<T>::tag)
         bool sibling() const noexcept {
-            return !!impl::sibling<T>(ctx, component<MainKey>::id, index);
+            return !!ctx.sibling<T>(component<MainKey>::id, index);
         }
         template <typename T>
             requires (component<T>::id != EID && !component<T>::tag && !std::is_empty<T>::value)
         T* sibling() const noexcept {
-            return impl::sibling<T>(ctx, component<MainKey>::id, index);
+            return ctx.sibling<T>(component<MainKey>::id, index);
         }
         template <typename T>
             requires (component<T>::id == EID)
         T sibling() const noexcept {
-            return (T)impl::sibling<T>(ctx, component<MainKey>::id, index);
+            return (T)ctx.sibling<T>(component<MainKey>::id, index);
         }
         template <typename T>
             requires (component<T>::id == EID)
         T sibling(lua_State* L) const {
-            return (T)impl::sibling<T>(ctx, component<MainKey>::id, index, L);
+            return (T)ctx.sibling<T>(component<MainKey>::id, index, L);
         }
         template <typename T>
             requires (component<T>::id != EID && !std::is_empty<T>::value)
         T& sibling(lua_State* L) const {
-            return *impl::sibling<T>(ctx, component<MainKey>::id, index, L);
+            return *ctx.sibling<T>(component<MainKey>::id, index, L);
         }
         template <typename T>
             requires (component<T>::tag)
         void enable_tag() noexcept {
-            entity_enable_tag(impl::context(ctx), component<MainKey>::id, index, component<T>::id);
+            entity_enable_tag(ctx.ctx(), component<MainKey>::id, index, component<T>::id);
         }
         void enable_tag(int id) noexcept {
-            entity_enable_tag(impl::context(ctx), component<MainKey>::id, index, id);
+            entity_enable_tag(ctx.ctx(), component<MainKey>::id, index, id);
         }
         template <typename T>
             requires (component<T>::tag)
         void disable_tag() noexcept {
-            entity_disable_tag(impl::context(ctx), component<MainKey>::id, index, component<T>::id);
+            entity_disable_tag(ctx.ctx(), component<MainKey>::id, index, component<T>::id);
         }
         void disable_tag(int id) noexcept {
-            entity_disable_tag(impl::context(ctx), component<MainKey>::id, index, id);
+            entity_disable_tag(ctx.ctx(), component<MainKey>::id, index, id);
         }
     private:
         template <std::size_t Is, typename T>
@@ -292,7 +290,7 @@ namespace ecs_api {
         bool init_sibling(int i) noexcept {
             if constexpr (std::is_function<Component>::value) {
                 using C = typename std::invoke_result<Component, flags::absent>::type;
-                auto v = impl::sibling<C>(ctx, component<MainKey>::id, i);
+                auto v = ctx.sibling<C>(component<MainKey>::id, i);
                 if (v) {
                     return false;
                 }
@@ -302,7 +300,7 @@ namespace ecs_api {
                 return true;
             }
             else {
-                auto v = impl::sibling<Component>(ctx, component<MainKey>::id, i);
+                auto v = ctx.sibling<Component>(component<MainKey>::id, i);
                 if (!v) {
                     return false;
                 }
@@ -320,10 +318,10 @@ namespace ecs_api {
     };
 
     template <typename MainKey, typename ...SubKey>
-    using entity = basic_entity<ecs_context, MainKey, SubKey...>;
+    using entity = basic_entity<context, MainKey, SubKey...>;
 
     template <typename MainKey, typename ...SubKey>
-    using cached_entity = basic_entity<cached<MainKey, SubKey...>, MainKey, SubKey...>;
+    using cached_entity = basic_entity<cached_context<MainKey, SubKey...>, MainKey, SubKey...>;
 
     namespace impl {
         template <typename Context, typename ...Args>
@@ -335,7 +333,7 @@ namespace ecs_api {
                 entity_type e;
                 iterator(begin_t, Context& ctx) noexcept
                     : e(ctx) {
-                    impl::sync(ctx);
+                    ctx.sync();
                     e.next();
                 }
                 iterator(end_t, Context& ctx) noexcept
@@ -371,12 +369,11 @@ namespace ecs_api {
         };
         
         template <typename ...Args>
-        using selector = basic_selector<ecs_context, Args...>;
+        using selector = basic_selector<context, Args...>;
 
         template <typename ...Args>
-        using cached_selector = basic_selector<cached<Args...>, Args...>;
+        using cached_selector = basic_selector<cached_context<Args...>, Args...>;
 
-        
         template <typename Component>
         struct array_range {
             array_range(ecs_context* ctx) noexcept {
@@ -416,7 +413,7 @@ namespace ecs_api {
 
     template <typename Component>
     auto create_entity(ecs_context* ctx) noexcept {
-        entity<Component> e(*ctx);
+        entity<Component> e({ctx});
         int index = entity_new(ctx, component<Component>::id, NULL);
         if (index >= 0) {
             bool ok = e.init(index);
@@ -433,7 +430,7 @@ namespace ecs_api {
 
     template <typename ...Args>
     auto select(ecs_context* ctx) noexcept {
-        return impl::selector<Args...>(*ctx);
+        return impl::selector<Args...>(context::create(ctx));
     }
 
     template <typename Context, typename ...Args>
@@ -442,7 +439,7 @@ namespace ecs_api {
     }
 
     template <typename ...Args>
-    auto cached_select(cached<Args...>& cache) noexcept {
+    auto cached_select(cached_context<Args...>& cache) noexcept {
         return impl::cached_selector<Args...>(cache);
     }
 }
