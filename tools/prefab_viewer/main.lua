3a638939635aa97197e8b9ff4ddba037f50b4714
aimoonchen <kungfuchen@gmail.com>
2023-07-26 15:35:46 +0800

    add prefab viewer

---
+++ tools/prefab_viewer/main.lua
@@ -0,0 +1,25 @@
+package.path = "engine/?.lua"
+require "bootstrap"
+import_package "ant.window".start {
+    enable_mouse = true,
+    import = {
+        "@ant.tools.prefab_viewer",
+    },
+    pipeline = {
+        "init",
+        "update",
+        "exit",
+    },
+    system = {
+        "ant.tools.prefab_viewer|init_system",
+    },
+    interface = {
+        "ant.objcontroller|iobj_motion",
+    },
+    policy = {
+        "ant.general|name",
+        "ant.scene|scene_object",
+        "ant.render|render",
+        "ant.render|render_queue",
+    }
+}
---
+++ tools/prefab_viewer/pkg/ant.tools.prefab_viewer/init_system.lua
@@ -0,0 +1,239 @@
+local ecs   = ...
+local world = ecs.world
+local w     = world.w
+
+local math3d = require "math3d"
+local mathpkg = import_package "ant.math"
+local mu, mc = mathpkg.util, mathpkg.constant
+local iom = ecs.import.interface "ant.objcontroller|iobj_motion"
+local iani = ecs.import.interface "ant.animation|ianimation"
+local irmlui = ecs.import.interface "ant.rmlui|irmlui"
+local iterrain  = ecs.import.interface "mod.terrain|iterrain"
+local S = ecs.system "init_system"
+
+function S.init()
+
+end
+local imaterial = ecs.import.interface "ant.asset|imaterial"
+local iefk = ecs.import.interface "ant.efk|iefk"
+local fs = require "filesystem"
+function S.init_world()
+    local files = {}
+    for file in fs.pairs(fs.path("/pkg/ant.tools.prefab_viewer/assets/efk")) do
+        local s = file:string()
+        if s:match("%.texture$") then
+            files[#files+1] = s
+        end
+    end
+    iefk.preload(files)
+
+    local light = ecs.create_instance("/pkg/ant.tools.prefab_viewer/assets/prefabs/light.prefab")
+    world:create_object(light)
+	-- ground plane
+	-- ecs.create_entity {
+	-- 	policy = {
+	-- 		"ant.render|render",
+	-- 		"ant.general|name",
+	-- 	},
+	-- 	data = {
+	-- 		scene = {s = {200, 1, 200}},
+	-- 		mesh  = "/pkg/ant.tools.prefab_viewer/assets/glb/plane.glb|meshes/Plane_P1.meshbin",
+	-- 		material    = "/pkg/ant.tools.prefab_viewer/assets/materials/texture_plane.material",
+	-- 		render_layer = "background",
+	-- 		visible_state= "main_view",
+	-- 		name        = "ground",
+	-- 		on_ready = function (e)
+	-- 			imaterial.set_property(e, "u_uvmotion", math3d.vector{0, 0, 100, 100})
+	-- 		end
+	-- 	},
+	-- }
+
+	-- terrain
+	iterrain.gen_terrain_field(128, 128, 64, 10)
+	-- test prefab
+    local miner = ecs.create_instance("/pkg/ant.tools.prefab_viewer/assets/prefabs/miner-1.prefab")
+    function miner:on_init() end
+    miner.on_ready = function(instance)
+        for _, eid in ipairs(instance.tag["*"]) do
+            local e <close> = w:entity(eid, "tag?in anim_ctrl?in")
+            if e.anim_ctrl then
+                iani.load_events(eid, "/pkg/ant.tools.prefab_viewer/assets/prefabs/miner-1.event")
+            end
+        end
+        iani.play(instance, {name = "work", loop = true, speed = 1.0, manual = false})
+    end
+    function miner:on_message(msg) end
+    function miner:on_update() end
+    world:create_object(miner)
+
+    -- camera
+    local mq = w:first "main_queue camera_ref:in"
+    local ce<close> = w:entity(mq.camera_ref, "camera:in")
+    local eyepos = math3d.vector(0, 60, -60)
+    iom.set_position(ce, eyepos)
+    local dir = math3d.normalize(math3d.sub(math3d.vector(0.0, 0.0, 0.0, 1.0), eyepos))
+    iom.set_direction(ce, dir)
+
+    -- rmlui
+    irmlui.set_prefix "/pkg/ant.tools.prefab_viewer/assets/ui"
+    irmlui.add_bundle "/pkg/ant.tools.prefab_viewer/assets/ui/ui.bundle"
+    irmlui.font_dir "/pkg/ant.tools.prefab_viewer/assets/ui/font"
+    local window = irmlui.open "joystick.rml"
+	window.addEventListener("message", function(event) world:pub {"joystick", event.data} end)
+end
+
+local last_mousex
+local last_mousey
+local mousex
+local mousey
+local rotate_mode
+local move_mode
+local camera_speed = 1.0
+local move_speed = 2.5
+local zoom_speed = 1.0
+local rotate_speed = 0.002
+
+local joystick_dir = {0, 0}-- = math3d.ref()
+local joystick_active = false
+local function on_joystick()
+	if not joystick_active then
+		return
+	end
+	local mq = w:first("main_queue camera_ref:in render_target:in")
+	local ce<close> = w:entity(mq.camera_ref, "scene:update")
+	local pos = iom.get_position(ce)
+	local mat = math3d.matrix{s = iom.get_scale(ce), r = iom.get_rotation(ce), t = pos}
+	local xdir = math3d.normalize(math3d.index(mat, 1))
+	local zdir = math3d.normalize(math3d.index(mat, 3))
+	local dir = math3d.normalize(math3d.add(math3d.mul(xdir, joystick_dir[1]), math3d.mul(zdir, joystick_dir[2])))
+	-- local dir = math3d.transform(joystick_angle, math3d.vector(0, 0, 1), 0)
+	iom.set_position(ce, math3d.add(pos, math3d.mul(dir, 0.5)))
+end
+
+local function on_key(key, press)
+	local pressed = (press == 1 or press == 2)
+	local pan = false
+	if key == "A" or key == "D" or key == "W" or key == "S" or key == "F" then
+		pan = true
+	end
+	if not pressed or not pan then
+		return
+	end
+	local mq = w:first("main_queue camera_ref:in render_target:in")
+	local ce<close> = w:entity(mq.camera_ref, "scene:update")
+	local pos = iom.get_position(ce)
+	local dt = move_speed * camera_speed
+	local mat = math3d.matrix{s = iom.get_scale(ce), r = iom.get_rotation(ce), t = pos}
+	local xdir = math3d.normalize(math3d.index(mat, 1))
+	local zdir = math3d.normalize(math3d.index(mat, 3))
+	local newpos
+	if key == "A" then
+		newpos = math3d.add(pos, math3d.mul(math3d.normalize(xdir), -dt))
+	elseif key == "D" then
+		newpos = math3d.add(pos, math3d.mul(math3d.normalize(xdir), dt))
+	elseif key == "W" then
+		newpos = math3d.add(pos, math3d.mul(math3d.normalize(zdir), dt))
+	elseif key == "S" then
+		newpos = math3d.add(pos, math3d.mul(math3d.normalize(zdir), -dt))
+	end
+	if newpos then
+		iom.set_position(ce, newpos)
+	end
+end
+
+local function on_right_mouse(dx, dy)
+	local mq = w:first("main_queue camera_ref:in render_target:in")
+	local ce<close> = w:entity(mq.camera_ref, "scene:update")
+	local delta_yaw = dx * rotate_speed-- * camera_speed
+	local delta_pitch = dy * rotate_speed
+	local rad = math3d.tovalue(math3d.quat2euler(iom.get_rotation(ce)))
+	local yaw = rad[2] - delta_yaw
+	local pitch = rad[1] - delta_pitch
+	-- min/max pitch
+	if pitch > 1.47 then
+		pitch = 1.47
+	elseif pitch < -1.47 then
+		pitch = -1.47
+	end
+	local rot = math3d.quaternion{pitch, yaw, 0}
+	iom.set_rotation(ce, rot)
+end
+
+local function on_middle_mouse(dx, dy)
+	local mq = w:first("main_queue camera_ref:in render_target:in")
+	local ce<close> = w:entity(mq.camera_ref, "scene:update")
+	local pos = iom.get_position(ce)
+	local mat = math3d.matrix{s = iom.get_scale(ce), r = iom.get_rotation(ce), t = pos}
+	local xdir = math3d.normalize(math3d.index(mat, 1))
+	local ydir = math3d.normalize(math3d.index(mat, 2))
+	local dir = math3d.add(math3d.mul(xdir, dx), math3d.mul(ydir, -dy))
+	iom.set_position(ce, math3d.add(pos, math3d.mul(dir, 0.02 * move_speed * camera_speed)))
+end
+
+local function on_wheel(delta)
+	local mq = w:first("main_queue camera_ref:in render_target:in")
+	local ce<close> = w:entity(mq.camera_ref, "scene:update")
+	local dt = delta * zoom_speed * move_speed * camera_speed
+	local pos = iom.get_position(ce)
+	local zdir = math3d.index(math3d.matrix{s = iom.get_scale(ce), r = iom.get_rotation(ce), t = pos}, 3)
+	iom.set_position(ce, math3d.add(pos, math3d.mul(math3d.normalize(zdir), dt)))
+end
+
+local kb_mb         = world:sub {"keyboard"}
+local mouse_mb      = world:sub {"mouse"}
+local gesture_pinch = world:sub { "gesture", "pinch"}
+local gesture_pan   = world:sub {"gesture", "pan"}
+local joystick_mb = world:sub {"joystick"}
+function S:data_changed()
+    for _, key, press, state in kb_mb:unpack() do
+        on_key(key, press)
+    end
+
+    for _, what, e in gesture_pinch:unpack() do
+		on_wheel(e.velocity)
+	end
+
+    for _, _, e in gesture_pan:unpack() do
+        on_right_mouse(-e.dx, -e.dy)
+    end
+	--joystick
+	for _, e in joystick_mb:unpack() do
+		joystick_active = e.moving
+		if e.moving then
+			local angle = math.atan(-e.dy, e.dx)
+			joystick_dir[1] = math.cos(angle)
+			joystick_dir[2] = math.sin(angle)
+		end
+    end
+	on_joystick()
+
+    for _, btn, state, x, y in mouse_mb:unpack() do
+		if state == "DOWN" then
+			last_mousex, last_mousey = x, y
+			if btn == "MIDDLE" then
+				move_mode = true
+            -- elseif btn == "RIGHT" then
+            --     rotate_mode = true
+			end
+		end
+		if state == "MOVE" then
+			if move_mode then
+				mousex, mousey = x, y
+				on_middle_mouse(last_mousex - mousex, last_mousey - mousey)
+				last_mousex, last_mousey = x, y
+            -- elseif rotate_mode then
+            --     mousex, mousey = x, y
+            --     on_right_mouse(last_mousex - mousex, last_mousey - mousey)
+            --     last_mousex, last_mousey = x, y
+			end
+		end
+		if state == "UP" then
+			rotate_mode = false
+			move_mode = false
+		end
+	end
+end
+
+function S:camera_usage()
+
+end
