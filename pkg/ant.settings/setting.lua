c8d2c1ac9394741908ed5ef077eef23f5167eca4
actboy168 <actboy168@gmail.com>
2023-08-02 14:15:59 +0800

    remove graphic/api

--- pkg/ant.settings/setting.lua
+++ pkg/ant.settings/setting.lua
@@ -3,9 +3,6 @@ local fs = require "filesystem"
 local reg = require "registry"
 local settings = assert(reg.create(fs.path "/settings"))
 
-local BgfxOS <const> = {
-    macos = "osx",
-}
-reg.use(settings, BgfxOS[platform.os] or platform.os)
+reg.use(settings, platform.os)
 
 return settings
