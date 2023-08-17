30e324eaccc4f0dfadb50246d25152656cb0ee2c
actboy168 <actboy168@gmail.com>
2023-08-17 15:51:41 +0800

    remove font_dir

--- tools/prefab_editor/pkg/tools.prefab_editor/init_system.lua
+++ tools/prefab_editor/pkg/tools.prefab_editor/init_system.lua
@@ -154,8 +154,9 @@ function m:init_world()
 end
 
 function m:post_init()
+	local font = import_package "ant.font"
+    font.import "/pkg/ant.resources.binary/ui/test/assets/font/simsun.ttc"
     iRmlUi.set_prefix "/pkg/tools.prefab_editor/res/ui"
-    iRmlUi.font_dir "/pkg/ant.resources.binary/ui/test/assets/font/"
 end
 
 function m:data_changed()
