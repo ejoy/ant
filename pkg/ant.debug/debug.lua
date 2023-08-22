154c40d4ef590c7e5c1e5b9a1bc8a39ca8a7053b
von <1052019421@qq.com>
2023-08-22 14:28:16 +0800

    ant.debug

---
+++ pkg/ant.debug/debug.lua
@@ -0,0 +1,54 @@
+local ecs = ...
+local world = ecs.world
+local w = world.w
+
+local debug_sys = ecs.system "debug_system"
+
+local tick = 30
+
+local function hitch_cull_test()
+    if tick == 30 then
+        local groups = {}
+        local queuemgr = ecs.require "ant.render|queue_mgr"
+        local mainmask = queuemgr.queue_mask "main_queue"
+        local group_culled, group_no_culled, group_sum = 0, 0, 0
+        for e in w:select "hitch:in" do
+            local gid = e.hitch.group
+            if not groups[gid] then groups[gid] = {culled = 0, no_culled = 0, hitch_sum = 0, hitch_tag = 0} end
+            local gg = groups[gid]
+            gg.hitch_sum = gg.hitch_sum + 1
+            if 0 ~= (e.hitch.cull_masks & mainmask) then
+                gg.culled = gg.culled + 1
+            else
+                gg.no_culled = gg.no_culled + 1
+            end
+        end
+    
+        for gid, gg in pairs(groups) do
+            local g = ecs.group(gid)
+            g:enable "hitch_tag"
+            ecs.group_flush "hitch_tag"
+            local culled, no_culled, hitch_sum = gg.culled, gg.no_culled, gg.hitch_sum
+            for e in w:select "hitch_tag:in render_object:in" do
+                gg.hitch_tag = gg.hitch_tag + 1
+            end
+            group_culled, group_no_culled, group_sum = group_culled + gg.hitch_tag * culled, group_no_culled + gg.hitch_tag * no_culled, group_sum + gg.hitch_tag * hitch_sum
+            g:disable "hitch_tag"
+            ecs.group_flush "hitch_tag"
+        end
+    
+        print("hitch object group_culled:", group_culled)
+        print("hitch object group_no_culled:", group_no_culled)
+        print("hitch object group_sum:", group_sum)
+        print("----------------------------------------------------------------------")
+    else
+        tick = tick + 1 
+    end   
+end
+
+function debug_sys:init()
+end
+
+function debug_sys:data_changed()
+    hitch_cull_test()
+end
