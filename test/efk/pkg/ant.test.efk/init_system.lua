bf6af91af2cf5a1f9e8a5a446781f5fe0b77baed
junjie020 <kitchen.gz.020@gmail.com>
2023-08-30 11:22:10 +0800

    add group test for efk

--- test/efk/pkg/ant.test.efk/init_system.lua
+++ test/efk/pkg/ant.test.efk/init_system.lua
@@ -9,6 +9,7 @@ local iom       = ecs.require "ant.objcontroller|obj_motion"
 
 local is = ecs.system "init_system"
 
+local test_gid<const> = 1000001
 function is:init()
     iefk.preload{
         "/pkg/ant.test.efk/assets/miner_efk/a1.texture",
@@ -16,11 +17,53 @@ function is:init()
         "/pkg/ant.test.efk/assets/miner_efk/a3.texture",
     }
 
-    iefk.create("/pkg/ant.test.efk/assets/miner_efk/miner_dust.efk", {
-        visible = true,
-        loop = true,
-        auto_play = true
-    })
+    world:create_entity{
+        policy = {
+            "ant.general|name",
+            "ant.render|hitch_object",
+        },
+        data = {
+            name = "test_efk_hitch",
+            hitch = {
+                group = test_gid,
+            },
+            visible_state = "main_view",
+            scene = {
+                t = {5, 2, 0, 1}
+            }
+        }
+    }
+
+    world:create_entity({
+        policy = {
+            "ant.general|name",
+            "ant.scene|scene_object",
+            "ant.efk|efk",
+            "ant.general|tag"
+        },
+        data = {
+            name = "root",
+            tag = {"effect"},
+            scene = {},
+            efk = {
+                path = "/pkg/ant.test.efk/assets/miner_efk/miner_dust.efk",
+                auto_play = true,
+                loop = true,
+                visible = false,
+            },
+            view_visible = false,
+            on_ready = function (e)
+                if nil ~= test_gid then
+                    w:extend(e, "view_visible?out")
+                    e.view_visible = false
+                    w:submit(e)
+                end
+            end
+        },
+    }, test_gid)
+
+    world:group_enable_tag("view_visible", test_gid)
+    world:group_flush "view_visible"
 end
 
 function is:init_world()
