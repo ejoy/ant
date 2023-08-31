393fc49072b2881cb193ffa96799d0d6fbfe89c4
junjie020 <kitchen.gz.020@gmail.com>
2023-08-31 11:16:37 +0800

    add ekf_object init function

--- pkg/ant.efk/efk_object.lua
+++ pkg/ant.efk/efk_object.lua
@@ -6,10 +6,21 @@ local mathpkg   = import_package "ant.math"
 local mu        = mathpkg.constant
 
 local eo = ecs.component "efk_object"
-function eo.init(v)
+local function init_eo()
     return {
         visible_masks = 0,
         handle = 0,
         worldmat = mu.NULL,
     }
+end
+function eo.init()
+    return init_eo()
+end
+
+function eo.marshal()
+    return ""
+end
+
+function eo.unmarshal()
+    return init_eo()
 end
