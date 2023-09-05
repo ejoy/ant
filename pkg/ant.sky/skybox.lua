862d29ac566648c43337fd19440952bc308e0819
junjie020 <kitchen.gz.020@gmail.com>
2023-09-05 09:13:35 +0800

    clean skybox code

--- pkg/ant.sky/skybox.lua
+++ pkg/ant.sky/skybox.lua
@@ -2,15 +2,10 @@ local ecs 		= ...
 local world 	= ecs.world
 local w 		= world.w
 
-local setting   = import_package "ant.settings"
-
-local assetmgr	= import_package "ant.asset"
-
 local geopkg 	= import_package "ant.geometry"
 local geo 		= geopkg.geometry
 
 local imesh		= ecs.require "ant.asset|mesh"
-local imaterial	= ecs.require "ant.asset|material"
 
 local skybox_sys = ecs.system "skybox_system"
 
@@ -35,23 +30,5 @@ function skybox_sys:component_init()
 		e.simplemesh = m
 		e.owned_mesh_buffer = true
 		e.skybox_changed = true
-	end
-end
-
-local function res_tex_name(e)
-    return imaterial.resource(e).properties.s_skybox.texture
-end
-
-function skybox_sys:entity_ready()
-	for e in w:select "skybox_changed:update render_object filter_material:in skybox:in" do
-		local tn = res_tex_name(e)
-		local tex = assetmgr.resource(tn)
-		local texid = tex.id
-		if not assetmgr.invalid_texture(texid) then
-			e.skybox_changed = false
-			if not tex.texinfo.cubeMap then
-				error(("Invalid cubemap texture file:%s, it need to convert to cubemap texture file(add 'equirect: true' in *.texture file to convert it into cubemap texture)"):format(tn))
-			end
-        end
 	end
 end
