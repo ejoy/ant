33dd87fff3a2a3b12dc6fcafa5fe74e10de46047
junjie020 <kitchen.gz.020@gmail.com>
2023-09-06 10:19:31 +0800

    update group test

--- test/features/pkg/ant.test.features/group_test.lua
+++ test/features/pkg/ant.test.features/group_test.lua
@@ -8,6 +8,11 @@ local ig    = ecs.require "ant.group|group"
 local iom   = ecs.require "ant.objcontroller|obj_motion"
 local group_test_sys = ecs.system "group_test_system"
 
+local group_states = {
+    group_test1 = true,
+    group_test2 = true,
+}
+
 function group_test_sys:init()
     local g1 = ig.register "group_test1"
     local g2 = ig.register "group_test2"
@@ -50,21 +55,49 @@ function group_test_sys:init()
             iom.set_position(ee, math3d.vector(10, 0, 10, 1))
         end
     }
+
+    for k, v in pairs(group_states) do
+        ig.enable_from_name(k, "view_visible", v)
+        ig.enable_from_name(k, "view_visible", v)
+    end
+end
+
+function group_test_sys:entity_init()
+    for e in w:select "INIT render_object eid:in name?in" do
+        w:extend(e, "view_visible?in")
+        print("render_object INIT:", e.eid, "view_visible:", e.view_visible, e.name)
+    end
 end
 
 local kb_mb = world:sub{"keyboard"}
 
-local gname = "group_test1"
 function group_test_sys:data_changed()
     for _, key, press in kb_mb:unpack() do
         if key == "G" and press == 0 then
-            local gn1 = gname
-            local gn2 = gname == "group_test1" and "group_test2" or "group_test1"
-
-            ig.enable_from_name(gn1, "view_visible", gn1 == "group_test1")
-            ig.enable_from_name(gn2, "view_visible", gn2 == "group_test1")
-            
-            gname = gn2
+            group_states["group_test1"] = not group_states["group_test1"]
+            ig.enable_from_name("group_test1", "view_visible", group_states["group_test1"])
+        elseif key == "H" and press == 0 then
+            group_states["group_test2"] = not group_states["group_test2"]
+            ig.enable_from_name("group_test2", "view_visible", group_states["group_test2"])
+        elseif key == "B" and press == 0 then
+            world:create_instance {
+                prefab = "/pkg/ant.resources.binary/meshes/base/ring.glb|mesh.prefab",
+                group = ig.groupid "group_test2",
+                on_ready = function(e)
+                    local eid = e.tag['*'][1]
+                    local ee<close> = world:entity(eid, "scene:in")
+                    iom.set_position(ee, math3d.vector(0, 0, 10, 1))
+                end
+            }
         end
     end
+end
+
+function group_test_sys:render_submit()
+    -- do
+	-- 	print("view_visible:", w:count "view_visible")
+	-- 	print("render_object:", w:count "render_object")
+	-- 	print("render_object_visible:", w:count "render_object_visible")
+	-- 	print("render_object view_visible:", w:count "render_object view_visible")
+	-- end
 end
