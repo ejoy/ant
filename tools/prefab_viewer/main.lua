69f69c4683b5b1cb4d0f918e350d7cb657d66104
actboy168 <actboy168@gmail.com>
2023-09-15 16:28:51 +0800

    cleanup

--- tools/prefab_viewer/main.lua
+++ tools/prefab_viewer/main.lua
@@ -14,6 +14,7 @@ import_package "ant.window".start {
         "ant.animation",
         "ant.efk",
         "ant.landform",
+        "ant.objcontroller|pickup_detect",
         "ant.rmlui",
         "ant.sky|sky",
     },
