41260f7cff174d0245c1c50fdb7f64afd77c8419
actboy168 <actboy168@gmail.com>
2023-10-08 15:04:15 +0800

    fixes bug

--- clibs/luadebug/src/rdebug_debughost.cpp
+++ clibs/luadebug/src/rdebug_debughost.cpp
@@ -167,9 +167,12 @@ namespace luadebug::debughost {
     static int setenv(lua_State* hL) {
         const char* name  = luaL_checkstring(hL, 1);
         const char* value = luaL_checkstring(hL, 2);
+#if defined(_WIN32)
+        // TODO: support MINGW
 #if defined(_MSC_VER)
         lua_pushfstring(hL, "%s=%s", name, value);
         luadebug::putenv(lua_tostring(hL, -1));
+#endif
 #else
         ::setenv(name, value, 1);
 #endif
