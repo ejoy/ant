07ce8d8ce9e17c5c84dfee56b036713b489eadf5
actboy168 <actboy168@gmail.com>
2023-10-09 10:28:36 +0800

    update debugger

--- engine/firmware/init_thread.lua
+++ engine/firmware/init_thread.lua
@@ -7,19 +7,30 @@ local vfs, global = ...
 __ANT_RUNTIME__ = package.preload.firmware ~= nil
 __ANT_EDITOR__ = global.editor
 
-local fd = socket.fd(global.fd)
+local fd = socket.fd(global.fd, true)
+
+local function notify()
+	local n, err = fd:send "T"
+	if n == nil then
+		error(string.format("%s: %s", tostring(fd), err))
+	elseif n == 0 then
+		assert(false, "TODO")
+	else
+		assert(n == 1)
+	end
+end
 
 local function call(...)
 	local r, _ = thread.rpc_create()
 	io_req:push(r, ...)
-	fd:send "T"
+	notify()
 	return thread.rpc_wait(r)
 end
 
 local function send(...)
 	local r, _ = thread.rpc_create()
 	io_req:push(r, ...)
-	fd:send "T"
+	notify()
 end
 
 vfs.call = call
