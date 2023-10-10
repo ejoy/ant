5ac96b40e84752fa9dc264deed63efc6c42b6e33
actboy168 <actboy168@gmail.com>
2023-10-10 11:17:06 +0800

    update debugger

--- engine/debugger.lua
+++ engine/debugger.lua
@@ -1,12 +1,12 @@
-local function dofile(filename, ...)
+local function dofile(filename)
     local f = assert(io.open(filename))
     local str = f:read "a"
     f:close()
-    return assert(load(str, "=(debugger.lua)"))(...)
+    return assert(load(str, "=(debugger.lua)"))(filename)
 end
 
 local path = os.getenv "LUA_DEBUG_PATH"
 if path then
-    return dofile(path .. "/script/debugger.lua", path)
+    return dofile(path)
         : attach {}
 end
