aebdb54b6bbe94c7272e1eff0644fdda18545aa8
actboy168 <actboy168@gmail.com>
2023-10-11 11:10:40 +0800

    cleanup

--- pkg/ant.rmlui/core/DOM/window.lua
+++ pkg/ant.rmlui/core/DOM/window.lua
@@ -2,7 +2,7 @@ local rmlui = require "rmlui"
 local event = require "core.event"
 local timer = require "core.timer"
 local task = require "core.task"
-local contextManager = require "core.contextManager"
+local document_manager = require "core.document_manager"
 local windowManager = require "core.windowManager"
 local datamodel = require "core.datamodel.api"
 local environment = require "core.environment"
@@ -16,29 +16,29 @@ local function createWindow(document, source)
         return datamodel.create(document, view)
     end
     function window.open(url)
-        local newdoc = contextManager.open(url)
+        local newdoc = document_manager.open(url)
         if not newdoc then
             return
         end
-        contextManager.onload(newdoc)
+        document_manager.onload(newdoc)
         return createWindow(newdoc, document)
     end
     function window.close()
         task.new(function ()
-            contextManager.close(document)
+            document_manager.close(document)
             for t in pairs(timer_object) do
                 t:remove()
             end
         end)
     end
     function window.show()
-        contextManager.show(document)
+        document_manager.show(document)
     end
     function window.hide()
-        contextManager.hide(document)
+        document_manager.hide(document)
     end
     function window.flush()
-        contextManager.flush(document)
+        document_manager.flush(document)
     end
     function window.setTimeout(f, delay)
         local t = timer.wait(delay, f)
