6ce3d1db344845b9dacfbb45ab93470c667d9154
actboy168 <actboy168@gmail.com>
2023-10-18 10:49:51 +0800

    clibs/window -> pkg/ant.window

---
+++ pkg/ant.window/make.lua
@@ -0,0 +1,47 @@
+local lm = require "luamake"
+
+local ROOT <const> = "../../"
+
+lm:lua_source "window" {
+    includes = {
+        ROOT.."3rd/bee.lua/3rd/lua-seri",
+        ROOT.."3rd/bee.lua",
+    },
+    sources = {
+        "src/*.cpp",
+    },
+    windows = {
+        sources = {
+            "src/platform/windows/*.cpp",
+            "src/peek/*.cpp",
+        },
+        links = {
+            "user32",
+            "shell32",
+        },
+    },
+    macos = {
+        sources = {
+            "src/platform/osx/*.mm",
+            "src/peek/*.cpp",
+        },
+    },
+    ios = {
+        sources = {
+            "src/platform/ios/*.mm",
+            "src/loop/*.cpp",
+        },
+    },
+    android = {
+        includes = {
+            "src/platform/android/include",
+            ROOT.."runtime/common",
+        },
+        sources = {
+            "src/platform/android/include/**/*.cpp",
+            "src/platform/android/include/**/*.c",
+            "src/platform/android/*.cpp",
+            "src/peek/*.cpp",
+        },
+    }
+}
