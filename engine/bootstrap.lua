eb84c92767ce739f26e24eeda5c9316e2177702e
actboy168 <actboy168@gmail.com>
2023-10-25 11:33:24 +0800

    cleanup

--- engine/bootstrap.lua
+++ engine/bootstrap.lua
@@ -1,16 +1,7 @@
-if not __ANT_RUNTIME__ then
-    require "editor.init_vfs"
-    package.path = package.path:gsub("[^;]+", function (s)
-        if s:sub(1,1) ~= "/" then
-            s = "/" .. s
-        end
-        return s
-    end)
-end
-
-require "log"
-require "packagemanager"
-
 if __ANT_RUNTIME__ then
+    require "packagemanager"
     require "runtime.debug"
+else
+    require "editor.init_vfs"
+    require "packagemanager"
 end
