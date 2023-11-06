0b538344155aeabd730bdb324a94ee066e8d1ab9
actboy168 <actboy168@gmail.com>
2023-11-06 10:20:18 +0800

    fixes debugger

--- pkg/ant.hwi/font/fontmanager.lua
+++ pkg/ant.hwi/font/fontmanager.lua
@@ -4,7 +4,7 @@ local fontvm = [[
     local dbg = assert(loadfile '/engine/debugger.lua')()
     if dbg then
         dbg:event("setThreadName", "Thread: Font")
-        --dbg:event "wait"
+        dbg:event "wait"
     end
     dofile "/pkg/ant.hwi/font/manager.lua"
 ]]
