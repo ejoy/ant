ee745f58572da0e6fa24bd0540ce7c9edc6bc939
junjie020 <kitchen.gz.020@gmail.com>
2023-11-22 16:38:52 +0800

    move render features to ant.render|render and make renderqueue as a standalone feature

--- test/features/main.lua
+++ test/features/main.lua
@@ -3,6 +3,7 @@ require "bootstrap"
 import_package "ant.window".start {
     enable_mouse = true,
     feature = {
+        "ant.render|render",
         "ant.test.features",
         "ant.animation",
         "ant.camera|camera_controller",
