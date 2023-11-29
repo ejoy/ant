3ea9dce7f3c6c9dc9c70a4214b59df904de837eb
aimoonchen <kungfuchen@gmail.com>
2023-11-29 14:18:56 +0800

    fix bug

--- tools/editor/pkg/tools.editor/common/utils.lua
+++ tools/editor/pkg/tools.editor/common/utils.lua
@@ -28,15 +28,7 @@ end
 local fs = require "filesystem"
 
 function utils.write_file(filename, data)
-    local f
-    if string.sub(filename, 1, 1) == "/" then
-        local filepath = fs.path(filename)
-        local name = filepath:filename()
-        local path = filepath:remove_filename()
-        f = assert(io.open((path:localpath() / name:string()):string(), "wb"))
-    else
-        f = assert(io.open(filename, "wb"))
-    end
+    local f = (string.sub(filename, 1, 1) == "/") and assert(io.open(fs.path(filename):localpath():string(), "wb")) or assert(io.open(filename, "wb"))
     f:write(data)
     f:close()
 end
