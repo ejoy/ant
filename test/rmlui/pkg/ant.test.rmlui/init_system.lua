077c991c210c63e48e4baadf9511ae58b22063e5
actboy168 <actboy168@gmail.com>
2023-12-27 17:57:17 +0800

    cleanup

--- test/rmlui/pkg/ant.test.rmlui/init_system.lua
+++ test/rmlui/pkg/ant.test.rmlui/init_system.lua
@@ -1,12 +1,13 @@
 local ecs = ...
 
-local init_sys = ecs.system "init_system"
 local iRmlUi = ecs.require "ant.rmlui|rmlui_system"
 local font = import_package "ant.font"
 
+local m = ecs.system "init_system"
+
 font.import "/pkg/ant.resources.binary/ui/test/assets/font/simsun.ttc"
 
-function init_sys:init()
+function m:init()
     iRmlUi.open "/pkg/ant.test.rmlui/start.rml"
     iRmlUi.onMessage("click", function (msg)
         print(msg)
