ac9bfef5e00c46c92e732602912bf5ed074e5115
junjie020 <kitchen.gz.020@gmail.com>
2023-12-29 17:42:08 +0800

    add shadow test

---
+++ test/features/pkg/ant.test.features/assets/terrain/terrain_fs_func.sh
@@ -0,0 +1,65 @@
+#include <bgfx_shader.sh>
+#include <bgfx_compute.sh>
+#include <shaderlib.sh>
+#include "common/camera.sh"
+#include "common/transform.sh"
+#include "common/utils.sh"
+#include "common/cluster_shading.sh"
+#include "common/constants.sh"
+#include "common/uvmotion.sh"
+#include "pbr/common.sh"
+#include "pbr/lighting.sh"
+#include "pbr/material_info.sh"
+
+material_info terrain_material_info_init(vec3 gnormal, vec3 normal, vec3 posWS, vec4 basecolor, vec4 fragcoord, float metallic, float roughness)
+{
+    material_info mi  = (material_info)0;
+    mi.basecolor         = basecolor;
+    mi.posWS             = posWS.xyz;
+    mi.V                 = normalize(u_eyepos.xyz - posWS.xyz);
+    mi.gN                = gnormal;  //geomtery normal
+    mi.N                 = normal;
+
+    mi.perceptual_roughness  = roughness;
+    mi.metallic          = metallic;
+    mi.occlusion         = 1.0;
+
+    mi.screen_uv         = calc_normalize_fragcoord(fragcoord.xy);
+    return mi;
+}
+
+vec3 blend_terrain_color(vec3 sand_basecolor, vec3 stone_basecolor, float sand_height, float sand_alpha)
+{
+    float sand_weight = min(1.0, 2.5 * abs(sand_height - sand_alpha));
+	return lerp(stone_basecolor, sand_basecolor, sand_weight);
+}
+
+mediump vec3 terrain_normal_from_tangent_frame(mat3 tbn, vec3 texcoord)
+{
+    vec3 normalTS = fetch_normal_from_tex_array(s_normal_array, texcoord);
+    return transform_normal_from_tbn(tbn, normalTS);
+}
+
+
+void CUSTOM_FS(in Varyings varyings, out FSOutput fsoutput)
+{
+    vec2 terrain_uv    = varyings.texcoord0.xy;
+    vec2 alpha_uv      = varyings.texcoord0.zw;
+
+    vec4 sand_basecolor = texture2DArray(s_basecolor_array, vec3(terrain_uv, 0.0));
+    vec4 stone_basecolor= texture2DArray(s_basecolor_array, vec3(terrain_uv, 1.0));
+
+    float sand_height   = texture2DArray(s_height, vec3(terrain_uv, 0.0)).r;
+    float stone_height  = texture2DArray(s_height, vec3(terrain_uv, 1.0)).r;
+    float sand_alpha    = texture2DArray(s_height, vec3(alpha_uv,   2.0)).r;
+
+    vec3 terrain_color = blend_terrain_color(sand_basecolor.rgb, stone_basecolor.rgb, sand_height, sand_alpha);
+
+    mat3 tbn = mat3(varyings.tangent, varyings.bitangent, varyings.normal);
+    vec3 stone_normal = terrain_normal_from_tangent_frame(tbn, vec3(terrain_uv, 1.0));
+
+    material_info mi = terrain_material_info_init(varyings.normal, stone_normal, varyings.posWS, vec4(terrain_color, 1.0), varyings.frag_coord, u_metallic_factor, u_roughness_factor);
+    mi.distanceVS = varyings.frag_coord.w;
+    build_material_info(mi);
+    fsoutput.color = compute_lighting(mi); 
+}
--- test/features/pkg/ant.test.features/test/shadow_test.lua
+++ test/features/pkg/ant.test.features/test/shadow_test.lua
@@ -7,6 +7,7 @@ local ientity 	= ecs.require "ant.render|components.entity"
 local imesh		= ecs.require "ant.asset|mesh"
 local imaterial = ecs.require "ant.asset|material"
 local iom		= ecs.require "ant.objcontroller|obj_motion"
+local ipt		= ecs.require "ant.landform|plane_terrain"
 local common 	= ecs.require "common"
 
 local util		= ecs.require "util"
@@ -35,41 +36,66 @@ end
 
 local st_sys	= common.test_system "shadow_test_system"
 function st_sys:init()
-	util.create_instance("/pkg/ant.resources.binary/meshes/DamagedHelmet.glb|mesh.prefab", function (e)
-        local root<close> = world:entity(e.tag['*'][1])
-        iom.set_position(root, math3d.vector(3, 1, 0))
-		PC:add_prefab(e)
-    end)
 
-	create_instance("/pkg/ant.resources.binary/meshes/base/cube.glb|mesh.prefab", {10, 0.1, 10}, nil, {10, 0, 0, 1})
-	local root = PC:create_entity {
-		policy = {
-			"ant.scene|scene_object",
-		},
-		data = {
-			scene =  {t={10, 0, 0}},
-		}
+	local rn = 12
+	for i=1, rn * 24 do
+		local xidx, zidx = (i-1)%rn, (i-1)//rn
+		local pos = math3d.vector(xidx * 80 - 80, 10, zidx * 80 - 80)
+		util.create_instance("/pkg/ant.resources.binary/meshes/DamagedHelmet.glb|mesh.prefab", function (e)
+			local root<close> = world:entity(e.tag['*'][1])
+			iom.set_scale(root, 10)
+			iom.set_position(root, pos)
+			PC:add_prefab(e)
+		end)
+	end
+
+
+	-- create_instance("/pkg/ant.resources.binary/meshes/base/cube.glb|mesh.prefab", {10, 0.1, 10}, nil, {10, 0, 0, 1})
+	-- local root = PC:create_entity {
+	-- 	policy = {
+	-- 		"ant.scene|scene_object",
+	-- 	},
+	-- 	data = {
+	-- 		scene =  {t={10, 0, 0}},
+	-- 	}
+	-- }
+
+	-- PC:create_entity{
+	-- 	policy = {
+	-- 		"ant.render|simplerender",
+	-- 	},
+	-- 	data = {
+	-- 		scene 		= {
+    --             t = {0, 0, 0, 1},
+	-- 			s = {50, 1, 50, 0},
+	-- 			parent = root,
+    --         },
+	-- 		material 	= "/pkg/ant.resources/materials/mesh_shadow.material",
+	-- 		visible_state= "main_view",
+	-- 		simplemesh 	= imesh.init_mesh(ientity.plane_mesh()),
+    --         debug_mesh_bounding = true,
+	-- 		on_ready = function (e)
+	-- 			imaterial.set_property(e, "u_basecolor_factor", math3d.vector(0.8, 0.8, 0.8, 1))
+	-- 		end,
+	-- 	}
+    -- }
+
+	local cs = 16 * 10
+
+	local positions = {}
+	local size = 32
+	local offset = (size // 2) * cs
+	for j=1, size do
+		for i=1, size do
+			positions[#positions+1] = {x=(i-1)*cs-offset, y=(j-1)*cs-offset}
+		end
+	end
+
+	local groups = {
+		[0] = positions,
 	}
 
-	PC:create_entity{
-		policy = {
-			"ant.render|simplerender",
-		},
-		data = {
-			scene 		= {
-                t = {0, 0, 0, 1},
-				s = {50, 1, 50, 0},
-				parent = root,
-            },
-			material 	= "/pkg/ant.resources/materials/mesh_shadow.material",
-			visible_state= "main_view",
-			simplemesh 	= imesh.init_mesh(ientity.plane_mesh()),
-            debug_mesh_bounding = true,
-			on_ready = function (e)
-				imaterial.set_property(e, "u_basecolor_factor", math3d.vector(0.8, 0.8, 0.8, 1))
-			end,
-		}
-    }
+	ipt.create_plane_terrain(groups, "opacity", cs, "/pkg/ant.test.features/assets/terrain/plane_terrain.material")
 end
 
 function st_sys:entity_init()
@@ -80,4 +106,5 @@ end
 
 function st_sys:exit()
 	PC:clear()
+	ipt.clear_plane_terrain()
 end
