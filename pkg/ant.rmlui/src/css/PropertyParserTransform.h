eec0ca05ccb65c3b939761098519f5cb08c72a02
actboy168 <actboy168@gmail.com>
2024-01-09 10:45:20 +0800

    cleanup

--- pkg/ant.rmlui/src/css/PropertyParserTransform.h
+++ pkg/ant.rmlui/src/css/PropertyParserTransform.h
@@ -7,7 +7,6 @@ namespace Rml {
 
 class PropertyParserTransform : public PropertyParser {
 public:
-	PropertyParserTransform();
 	Property ParseValue(PropertyId id, const std::string& value) const override;
 };
 
