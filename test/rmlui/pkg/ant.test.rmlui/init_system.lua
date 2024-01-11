4fc2c9c33da88212390be291af5fde53a7c39cea
actboy168 <actboy168@gmail.com>
2024-01-11 16:15:00 +0800

    rmlui test support ios

--- test/rmlui/pkg/ant.test.rmlui/init_system.lua
+++ test/rmlui/pkg/ant.test.rmlui/init_system.lua
@@ -2,10 +2,18 @@ local ecs = ...
 
 local iRmlUi = ecs.require "ant.rmlui|rmlui_system"
 local font = import_package "ant.font"
+local platform = require "bee.platform"
 
 local m = ecs.system "init_system"
 
-font.import "宋体"
+if platform.os == "windows" then
+    font.import "黑体"
+elseif platform.os == "macos" then
+    font.import "苹方-简"
+elseif platform.os == "ios" then
+    font.import "Heiti SC"
+end
+
 
 function m:init()
     iRmlUi.open "/pkg/ant.test.rmlui/start.html"
