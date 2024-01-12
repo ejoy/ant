2fe366bc50f846b076919cb9e4a5d00ce23c6b64
actboy168 <actboy168@gmail.com>
2024-01-12 10:52:42 +0800

    rewrite components prebulit

--- projects/luamake/ecs.lua
+++ projects/luamake/ecs.lua
@@ -1,3 +1,75 @@
+local function ecs_components(output, namespace, userheader, components)
+    local out = {}
+    local function writefile(filename)
+        local f <close> = assert(io.open(filename, "w"))
+        f:write(table.concat(out, "\n"))
+        out = {}
+    end
+    local function write(line)
+        out[#out+1] = line
+    end
+    write "#pragma once"
+    write ""
+    write "#include \"ecs/select.h\""
+    write(("#include \"%s\""):format(userheader))
+    write "#include <cstdint>"
+    write "#include <tuple>"
+    write ""
+    write(("namespace %s {"):format(namespace))
+    write ""
+    write "using eid = uint64_t;"
+    write ""
+    write "struct REMOVED {};"
+    write ""
+    for _, info in ipairs(components) do
+        local name, type = info[1], info[2]
+        if type == "c" then
+            local fields = info[3]
+            write(("struct %s {"):format(name))
+            for _, field in ipairs(fields) do
+                write(("\t%s %s;"):format(field[1], field[2]))
+            end
+            write("};")
+            write ""
+        elseif type == "raw" then
+            local size = info[3]
+            write(("struct %s { uint8_t raw[%d]; }"):format(name, size))
+            write ""
+        elseif type == "tag" then
+            write(("struct %s {};"):format(name))
+            write ""
+        elseif type == "lua" then
+            write(("struct %s { unsigned int lua_object; };"):format(name))
+            write ""
+        elseif type == "int" then
+            local field = info[2]
+            write(("using %s = %s;"):format(name, field))
+            write ""
+        end
+    end
+    write "using _all_ = ::std::tuple<"
+    for i = 1, #components-1 do
+        local c = components[i]
+        write(("\t%s,"):format(c[1]))
+    end
+    write(("\t%s"):format(components[#components][1]))
+    write ">;"
+    write ""
+    write "}"
+    write ""
+    write(("namespace component = %s;"):format(namespace))
+    write ""
+    write "template <>"
+    write "constexpr int ecs::component_id<component::eid> = ecs::COMPONENT::EID;"
+    write "template <>"
+    write "constexpr int ecs::component_id<component::REMOVED> = ecs::COMPONENT::REMOVED;"
+    write "template <typename T>"
+    write "    requires (ecs::helper::component_has_v<T, component::_all_>)"
+    write "constexpr int ecs::component_id<T> = ecs::helper::component_id_v<T, component::_all_>;"
+    write ""
+    writefile(output)
+end
+
 local argn = select("#", ...)
 if argn < 2 then
     print [[
@@ -61,6 +133,24 @@ local function createEnv(class)
     }
 end
 
+local TYPENAMES <const> = {
+    int = "int32_t",
+    int64 = "int64_t",
+    dword = "uint32_t",
+    word = "uint16_t",
+    byte = "uint8_t",
+    float = "float",
+    userdata = "int64_t",
+}
+
+local function typenames(v)
+    local _, ud = v:match "^([^|]+)|(.*)$"
+    if ud then
+        return ud
+    end
+    return assert(TYPENAMES[v], ("Invalid %s"):format(v))
+end
+
 local function loadComponents()
     local class = {}
     local env = createEnv(class)
@@ -88,11 +178,16 @@ local function loadComponents()
             if t == "lua" then
                 --components[#components+1] = {name, "lua"}
             elseif t == "c" then
-                components[#components+1] = {name, "c", info.field}
+                local fields = {}
+                for _, field in ipairs(info.field) do
+                    local fieldname, typename = field:match "^([%w_]+):(.+)$"
+                    fields[#fields+1] = { typenames(typename), fieldname }
+                end
+                components[#components+1] = {name, "c", fields}
             elseif t == "raw" then
-                components[#components+1] = {name, "raw", info.field[1], info.size[1]}
+                components[#components+1] = {name, "raw", info.size[1]}
             else
-                components[#components+1] = {name, t}
+                components[#components+1] = {name, "int", typenames(t)}
             end
         end
     end
@@ -104,101 +199,9 @@ end
 
 local components = loadComponents()
 
-
-local out = {}
-
-local function writefile(filename)
-    local f <close> = assert(io.open(filename, "w"))
-    f:write(table.concat(out, "\n"))
-    out = {}
-end
-
-local function write(line)
-    out[#out+1] = line
-end
-
-local TYPENAMES <const> = {
-    int = "int32_t",
-    int64 = "int64_t",
-    dword = "uint32_t",
-    word = "uint16_t",
-    byte = "uint8_t",
-    float = "float",
-    userdata = "int64_t",
-}
-
-local function typenames(v)
-    local _, ud = v:match "^([^|]+)|(.*)$"
-    if ud then
-        return ud
-    end
-    return assert(TYPENAMES[v], ("Invalid %s"):format(v))
-end
-
-do
-    write "#pragma once"
-    write ""
-    write "#include \"ecs/select.h\""
-    write "#include \"ecs/user.h\""
-    write "#include <cstdint>"
-    write "#include <tuple>"
-    write ""
-    write "namespace ant_component {"
-    write ""
-    write "using eid = uint64_t;"
-    write ""
-    write "struct REMOVED {};"
-    write ""
-    for _, info in ipairs(components) do
-        local name, type = info[1], info[2]
-        if type == "c" then
-            local fields = info[3]
-            write(("struct %s {"):format(name))
-            for _, field in ipairs(fields) do
-                local name, typename = field:match "^([%w_]+):(.+)$"
-                write(("\t%s %s;"):format(typenames(typename), name))
-            end
-            write("};")
-            write ""
-        elseif type == "raw" then
-            local field, size = info[3], info[4]
-            write(("struct %s {"):format(name))
-            write(field:match "^(.-)[ \t\r\n]*$")
-            write("};")
-            write(("static_assert(sizeof(%s) == %s);"):format(name, size))
-            write ""
-        elseif type == "tag" then
-            write(("struct %s {};"):format(name))
-            write ""
-        elseif type == "lua" then
-            write(("struct %s { unsigned int lua_object; };"):format(name))
-            write ""
-        else
-            write(("using %s = %s;"):format(name, typenames(type)))
-            write ""
-        end
-    end
-
-    write "using _all_ = ::std::tuple<"
-    for i = 1, #components-1 do
-        local c = components[i]
-        write(("    %s,"):format(c[1]))
-    end
-    write(("    %s"):format(components[#components][1]))
-    write ">;"
-    write ""
-    write "}"
-    write ""
-    write "namespace component = ant_component;"
-    write ""
-    write "template <>"
-    write "constexpr int ecs::component_id<ant_component::eid> = ecs::COMPONENT::EID;"
-    write "template <>"
-    write "constexpr int ecs::component_id<ant_component::REMOVED> = ecs::COMPONENT::REMOVED;"
-    write "template <typename T>"
-    write "    requires (ecs::helper::component_has_v<T, ant_component::_all_>)"
-    write "constexpr int ecs::component_id<T> = ecs::helper::component_id_v<T, ant_component::_all_>;"
-    write ""
-
-    writefile(component_h .. "/component.hpp")
-end
+ecs_components(
+    component_h .. "/component.hpp",
+    "ant_component",
+    "ecs/user.h",
+    components
+)
