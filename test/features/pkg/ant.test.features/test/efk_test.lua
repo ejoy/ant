19b47b5ef57eac3c17dad580ac7ba45c3591d5a4
junjie020 <kitchen.gz.020@gmail.com>
2024-01-17 14:50:32 +0800

    move efk to features

--- test/features/pkg/ant.test.features/test/efk_test.lua
+++ test/features/pkg/ant.test.features/test/efk_test.lua
@@ -3,14 +3,14 @@ local world = ecs.world
 local w     = world.w
 
 local math3d    = require "math3d"
+local efk_test_sys = ecs.system "efk_test_system"
 
 local iefk      = ecs.require "ant.efk|efk"
 local ig        = ecs.require "ant.group|group"
 local iom       = ecs.require "ant.objcontroller|obj_motion"
 
-local is = ecs.system "init_system"
 local efkeid_group
-function is:init()
+function efk_test_sys:init()
     local test_gid<const> = ig.register "group_test"
 
     -- world:create_entity{
@@ -23,7 +23,7 @@ function is:init()
     --             t = {-2, 0, 0, 1}
     --         },
     --         efk = {
-    --             path = "/pkg/ant.test.efk/assets/miner_efk/miner_dust.efk",
+    --             path = "/pkg/ant.test.features/assets/efk/miner_efk/miner_dust.efk",
     --         },
     --         visible_state = "main_queue",
     --     }
@@ -39,7 +39,7 @@ function is:init()
     --             t = {3, 0, 0, 1}
     --         },
     --         efk = {
-    --             path = "/pkg/ant.test.efk/assets/miner_efk/miner_dust.efk",
+    --             path = "/pkg/ant.test.features/assets/efk/miner_efk/miner_dust.efk",
     --         },
     --         visible_state = "main_queue",
     --     }
@@ -55,7 +55,7 @@ function is:init()
             data = {
                 scene = {},
                 efk = {
-                    path = "/pkg/ant.test.efk/assets/miner_efk/miner_dust.efk",
+                    path = "/pkg/ant.test.features/assets/efk/miner_efk/miner_dust.efk",
                 },
                 visible_state = "main_queue",
             }
@@ -83,16 +83,16 @@ function is:init()
     end
 end
 
-function is:init_world()
-
+function efk_test_sys:init_world()
     local mq = w:first "main_queue camera_ref:in"
     local ce <close> = world:entity(mq.camera_ref)
     iom.set_position(ce, math3d.vector(0.0, 0.0, 10.0))
     iom.set_direction(ce, math3d.vector(0.0, 0.0, -1.0))
 end
 
+
 local kb_mb = world:sub{"keyboard"}
-function is:data_changed()
+function efk_test_sys:data_changed()
     for _, key, press in kb_mb:unpack() do
         if press == 0 and key == "T" then
             local e <close> = world:entity(efkeid_group, "efk:in")
