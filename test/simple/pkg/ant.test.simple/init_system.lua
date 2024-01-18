dbdb98a9db37888b6ee0d9ff574d1c5c09920c7a
actboy168 <actboy168@gmail.com>
2024-01-18 20:50:45 +0800

    cleanup

--- test/simple/pkg/ant.test.simple/init_system.lua
+++ test/simple/pkg/ant.test.simple/init_system.lua
@@ -1,13 +1,10 @@
 local ecs = ...
 local world = ecs.world
-local w = world.w
 
 local m = ecs.system "init_system"
 
 local ientity = ecs.require "ant.render|components.entity"
 local imesh = ecs.require "ant.asset|mesh"
-local imaterial = ecs.require "ant.asset|material"
-local math3d = require "math3d"
 
 function m:init_world()
 	world:create_entity {
@@ -22,9 +19,6 @@ function m:init_world()
 			material = "/pkg/ant.resources/materials/mesh_shadow.material",
 			visible_state = "main_view",
 			simplemesh = imesh.init_mesh(ientity.plane_mesh()),
-			on_ready = function (e)
-				imaterial.set_property(e, "u_basecolor_factor", math3d.vector(0.8, 0.8, 0.8, 1))
-			end,
 		}
 	}
 
