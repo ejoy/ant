3bfaa45d8861d3df896a380026e4890ee873ee48
von <1052019421@qq.com>
2024-01-18 10:11:47 +0800

    fix billborad

--- pkg/ant.render/billboard/billboard.lua
+++ pkg/ant.render/billboard/billboard.lua
@@ -10,7 +10,7 @@ function bb_sys:camera_usage()
     for e in w:select "billboard render_object:update scene:update" do
         local mq = w:first("main_queue render_target:in camera_ref:in")
         local ce = world:entity(mq.camera_ref, "camera:in")
-        local obj_world_nmat = math3d.set_columns(math3d.inverse(ce.camera.viewmat), 4, math3d.index(e.scene.worldmat, 4))
+        local obj_world_nmat = math3d.set_index(math3d.inverse(ce.camera.viewmat), 4, math3d.index(e.scene.worldmat, 4))
 
         math3d.unmark(e.scene.worldmat)
         e.scene.worldmat=math3d.mark(obj_world_nmat)
