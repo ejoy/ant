a24353a50f3bcad2220107011cbc0a86157290f7
junjie020 <kitchen.gz.020@gmail.com>
2024-02-01 14:57:45 +0800

    add shadow plane for simple test

--- test/simple/pkg/ant.test.simple/main_system.lua
+++ test/simple/pkg/ant.test.simple/main_system.lua
@@ -4,16 +4,34 @@ local w = world.w
 
 local icamera = ecs.require "ant.camera|camera"
 local math3d = require "math3d"
-local widget = ecs.require "widget"
+--local widget = ecs.require "widget"
 
 local m = ecs.system "main_system"
 
 local entities
 
+local imesh = ecs.require "ant.asset|mesh"
+local ientity = ecs.require "ant.render|components.entity"
+
 function m:init_world()
     world:create_instance {
         prefab = "/pkg/ant.test.simple/resource/light.prefab"
     }
+
+    world:create_entity{
+		policy = {
+			"ant.render|simplerender",
+		},
+		data = {
+			scene 		= {
+				s = {250, 1, 250},
+            },
+			material 	= "/pkg/ant.resources/materials/mesh_shadow.material",
+			visible_state= "main_view",
+			simplemesh 	= imesh.init_mesh(ientity.plane_mesh()),
+		}
+	}
+
     local prefab = world:create_instance {
         prefab = "/pkg/ant.test.simple/resource/miner/miner.gltf|mesh.prefab",
         on_ready = function ()
@@ -29,5 +47,5 @@ function m:init_world()
 end
 
 function m:data_changed()
-    widget.AnimationView(entities)
+    --widget.AnimationView(entities)
 end
