1c030efc2d2a20db54431cfec71ca79a1a3e7b95
von <1052019421@qq.com>
2024-02-01 15:07:47 +0800

    update indirect_entity

---
+++ pkg/ant.render/draw_indirect/indirect_entity.lua
@@ -0,0 +1,144 @@
+local ecs   = ...
+local world = ecs.world
+local w     = world.w
+local math3d = require "math3d"
+local datalist  = require "datalist"
+local ie_sys = ecs.system "indirect_entity_system"
+local aio       = import_package "ant.io"
+local icompute  = ecs.require "ant.render|compute.compute"
+local INDIRECT_MATERIAL<const> = "/pkg/ant.resources/materials/hitch/hitch_compute.material"
+local DEFAULT_SIZE<const> = 50
+local hwi       = import_package "ant.hwi"
+local main_viewid<const> = hwi.viewid_get "main_view"
+local idi       = ecs.require "ant.render|draw_indirect.draw_indirect"
+
+
+local function build_instance_buffer(srts)
+    if not srts then return end
+    local memory = {}
+    for _, srt in ipairs(srts) do
+        local wm = math3d.matrix {s = srt.s, r = srt.r, t = srt.t}
+        wm = math3d.transpose(wm)
+        local c1, c2, c3 = math3d.index(wm, 1, 2, 3)
+        memory[#memory+1] = ("%s%s%s"):format(math3d.serialize(c1), math3d.serialize(c2), math3d.serialize(c3))
+    end
+    
+    return table.concat(memory, "")
+end
+
+local function to_dispatch_num(draw_num)
+    return (draw_num+63) // 64
+end
+
+local function dispatch_compute_entity(e, dieid)
+
+    local die = world:entity(dieid, "draw_indirect:in")
+    local di = die.draw_indirect
+    local draw_num = di.instance_buffer.params[1]
+
+    if draw_num > 0 then
+        local dis = e.dispatch
+        dis.size[1] = to_dispatch_num(draw_num)
+        local m = dis.material
+        m.u_mesh_params = math3d.vector(di.instance_buffer.params)
+        m.b_indirect_buffer = {
+            type = "b",
+            access = "w",
+            value = di.handle,
+            stage = 0,
+        }
+        icompute.dispatch(main_viewid, dis)
+    end
+end
+
+local function create_draw_indirect_entity(gid, srts, mesh, material, render_layer, visible_state, draw_num)
+    local memory = build_instance_buffer(srts)
+    local ib_num = datalist.parse(aio.readall(mesh)).ib.num
+    return world:create_entity {
+        group = gid,
+        policy = {
+            "ant.render|render",
+            "ant.render|draw_indirect",
+        },
+        data = {
+            scene = {},
+            mesh  = mesh,
+            material    = material,
+            visible_state = visible_state,
+            render_layer = render_layer,
+            draw_indirect = {
+                instance_buffer = {
+                    memory  = memory,
+                    flag    = "ra",
+                    layout  = "t45NIf|t46NIf|t47NIf",
+                    num     = draw_num,
+                    size    = draw_num > DEFAULT_SIZE and draw_num * 2 or DEFAULT_SIZE,
+                    params =  {draw_num, 0, 0, ib_num}
+                },
+            },
+        }
+    }
+end
+
+local function create_compute_entity(dieid, draw_num)
+    return world:create_entity{
+        policy = {
+            "ant.render|compute",
+        },
+        data = {
+            material = INDIRECT_MATERIAL,
+            dispatch = {
+                size = {to_dispatch_num(draw_num), 1, 1},
+            },
+            on_ready = function (e)
+                w:extend(e, "dispatch:update")
+                dispatch_compute_entity(e, dieid)
+            end
+        }
+    }
+end
+
+local function update_draw_indirect_entity(dieid, srts)
+    local memory = build_instance_buffer(srts)
+    local draw_num = #srts
+    local die = world:entity(dieid, "draw_indirect:in")
+    local di = die.draw_indirect
+    di.instance_buffer.params[1] = draw_num
+    idi.update_instance_buffer(die, memory, draw_num)
+end
+
+local function update_compute_entity(ceid, dieid)
+    local ce = world:entity(ceid, "dispatch:update")
+    dispatch_compute_entity(ce, dieid)
+
+end
+
+function ie_sys:entity_init()
+    for e in w:select "INIT indirect_entity:in" do
+        local ie = e.indirect_entity
+        local gid, srts, mesh, material, render_layer, visible_state = ie.gid, ie.srts, ie.mesh, ie.material, ie.render_layer, ie.visible_state
+        local draw_num = srts and #srts or 0
+        local dieid = create_draw_indirect_entity(gid, srts, mesh, material, render_layer, visible_state, draw_num)
+        local ceid  = create_compute_entity(dieid, draw_num)
+        ie.dieid, ie.ceid = dieid, ceid
+    end
+end
+
+function ie_sys:data_changed()
+    for e in w:select "indirect_update:update indirect_entity:in" do
+        local ie = e.indirect_entity
+        local dieid, ceid, srts = ie.dieid, ie.ceid, ie.srts
+        update_draw_indirect_entity(dieid, srts)
+        update_compute_entity(ceid, dieid)
+        e.indirect_update = false
+    end
+end
+
+function ie_sys:entity_remove()
+    for e in w:select "REMOVED indirect_entity:in" do
+        local ie = e.indirect_entity
+        local dieid, ceid = ie.dieid, ie.ceid
+        w:remove(dieid)
+        w:remove(ceid)
+    end
+end
--- test/features/pkg/ant.test.features/test/draw_indirect_test.lua
+++ test/features/pkg/ant.test.features/test/draw_indirect_test.lua
@@ -2,92 +2,70 @@ local ecs   = ...
 local world = ecs.world
 local w     = world.w
 
+local math3d = require "math3d"
 local common = ecs.require "common"
 local util  = ecs.require "util"
 local PC    = util.proxy_creator()
-local icompute  = ecs.require "ant.render|compute.compute"
+local ig    = ecs.require "ant.group|group"
 local dit_sys = common.test_system "draw_indirect"
-local math3d  = require "math3d"
-local hwi       = import_package "ant.hwi"
-local main_viewid<const> = hwi.viewid_get "main_view"
+local draw_indirect_test_group_id<const> = ig.register "draw_indirect_test"
+local ie_eid
 
-local function dispatch_instance_buffer(e, diid, draw_num)
+local function build_test_srts(rows, cols)
+    local srts = {}
 
-    local function to_dispath_num(indirectnum)
-        return (indirectnum+63) // 64
+    local r = math3d.quaternion{0, 0, math.rad(90)}
+    for col = -rows, rows, 2 do
+        local scale, scale_step = 0.1, 0.1
+        for row = -cols, cols, 2 do
+            local s = math3d.vector(scale, scale, scale)
+            local t = math3d.vector(row, 0 ,col)
+            srts[#srts+1] = {s = s, r = r, t = t}
+            scale = scale + scale_step
+        end
     end
-
-    local die = world:entity(diid, "draw_indirect:in mesh:in")
-    local di = die.draw_indirect
-
-   assert(di.instance_buffer.num == draw_num)
-    if draw_num > 0 then
-        local dis = e.dispatch
-        dis.size[1] = to_dispath_num(draw_num)
-        local m = dis.material
-        die.draw_indirect.instance_buffer.params =  {draw_num, 0, 0, die.mesh.ib.num}
-        m.u_mesh_params = math3d.vector(di.instance_buffer.params)
-        m.b_indirect_buffer = {
-            type = "b",
-            access = "w",
-            value = di.handle,
-            stage = 0,
-        }
-        icompute.dispatch(main_viewid, dis)
-    end
+    return srts
 end
 
 function dit_sys.init_world()
-    local memory = {}
-    local edge = 10
-    local draw_num = math.tointeger( edge ^ 2 )
-    local r = math3d.quaternion { axis = {1,0,0}, r = math.rad(60) }
-    for i = 1, edge do
-        for j = 1, edge do
-            local t = math3d.vector(i, 0, j)
-            local wm = math3d.matrix {s=0.1, r=r, t=t}
-            wm = math3d.transpose(wm)
-            local c1, c2, c3 = math3d.index(wm, 1, 2, 3)
-            memory[#memory+1] = ("%s%s%s"):format(math3d.serialize(c1), math3d.serialize(c2), math3d.serialize(c3))
+
+    local test_mesh = "/pkg/ant.resources.binary/meshes/base/cube.glb|meshes/Cube_P1.meshbin"
+    local test_material = "/pkg/ant.resources.binary/meshes/base/cube.glb|materials/Material.001.material"
+    local test_visible_state = "main_view|selectable|cast_shadow"
+    local test_render_layer = "opacity"
+    local test_srts = build_test_srts(4, 4)
+
+    -- indirect_entity's visibility depends on its binding group
+    ig.enable(draw_indirect_test_group_id, "view_visible", true)
+
+    ie_eid = PC:create_entity{
+        policy = {
+            "ant.render|indirect_entity",
+        },
+        data = {
+            indirect_entity = {
+                gid = draw_indirect_test_group_id,
+                mesh = test_mesh,
+                material = test_material,
+                visible_state = test_visible_state,
+                render_layer = test_render_layer,
+                srts = test_srts
+            }
+        }
+    }
+end
+
+local key_mb = world:sub {"keyboard"}
+function dit_sys:data_changed()
+    for _, key, press in key_mb:unpack() do
+        if key == "G" and press == 0 then
+            local test_srts = build_test_srts(8, 8)
+            local ie_entity<close> = world:entity(ie_eid, "indirect_update?update indirect_entity:update")
+            ie_entity.indirect_update = true
+            ie_entity.indirect_entity.srts = test_srts
         end
     end
 
-    local dieid = PC:create_entity {
-        policy = {
-            "ant.render|render",
-            "ant.render|draw_indirect",
-        },
-        data = {
-            scene = {},
-            mesh = "/pkg/ant.resources.binary/meshes/base/cube.glb|meshes/Cube_P1.meshbin",
-            material = "/pkg/ant.resources.binary/meshes/base/cube.glb|materials/Material.001.material",
-            visible_state = "main_view|selectable",
-            draw_indirect = {
-                instance_buffer = {
-                    memory  = table.concat(memory, ""),
-                    flag    = "r",
-                    layout  = "t45NIf|t46NIf|t47NIf",
-                    num     = draw_num,
-                },
-            },
-        },
-    }
-
-    local cid = PC:create_entity{
-        policy = {
-            "ant.render|compute",
-        },
-        data = {
-            material = "/pkg/ant.test.features/assets/indirect_compute.material",
-            dispatch = {
-                size = {((draw_num+63)//64), 1, 1},
-            },
-            on_ready = function (e)
-                w:extend(e, "dispatch:update")
-                dispatch_instance_buffer(e, dieid, draw_num)
-            end
-        }
-    }
 end
 
 function dit_sys:exit()
