894b9aff8942a00deac775e4f3ebe5a40266424c
actboy168 <actboy168@gmail.com>
2024-02-01 11:31:29 +0800

    simple use gltf

--- test/simple/pkg/ant.test.simple/main_system.lua
+++ test/simple/pkg/ant.test.simple/main_system.lua
@@ -2,12 +2,9 @@ local ecs = ...
 local world = ecs.world
 local w = world.w
 
-local ImGui = import_package "ant.imgui"
-local ivs = ecs.require "ant.render|visible_state"
-local ianimation = ecs.require "ant.animation|animation"
-local iplayback = ecs.require "ant.animation|playback"
 local icamera = ecs.require "ant.camera|camera"
 local math3d = require "math3d"
+local widget = ecs.require "widget"
 
 local m = ecs.system "main_system"
 
@@ -18,12 +15,12 @@ function m:init_world()
         prefab = "/pkg/ant.test.simple/resource/light.prefab"
     }
     local prefab = world:create_instance {
-        prefab = "/pkg/ant.test.simple/resource/miner-1.glb|mesh.prefab",
+        prefab = "/pkg/ant.test.simple/resource/miner/miner.gltf|mesh.prefab",
         on_ready = function ()
-            local mq = w:first "main_queue camera_ref:in"
-            local ce <close> = world:entity(mq.camera_ref, "camera:in")
+            local main_queue = w:first "main_queue camera_ref:in"
+            local main_camera <close> = world:entity(main_queue.camera_ref, "camera:in")
             local dir = math3d.vector(0, -1, 1)
-            if not icamera.focus_prefab(ce, entities, dir) then
+            if not icamera.focus_prefab(main_camera, entities, dir) then
                 error "aabb not found"
             end
         end
@@ -32,73 +29,5 @@ function m:init_world()
 end
 
 function m:data_changed()
-    if ImGui.Begin("entities", nil, ImGui.WindowFlags {"AlwaysAutoResize", "NoMove", "NoTitleBar"}) then
-        local animation_eid
-        if ImGui.TreeNode "mesh" then
-            for i = 1, #entities do
-                local eid = entities[i]
-                local e <close> = world:entity(eid, "render_object?in animation?in")
-                if e.render_object then
-                    local value = { ivs.has_state(e, "main_view") }
-                    if ImGui.Checkbox(""..eid, value) then
-                        ivs.set_state(e, "main_view", value[1])
-                        ivs.set_state(e, "cast_shadow", value[1])
-                    end
-                end
-                if e.animation then
-                    animation_eid = eid
-                end
-            end
-            ImGui.TreePop()
-        else
-            for i = 1, #entities do
-                local eid = entities[i]
-                local e <close> = world:entity(eid, "animation?in")
-                if e.animation then
-                    animation_eid = eid
-                end
-            end
-        end
-        if animation_eid and ImGui.TreeNodeEx("animation", ImGui.TreeNodeFlags {"DefaultOpen"}) then
-            local e <close> = world:entity(animation_eid, "animation:in")
-            local animation = e.animation
-            for name, status in pairs(animation.status) do
-                if ImGui.TreeNode(name) then
-                    do
-                        local v = { status.play }
-                        if ImGui.Checkbox("play", v) then
-                            iplayback.set_play(e, name, v[1])
-                        end
-                    end
-                    do
-                        local v = { status.loop }
-                        if ImGui.Checkbox("loop", v) then
-                            iplayback.set_loop(e, name, v[1])
-                        end
-                    end
-                    do
-                        local value = { status.speed and math.floor(status.speed*100) or 100 }
-                        if ImGui.DragIntEx("speed", value, 5.0, -500, 500, "%d%%") then
-                            iplayback.set_speed(e, name, value[1] / 100)
-                        end
-                    end
-                    do
-                        local value = { status.weight }
-                        if ImGui.SliderFloat("weight", value, 0, 1) then
-                            ianimation.set_weight(e, name, value[1])
-                        end
-                    end
-                    do
-                        local value = { status.ratio }
-                        if ImGui.SliderFloat("ratio", value, 0, 1) then
-                            ianimation.set_ratio(e, name, value[1])
-                        end
-                    end
-                    ImGui.TreePop()
-                end
-            end
-            ImGui.TreePop()
-        end
-    end
-    ImGui.End()
+    widget.AnimationView(entities)
 end
---
+++ test/simple/pkg/ant.test.simple/widget.lua
@@ -0,0 +1,83 @@
+local ecs = ...
+local world = ecs.world
+
+local ImGui = import_package "ant.imgui"
+local ivs = ecs.require "ant.render|visible_state"
+local ianimation = ecs.require "ant.animation|animation"
+local iplayback = ecs.require "ant.animation|playback"
+
+local m = {}
+
+function m.AnimationView(entities)
+    if ImGui.Begin("entities", nil, ImGui.WindowFlags {"AlwaysAutoResize", "NoMove", "NoTitleBar"}) then
+        local animation_eid
+        if ImGui.TreeNode "mesh" then
+            for i = 1, #entities do
+                local eid = entities[i]
+                local e <close> = world:entity(eid, "render_object?in animation?in")
+                if e.render_object then
+                    local value = { ivs.has_state(e, "main_view") }
+                    if ImGui.Checkbox(""..eid, value) then
+                        ivs.set_state(e, "main_view", value[1])
+                        ivs.set_state(e, "cast_shadow", value[1])
+                    end
+                end
+                if e.animation then
+                    animation_eid = eid
+                end
+            end
+            ImGui.TreePop()
+        else
+            for i = 1, #entities do
+                local eid = entities[i]
+                local e <close> = world:entity(eid, "animation?in")
+                if e.animation then
+                    animation_eid = eid
+                end
+            end
+        end
+        if animation_eid and ImGui.TreeNodeEx("animation", ImGui.TreeNodeFlags {"DefaultOpen"}) then
+            local e <close> = world:entity(animation_eid, "animation:in")
+            local animation = e.animation
+            for name, status in pairs(animation.status) do
+                if ImGui.TreeNode(name) then
+                    do
+                        local v = { status.play }
+                        if ImGui.Checkbox("play", v) then
+                            iplayback.set_play(e, name, v[1])
+                        end
+                    end
+                    do
+                        local v = { status.loop }
+                        if ImGui.Checkbox("loop", v) then
+                            iplayback.set_loop(e, name, v[1])
+                        end
+                    end
+                    do
+                        local value = { status.speed and math.floor(status.speed*100) or 100 }
+                        if ImGui.DragIntEx("speed", value, 5.0, -500, 500, "%d%%") then
+                            iplayback.set_speed(e, name, value[1] / 100)
+                        end
+                    end
+                    do
+                        local value = { status.weight }
+                        if ImGui.SliderFloat("weight", value, 0, 1) then
+                            ianimation.set_weight(e, name, value[1])
+                        end
+                    end
+                    do
+                        local value = { status.ratio }
+                        if ImGui.SliderFloat("ratio", value, 0, 1) then
+                            ianimation.set_ratio(e, name, value[1])
+                        end
+                    end
+                    ImGui.TreePop()
+                end
+            end
+            ImGui.TreePop()
+        end
+    end
+    ImGui.End()
+end
+
+return m
