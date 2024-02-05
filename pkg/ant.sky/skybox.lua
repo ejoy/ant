d964d8df9526cf5f1b62d312c52e4ce9102c05b7
junjie020 <kitchen.gz.020@gmail.com>
2024-02-05 17:50:02 +0800

    init mesh_result in component_init

--- pkg/ant.sky/skybox.lua
+++ pkg/ant.sky/skybox.lua
@@ -9,10 +9,11 @@ local ientity	= ecs.require "ant.render|components.entity"
 local skybox_sys = ecs.system "skybox_system"
 
 function skybox_sys:component_init()
-	for e in w:select "INIT skybox:in simplemesh:out owned_mesh_buffer?out" do
+	for e in w:select "INIT skybox:in simplemesh:out mesh_result:out owned_mesh_buffer?out" do
 		local vb, ib = geo.box(1, true, false)
 		local m = ientity.create_mesh({"p3", vb}, ib)
 		e.simplemesh = m
+		e.mesh_result = m
 		e.owned_mesh_buffer = true
 	end
 end
