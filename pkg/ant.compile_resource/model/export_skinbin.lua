658435fe827676c37abecd3ca4261fe8ccf5cfd1
actboy168 <actboy168@gmail.com>
2024-02-21 10:53:20 +0800

    build joint hierarchy

--- pkg/ant.compile_resource/model/export_skinbin.lua
+++ pkg/ant.compile_resource/model/export_skinbin.lua
@@ -29,13 +29,23 @@ local function GetSkinsForScene(model, scene)
     return skins
 end
 
-local function FindSkinRootJointIndices(model, scene)
+local function FindSkinRootJointIndices(model)
+    local sceneIndex = model.scene or 0
+    local scene = model.scenes[sceneIndex+1]
     local skins = GetSkinsForScene(model, scene)
+    local mark = {}
     local roots = {}
-    if #skins  == 0 then
-        for _, nodeIndex in ipairs(scene.nodes) do
+    local function add_root(nodeIndex)
+        if not mark[nodeIndex] then
+            mark[nodeIndex] = true
             roots[#roots+1] = nodeIndex
         end
+    end
+    if #skins == 0 then
+        for _, nodeIndex in ipairs(scene.nodes) do
+            add_root(nodeIndex)
+        end
+        table.sort(roots)
         return roots
     end
     local parents = {}
@@ -54,7 +64,8 @@ local function FindSkinRootJointIndices(model, scene)
         end
         if skin.skeleton then
             parents[skin.skeleton] = visited
-            roots[#roots+1] = skin.skeleton
+            skin.pivotPoint = skin.skeleton
+            add_root(skin.skeleton)
             goto continue
         end
 
@@ -63,60 +74,57 @@ local function FindSkinRootJointIndices(model, scene)
             root = parents[root]
         end
         if root ~= visited then
-            roots[#roots+1] = root
+            add_root(root)
         end
+        skin.pivotPoint = root
         ::continue::
     end
+    table.sort(roots)
     return roots
 end
 
-local function fetch_skininfo(status, gltfscene, skin, remap)
-    local math3d       = status.math3d
-    local r2l_mat      = math3d.ext_constant.R2L_MAT
-    local ibm_idx      = skin.inverseBindMatrices
-    local ibm          = gltfscene.accessors[ibm_idx+1]
-    local ibm_bv       = gltfscene.bufferViews[ibm.bufferView+1]
-    local start_offset = ibm_bv.byteOffset + 1
-    local end_offset   = start_offset + ibm_bv.byteLength
-    local joints       = skin.joints
-    local jointsRemap = {}
-    for i = 1, #joints do
-        jointsRemap[i] = string.pack("<I2", assert(remap[joints[i]]))
-    end
-    local buf = gltfscene.buffers[ibm_bv.buffer+1]
-    local inverseBindMatrices = buf.bin:sub(start_offset, end_offset-1)
-    inverseBindMatrices = math3d.serialize(math3d.mul_array(math3d.array_matrix(inverseBindMatrices), r2l_mat))
-    return {
-        inverseBindMatrices = inverseBindMatrices,
-        jointsRemap = table.concat(jointsRemap),
-    }
+local function FetchAccessor(model, accessorIndex)
+    local accessor   = model.accessors[accessorIndex+1]
+    local bufferView = model.bufferViews[accessor.bufferView+1]
+    local buffer     = model.buffers[bufferView.buffer+1]
+    return buffer.bin:sub(bufferView.byteOffset + 1, bufferView.byteOffset + bufferView.byteLength)
 end
 
 return function (status)
-    local gltfscene = status.gltfscene
-    local sceneidx = gltfscene.scene or 0
-    local scene = gltfscene.scenes[sceneidx+1]
-    local roots = FindSkinRootJointIndices(gltfscene, scene)
+    local model = status.gltfscene
+    local roots = FindSkinRootJointIndices(model)
     local jointIndex = 0
-    local remap = {}
-    local function ImportNode(nodes)
+    local function BuildJointHierarchy(nodes)
         for _, nodeIndex in ipairs(nodes) do
-            remap[nodeIndex] = jointIndex
+            local node = model.nodes[nodeIndex+1]
+            node.jointIndex = jointIndex
             jointIndex = jointIndex + 1
-            local node = gltfscene.nodes[nodeIndex+1]
             local c = node.children
             if c then
-                ImportNode(c)
+                BuildJointHierarchy(c)
             end
         end
     end
-    ImportNode(roots)
+    BuildJointHierarchy(roots)
 
     status.animation.skins = {}
-    for skinidx, skin in ipairs(gltfscene.skins) do
-        local skinname = skin.name and ("skin_"..skin.name) or ("skin"..skinidx)
-        local resname = skinname..".skinbin"
-        utility.save_bin_file(status, "animations/"..resname, fetch_skininfo(status, gltfscene, skin, remap))
+    local math3d  = status.math3d
+    local r2l_mat = math3d.ext_constant.R2L_MAT
+    for skinidx, skin in ipairs(model.skins) do
+        local resname = skin.name and ("skin_"..skin.name..".skinbin") or ("skin"..skinidx..".skinbin")
+        local inverseBindMatrices = FetchAccessor(model, skin.inverseBindMatrices)
+        local joints = skin.joints
+        local jointsRemap  = {}
+        for i = 1, #joints do
+            local nodeIndex = joints[i]
+            local node = model.nodes[nodeIndex+1]
+            jointsRemap[i] = string.pack("<I2", assert(node.jointIndex))
+        end
+        local skinbin = {
+            inverseBindMatrices = math3d.serialize(math3d.mul_array(math3d.array_matrix(inverseBindMatrices), r2l_mat)),
+            jointsRemap = table.concat(jointsRemap),
+        }
+        utility.save_bin_file(status, "animations/"..resname, skinbin)
         status.animation.skins[skinidx] = resname
     end
 end
