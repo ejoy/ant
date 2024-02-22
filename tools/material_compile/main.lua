40016ad87a8e280c43bc30a300616ced1f933b79
junjie020 <kitchen.gz.020@gmail.com>
2024-02-22 10:12:12 +0800

    remove files

--- tools/material_compile/main.lua
+++
@@ -1,7 +0,0 @@
-package.path = "/engine/?.lua"
-require "bootstrap"
-
-dofile "/engine/ltask.lua" {
-    bootstrap = { "tools.material_compile|init", arg },
-    exclusive = { "timer", "subprocess" },
-}
