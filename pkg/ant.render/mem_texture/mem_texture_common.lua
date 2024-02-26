aa03010fecb7b6436119dbdbd55ef5d86d9dd9e9
von <1052019421@qq.com>
2024-02-26 15:52:37 +0800

    fix mem texture

--- pkg/ant.render/mem_texture/mem_texture_common.lua
+++ pkg/ant.render/mem_texture/mem_texture_common.lua
@@ -15,6 +15,10 @@ local mc        = import_package "ant.math".constant
 local irender   = ecs.require "ant.render|render"
 local RENDER_ARG
 
+local function get_diagonal_length(a, b, c)
+    return math.sqrt(a^2+b^2+c^2)
+end
+
 local params = {
     WAIT_QUEUE = {},
     PREFABS = {},
@@ -31,8 +35,8 @@ local params = {
         RT  =   "RT_ON",
     },
     DEFAULT_FB = {},
-    DEFAULT_EXTENTS         = math3d.mark(math3d.vector(50, 50, 50)),
-    DEFAULT_LENGTH          = math3d.length(math3d.mul(1.8, math3d.vector(50, 50, 50))),
+    DEFAULT_EXTENTS         = get_diagonal_length(50, 50, 50),
+    DEFAULT_LENGTH          = math3d.length(math3d.mul(2, math3d.vector(50, 50, 50))),
     DEFAULT_SIZE_CONFIGS    = {
         {
             width  = 512,
@@ -59,7 +63,8 @@ local params = {
 local m = {params = params}
 
 local function add_wait_queue(name)
-    params.WAIT_QUEUE[#params.WAIT_QUEUE+1] = name
+    table.insert(params.WAIT_QUEUE, 1, name)
+    --params.WAIT_QUEUE[#params.WAIT_QUEUE+1] = name
 end
 
 local function parse_config(config)
@@ -98,7 +103,12 @@ local function destroy_prefab_cache(handle, destroy_rb)
         fbmgr.destroy_rb(fb[1].rbidx, true)
         fbmgr.destroy_rb(fb[2].rbidx, true) 
     end
-    params.PREFABS[name] = {}
+    for i, v in ipairs(params.WAIT_QUEUE) do
+        if name:match(v) then
+            table.remove(params.WAIT_QUEUE, i)
+        end
+    end
+    params.PREFABS[name] = {type = prefab.type}
 end
 
 function m.register_new_rt()
@@ -205,15 +215,6 @@ end
 
 function m.create_prefab_entity(name, path, rotation, distance, type)
 
-    local function calc_srt(camera_rot, aabb, distance)
-        if not math3d.aabb_isvalid(aabb) then return end
-        local origin_world_center, world_extents = math3d.aabb_center_extents(aabb)
-        local view_dir = math3d.todirection(camera_rot)
-        local view_len = params.DEFAULT_LENGTH * distance
-        local camera_pos = math3d.sub(origin_world_center, math3d.mul(view_dir, view_len))
-        return camera_pos
-    end
-
     local function set_prefab_params()
         local prefab = params.PREFABS[name]
         prefab.rotation, prefab.distance, prefab.type = rotation, distance, type
@@ -222,28 +223,16 @@ function m.create_prefab_entity(name, path, rotation, distance, type)
     world:create_instance {
         prefab = path,
         on_ready = function (inst)
-            local scene_aabb = math3d.aabb()
             set_prefab_params()
             local alleid = inst.tag['*']
             params.PREFABS[name].objects = alleid
             for _, eid in ipairs(alleid) do
-                local ee <close> = world:entity(eid, "mesh?in bounding?in mem_texture_ready?out")
+                local ee <close> = world:entity(eid, "mesh?in mem_texture_ready?out")
                 if ee.mesh then
                     ee.mem_texture_ready = true
                 end
-                if ee.bounding and ee.bounding.scene_aabb ~= mc.NULL and math3d.aabb_isvalid(ee.bounding.scene_aabb) then
-                    scene_aabb = math3d.aabb_merge(scene_aabb, ee.bounding.scene_aabb)
-                end
             end
-            if math3d.aabb_isvalid(scene_aabb) then
-                local select_tag = ("%s camera_ref:in"):format(params.QUEUE_NAME)
-                local mtq = w:first(select_tag)
-                local camera<close> = world:entity(mtq.camera_ref, "scene:update camera:in")
-                local camera_rot = math3d.quaternion(rotation)
-                local camera_pos = calc_srt(camera_rot, scene_aabb, distance)
-                params.PREFABS[name].camera_srt = {r = math3d.mark(camera_rot), t = math3d.mark(camera_pos)}
-                add_wait_queue(name)
-            end
+            add_wait_queue(name)
         end
     }
 end
@@ -325,14 +314,45 @@ function m.process_wait_queue()
         end
     end
 
-    local function set_camera_srt(camera_srt)
-        if camera_srt then
+    local function set_camera_srt(prefab, name)
+
+--[[         if prefab.camera_srt then
+            return
+        end ]]
+
+        local objects, rotation, distance = prefab.objects, prefab.rotation, prefab.distance
+
+        local function calc_srt(camera_rot, aabb, distance)
+            if not math3d.aabb_isvalid(aabb) then return end
+            local origin_world_center, world_extents = math3d.aabb_center_extents(aabb)
+            local a, b, c = math3d.index(world_extents, 1, 2, 3)
+            local r = get_diagonal_length(a, b, c) / params.DEFAULT_EXTENTS
+            local view_dir = math3d.todirection(camera_rot)
+            local view_len = params.DEFAULT_LENGTH * distance * r
+            local camera_pos = math3d.sub(origin_world_center, math3d.mul(view_dir, view_len))
+            return camera_pos
+        end
+
+        local scene_aabb = math3d.aabb()
+        for _, eid in ipairs(objects) do
+            local ee <close> = world:entity(eid, "bounding?in scene?in")
+            if ee.bounding and ee.bounding.scene_aabb ~= mc.NULL and math3d.aabb_isvalid(ee.bounding.scene_aabb) then
+                scene_aabb = math3d.aabb_merge(scene_aabb, ee.bounding.scene_aabb)
+            end
+        end
+        if math3d.aabb_isvalid(scene_aabb) then
             local select_tag = ("%s camera_ref:in"):format(params.QUEUE_NAME)
             local mtq = w:first(select_tag)
             local camera<close> = world:entity(mtq.camera_ref, "scene:update camera:in")
-            iom.set_position(camera, camera_srt.t)
-            iom.set_rotation(camera, camera_srt.r) 
+            local camera_rot = math3d.quaternion(rotation)
+            local camera_pos = calc_srt(camera_rot, scene_aabb, distance)
+            params.PREFABS[name].camera_srt = {r = math3d.mark(camera_rot), t = math3d.mark(camera_pos)}
+            iom.set_position(camera, camera_pos)
+            iom.set_rotation(camera, camera_rot) 
         end
+
+--[[         local center, _ = math3d.aabb_center_extents(scene_aabb)
+        prefab.center = center ]]
     end
 
     local function set_objects_visible_state(prefab, state)
@@ -352,6 +372,7 @@ function m.process_wait_queue()
             for _, eid in ipairs(objects) do
                 local e <close> = world:entity(eid, "scene?in")
                 if e.scene and e.scene.parent == 0 then
+                    --iom.set_position(e, math3d.sub(e.scene.t, prefab.center))
                     iom.set_rotation(e, math3d.quaternion(prefab_rotation))
                 end
             end 
@@ -376,23 +397,30 @@ function m.process_wait_queue()
     if #wait_queue >= 1 then
         local cur_name = wait_queue[1]
         local cur_prefab = params.PREFABS[cur_name]
-        if not is_prefab_ready(cur_prefab) then
-            goto continue
+        if not cur_prefab then
+            table.remove(wait_queue, 1)
+        elseif not is_prefab_ready(cur_prefab) then
+            -- should be removed later
+        else
+            table.remove(wait_queue, 1)
+            update_fb(cur_prefab.fb)
+            set_camera_srt(cur_prefab, cur_name)
+            set_objects_visible_state(cur_prefab, true)
+            adjust_prefab_rot(cur_prefab)
+            if (params.LAST_PREFAB and params.LAST_PREFAB ~= cur_name) or (not params.LAST_PREFAB) then
+                set_objects_visible_state(params.PREFABS[params.LAST_PREFAB], false)
+            end
+            params.LAST_PREFAB = cur_name
         end
 
-        table.remove(wait_queue, 1)
-        local fb, camera_srt = cur_prefab.fb, cur_prefab.camera_srt
-        update_fb(fb)
-        set_camera_srt(camera_srt)
-        set_objects_visible_state(cur_prefab, true)
-        adjust_prefab_rot(cur_prefab)
-        if (params.LAST_PREFAB and params.LAST_PREFAB ~= cur_name) or (not params.LAST_PREFAB) then
-            set_objects_visible_state(params.PREFABS[params.LAST_PREFAB], false)
+        -- extra dynamic texture should be removed
+        for i, v in ipairs(wait_queue) do
+            local prefab = params.PREFABS[v]
+            if not prefab or (prefab and prefab.type and prefab.type:match "dynamic")  then
+                table.remove(wait_queue, i)
+            end
         end
-        params.LAST_PREFAB = cur_name
     end
-    ::continue::
-
 end
 
 return m
