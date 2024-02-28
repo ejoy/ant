33bd82f5d9204eaa3110bd1c405956d5eaf15042
Cloud Wu <cloudwu@gmail.com>
2024-02-28 16:02:43 +0800

    merge settings

--- pkg/ant.settings/main.lua
+++ pkg/ant.settings/main.lua
@@ -3,46 +3,60 @@ local datalist = require "datalist"
 local aio = import_package "ant.io"
 
 local function split(s)
-    local r = {}
-    s:match "^/?(.-)/?$":gsub('[^/]*', function (w) r[#r+1] = w end)
-    return r
+	local r = {}
+	s:match "^/?(.-)/?$":gsub('[^/]*', function (w) r[#r+1] = w end)
+	return r
 end
 
 local function get_internal(t, sp, n)
-    if n >= #sp then
-        return t
-    end
-    local node = t[sp[n]]
-    if type(node) ~= 'table' then
-        return
-    end
-    return get_internal(node, sp, n+1)
+	if n >= #sp then
+		return t
+	end
+	local node = t[sp[n]]
+	if type(node) ~= 'table' then
+		return
+	end
+	return get_internal(node, sp, n+1)
+end
+
+local function merge(root, tbl)
+	for k,v in pairs(tbl) do
+		if root[k] == nil then
+			root[k] = v
+		else
+			local oldv = root[k]
+			if type(v) == "table" then
+				assert(type(oldv) == "table")
+				merge(oldv, v)
+			else
+				assert(type(oldv) ~= "table")
+			end
+		end
+	end
 end
 
 local function create(paths)
-    local lst = {}
-    for _, path in ipairs(paths) do
-        if fs.exists(fs.path(path)) then
-            lst[#lst+1] = assert(datalist.parse(aio.readall(path)))
-        end
-    end
-    local obj = {}
-    function obj:get(key)
-        local sp = split(key)
-        for _, l in ipairs(lst) do
-            local t = get_internal(l, sp, 1)
-            if t then
-                local k = sp[#sp]
-                return t[k]
-            end
-        end
-    end
-    return obj
+	local root = {}
+	for _, path in ipairs(paths) do
+		if fs.exists(fs.path(path)) then
+			merge(root, assert(datalist.parse(aio.readall(path))))
+		end
+	end
+	local obj = {}
+	function obj:get(key)
+		local sp = split(key)
+		local t = get_internal(root, sp, 1)
+		if t then
+			local k = sp[#sp]
+			return t[k]
+		end
+	end
+	return obj
 end
 
 return create {
-    "/graphic_settings.ant",
-    "/pkg/ant.settings/default/graphic_settings.ant",
-    "/general_settings.ant",
-    "/pkg/ant.settings/default/general_settings.ant",
+	"/settings.ant",
+	"/graphic_settings.ant",
+	"/pkg/ant.settings/default/graphic_settings.ant",
+	"/pkg/ant.settings/default/general_settings.ant",
 }
