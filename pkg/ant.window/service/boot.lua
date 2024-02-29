3f94b16fcedb137c978b55d42ffde160cf840cda
Cloud Wu <cloudwu@gmail.com>
2024-02-29 19:30:21 +0800

    user boot service

--- pkg/ant.window/service/boot.lua
+++ pkg/ant.window/service/boot.lua
@@ -1,4 +1,9 @@
 local ltask = require "ltask"
+local config = ...
+
+if config.boot then
+	ltask.uniqueservice(config.boot, config)
+end
 
 local ServiceWindow = ltask.uniqueservice("ant.window|window", ...)
 ltask.call(ServiceWindow, "wait")
