6ea1a223820b29dddc9efc29576cabc1a0111a30
actboy168 <actboy168@gmail.com>
2024-03-06 16:57:39 +0800

    remove unused code

--- pkg/ant.rmlui/ui_rt_global.lua
+++
@@ -1,7 +0,0 @@
-local hwi       = import_package "ant.hwi"
-local lastname = "fxaa"
-return setmetatable({}, {
-    __index = function(t,name) 
-    t[name] = hwi.viewid_generate(name, lastname) 
-    return t[name] end
-})
