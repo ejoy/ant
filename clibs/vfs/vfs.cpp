e46277356b1beedb89fa963c0b6ffffcb4b3ee62
actboy168 <actboy168@gmail.com>
2024-03-18 15:15:49 +0800

    fixes bug

--- clibs/vfs/vfs.cpp
+++ clibs/vfs/vfs.cpp
@@ -15,8 +15,14 @@ local vfs = {}
 local fastio = require "fastio"
 local __ANT_RUNTIME__ = package.preload.firmware ~= nil
 if not __ANT_RUNTIME__ then
+    local function realpath(path)
+        if path:sub(1,8) == "/engine/" then
+            return path:sub(2)
+        end
+        return path
+    end
     function vfs.read(path)
-        local lpath = path:sub(2)
+        local lpath = realpath(path)
         local data = fastio.readall_v(lpath, path)
         return data, lpath
     end
