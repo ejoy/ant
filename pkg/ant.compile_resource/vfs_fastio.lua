37cee881344eccee5d7dd61b25591c26c0891567
actboy168 <actboy168@gmail.com>
2024-03-29 11:10:11 +0800

    cleanup

--- pkg/ant.compile_resource/vfs_fastio.lua
+++ pkg/ant.compile_resource/vfs_fastio.lua
@@ -7,9 +7,4 @@ function m.readall_f(vfs, path)
     return fastio.readall_f(realpath, path)
 end
 
-function m.readall_s(vfs, path)
-    local realpath = assert(vfs.realpath(path), path)
-    return fastio.readall_s(realpath, path)
-end
-
 return m
