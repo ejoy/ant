924ea65603dbd1f845d60ae42e20010e01aa1996
actboy168 <actboy168@gmail.com>
2024-04-01 09:35:17 +0800

    only encoder api

--- clibs/bgfx/bgfx.lua
+++ clibs/bgfx/bgfx.lua
@@ -5,7 +5,10 @@ lm.BxDir = lm.AntDir.."/3rd/bx"
 lm.BimgDir = lm.AntDir.."/3rd/bimg"
 
 lm:conf {
-    defines = "BGFX_CONFIG_DEBUG_UNIFORM=0",
+    defines = {
+        lm.mode == "debug" and "BGFX_CONFIG_DEBUG_UNIFORM=0",
+        "BGFX_CONFIG_ENCODER_API_ONLY=1",
+    }
 }
 
 lm:import(lm.AntDir.."/3rd/bgfx.luamake/use.lua")
