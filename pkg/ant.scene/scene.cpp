a30e42da858b359e1301f2e1e2a6f55bb2bd4ecd
actboy168 <actboy168@gmail.com>
2024-04-08 09:59:40 +0800

    format

--- pkg/ant.scene/scene.cpp
+++ pkg/ant.scene/scene.cpp
@@ -32,7 +32,7 @@ math3d_update(struct math_context* math3d, math_t& id, math_t const& m) {
 }
 
 static bool
-worldmat_update(flatmap<component::eid, math_t>& worldmats, struct math_context* math3d, component::scene& s, component::eid& id, struct ecs_world *w) {
+worldmat_update(ant::flatmap<component::eid, math_t>& worldmats, struct math_context* math3d, component::scene& s, component::eid& id, struct ecs_world *w) {
 	math_t mat = math3d_make_srt(math3d, s.s, s.r, s.t);
 	if (!math_isnull(s.mat)) {
 		mat = math3d_mul_matrix(math3d, mat, s.mat);
@@ -89,12 +89,12 @@ is_constant(struct ecs_world *w, component::eid eid) {
 }
 
 static inline bool
-is_changed(flatset<component::eid> &changed, component::eid eid) {
+is_changed(ant::flatset<component::eid> &changed, component::eid eid) {
 	return changed.contains(eid);
 }
 
 static void
-rebuild_mutable_set(struct ecs_world *w, flatset<component::eid> &changed) {
+rebuild_mutable_set(struct ecs_world *w, ant::flatset<component::eid> &changed) {
 	using namespace ecs::flags;
 	for (auto& e : ecs::select< component::scene, component::scene_mutable(absent), component::eid>(w->ecs)) {
 		auto& s = e.get<component::scene>();
@@ -111,7 +111,7 @@ scene_changed(lua_State *L) {
 	auto math3d = w->math3d->M;
 	math3d_checkpoint cp(math3d);
 
-	flatset<component::eid> changed;
+	ant::flatset<component::eid> changed;
 
 	// step.1
 	auto selector = ecs::select<component::scene_needchange, component::eid>(w->ecs);
@@ -137,7 +137,7 @@ scene_changed(lua_State *L) {
 	}
 
 	// step.2
-	flatmap<component::eid, math_t> worldmats;
+	ant::flatmap<component::eid, math_t> worldmats;
 	for (auto& e : ecs::select<component::scene_mutable, component::scene, component::eid>(w->ecs)) {
 		auto& s = e.get<component::scene>();
 		component::eid id = e.get<component::eid>();
