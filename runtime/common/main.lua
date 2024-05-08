91a9f656e3c937f72cf46508a24ca46066f17f2e
actboy168 <actboy168@gmail.com>
2024-05-08 21:01:34 +0800

    simplify

--- runtime/common/main.lua
+++ runtime/common/main.lua
@@ -1,28 +1,6 @@
-local arguments = {} ; do
-    local options = {}
-    local i = 1
-    while true do
-        if arg[i] == nil then
-            break
-        elseif arg[i]:sub(1, 1) == "-" then
-            options[arg[i]] = arg[i+1]
-            i = i + 1
-        else
-            arguments[#arguments+1] = arg[i]
-        end
-        i = i + 1
-    end
-    arguments[0] = table.remove(arguments, 1)
-    for k, v in pairs(options) do
-        arguments[#arguments+1] = k
-        arguments[#arguments+1] = v
-    end
-end
-
-if arguments[0] == nil then
+arg[0] = table.remove(arg, 1)
+if arg[0] == nil then
     return
 end
-
-arg = arguments
 
 dofile "/engine/console/bootstrap.lua"
