62522093a12e5b78879cfefd677b8c7aea44878d
Heng <huangxinghuh@163.com>
2024-05-14 14:47:09 +0800

    fix test atlas

--- test/atlas/pkg/ant.test.atlas/main_system.lua
+++ test/atlas/pkg/ant.test.atlas/main_system.lua
@@ -4,15 +4,28 @@ local w = world.w
 local image  = require "image"
 local fastio = require "fastio"
 local m = ecs.system "main_system"
-local iatlas = ecs.require "ant.atlas|atlas"
+local iatlas = import_package "ant.atlas"
 local fs = require "bee.filesystem"
 local vfs = require "vfs"
 
 local vpath = "/pkg/ant.resources/textures/atlas"
+local output = "/test/atlas/pkg/ant.test.atlas"
+local rects = {}
+
+for i = 1, 6 do
+    table.insert(rects, {
+        irpath = fs.current_path():string() .. vpath .. "/t" .. i .. ".png",
+        arpath = fs.current_path():string() .. output .. "/t" .. i .. ".a"
+    })
+end
+
 local atlas = {
     name = "test", x = 1, y = 1, w = 1024, h = 1024, bottom_y = 1,
-    vpath = vpath,
-    rpath = fs.current_path():string() .. vpath
+    irpath = fs.current_path():string() .. output .. '/out.png',
+    ivpath = "/pkg/ant.test.atlas/out.png",
+    trpath = fs.current_path():string() .. output .. "/out.texture",
+    tvpath = "/pkg/ant.test.atlas/out.texture",
+    rects = rects
 }
 
 function m:init_world()
