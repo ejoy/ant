74557f0cc9e9d0a6cf95d87436fa9efa3711eda6
Cloud Wu <cloudwu@gmail.com>
2024-07-04 11:10:46 +0800

    remove luaforward.lua

--- runtime/common/runtime.cpp
+++ runtime/common/runtime.cpp
@@ -57,7 +57,22 @@ do
         end
         assert(load(expr, "=(expr)", "t", env))()
     end
-    dofile "/engine/firmware/luaforward.lua"
+	local function ExportLua()
+		local platform = require "bee.platform"
+		local luaforward = require "luaforward"
+		local ext = platform.os == "windows" and ".dll" or ".so"
+		local sys = require "bee.sys"
+		local localpath = sys.exe_path():remove_filename():string()
+		localpath = localpath .. "lua54" .. ext
+
+		local init_func, err = package.loadlib(localpath, "luaapi_init")
+		if init_func then
+			luaforward.register(init_func)
+		end
+	end
+
+	ExportLua()
+
     local i = 1
     while true do
         if arg[i] == nil then
