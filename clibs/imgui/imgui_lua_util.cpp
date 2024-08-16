681f1149ddb32ade59aae4eacc2c8bf6b2daba80
Cloud Wu <cloudwu@gmail.com>
2024-08-16 23:06:34 +0800

    Cache combined flags

--- clibs/imgui/imgui_lua_util.cpp
+++ clibs/imgui/imgui_lua_util.cpp
@@ -179,9 +179,51 @@ static void set_table(lua_State* L, std::span<luaL_Reg> l, int nup) {
     lua_pop(L, nup);
 }
 
+static const char *
+next_key(lua_State *L, const char *keys) {
+	while (*keys == '|')
+		++keys;
+	const char *p = keys;
+	while (*p != '|' && *p != 0)
+		++p;
+	lua_pushlstring(L, keys, p-keys);
+	return p;
+}
+
+static int
+cache_flags(lua_State *L) {
+	lua_pushvalue(L, 1);
+	if (lua_gettable(L, lua_upvalueindex(1)) == LUA_TNUMBER) {
+		return 1;
+	}
+	lua_pop(L, 1);
+	const char *keys = lua_tostring(L, 1);
+	int r = 0;
+	while (keys[0]) {
+		const char *next_keys = next_key(L, keys);
+		if (lua_gettable(L, lua_upvalueindex(1)) != LUA_TNUMBER) {
+			next_key(L, keys);
+			return luaL_error(L, "Invalid flag %s.%s", lua_tostring(L, lua_upvalueindex(2)), lua_tostring(L, -1));
+		}
+        lua_Integer v = lua_tointeger(L, -1);
+        lua_pop(L, 1);
+        r |= v;
+		keys = next_keys;
+	}
+    lua_pushinteger(L, r);
+	lua_settable(L, lua_upvalueindex(1));
+    lua_pushinteger(L, r);
+    return 1;
+}
+
 static int make_flags(lua_State* L) {
-    luaL_checktype(L, 1, LUA_TTABLE);
-    int i, t;
+	int t = lua_type(L, 1);
+	if (t == LUA_TSTRING) {
+		return cache_flags(L);
+	} else if (t != LUA_TTABLE) {
+		return luaL_error(L, "flags should be table or string");
+	}
+    int i;
     lua_Integer r = 0;
     for (i = 1; (t = lua_geti(L, 1, i)) != LUA_TNIL; i++) {
         if (t != LUA_TSTRING)
